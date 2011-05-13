e672a090a954b08b29ae248857b347077b019a9f
hobnob <hobnob@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2011-05-13 02:17:26 +0000

    Generalize screen-picking proc to helper class; fix config file for debug client; remove a couple of log files from the repo

---
+++ SS3D_Client/HelperClasses/AtomUtil.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SS3D_shared;
+using Mogre;
+
+// Helper class for client-side AtomBaseClass-related procedures
+// Could be refactored into SS3D_Shared
+namespace SS3D.HelperClasses
+{
+    public class AtomUtil
+    {
+        /// <summary>
+        /// Gets the AtomBaseClass at a screen position given the current camera,
+        /// usually used for picking with the mouse.
+        /// </summary>
+        /// <param name="engine">The ogre engine</param>
+        /// <param name="screenPos">The screen position</param>
+        /// <param name="worldPos">Outputs the world position of the picked point</param>
+        /// <returns>The Atom at the position, or null if none</returns>
+        public static AtomBaseClass PickAtScreenPosition(SS3D.Modules.OgreManager engine, Vector2 screenPos, out Vector3 worldPos)
+        {
+            Ray screenRay = engine.Camera.GetCameraToViewportRay(screenPos.x, screenPos.y);
+            RaySceneQuery sceneQuery = engine.SceneMgr.CreateRayQuery(screenRay);
+            sceneQuery.QueryTypeMask = Mogre.SceneManager.ENTITY_TYPE_MASK;
+            sceneQuery.SetSortByDistance(true);
+            RaySceneQueryResult rayResult = sceneQuery.Execute();
+            sceneQuery.Dispose();
+
+            if (rayResult.Count == 0)
+            {
+                worldPos = Vector3.ZERO;
+                return null; //Nothing there. The fuck?
+            }
+
+            worldPos = screenRay.GetPoint(rayResult.Front.distance);
+            return (AtomBaseClass)rayResult.Front.movable.UserObject; //UserObject should ALWAYS be a reference to the object as AtomBaseClass.
+        }
+    }
+}

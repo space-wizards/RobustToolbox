45b24ebf7bbd0fa25cfdc46b0ff9e56e985ac939
spoogemonster <spoogemonster@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2011-05-15 17:33:12 +0000

    fixed packaging batch file

--- packagebuild.bat
+++ packagebuild.bat
@@ -8,9 +8,9 @@ mkdir %dir%
 mkdir %dir%\bin
 mkdir %dir%\bin\client
 mkdir %dir%\bin\server
-cp -R Media %dir%
-cp -R SS3D_Client\bin\Release\* %dir%\bin\client
-cp -R SS3d_server\bin\Release\* %dir%\bin\server
+xcopy /E Media %dir%\Media\
+xcopy /E SS3D_Client\bin\Release\* %dir%\bin\client
+xcopy /E SS3d_server\bin\Release\* %dir%\bin\server
 cd %dir%
 FOR /F "tokens=*" %%G IN ('DIR /B /AD /S *.svn*') DO RMDIR /S /Q "%%G"
 cd ..

22d9b589bb5d8825acf56f7a1b75e8ae272d5247
spoogemonster <spoogemonster@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2011-05-20 22:21:25 +0000

    More shit laid out for the item/mob -> atom port

---
+++ SS3D_Client/Atom/Atom.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Mogre;
+using SS3D_shared.HelperClasses;
+
+namespace SS3D.Atom
+{
+    public class Atom // CLIENT SIDE
+    {
+        // GRAPHICS
+        public SceneNode Node;
+        public Entity Entity;
+        public string meshName = "ogrehead.mesh";
+
+        public string name;
+        public ushort UID
+        {
+            private set;
+            get;
+        }
+
+        public List<InterpolationPacket> interpolationPacket;
+    
+    }
+}

164f62fba2af90c328a86c88ede4001cb36b9e94
spoogemonster <spoogemonster@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2011-05-22 02:49:11 +0000

    More work on atom movement.

--- SS3D_Client/Atom/Atom.cs
+++ SS3D_Client/Atom/Atom.cs
@@ -22,12 +22,12 @@ namespace SS3D.Atom
         public AtomManager atomManager;
 
         // Position data
-        public SS3D_shared.HelperClasses.Vector3 position;
+        public Mogre.Vector3 position;
         public float rotW;
         public float rotY;
         public bool positionChanged;
 
-        public List<InterpolationPacket> interpolationPacket;
+        public List<InterpolationPacket> interpolationPackets;
 
         public Atom()
         {
@@ -55,12 +55,67 @@ namespace SS3D.Atom
 
         public virtual void HandleInterpolationPacket(NetIncomingMessage message)
         {
+            SS3D_shared.HelperClasses.InterpolationPacket intPacket = new SS3D_shared.HelperClasses.InterpolationPacket(message);
+            
+            //Add an interpolation packet to the end of the list. If the list is more than 10 long, delete a packet.
+            //TODO: For the Player class, override this function to do some sort of intelligent checking on the interpolation packets 
+            // recieved to make sure they don't greatly disagree with the client's own data.
+            interpolationPackets.Add(intPacket);
 
+            if (interpolationPackets.Count > 10)
+            {
+                interpolationPackets.RemoveAt(0);
+            }
+
+            // Need an update.
+            updateRequired = true;
         }
 
-        public void Update()
-        { 
+        public virtual void Update()
+        {
+            //This lets the atom only update when it needs to. If it needs to update subsequent to this, the functions below will set that flag.
+            updateRequired = false;
+
+            if (interpolationPackets.Count > 0)
+            {
+                UpdatePosition();
+            }
             
+        }
+
+        // Mobs may need to override this for animation, or they could use this.
+        public virtual void UpdatePosition()
+        {
+            Mogre.Vector3 difference;
+            float rotW, rotY;
+
+            
+            difference = interpolationPackets[0].position - Node.Position;
+            
+            // Set rotation. The packet may be rotation only.
+            rotW = interpolationPackets[0].rotW;
+            rotY = interpolationPackets[0].rotY;
+            Node.SetOrientation(rotW, 0, rotY, 0);
+
+            //Check interpolation packet to see if we're close enough to the interpolation packet on the top of the stack.
+            if (difference.Length < 1)
+            {
+                interpolationPackets.RemoveAt(0);
+                UpdatePosition(); // RECURSION :D - this just discards interpolationpackets we don't need anymore.
+            }
+            else
+            {
+                //This constant should be time interval based.
+                //TODO: Make this better if it isn't good enough.
+                difference /= 5;
+                Node.Position += difference;
+                updateRequired = true;
+            }
+        }
+
+        public void Draw()
+        {
+
         }
 
         // Sends a message to the server to request the atom's data.

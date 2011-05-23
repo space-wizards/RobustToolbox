a68382a12dc10b5c03472eefcb7d60da9c16d385
spoogemonster <spoogemonster@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2011-05-23 18:40:50 +0000

    Adding more key/movement handling

--- SS3d_server/Atom/Mob/Mob.cs
+++ SS3d_server/Atom/Mob/Mob.cs
@@ -10,8 +10,6 @@ namespace SS3d_server.Atom.Mob
         public float walkSpeed = 1.0f;
         public float runSpeed = 2.0f;
 
-        public float speed = 0.0f;
-
         public Atom leftHandItem; // Just a temporary storage spot, for now.
         public Atom rightHandItem;
         public MobHand selectedHand = MobHand.LHand; // The hand we are currently using, and also used to tell clients which

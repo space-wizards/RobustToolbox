31ce99bcbc742cc7dee1b92378e085395157cdb4
keelin <keelin@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2011-05-23 07:20:32 +0000

    Uploading this tiny change.

--- SS3D_Client/States/EditorScreen.cs
+++ SS3D_Client/States/EditorScreen.cs
@@ -192,6 +192,9 @@ namespace SS3D.States
             };
             loadPanel.Controls.Add(filePanel);
 
+            if (!Directory.Exists(@".\Maps")) 
+                Directory.CreateDirectory(@".\Maps");
+
             string[] mapFiles = Directory.GetFiles(@".\Maps", "*.map");
             int currY = 0;
             foreach (string currFile in mapFiles)

7fdc05c5ee00f5ead09d28d9b6fd5b4d7b14d8e5
keelin <keelin@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2011-05-24 18:06:07 +0000

    Just a few changes. Nothing interesting. Move on.

--- SS3D_Client/States/EditorScreen.cs
+++ SS3D_Client/States/EditorScreen.cs
@@ -33,6 +33,11 @@ namespace SS3D.States
         public ItemManager itemManager;
         public Map currentMap;
 
+        public LoadingTracker currentLoadingTracker = null;
+
+        public EditorStatusBar statusBar;
+        public EditorToolbar toolBar;
+
         //private AtomBaseClass prevMouseOverEntity;
 
         //private AtomBaseClass selectedEntity;
@@ -59,10 +64,17 @@ namespace SS3D.States
 
             mEngine.SceneMgr.SetSkyBox(true, "SkyBox", 900f, true);
 
+            mEngine.Camera.Position = new Mogre.Vector3(0, 300, 0);
+            mEngine.Camera.LookAt(new Mogre.Vector3(160, 64, 160));
+
             currentMap = new Map(mEngine, true);
             itemManager = new ItemManager(mEngine, currentMap, mEngine.mNetworkMgr, null);
 
-            mEngine.mMiyagiSystem.GUIManager.GUIs.Add(new EditorToolbar(mStateMgr));
+            toolBar = new EditorToolbar(mStateMgr, this);
+            mEngine.mMiyagiSystem.GUIManager.GUIs.Add(toolBar);
+
+            statusBar = new EditorStatusBar(mStateMgr, this);
+            mEngine.mMiyagiSystem.GUIManager.GUIs.Add(statusBar);
 
             return true;
         }
@@ -75,6 +87,22 @@ namespace SS3D.States
         public override void Update(long _frameTime)
         {
             mEngine.SceneMgr.SkyBoxNode.Rotate(Mogre.Vector3.UNIT_Y, 0.0001f);
+
+            if (currentLoadingTracker != null)
+            {
+                statusBar.StatusTextPanel.Text = currentLoadingTracker.loadingText;
+                statusBar.StatusProgressBar.Value = (int)currentLoadingTracker.loadingPercent;
+                statusBar.StatusProgressBar.Text = System.Math.Floor(currentLoadingTracker.loadingPercent).ToString() + "%";
+            }
+        }
+
+        public void LoadMap(string mapPath)
+        {
+            this.currentLoadingTracker = this.currentMap;
+            MapFile loadedMap = MapFileHandler.LoadMap(mapPath);
+            this.currentMap.Shutdown();
+            System.GC.Collect(); //Oh boy.
+            this.currentMap.LoadMap(loadedMap);
         }
 
         #region Input
@@ -112,16 +140,18 @@ namespace SS3D.States
         Button cancelButton;
 
         StateManager mStateMgr;
+        EditorScreen editorState;
 
-        public EditorLoadWindow(StateManager _mgr) 
+        public EditorLoadWindow(StateManager _mgr, EditorScreen editScreen) 
             : base("editorLoadBox")
         {
             mStateMgr = _mgr;
+            editorState = editScreen;
 
             this.loadPanel = new Panel("EditorLoadPanel")
             {
                 Location = new Point(150, 150),
-                Size = new Size(230, 300),
+                Size = new Size(236, 300),
                 ResizeMode = ResizeModes.None,
                 Movable = true,
                 BorderStyle =
@@ -132,7 +162,7 @@ namespace SS3D.States
             };
             this.Controls.Add(loadPanel);
 
-            loadPanel.Controls.Add(new Panel("EditorLoadPanel")
+            loadPanel.Controls.Add(new Panel("EditorLoadPanelText")
             {
                 Location = new Point(25, 0),
                 Size = new Size(180, 30),
@@ -192,14 +222,36 @@ namespace SS3D.States
             };
             loadPanel.Controls.Add(filePanel);
 
-            if (!Directory.Exists(@".\Maps")) 
+            cancelButton = new Button("mapCancelLoadButton")
+            {
+                Size = new Size(80, 35),
+                Location = new Point(75, filePanel.Bottom + 5),
+                Text = "Cancel",
+                TextStyle =
+                {
+                    Alignment = Alignment.MiddleCenter,
+                    ForegroundColour = Colours.Black
+                },
+                Skin = MiyagiResources.Singleton.Skins["ButtonSkinGreen"]
+            };
+            cancelButton.Click += new EventHandler(cancelButton_Click);
+            loadPanel.Controls.Add(cancelButton);
+
+            populateMapList();
+        }
+
+        private void populateMapList()
+        {
+            if (filePanel == null) return;
+
+            if (!Directory.Exists(@".\Maps"))//Create the folder if its not there.
                 Directory.CreateDirectory(@".\Maps");
 
-            string[] mapFiles = Directory.GetFiles(@".\Maps", "*.map");
+            string[] mapFiles = Directory.GetFiles(@".\Maps", "*.map"); //We want a list of all map files.
             int currY = 0;
             foreach (string currFile in mapFiles)
             {
-                Button newButton = new Button("mapLoadButton"+currY.ToString())
+                Button newButton = new Button("mapLoadButton" + currY.ToString())
                 {
                     Size = new Size(130, 35),
                     Location = new Point(0, currY),
@@ -217,39 +269,16 @@ namespace SS3D.States
                 currY += newButton.Size.Height;
                 newButton.Click += new EventHandler(loadMapFile_Click);
             }
-
-            cancelButton = new Button("mapCancelLoadButton")
-            {
-                Size = new Size(80, 35),
-                Location = new Point(75, filePanel.Bottom + 5),
-                Text = "Cancel",
-                TextStyle =
-                {
-                    Alignment = Alignment.MiddleCenter,
-                    ForegroundColour = Colours.Black
-                },
-                Skin = MiyagiResources.Singleton.Skins["ButtonSkinGreen"]
-            };
-            cancelButton.Click += new EventHandler(cancelButton_Click);
-            loadPanel.Controls.Add(cancelButton);
-
-            mStateMgr.Engine.mMiyagiSystem.GUIManager.GUIs.Add(this);
         }
 
         void loadMapFile_Click(object sender, EventArgs e)
         {
             Button sButton = (Button)sender;
-            MapFile loadedMap = MapFileHandler.LoadMap((string)sButton.UserData);
-            //Now this is where it gets funky.
-            if (mStateMgr.mCurrentState.GetType() != typeof(EditorScreen))
-                throw new Exception("UI Element 'Editor Toolbar' in unsupported state.");
-            else
-            {
-                EditorScreen editorState = (EditorScreen)mStateMgr.mCurrentState;
-                editorState.currentMap.LoadMap(loadedMap);
-                //SEE MAP.CS @ Line 102 - LoadMap Method
-            }
-            
+            this.Visible = false;
+            //TODO: Block Main toolbar while loading.
+            editorState.LoadMap((string)sButton.UserData); //This contains the path.
+            this.Dispose();
+            mStateMgr.Engine.mMiyagiSystem.GUIManager.GUIs.Remove(this);
         }
 
         void cancelButton_Click(object sender, EventArgs e)
@@ -271,15 +300,18 @@ namespace SS3D.States
         private LinearFunctionValueController<float> FaderOut;
 
         StateManager mStateMgr;
+        EditorScreen editorState;
 
-        public EditorToolbar(StateManager _mgr)
+        public EditorToolbar(StateManager _mgr, EditorScreen editScreen) 
             : base("EditToolbar")
         {
             mStateMgr = _mgr;
+            editorState = editScreen;
+
             this.toolbarPanel = new Panel("ToolbarPanel")
             {
                 Location = new Point(0, 0),
-                Size = new Size((int)mStateMgr.Engine.mWindow.Width, 75),
+                Size = new Size((int)mStateMgr.Engine.mWindow.Width, 50),
                 ResizeMode = ResizeModes.None,
                 Skin = MiyagiResources.Singleton.Skins["ConsolePanelSkin"]
             };
@@ -287,7 +319,7 @@ namespace SS3D.States
             this.newButton = new Button("EditNewButton")
             {
                 Size = new Size(80, 32),
-                Location = new Point(10, 20),
+                Location = new Point(10, 10),
                 Text = "New",
                 TextStyle =
                 {
@@ -300,7 +332,7 @@ namespace SS3D.States
             this.loadButton = new Button("EditLoadButton")
             {
                 Size = new Size(80, 32),
-                Location = new Point(100, 20),
+                Location = new Point(100, 10),
                 Text = "Load",
                 TextStyle =
                 {
@@ -314,7 +346,7 @@ namespace SS3D.States
             this.saveButton = new Button("EditSaveButton")
             {
                 Size = new Size(80, 32),
-                Location = new Point(190, 20),
+                Location = new Point(190, 10),
                 Text = "Save",
                 TextStyle =
                 {
@@ -333,7 +365,7 @@ namespace SS3D.States
 
         void loadButton_Click(object sender, EventArgs e)
         {
-            mStateMgr.Engine.mMiyagiSystem.GUIManager.GUIs.Add(new EditorLoadWindow(mStateMgr) { Visible = true });
+            mStateMgr.Engine.mMiyagiSystem.GUIManager.GUIs.Add(new EditorLoadWindow(mStateMgr, editorState) { Visible = true });
         }
 
         #region Toolbar Fading
@@ -363,5 +395,78 @@ namespace SS3D.States
         } 
         #endregion
 
+        }
+
+    public sealed class EditorStatusBar : GUI
+    {
+        Panel BackgroundPanel;
+        public Panel StatusTextPanel;
+        public ProgressBar StatusProgressBar;
+
+        StateManager mStateMgr;
+        EditorScreen editorState;
+
+        public EditorStatusBar(StateManager _mgr, EditorScreen editScreen)
+            : base("editorStatusBar")
+        {
+            mStateMgr = _mgr;
+            editorState = editScreen;
+
+            this.BackgroundPanel = new Panel("EditorStatusBackground")
+            {
+                Location = new Point(0, (int)mStateMgr.Engine.mWindow.Height - 35),
+                Size = new Size((int)mStateMgr.Engine.mWindow.Width, 35),
+                ResizeMode = ResizeModes.None,
+                HitTestVisible = true,
+                Movable = false,
+                Skin = MiyagiResources.Singleton.Skins["ConsolePanelSkin"]
+            };
+            this.Controls.Add(BackgroundPanel);
+
+            StatusTextPanel = new Panel("EditorStatusTextPanel")
+            {
+                Location = new Point(0, 3),
+                Size = new Size(180, 30),
+                ResizeMode = ResizeModes.None,
+                Text = "Status...",
+                HitTestVisible = false,
+                TextStyle =
+                {
+                    Alignment = Alignment.MiddleCenter,
+                    ForegroundColour = Colours.White,
+                },
+                TextureFiltering = TextureFiltering.Anisotropic,
+                Skin = MiyagiResources.Singleton.Skins["ConsolePanelSkin"]
+            };
+            BackgroundPanel.Controls.Add(StatusTextPanel);
+
+            StatusProgressBar = new ProgressBar("EditorStatusProgressBar")
+            {
+                Location = new Point(180, 3),
+                Size = new Size(300, 30),
+                ProgressBarStyle = 
+                {
+                    Orientation = Orientation.Horizontal,
+                    Mode = ProgressBarMode.Continuous, 
+                    Offset = new Point(1,1)
+                }, 
+                Text = "0%",
+                Max = 100,
+                Min = 0,
+                HitTestVisible = false,
+                BorderStyle =
+                {
+                    Thickness = new Thickness(3, 3, 3, 3)
+                },
+                TextStyle =
+                {
+                    Alignment = Alignment.MiddleCenter,
+                    ForegroundColour = Colours.White
+                },
+                TextureFiltering = TextureFiltering.Anisotropic,
+                Skin = MiyagiResources.Singleton.Skins["ProgressBarHSkin"]
+            };
+            BackgroundPanel.Controls.Add(StatusProgressBar);
+        }
     }
 }

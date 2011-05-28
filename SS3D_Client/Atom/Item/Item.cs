aef6e07123e976b17730277bd38a266209415e9f
spoogemonster <spoogemonster@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2011-05-28 21:00:27 +0000

    basics of click handling

--- SS3D_Client/Atom/Item/Item.cs
+++ SS3D_Client/Atom/Item/Item.cs
@@ -4,11 +4,26 @@ using System.Linq;
 using System.Text;
 using Mogre;
 using SS3D_shared.HelperClasses;
+using Lidgren.Network;
 
 namespace SS3D.Atom.Item
 {
     public class Item : Atom
     {
+        public Item()
+            : base()
+        {
 
+        }
+
+        protected override void HandleExtendedMessage(NetIncomingMessage message)
+        {
+            ItemMessage i = (ItemMessage)message.ReadByte();
+            switch (i)
+            {
+                default:
+                    break;
+            }
+        }
     }
 }

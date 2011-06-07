2a29b906baa5a03f8bfd1b11cac4823ca9194ef0
spoogemonster <spoogemonster@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2011-06-07 21:28:18 +0000

    Added a nifty (made by yours truly) toolbox. It is now hilariously easy to add new items.

---
+++ SS3D_Client/Atom/Item/Container/Toolbox.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SS3D.Atom.Item.Container
+{
+    public class Toolbox : Item
+    {
+        public Toolbox()
+            : base()
+        {
+            meshName = "toolbox.mesh";
+            name = "Toolbox";
+            heldQuat = new Mogre.Quaternion(new Mogre.Degree(90), new Mogre.Vector3(1,0,1));
+            heldOffset = new Mogre.Vector3(5f, 0f, 0.6f);
+
+        }
+    }
+}

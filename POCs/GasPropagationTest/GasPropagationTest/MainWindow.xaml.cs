f999cc27ad1a8f7beea4cb440d9dacadd9c4c206
spoogemonster <spoogemonster@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2011-06-30 01:01:55 +0000

    added gas propagation POC. Kind of fun to play with...

---
+++ POCs/GasPropagationTest/GasPropagationTest/MainWindow.xaml.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GasPropagationTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        GasCell[,] cellArray;
+        DateTime lastUpdate;
+        double propagationthreshold = 0.02;
+        double propagationrateconstant = 1;
+
+        public MainWindow()
+        {
+            cellArray = new GasCell[40, 40];
+            InitializeComponent();
+            Initialize();
+            CompositionTarget.Rendering += Update;
+            KeyDown += KeyDownEventHandler;
+            KeyUp += KeyUpEventHandler;
+            MouseDown += MouseDownEventHandler;
+            Instructions i = new Instructions();
+            i.Show();
+        }
+
+        public void Initialize()
+        {
+            lastUpdate = DateTime.Now;
+            Ellipse e;
+            for (int i = 0; i < 39; i++)
+            {
+                for (int j = 0; j < 39; j++)
+                {
+                    e = new Ellipse();
+                    e.Width = 15;
+                    e.Height = 15;
+                    e.Fill = Brushes.Blue;                    
+                    canvas.Children.Add(e);
+                    Canvas.SetLeft(e, (double)i * 15);
+                    Canvas.SetTop(e, (double)j * 15);
+                    cellArray[i, j] = new GasCell(e, i*15, j*15);
+                }
+            }
+
+            //cellArray[20, 20].nextGasAmount = 10;
+        }
+
+        public void Update(object sender, EventArgs e)
+        {
+            TimeSpan t = DateTime.Now - lastUpdate;
+            if (t.TotalMilliseconds < 100)
+                return;
+            else
+                lastUpdate = DateTime.Now;
+            Dictionary<GasCell, double> l;
+            for (int i = 0; i < 39; i++)
+            {
+                for (int j = 0; j < 39; j++)
+                {
+                    if(cellArray[i,j].gasAmount < 0.1)
+                        continue;//Threshold
+                    l = new Dictionary<GasCell, double>();
+                    if (i < 0 && j < 0 && cellArray[i, j].gasAmount > cellArray[i - 1, j - 1].gasAmount)
+                        l.Add(cellArray[i - 1, j - 1],cellArray[i,j].gasAmount - cellArray[i-1,j-1].gasAmount);
+                    
+                    if (i < 38 && j < 38 && cellArray[i, j].gasAmount > cellArray[i + 1, j + 1].gasAmount)
+                        l.Add(cellArray[i + 1, j + 1],cellArray[i, j].gasAmount - cellArray[i+1, j+1].gasAmount);
+                    
+                    if (i < 38 && j > 0 && cellArray[i, j].gasAmount > cellArray[i + 1, j - 1].gasAmount)
+                        l.Add(cellArray[i + 1, j - 1],cellArray[i, j].gasAmount - cellArray[i+1, j-1].gasAmount);
+                    
+                    if (i > 0 && j < 38 && cellArray[i, j].gasAmount > cellArray[i - 1, j + 1].gasAmount)
+                        l.Add(cellArray[i - 1, j + 1],cellArray[i, j].gasAmount - cellArray[i-1, j+1].gasAmount);
+                    
+                    if (j > 0 && cellArray[i, j].gasAmount > cellArray[i, j - 1].gasAmount)
+                        l.Add(cellArray[i, j - 1],cellArray[i, j].gasAmount - cellArray[i, j-1].gasAmount);
+                    
+                    if (j < 38 && cellArray[i, j].gasAmount > cellArray[i, j + 1].gasAmount)
+                        l.Add(cellArray[i, j + 1],cellArray[i, j].gasAmount - cellArray[i, j+1].gasAmount);
+                    
+                    if (i > 0 && cellArray[i, j].gasAmount > cellArray[i - 1, j].gasAmount)
+                        l.Add(cellArray[i - 1, j],cellArray[i, j].gasAmount - cellArray[i-1, j].gasAmount);
+                    
+                    if (i < 38 && cellArray[i, j].gasAmount > cellArray[i + 1, j].gasAmount)
+                        l.Add(cellArray[i + 1, j],cellArray[i, j].gasAmount - cellArray[i+1, j].gasAmount);
+
+                    if (l.Count == 0) //if there are no gas differences, continue.
+                        continue;
+
+                    /// Find the sum of all differences, average them and subtract that amount of gas from the source.
+                    double differenceSum=0;
+                    foreach (var c in l)
+                    {
+                        differenceSum += c.Value;
+                    }
+                    if (differenceSum < propagationthreshold) // If the difference is neglibible, continue.
+                        continue;
+                    double adjustedSum = differenceSum / l.Count * propagationrateconstant;
+                    if (adjustedSum > cellArray[i, j].gasAmount / 5)
+                        adjustedSum = cellArray[i, j].gasAmount / 5; // Limit the amount of gas that can flow out of a cell per turn
+                    
+                    foreach (var c in l)
+                    {
+                        c.Key.nextGasAmount += (c.Value/differenceSum) * adjustedSum;
+                    }
+
+                    cellArray[i, j].nextGasAmount -= adjustedSum;
+                    if (cellArray[i,j].nextGasAmount < 0)
+                        cellArray[i, j].nextGasAmount = 0;
+
+                }
+            }
+
+            for (int i = 0; i < 39; i++)
+            {
+                for (int j = 0; j < 39; j++)
+                {
+                    cellArray[i, j].Update();
+                }
+            }
+
+        }
+
+        
+        public void SetAllRadii(int n)
+        {
+            for (int i = 0; i < 39; i++)
+            {
+                for (int j = 0; j < 39; j++)
+                {
+                    cellArray[i, j].gasAmount = n * 0.1;
+                }
+            }
+        }
+
+        private GasCell GetCellAtPoint(Point p)
+        {
+            int x = (int)Math.Round(p.X / 15);
+            int y = (int)Math.Round(p.Y / 15);
+            if (x < 0 || y < 0 || x > 38 || y > 38)
+                return null;
+            else
+                return cellArray[x, y];
+        }
+
+        private void KeyUpEventHandler(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.D0:
+                    SetAllRadii(0);
+                    break;
+                case Key.D1:
+                    SetAllRadii(1);
+                    break;
+                case Key.D2:
+                    SetAllRadii(2);
+                    break;
+                case Key.D3:
+                    SetAllRadii(3);
+                    break;
+                case Key.D4:
+                    SetAllRadii(4);
+                    break;
+                case Key.D5:
+                    SetAllRadii(5);
+                    break;
+                case Key.D6:
+                    SetAllRadii(6);
+                    break;
+                case Key.D7:
+                    SetAllRadii(7);
+                    break;
+                case Key.D8:
+                    SetAllRadii(8);
+                    break;
+                case Key.D9:
+                    SetAllRadii(9);
+                    break;
+
+            }
+        }
+
+        private void KeyDownEventHandler(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void MouseDownEventHandler(object sender, MouseEventArgs e)
+        {
+            double addamount = 2;
+            Point p = e.GetPosition(this);
+            GasCell g = GetCellAtPoint(p);
+            if (g == null)
+                return;
+            if (e.LeftButton == MouseButtonState.Pressed && Keyboard.IsKeyDown(Key.LeftShift))
+                g.sink = !g.sink;
+            if (e.LeftButton == MouseButtonState.Pressed)
+                g.nextGasAmount += addamount;
+            else if (e.RightButton == MouseButtonState.Pressed)
+                g.nextGasAmount += addamount * 10;
+
+            
+        }
+    }
+}

4dcca64bbc9e9f3fcada8712a82cee93ec414a7d
ostaf <ostaf@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2011-07-17 22:36:05 +0000

    Lights no longer go mental if you walk diagonally

--- SS3D_Client/Atom/Light.cs
+++ SS3D_Client/Atom/Light.cs
@@ -12,11 +12,12 @@ namespace SS3D.Atom
         public System.Drawing.Color color;
         public int range;
         public LightState state;
-        public System.Drawing.Point position;
-        public System.Drawing.Point lastPosition;
+        public System.Drawing.Point tilePosition;
+        public Vector2D position;
+        public Vector2D lastPosition;
         public List<Tiles.Tile> tiles;
         public Modules.Map.Map map;
-        public LightDirection direction;
+        public List<LightDirection> direction;
 
         public Light(Modules.Map.Map _map, System.Drawing.Color _color, int _range, LightState _state, System.Drawing.Point _position, LightDirection _direction)
         {
@@ -25,40 +26,56 @@ namespace SS3D.Atom
             color = _color;
             range = _range;
             state = _state;
-            position = _position;
+            tilePosition = _position;
             lastPosition = _position;
-            direction = _direction;
+            direction = new List<LightDirection>();
+            direction.Add(_direction);
             UpdateLight();
         }
 
         public void UpdateLight()
         {
+            LightDirection lastDir = direction[0];
             foreach (Tiles.Tile t in tiles)
             {
                 t.lights.Remove(this);
             }
             tiles.Clear();
-            if (direction != LightDirection.All) // If we're spreading in all directions we don't want to change that if we moved
+            if (!direction.Contains(LightDirection.All)) // If we're spreading in all directions we don't want to change that if we moved
             {
+                direction.Clear();
                 if (position.X > lastPosition.X)
                 {
-                    direction = LightDirection.East;
+                    direction.Add(LightDirection.East);
                 }
                 else if (position.X < lastPosition.X)
                 {
-                    direction = LightDirection.West;
+                    direction.Add(LightDirection.West);
                 }
-                else if (position.Y > lastPosition.Y)
+                if (position.Y > lastPosition.Y)
                 {
-                    direction = LightDirection.South;
+                    direction.Add(LightDirection.South);
                 }
                 else if (position.Y < lastPosition.Y)
                 {
-                    direction = LightDirection.North;
+                    direction.Add(LightDirection.North);
                 }
             }
+            if (direction.Count == 0)
+                direction.Add(lastDir);
+            map.compute_visibility(tilePosition.X, tilePosition.Y, this);
+        }
 
-            map.compute_visibility(position.X, position.Y, this);
+        public void UpdatePosition(Vector2D newPosition)
+        {
+            lastPosition = position;
+            position = newPosition;
+            tilePosition = map.GetTileArrayPositionFromWorldPosition(position);
+            if (position != lastPosition)
+            {
+                UpdateLight();
+            }
+
         }
 
     }

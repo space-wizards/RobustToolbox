8716364b652847ba82f675c090864d70aeb178b6
ostaf <ostaf@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2011-07-17 21:19:06 +0000

    Much improved lighting and visibility by messing with the inbuilt shaders and reordering the tile rendering. See what you think I guess keelin...

--- SS3D_Client/Tiles/Wall/Wall.cs
+++ SS3D_Client/Tiles/Wall/Wall.cs
@@ -25,43 +25,9 @@ namespace SS3D.Tiles.Wall
             if (Visible && ((surroundDirs&4) == 0))
             {
                 sideSprite.SetPosition(tilePosition.X * tileSpacing - xTopLeft, tilePosition.Y * tileSpacing - yTopLeft);
-                if (lights.Count > 0)
-                {
-                    if (lighting)
-                    {
-                        System.Drawing.Color col = System.Drawing.Color.Black;
-                        foreach (Atom.Light l in lights)
-                        {
-                            double d = 1;
-                            Point p = new Point(tilePosition.X - l.position.X, tilePosition.Y - l.position.Y);
-                            p.X *= p.X;
-                            p.Y *= p.Y;
-                            d = Math.Sqrt(p.X + p.Y);
-                            if (d < 2)
-                                d = 2;
-                            col = Blend(col, l.color, 1 / d);
-                        }
-                        sideSprite.Color = col;
-                    }
-                    else
-                    {
-                        sideSprite.Color = System.Drawing.Color.White;
-                    }
-                    sideSprite.Draw();
-                }
-                else
-                {
-                    if (lighting)
-                    {
-                        sideSprite.Color = System.Drawing.Color.Black;
-                    }
-                    else
-                    {
-                        sideSprite.Color = System.Drawing.Color.White;
-                    }
-                    sideSprite.SetPosition(tilePosition.X * tileSpacing - xTopLeft, tilePosition.Y * tileSpacing - yTopLeft);
-                    sideSprite.Draw();
-                }
+                sideSprite.Color = color;
+                ShadeCorners(sideSprite);
+                sideSprite.Draw();
             }
         }
 
@@ -71,42 +37,9 @@ namespace SS3D.Tiles.Wall
             {
                 sprite.SetPosition(tilePosition.X * tileSpacing - xTopLeft, tilePosition.Y * tileSpacing - yTopLeft);
                 sprite.Position -= new Vector2D(0, tileSpacing);
-                if (lights.Count > 0)
-                {
-                    if (lighting)
-                    {
-                        System.Drawing.Color col = System.Drawing.Color.Black;
-                        foreach (Atom.Light l in lights)
-                        {
-                            double d = 1;
-                            Point p = new Point(tilePosition.X - l.position.X, tilePosition.Y - l.position.Y);
-                            p.X *= p.X;
-                            p.Y *= p.Y;
-                            d = Math.Sqrt(p.X + p.Y);
-                            if (d < 2)
-                                d = 2;
-                            col = Blend(col, l.color, 1 / d);
-                        }
-                        sprite.Color = col;
-                    }
-                    else
-                    {
-                        sprite.Color = System.Drawing.Color.White;
-                    }
-                    sprite.Draw();
-                }
-                else
-                {
-                    if (lighting)
-                    {
-                        sprite.Color = System.Drawing.Color.Black;
-                    }
-                    else
-                    {
-                        sprite.Color = System.Drawing.Color.White;
-                    }
-                    sprite.Draw();
-                }
+                sprite.Color = color;
+                ShadeCorners(sprite);
+                sprite.Draw();
             }
         }
     }

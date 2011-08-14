621c803de840cb9136a785608818fe38c85734c9
spoogemonster <spoogemonster@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2011-08-14 22:26:49 +0000

    Gas is now drawn clientside. This is SLOW AS FUCK. use the chatbox to test it out -- hit the t button to start typing, then type /addgas 1000 to add some toxins and watch the framerate plummet...

--- SS3D_Client/Tiles/Tile.cs
+++ SS3D_Client/Tiles/Tile.cs
@@ -24,6 +24,7 @@ namespace SS3D.Tiles
         public Tile[] surroundingTiles;
         //public Dictionary<VertexLocations, bool> vertexVisibility;
         public List<Light> tileLights;
+        public Dictionary<GasType, int> gasAmounts;
 
         public Tile(Sprite _sprite, TileState state, float size, Vector2D _position, Point _tilePosition)
         {
@@ -35,6 +36,7 @@ namespace SS3D.Tiles
             sightBlocked = false;
             surroundingTiles = new Tile[4];
             tileLights = new List<Light>();
+            gasAmounts = new Dictionary<GasType, int>();
         }
 
         public Tile(Sprite _sprite, Sprite _side, TileState state, float size, Vector2D _position, Point _tilePosition)
@@ -50,6 +52,7 @@ namespace SS3D.Tiles
             sightBlocked = false;
             surroundingTiles = new Tile[4];
             tileLights = new List<Light>();
+            gasAmounts = new Dictionary<GasType, int>();
         }
 
         public void SetSprites(Sprite _sprite, Sprite _side, byte _surroundDirs)
@@ -67,11 +70,52 @@ namespace SS3D.Tiles
                 sprite.SetPosition(tilePosition.X * tileSpacing - xTopLeft, tilePosition.Y * tileSpacing - yTopLeft);
                 LightManager.Singleton.ApplyLightsToSprite(tileLights, sprite, new Vector2D(xTopLeft, yTopLeft));
                 sprite.Draw();
+                
+            }
+        }
+        public virtual void RenderGas(float xTopLeft, float yTopLeft, int tileSpacing)
+        {
+            if (Visible && gasAmounts.Count > 0)
+            {
+                Sprite gasSprite = ResMgr.Singleton.GetSpriteFromImage("gas");
+                gasSprite.SetPosition(tilePosition.X * tileSpacing - xTopLeft, tilePosition.Y * tileSpacing - yTopLeft);
+                foreach (var gasAmount in gasAmounts)
+                {
+                    if (gasAmount.Value <= 1) //Meh.
+                        continue;
+                    int opacity = (int)Math.Floor(((double)gasAmount.Value / 15) * 255);
+                    
+                    switch (gasAmount.Key)
+                    {
+                        case GasType.HighVel:
+                            gasSprite.Color = Color.FromArgb(opacity, Color.White);
+                            break;
+                        case GasType.Toxin:
+                            gasSprite.Color = Color.FromArgb(opacity, Color.Orange);
+                            break;
+                        case GasType.WVapor:
+                            gasSprite.Color = Color.FromArgb(opacity, Color.LightBlue);
+                            break;
+                    }
+                    gasSprite.Draw();//UGH THIS IS SLOW AS FUCK
+                }
             }
         }
 
         public virtual void RenderTop(float xTopLeft, float yTopLeft, int tileSpacing)
         {
+        }
+
+        public virtual void SetAtmosDisplay(byte displayByte)
+        {
+            int _type = displayByte >> 4;
+            GasType type = (GasType)_type;
+            int amount = displayByte & 15;
+
+            if (gasAmounts.Keys.Contains(type))
+                gasAmounts[type] = amount;
+            else
+                gasAmounts.Add(type, amount);
         }
     }
 }

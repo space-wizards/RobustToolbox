ae02a8790dc245c984f07aedf7b5378fc3d8451a
spoogemonster <spoogemonster@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2011-08-31 22:22:00 +0000

    FUCK YOU YOU STUPID FUCKING PIECE OF DICKCHEESE

---
+++ SS3D_Client/Atom/Object/Wall/Glass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace SS3D.Atom.Object.Wall
+{
+    class Glass : Object
+    {
+        public Glass()
+            : base()
+        {
+            spritename = "glass";
+            collidable = true;
+            snapTogrid = true;
+        }
+
+        public override RectangleF GetAABB()
+        {
+            return new RectangleF(position.X - ((sprite.Width * sprite.UniformScale) / 2),
+            position.Y + ((sprite.Height * sprite.UniformScale) / 2) - 1,
+            (sprite.Width * sprite.UniformScale),
+            1);
+        }
+    }
+}
---
+++ SS3d_server/Atom/Object/Wall/Glass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SS3D_Server.Atom.Object.Wall
+{
+    [Serializable()]
+    class Glass : Object
+    {
+        public Glass()
+            : base()
+        {
+            name = "worktop";
+        }
+    }
+}

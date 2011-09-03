599e41a42b77d6d2f2bb85127afc5f4ade55e17c
keelin <keelin@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2011-09-03 13:51:20 +0000

    Getting there. Still F4 to dick around with placement. This time wall mounted objects placement.

--- SS3D_shared/HelperClasses/BuildPermission.cs
+++ SS3D_shared/HelperClasses/BuildPermission.cs
@@ -10,9 +10,7 @@ namespace SS3D_shared.HelperClasses
         public ushort mobUid;               //UID of mob this permission is for.
         public ushort range = 0;            //Valid range from mob.
         public string type = "";            //Object name / type.
-        public bool attachesToWall = false; //Can only be placed on 'solid' tiles.
-        public bool snapToSimilar = false;  //Will snap to similar nearby objects. (For windows, tables etc.). THIS DOES NOT WORK WITH TILES. USE SNAPTOGRID FOR THOSE.
-        public bool snapToTiles = false;    //Will snap to tiles.
-        public bool placeAnywhere = false;  //Can be placed anywhere without limitations. Overrides other options.
+        public AlignmentOptions AlignOption = AlignmentOptions.AlignTile; //Alignment type. See enum declaration for infos.
+        public bool placeAnywhere = false;  //Can be placed anywhere without limitations. Overrides other options. Tiles will still be placed in grid. Derp.
     }
 }

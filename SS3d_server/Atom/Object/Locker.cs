5f11895b14be10a0a40465b764372cfc78d23e86
spoogemonster <spoogemonster@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2011-09-04 20:36:45 +0000

    added lockers

---
+++ SS3d_server/Atom/Object/Locker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace SS3D_Server.Atom.Object
+{
+    [Serializable()]
+    class Locker : Object
+    {
+        private bool open = false;
+        public Locker()
+            : base()
+        {
+
+        }
+
+        public Locker(SerializationInfo info, StreamingContext ctxt)
+        {
+            SerializeBasicInfo(info, ctxt);
+        }
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext ctxt)
+        {
+            base.GetObjectData(info, ctxt);
+        }
+
+        
+        protected override void ApplyAction(Atom a, Mob.Mob m)
+        {
+            base.ApplyAction(a, m);
+
+            open = !open;
+            if (open)
+                SetSpriteState(1);
+            else
+                SetSpriteState(0);
+        }
+    }
+}

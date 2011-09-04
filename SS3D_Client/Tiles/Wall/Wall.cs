9cc6d9b038a8feba6f512818f33a07aa97b1c24b
spoogemonster <spoogemonster@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2011-09-04 12:47:28 +0000

    Fixed the way walltops are drawn when the adjacent tile to the north is not visible.

--- SS3D_Client/Tiles/Wall/Wall.cs
+++ SS3D_Client/Tiles/Wall/Wall.cs
@@ -11,7 +11,9 @@ namespace SS3D.Tiles.Wall
 {
     public class Wall : Tile
     {
-
+        private Sprite plainWall;
+        private Sprite wallCorner1;
+        private Sprite wallCorner2;
         public Wall(Sprite _sprite, Sprite _side, TileState state, float size, Vector2D _position, Point _tilePosition)
             : base(_sprite, _side, state, size, _position, _tilePosition)
         {
@@ -19,10 +21,19 @@ namespace SS3D.Tiles.Wall
             name = "Wall";
             sprite = _sprite;
             sideSprite = _side;
+            plainWall = ResMgr.Singleton.GetSprite("WallSide");
+            wallCorner1 = ResMgr.Singleton.GetSprite("wallcorner");
+            wallCorner2 = ResMgr.Singleton.GetSprite("wallcorner2");
         }
 
         public override void Render(float xTopLeft, float yTopLeft, int tileSpacing)
         {
+            if (surroundDirs == 3 || surroundDirs == 2) //north and east
+                sideSprite = wallCorner1;
+            else if (surroundDirs == 9 || surroundDirs == 8) // north and west
+                sideSprite = wallCorner2;
+            else
+                sideSprite = plainWall;
             if (Visible && ((surroundDirs&4) == 0))
             {
                 sideSprite.SetPosition(tilePosition.X * tileSpacing - xTopLeft, tilePosition.Y * tileSpacing - yTopLeft);
@@ -46,10 +57,21 @@ namespace SS3D.Tiles.Wall
         {
             if (Visible)
             {
+                
                 sprite.SetPosition(tilePosition.X * tileSpacing - xTopLeft, tilePosition.Y * tileSpacing - yTopLeft);
                 sprite.Position -= new Vector2D(0, tileSpacing);
-                sprite.Color = Color.FromArgb(200,Color.White);
+                sprite.Color = Color.FromArgb(200, Color.White);
                 wallTopsBatch.AddClone(sprite);
+            }
+            else 
+            {
+                if (surroundingTiles[0].Visible) //if the tile directly north of this one is visible, we should draw the wall top for this tile.
+                {
+                    sprite.SetPosition(tilePosition.X * tileSpacing - xTopLeft, tilePosition.Y * tileSpacing - yTopLeft);
+                    sprite.Position -= new Vector2D(0, tileSpacing);
+                    sprite.Color = Color.FromArgb(200, Color.White);
+                    wallTopsBatch.AddClone(sprite);
+                }
             }
         }
     }

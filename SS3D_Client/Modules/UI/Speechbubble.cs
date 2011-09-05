cdbab59fc3de763a0a41bd5a92f78f471ab36fb5
spoogemonster <spoogemonster@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2011-09-05 19:33:02 +0000

    Chatbubbles reimplemented.

--- SS3D_Client/Modules/UI/Speechbubble.cs
+++ SS3D_Client/Modules/UI/Speechbubble.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 
-using Mogre;
 using Lidgren.Network;
 
 using SS3D.Modules;
@@ -11,20 +10,159 @@ using SS3D.Atom;
 
 using SS3D_shared;
 
-using Miyagi;
-using Miyagi.UI;
-using Miyagi.UI.Controls;
-using Miyagi.Common;
-using Miyagi.Common.Data;
-using Miyagi.Common.Animation;
-using Miyagi.Common.Resources;
-using Miyagi.Common.Events;
-using Miyagi.TwoD;
+using GorgonLibrary;
+using GorgonLibrary.Graphics;
+using SS3D;
 
 
-public class SpeechBubble : GUI
+public class SpeechBubble
 {
-    MiyagiSystem mMiyagiMgr;
+    private RenderImage m_completeBubble;
+    private Sprite m_completeBubbleSprite;
+    private TextSprite textSprite;
+    private string m_text;
+    private string m_mobname;
+    private bool spriteupdaterequired = false;
+    private double secondstolive = 4;
+    private double secondsremaining = 0;
+    private DateTime lastUpdate;
+
+
+    public SpeechBubble(string mobname)
+    {
+        m_mobname = mobname;
+        lastUpdate = DateTime.Now;
+    }
+
+    public void Draw(Vector2D position, float xTopLeft, float yTopLeft, Sprite spriteToDrawAbove)
+    {
+        secondsremaining -= (DateTime.Now - lastUpdate).TotalMilliseconds / 1000;
+        if (secondsremaining <= 0)
+            return;
+        if(spriteupdaterequired)
+            DrawBubbleSprite();
+        int x = (int)Math.Round(position.X - xTopLeft - (m_completeBubbleSprite.Width / 2));
+        int y = (int)Math.Round(position.Y - yTopLeft - (m_completeBubbleSprite.Height) - (spriteToDrawAbove.Height / 2) - 5);
+        m_completeBubbleSprite.SetPosition(x,y);
+        m_completeBubbleSprite.Draw();
+        lastUpdate = DateTime.Now;
+    }
+
+    public void SetText(string text)
+    {
+        secondsremaining = secondstolive;
+        m_text = "";
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (i > 0 && i % 50 == 0)
+                m_text += "\n" + text.Substring(i, 1);
+            else
+                m_text += text.Substring(i, 1);
+        }
+        spriteupdaterequired = true;
+    }
+
+    public void DrawBubbleSprite()
+    {
+        var target = Gorgon.CurrentRenderTarget; // store current rendertarget
+        Sprite cornersprite = ResMgr.Singleton.GetSprite("corners");
+
+        //Set up dimensions
+
+        if (textSprite == null)
+        {
+            textSprite = new TextSprite("chatBubbleTextSprite_" + m_mobname, "", ResMgr.Singleton.GetFont("CALIBRI"));
+            textSprite.Color = System.Drawing.Color.Black;
+            textSprite.WordWrap = true;
+            textSprite.SetPosition(5, 3);
+        }
+
+        textSprite.Text = m_text;
+        textSprite.UpdateAABB();
+
+        if (m_completeBubble == null)
+            m_completeBubble = new RenderImage("ChatBubbleRenderImage_" + m_mobname, 1, 1, ImageBufferFormats.BufferRGB888A8);
+        if (m_completeBubbleSprite == null)
+            m_completeBubbleSprite = new Sprite("ChatBubbleRenderSprite_" + m_mobname, m_completeBubble);
+
+        m_completeBubble.SetDimensions((int)textSprite.Size.X + 10, (int)textSprite.Size.Y + 10);
+        m_completeBubbleSprite.SetSize(textSprite.Size.X + 10, textSprite.Size.Y + 10);
+
+        //BEGIN RENDERING
+        Gorgon.CurrentRenderTarget = m_completeBubble;
+        m_completeBubble.Clear(System.Drawing.Color.FromArgb(0, System.Drawing.Color.White));
+
+        //Draw black triangle at the bottom
+        VertexTypeList.PositionDiffuse2DTexture1[] blacktriangle = new VertexTypeList.PositionDiffuse2DTexture1[3];
+        blacktriangle[0].Position.X = (m_completeBubble.Width / 2) - 10;
+        blacktriangle[1].Position.X = (m_completeBubble.Width / 2) + 10;
+        blacktriangle[2].Position.X = (m_completeBubble.Width / 2);
+        blacktriangle[0].Position.Y = (m_completeBubble.Height - 15);
+        blacktriangle[1].Position.Y = (m_completeBubble.Height - 15);
+        blacktriangle[2].Position.Y = m_completeBubble.Height;
+        blacktriangle[0].TextureCoordinates.X = 0.0f;
+        blacktriangle[0].TextureCoordinates.Y = 0.0f;
+        blacktriangle[0].Color = System.Drawing.Color.FromArgb(255, System.Drawing.Color.Black);
+        blacktriangle[1].TextureCoordinates.X = 0.0f;
+        blacktriangle[1].TextureCoordinates.Y = 0.0f;
+        blacktriangle[1].Color = System.Drawing.Color.FromArgb(255, System.Drawing.Color.Black);
+        blacktriangle[2].TextureCoordinates.X = 0.0f;
+        blacktriangle[2].TextureCoordinates.Y = 0.0f;
+        blacktriangle[2].Color = System.Drawing.Color.FromArgb(255, System.Drawing.Color.Black);
+        m_completeBubble.Draw(blacktriangle);
+
+        //Draw the side lines
+        for (int i = 0; i < 3; i++)
+        {
+            m_completeBubble.Line(10, i, m_completeBubble.Width - 20, 1, System.Drawing.Color.Black);
+            m_completeBubble.Line(m_completeBubble.Width - 1 - i, 10, 1, m_completeBubble.Height - 26, System.Drawing.Color.Black);
+            m_completeBubble.Line(10, m_completeBubble.Height - 7 - i, m_completeBubble.Width - 20, 1, System.Drawing.Color.Black);
+            m_completeBubble.Line(i, 10, 1, m_completeBubble.Height - 26, System.Drawing.Color.Black);
+        }
+        //Fill in the middle
+        m_completeBubble.FilledRectangle(3, 3, m_completeBubble.Width - 6, m_completeBubble.Height - 12, System.Drawing.Color.White);
+
+        //Draw the white triangle at the bottom.
+        VertexTypeList.PositionDiffuse2DTexture1[] whitetriangle = new VertexTypeList.PositionDiffuse2DTexture1[3];
+        whitetriangle[0].Position.X = (m_completeBubble.Width / 2) - 7;
+        whitetriangle[1].Position.X = (m_completeBubble.Width / 2) + 7;
+        whitetriangle[2].Position.X = (m_completeBubble.Width / 2);
+        whitetriangle[0].Position.Y = (m_completeBubble.Height - 15);
+        whitetriangle[1].Position.Y = (m_completeBubble.Height - 15);
+        whitetriangle[2].Position.Y = m_completeBubble.Height - 4;
+        whitetriangle[0].TextureCoordinates.X = 0.0f;
+        whitetriangle[0].TextureCoordinates.Y = 0.0f;
+        whitetriangle[0].Color = System.Drawing.Color.FromArgb(255, System.Drawing.Color.White);
+        whitetriangle[1].TextureCoordinates.X = 0.0f;
+        whitetriangle[1].TextureCoordinates.Y = 0.0f;
+        whitetriangle[1].Color = System.Drawing.Color.FromArgb(255, System.Drawing.Color.White);
+        whitetriangle[2].TextureCoordinates.X = 0.0f;
+        whitetriangle[2].TextureCoordinates.Y = 0.0f;
+        whitetriangle[2].Color = System.Drawing.Color.FromArgb(255, System.Drawing.Color.White);
+        m_completeBubble.Draw(whitetriangle);
+
+        //Draw the corners.
+        cornersprite.SourceBlend = AlphaBlendOperation.One;
+        cornersprite.DestinationBlend = AlphaBlendOperation.Zero;
+        cornersprite.VerticalFlip = true;
+        cornersprite.SetPosition(0, 0);
+        cornersprite.Draw();
+        cornersprite.HorizontalFlip = true;
+        cornersprite.SetPosition(m_completeBubble.Width - 16, 0);
+        cornersprite.Draw();
+        cornersprite.VerticalFlip = false;
+        cornersprite.SetPosition(m_completeBubble.Width - 16, m_completeBubble.Height - 22);
+        cornersprite.Draw();
+        cornersprite.HorizontalFlip = false;
+        cornersprite.SetPosition(0, m_completeBubble.Height - 22);
+        cornersprite.Draw();
+        textSprite.Draw();
+
+        Gorgon.CurrentRenderTarget = target; // restore rendertarget
+        spriteupdaterequired = false; //Sprite is now up to date
+    }
+
+    /*MiyagiSystem mMiyagiMgr;
     OgreManager mEngine;
     MovableObject host;
 
@@ -212,5 +350,5 @@ public class SpeechBubble : GUI
         int tail_y = mainBubble.Location.Y + mainBubble.Size.Height - 3;
         this.addBubble.Location = new Point(tail_x, tail_y);
     }
-
+    */
 }

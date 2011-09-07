5987ae85e69d463d040fdfc688d5066fdb1b56e4
spoogemonster <spoogemonster@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2011-09-07 15:10:45 +0000

    Server and client both now run in a sandboxed AppDomain. I am a god.

--- SS3D_Client/Program.cs
+++ SS3D_Client/Program.cs
@@ -15,92 +15,121 @@ using Lidgren.Network;
 
 using CSScriptLibrary;
 using csscript;
+using System.Security.Policy;
+using System.Security;
+using System.Security.Permissions;
+using System.Runtime.Remoting;
 
 namespace SS3D
 {
-  public class Program
-  {
-    private StateManager stateMgr;
-    public StateManager mStateMgr
-    { 
-        get { return stateMgr; }
-        private set { stateMgr = value; }
-    }
-
-    private NetworkManager networkMgr;
-    public NetworkManager mNetworkMgr
+    public class Program : MarshalByRefObject, IProgram
     {
-        get { return networkMgr; }
-        private set { networkMgr = value; }
-    }
-
-    private MainWindow gorgonForm;
-    public MainWindow GorgonForm
-    {
-        get { return gorgonForm; }
-        private set { gorgonForm = value; }
-    }
-
-    /************************************************************************/
-    /* program starts here                                                  */
-    /************************************************************************/
-    [STAThread]
-    static void Main(string[] args)
-    {
-      // Create main program
-      Program prg = new Program();
-
-      // Load Config.
-      ConfigManager.Singleton.Initialize("./config.xml");
-
-        //Load Moar types
-      prg.loadTypes();
-
-      // Create state manager
-      prg.mStateMgr = new StateManager(prg);
-
-      // Create main form
-      prg.GorgonForm = new MainWindow(prg);
-      
-      // Create Network Manager
-      prg.mNetworkMgr = new NetworkManager(prg);
-
-      Gorgon.Idle += new FrameEventHandler(prg.GorgonIdle);
-      System.Windows.Forms.Application.Run(prg.GorgonForm);
-    }
-
-    public void GorgonIdle(object sender, FrameEventArgs e)
-    {
-
-    }
-
-    public void loadTypes()
-    {
-        /*var asm = CSScript.Load("hat.cs");
-        Module[] modules = asm.GetModules();
-        Type blah = null;
-        foreach (Module module in modules)
+        private StateManager stateMgr;
+        public StateManager mStateMgr
         {
-            if(module.GetType("SS3D.Atom.Item.Tool.Hat") != null)
-                blah = module.GetType("SS3D.Atom.Item.Tool.Hat");
+            get { return stateMgr; }
+            private set { stateMgr = value; }
         }
 
-        Atom.Atom atom;
+        private NetworkManager networkMgr;
+        public NetworkManager mNetworkMgr
+        {
+            get { return networkMgr; }
+            private set { networkMgr = value; }
+        }
 
-        if(blah != null)
+        private MainWindow gorgonForm;
+        public MainWindow GorgonForm
+        {
+            get { return gorgonForm; }
+            private set { gorgonForm = value; }
+        }
+
+        /************************************************************************/
+        /* program starts here                                                  */
+        /************************************************************************/
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Evidence e = new Evidence(AppDomain.CurrentDomain.Evidence); // Some evidence bullshit we need
+
+            AppDomainSetup info = new AppDomainSetup(); // Appdomain parameters
+            info.ApplicationBase = Assembly.GetExecutingAssembly().CodeBase;
+
+            /// SET UP PERMISSIONS
+            PermissionSet permissions = new PermissionSet(PermissionState.Unrestricted);
+            //permissions.AddPermission(new SecurityPermission(SecurityPermissionFlag.Execution));
+
+            /// Create app domain
+            AppDomain mainDomain = AppDomain.CreateDomain("mainDomain", e, info, permissions);
+
+            /// Load SS3D_Server into the new app domain and instantiate EntryPoint
+            ObjectHandle handle = Activator.CreateInstanceFrom(mainDomain, typeof(Program).Assembly.ManifestModule.FullyQualifiedName, typeof(Program).FullName);
+            /// Align to interface so we can use args
+            IProgram prg = (IProgram)handle.Unwrap();
+
+            ///Start the server with args.
+            prg.StartProgram(args);
+        }
+
+        public void GorgonIdle(object sender, FrameEventArgs e)
+        {
+
+        }
+
+        public void loadTypes()
+        {
+            /*var asm = CSScript.Load("hat.cs");
+            Module[] modules = asm.GetModules();
+            Type blah = null;
+            foreach (Module module in modules)
+            {
+            if(module.GetType("SS3D.Atom.Item.Tool.Hat") != null)
+            blah = module.GetType("SS3D.Atom.Item.Tool.Hat");
+            }
+
+            Atom.Atom atom;
+
+            if(blah != null)
             atom = (Atom.Atom)Activator.CreateInstance(blah);
-        //Type atomType = asm.GetModules().GetType("SS3D.Atom.Item.Tool.Hat");
-        //object atom = Activator.CreateInstance(atomType); // Create atom of type atomType with parameters uid, this
-        //AsmHelper script = new AsmHelper(Asm);
-        //var poo = script.CreateObject("SS3D.Atom.Item.Tool.Hat");
-        */
+            //Type atomType = asm.GetModules().GetType("SS3D.Atom.Item.Tool.Hat");
+            //object atom = Activator.CreateInstance(atomType); // Create atom of type atomType with parameters uid, this
+            //AsmHelper script = new AsmHelper(Asm);
+            //var poo = script.CreateObject("SS3D.Atom.Item.Tool.Hat");
+            */
+        }
+
+        public Program()
+        {
+            //Constructor
+        }
+
+        public void StartProgram(string[] args)
+        {
+            // Load Config.
+            ConfigManager.Singleton.Initialize("./config.xml");
+
+            //Load Moar types
+            loadTypes();
+
+            // Create state manager
+            mStateMgr = new StateManager(this);
+
+            // Create main form
+            GorgonForm = new MainWindow(this);
+
+            // Create Network Manager
+            mNetworkMgr = new NetworkManager(this);
+
+            Gorgon.Idle += new FrameEventHandler(GorgonIdle);
+            System.Windows.Forms.Application.Run(GorgonForm);
+        }
+
     }
 
-    public Program()
+    public interface IProgram
     {
-        //Constructor
+        void StartProgram(string[] args);
     }
-
-  }
 
 }
--- SS3d_server/Program.cs
+++ SS3d_server/Program.cs
@@ -5,20 +5,54 @@ using System.Text;
 
 using Lidgren.Network;
 using SS3D_Server.Modules;
+using System.Security;
+using System.Security.Policy;
+using System.Security.Permissions;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Remoting;
 
 namespace SS3D_Server
 {
-    class EntryPoint
+    class EntryPoint : MarshalByRefObject, IEntryPoint
     {
         private SS3DServer server;
 
         static void Main(string[] args)
         {
-            EntryPoint main = new EntryPoint();
-            LogManager.Log("Server -> Starting");
-            main.server = new SS3DServer();
+            Evidence e = new Evidence(AppDomain.CurrentDomain.Evidence); // Some evidence bullshit we need
 
-            if (main.server.Start())
+            AppDomainSetup info = new AppDomainSetup(); // Appdomain parameters
+            info.ApplicationBase = Assembly.GetExecutingAssembly().CodeBase;
+
+            /// SET UP PERMISSIONS
+            PermissionSet permissions = new PermissionSet(PermissionState.Unrestricted);
+            //permissions.AddPermission(new SecurityPermission(SecurityPermissionFlag.Execution));
+
+            /// Create app domain
+            AppDomain mainDomain = AppDomain.CreateDomain("mainDomain", e, info, permissions);
+
+            /// Load SS3D_Server into the new app domain and instantiate EntryPoint
+            ObjectHandle handle = Activator.CreateInstanceFrom(mainDomain, typeof(EntryPoint).Assembly.ManifestModule.FullyQualifiedName, typeof(EntryPoint).FullName);
+            /// Align to interface so we can use args
+            IEntryPoint entryPoint = (IEntryPoint)handle.Unwrap();
+
+            ///Start the server with args.
+            entryPoint.StartServer(args);
+        }
+
+        public EntryPoint()
+        {
+            server = new SS3DServer();
+        }
+
+        public void StartServer(string[] args)
+        {
+      
+            //EntryPoint main = new EntryPoint();
+            LogManager.Log("Server -> Starting");
+
+            if (server.Start())
             {
                 LogManager.Log("Server -> Can not start server", LogLevel.Fatal); //Not like you'd see this, haha. Perhaps later for logging.
                 Environment.Exit(0);
@@ -27,9 +61,14 @@ namespace SS3D_Server
             string strVersion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
             LogManager.Log("Server Version " + strVersion + " -> Ready");
 
-            main.server.MainLoop();
+            server.MainLoop();
         }
 
 
+    }
+
+    public interface IEntryPoint
+    {
+        void StartServer(string[] args);
     }
 }

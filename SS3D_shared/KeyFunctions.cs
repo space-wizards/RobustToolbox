9a02abab19c32dd31c2e6a43d004d8f0c60669b9
spoogemonster <spoogemonster@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2011-09-21 20:23:32 +0000

    Missed a few files

---
+++ SS3D_shared/KeyFunctions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SS3D_shared
+{
+    public enum BoundKeyState
+    {
+        Up,
+        Down
+    }
+
+    /// <summary>
+    /// Key Bindings - each corresponds to a logical function ingame.
+    /// </summary>
+    public enum BoundKeyFunctions
+    {
+        MoveUp,
+        MoveDown,
+        MoveLeft,
+        MoveRight,
+        SwitchHands,
+        Inventory,
+        ShowFPS,
+        Drop,
+        Run,
+    }
+}

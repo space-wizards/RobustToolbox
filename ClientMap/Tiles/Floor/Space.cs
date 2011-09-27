f1ba27a39c899a70633428c6f62f355bc991f2f8
spoogemonster <spoogemonster@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2011-09-27 18:22:13 +0000

    Stupid svn, it forgot to add the map files :p

---
+++ ClientMap/Tiles/Floor/Space.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GorgonLibrary;
+using GorgonLibrary.Graphics;
+using System.Drawing;
+using ClientInterfaces;
+
+namespace ClientMap.Tiles.Floor
+{
+    public class Space : Tile
+    {
+        public Space(Sprite _sprite, TileState state, float size, Vector2D _position, Point _tilePosition, ILightManager _lightManager)
+            : base(_sprite, state, size, _position, _tilePosition, _lightManager)
+        {
+            tileType = TileType.Space;
+            name = "Space";
+        }
+
+        public override void Render(float xTopLeft, float yTopLeft, int tileSpacing)
+        {
+            return;
+        }
+    }
+}

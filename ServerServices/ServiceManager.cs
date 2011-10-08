fd6d87e9dfc974643fad88f1a7ea5f0ba6f1e72b
spoogemonster <spoogemonster@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2011-10-08 15:31:12 +0000

    Adding serverinterfaces and serverservices (lol)

---
+++ ServerServices/ServiceManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SS3D_shared;
+using ServerInterfaces;
+
+namespace ClientServices
+{
+    /// <summary>
+    /// WTF is wrong with me im gay
+    /// </summary>
+    public class ServiceManager
+    {
+        Dictionary<ServerServiceType, IService> services;
+
+        private static ServiceManager singleton;
+        public static ServiceManager Singleton
+        {
+            get {
+                if (singleton == null)
+                    singleton = new ServiceManager();
+                return singleton;
+            }
+        }
+
+        public ServiceManager()
+        {
+            services = new Dictionary<ServerServiceType, IService>();
+        }
+
+        public void AddService(IService service)
+        {
+            if (services.ContainsKey(service.ServiceType))
+                return;
+            else
+                services.Add(service.ServiceType, service);
+        }
+
+        public void RemoveService(IService service)
+        {
+            if(services.ContainsKey(service.ServiceType))
+                services.Remove(service.ServiceType);
+        }
+
+        public IService GetService(ServerServiceType serviceType)
+        {
+            if (services.ContainsKey(serviceType))
+                return services[serviceType];
+            else
+                return null;
+        }
+    }
+}

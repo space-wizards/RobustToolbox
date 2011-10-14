67ca17f0f74769fd0f0ecc22f058efbc038ca31b
spoogemonster <spoogemonster@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2011-10-14 01:04:14 +0000

    More item capability shite

--- SGO/Component/Item/ItemCapability/ItemCapability.cs
+++ SGO/Component/Item/ItemCapability/ItemCapability.cs
@@ -10,11 +10,95 @@ namespace SGO.Component.Item.ItemCapability
     {
         public InteractsWith interactsWith; //What types of shit this interacts with
         public int priority; //Where in the stack this puppy is
-        public ItemCapabilityType itemType;
+        private ItemCapabilityType capabilityType;
+        public ItemCapabilityType CapabilityType
+        {
+            get { return capabilityType; }
+            protected set { capabilityType = value; }
+        }
+
 
         public bool ApplyTo(Entity target)
         {
             throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// Query datatype
+    /// </summary>
+    public struct ItemCapabilityQuery
+    {
+        public ItemCapabilityQueryType queryType;
+        public ItemCapabilityType capabilityType;
+        public ItemCapabilityQuery(ItemCapabilityQueryType _queryType, ItemCapabilityType _capabilityType)
+        {
+            queryType = _queryType;
+            capabilityType = _capabilityType;
+        }
+
+        public enum ItemCapabilityQueryType
+        {
+            HasCapability,
+            GetCapability,
+            GetAllCapabilities,
+        }
+    }
+
+    /// <summary>
+    /// Query result datatype
+    /// </summary>
+    public class ItemCapabilityQueryResult
+    {
+        private ItemCapabilityQueryResultType resultStatus;
+        public ItemCapabilityQueryResultType ResultStatus
+        {
+            get 
+            { 
+                if(resultStatus == null)
+                    return ItemCapabilityQueryResultType.Null;
+                else 
+                    return resultStatus;
+            }
+            set { resultStatus = value; }
+        }
+
+        private string errorMessage;
+        public string ErrorMessage
+        {
+            get { return errorMessage; }
+            set { errorMessage = value; }
+        }
+
+        private ItemCapability[] returnedCapabilities;
+
+        public ItemCapability[] Capabilities
+        {
+            get { return returnedCapabilities; }
+        }
+
+        /// <summary>
+        /// Adds a capability to the query result to be returned.
+        /// </summary>
+        /// <param name="cap"></param>
+        public void AddCapability(ItemCapability cap)
+        {
+            var retcap = returnedCapabilities.ToList();
+            retcap.Add(cap);
+            returnedCapabilities = retcap.ToArray();
+        }
+        
+        /// <summary>
+        /// Types of results
+        /// </summary>
+        public enum ItemCapabilityQueryResultType
+        {
+            True,
+            False,
+            Success,
+            Empty,
+            Error,
+            Null
         }
     }
 }

6e31b07ffb30d428cb8a8344370082ffb149dec6
spoogemonster <spoogemonster@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2012-01-26 16:16:41 +0000

    Added puddles, and worked on think component

---
+++ SGO/Component/Think/ThinkComponent/PuddleThinkComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SGO.Component.Think.ThinkComponent
+{
+    public class PuddleThinkComponent : ThinkComponent
+    {
+        public override void OnBump(object sender, params object[] list)
+        {
+            base.OnBump(sender, list);
+            ServerServices.LogManager.Log("Puddle Bumped!");
+        }
+    }
+}

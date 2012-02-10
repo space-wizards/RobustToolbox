e1dbac88d36f064a8236693ba1af84ed39ba3284
nyxation <nyxation@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2012-02-10 05:35:57 +0000

    Updated Chatbox so input, unsent text will shift appropriately when the line is larger than the input box.

--- ClientServices/UserInterface/Components/Chatbox.cs
+++ ClientServices/UserInterface/Components/Chatbox.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Drawing;
+using System.Text;
 using ClientInterfaces;
 using ClientInterfaces.Input;
 using ClientInterfaces.UserInterface;
@@ -22,14 +23,15 @@ namespace ClientServices.UserInterface.Components
         public event TextSubmitHandler TextSubmitted;
 
         private readonly List<Label> _entries = new List<Label>();
+        private readonly StringBuilder _currentInputText;
 
         private Label _textInputLabel;
         private Sprite _backgroundSprite;
         private RenderImage _renderImage;
 
-        private const int maxLines = 20;
-        private int _chatMessages;
+        private const int MaxLines = 20;
         private const int MaxLineLength = 90;
+        private const int MaxLinePixelLength = 585;
         private readonly Dictionary<ChatChannel, Color> _chatColors;
 
         private bool _disposing;
@@ -41,7 +43,6 @@ namespace ClientServices.UserInterface.Components
             set
             {
                 _active = value;
-                //HACK
                 _keyBindingManager.Enabled = !_active;
             }
         }
@@ -51,26 +52,34 @@ namespace ClientServices.UserInterface.Components
             _resourceManager = resourceManager;
             _userInterfaceManager = userInterfaceManager;
             _keyBindingManager = keyBindingManager;
+
+            _currentInputText = new StringBuilder();
             
             ClientArea = new Rectangle(5, Gorgon.Screen.Height - 205, 600, 200); //!!! Use this instead of Window Dimensions
             Position = new Point(5, Gorgon.Screen.Height - 205);
 
             _backgroundSprite = _resourceManager.GetSprite("1pxwhite");
 
-            _textInputLabel = new Label("", _resourceManager);
-            _textInputLabel.Text.Size = new Size(ClientArea.Width - 10, 12);
-            _textInputLabel.Position = new Point(Position.X + 2, Position.Y + ClientArea.Size.Height - 10);
-            _textInputLabel.Text.Color = Color.Green;
-            _textInputLabel.Text.WordWrap = true;
+            _textInputLabel = new Label("", _resourceManager)
+                                {
+                                    Text =
+                                        {
+                                            Size = new Size(ClientArea.Width - 10, 12),
+                                            Color = Color.Green
+                                        },
+                                    Position = new Point(Position.X + 2, Position.Y + ClientArea.Size.Height - 10),
+                                };
 
-            _chatColors = new Dictionary<ChatChannel, Color>();
-            _chatColors.Add(ChatChannel.Default, Color.Gray);
-            _chatColors.Add(ChatChannel.Damage, Color.Red);
-            _chatColors.Add(ChatChannel.Radio, Color.DarkGreen);
-            _chatColors.Add(ChatChannel.Server, Color.Blue);
-            _chatColors.Add(ChatChannel.Player, Color.Green);
-            _chatColors.Add(ChatChannel.Lobby, Color.White);
-            _chatColors.Add(ChatChannel.Ingame, Color.Green);
+            _chatColors = new Dictionary<ChatChannel, Color>
+                            {
+                                {ChatChannel.Default, Color.Gray},
+                                {ChatChannel.Damage, Color.Red},
+                                {ChatChannel.Radio, Color.DarkGreen},
+                                {ChatChannel.Server, Color.Blue},
+                                {ChatChannel.Player, Color.Green},
+                                {ChatChannel.Lobby, Color.White},
+                                {ChatChannel.Ingame, Color.Green}
+                            };
 
             _renderImage = new RenderImage("chatboxRI", ClientArea.Size.Width, ClientArea.Size.Height,
                                           ImageBufferFormats.BufferUnknown) {ClearEachFrame = ClearTargets.None};
@@ -83,42 +92,42 @@ namespace ClientServices.UserInterface.Components
             var renderPos = new Point(0, 0);
 
             _renderImage.BeginDrawing();
-            _backgroundSprite.Color = System.Drawing.Color.FromArgb(51, 56, 64);
+            _backgroundSprite.Color = Color.FromArgb(51, 56, 64);
             _backgroundSprite.Opacity = 240;
             _backgroundSprite.Draw(new Rectangle(renderPos, new Size(ClientArea.Width, ClientArea.Height)));
 
-            Sprite corner_top_left = _resourceManager.GetSprite("corner_top_left");
-            corner_top_left.Draw(new Rectangle(renderPos.X, renderPos.Y, (int)corner_top_left.Width, (int)corner_top_left.Height));
+            var cornerTopLeft = _resourceManager.GetSprite("corner_top_left");
+            cornerTopLeft.Draw(new Rectangle(renderPos.X, renderPos.Y, (int)cornerTopLeft.Width, (int)cornerTopLeft.Height));
 
-            Sprite corner_top_right = _resourceManager.GetSprite("corner_top_right");
-            corner_top_right.Draw(new Rectangle(renderPos.X + ClientArea.Width - (int)corner_top_right.Width, renderPos.Y, (int)corner_top_right.Width, (int)corner_top_right.Height));
+            var cornerTopRight = _resourceManager.GetSprite("corner_top_right");
+            cornerTopRight.Draw(new Rectangle(renderPos.X + ClientArea.Width - (int)cornerTopRight.Width, renderPos.Y, (int)cornerTopRight.Width, (int)cornerTopRight.Height));
 
-            Sprite border_top = _resourceManager.GetSprite("border_top");
-            border_top.Draw(new Rectangle(renderPos.X + (int)corner_top_left.Width, renderPos.Y, ClientArea.Width - (int)corner_top_left.Width - (int)corner_top_right.Width, (int)border_top.Height));
+            var borderTop = _resourceManager.GetSprite("border_top");
+            borderTop.Draw(new Rectangle(renderPos.X + (int)cornerTopLeft.Width, renderPos.Y, ClientArea.Width - (int)cornerTopLeft.Width - (int)cornerTopRight.Width, (int)borderTop.Height));
 
-            Sprite corner_bottom_left = _resourceManager.GetSprite("corner_bottom_left");
-            corner_bottom_left.Draw(new Rectangle(renderPos.X, renderPos.Y + ClientArea.Height - (int)corner_bottom_left.Height, (int)corner_bottom_left.Width, (int)corner_bottom_left.Height));
+            var cornerBottomLeft = _resourceManager.GetSprite("corner_bottom_left");
+            cornerBottomLeft.Draw(new Rectangle(renderPos.X, renderPos.Y + ClientArea.Height - (int)cornerBottomLeft.Height, (int)cornerBottomLeft.Width, (int)cornerBottomLeft.Height));
 
-            Sprite corner_bottom_right = _resourceManager.GetSprite("corner_bottom_right");
-            corner_bottom_right.Draw(new Rectangle(renderPos.X + ClientArea.Width - (int)corner_bottom_right.Width, renderPos.Y + ClientArea.Height - (int)corner_bottom_right.Height, (int)corner_bottom_right.Width, (int)corner_bottom_right.Height));
+            var cornerBottomRight = _resourceManager.GetSprite("corner_bottom_right");
+            cornerBottomRight.Draw(new Rectangle(renderPos.X + ClientArea.Width - (int)cornerBottomRight.Width, renderPos.Y + ClientArea.Height - (int)cornerBottomRight.Height, (int)cornerBottomRight.Width, (int)cornerBottomRight.Height));
 
-            Sprite border_left = _resourceManager.GetSprite("border_left");
-            border_left.Draw(new Rectangle(renderPos.X, renderPos.Y + (int)corner_top_left.Height, (int)border_left.Width, ClientArea.Height - (int)corner_bottom_left.Height - (int)corner_top_left.Height));
+            var borderLeft = _resourceManager.GetSprite("border_left");
+            borderLeft.Draw(new Rectangle(renderPos.X, renderPos.Y + (int)cornerTopLeft.Height, (int)borderLeft.Width, ClientArea.Height - (int)cornerBottomLeft.Height - (int)cornerTopLeft.Height));
 
-            Sprite border_right = _resourceManager.GetSprite("border_right");
-            border_right.Draw(new Rectangle(renderPos.X + ClientArea.Width - (int)border_right.Width, renderPos.Y + (int)corner_top_right.Height, (int)border_right.Width, ClientArea.Height - (int)corner_bottom_right.Height - (int)corner_top_right.Height));
+            var borderRight = _resourceManager.GetSprite("border_right");
+            borderRight.Draw(new Rectangle(renderPos.X + ClientArea.Width - (int)borderRight.Width, renderPos.Y + (int)cornerTopRight.Height, (int)borderRight.Width, ClientArea.Height - (int)cornerBottomRight.Height - (int)cornerTopRight.Height));
 
-            Sprite border_bottom = _resourceManager.GetSprite("border_bottom");
-            border_bottom.Draw(new Rectangle(renderPos.X + (int)corner_top_left.Width, renderPos.Y + ClientArea.Height - (int)border_bottom.Height, ClientArea.Width - (int)corner_bottom_left.Width - (int)corner_bottom_right.Width, (int)border_bottom.Height));
+            var borderBottom = _resourceManager.GetSprite("border_bottom");
+            borderBottom.Draw(new Rectangle(renderPos.X + (int)cornerTopLeft.Width, renderPos.Y + ClientArea.Height - (int)borderBottom.Height, ClientArea.Width - (int)cornerBottomLeft.Width - (int)cornerBottomRight.Width, (int)borderBottom.Height));
 
-            Sprite corner_middle_left = _resourceManager.GetSprite("corner_middle_left");
-            corner_middle_left.Draw(new Rectangle(renderPos.X, renderPos.Y + ClientArea.Height - 16 - (int)corner_middle_left.Height, (int)corner_middle_left.Width, (int)corner_middle_left.Height));
+            var cornerMiddleLeft = _resourceManager.GetSprite("corner_middle_left");
+            cornerMiddleLeft.Draw(new Rectangle(renderPos.X, renderPos.Y + ClientArea.Height - 16 - (int)cornerMiddleLeft.Height, (int)cornerMiddleLeft.Width, (int)cornerMiddleLeft.Height));
 
-            Sprite corner_middle_right = _resourceManager.GetSprite("corner_middle_right");
-            corner_middle_right.Draw(new Rectangle(renderPos.X + ClientArea.Width - (int)corner_middle_right.Width, renderPos.Y + ClientArea.Height - 16 - (int)corner_middle_right.Height, (int)corner_middle_right.Width, (int)corner_middle_right.Height));
+            var cornerMiddleRight = _resourceManager.GetSprite("corner_middle_right");
+            cornerMiddleRight.Draw(new Rectangle(renderPos.X + ClientArea.Width - (int)cornerMiddleRight.Width, renderPos.Y + ClientArea.Height - 16 - (int)cornerMiddleRight.Height, (int)cornerMiddleRight.Width, (int)cornerMiddleRight.Height));
 
-            Sprite border_middle = _resourceManager.GetSprite("border_middle_h");
-            border_middle.Draw(new Rectangle(renderPos.X + (int)corner_middle_left.Width, renderPos.Y + ClientArea.Height - 16 - (int)border_middle.Height, ClientArea.Width - (int)corner_middle_left.Width - (int)corner_middle_right.Width, (int)border_middle.Height));
+            var borderMiddle = _resourceManager.GetSprite("border_middle_h");
+            borderMiddle.Draw(new Rectangle(renderPos.X + (int)cornerMiddleLeft.Width, renderPos.Y + ClientArea.Height - 16 - (int)borderMiddle.Height, ClientArea.Width - (int)cornerMiddleLeft.Width - (int)cornerMiddleRight.Width, (int)borderMiddle.Height));
 
             _renderImage.EndDrawing();
         }
@@ -132,13 +141,16 @@ namespace ClientServices.UserInterface.Components
                             .GroupBy(w => (charCount += w.Length + 1) / MaxLineLength)
                             .Select(g => string.Join(" ", g));
 
-            foreach (string part in messageSplit)
+            foreach (var label in messageSplit.Select(part => new Label(part, _resourceManager)
+                                                                  {
+                                                                      Text =
+                                                                          {
+                                                                              Size = new Size(ClientArea.Width - 10, 12),
+                                                                              Color = _chatColors[channel]
+                                                                          }
+                                                                  }))
             {
-                var label = new Label(part, _resourceManager);
-                label.Text.Size = new Size(ClientArea.Width - 10, 12);
-                label.Text.Color = _chatColors[channel];
                 _entries.Add(label);
-                _chatMessages++;
             }
 
             DrawLines();
@@ -146,18 +158,36 @@ namespace ClientServices.UserInterface.Components
 
         private void DrawLines()
         {
-            _textInputLabel.Position = new Point(this.ClientArea.X + 4, ClientArea.Y + ClientArea.Height - 20);
+            CheckAndSetLine(_currentInputText.ToString());
+            
+            _textInputLabel.Position = new Point(ClientArea.X + 4, ClientArea.Y + ClientArea.Height - 20);
             _textInputLabel.Render();
 
-            while (_entries.Count > maxLines)
+            while (_entries.Count > MaxLines)
                 _entries.RemoveAt(0);
 
-            int start = Math.Max(0, _entries.Count - 12);
+            var start = Math.Max(0, _entries.Count - 12);
 
-            for (int i = _entries.Count - 1; i >= start; i--)
+            for (var i = _entries.Count - 1; i >= start; i--)
             {
-                _entries[i].Position = new Point(this.ClientArea.X + 2, this.ClientArea.Y + this.ClientArea.Height - (14 * (_entries.Count - i)) - 26);
+                _entries[i].Position = new Point(ClientArea.X + 2, ClientArea.Y + ClientArea.Height - (14 * (_entries.Count - i)) - 26);
                 _entries[i].Render();
+            }
+        }
+
+        private void CheckAndSetLine(string line)
+        {
+            // TODO: Refactor to use dynamic input box size.
+            // Magic number is pixel width of chatbox input rectangle at
+            // current resolution. Will need to modify once handling
+            // different resolutions and scaling.
+            if (_textInputLabel.Text.MeasureLine(line) > MaxLinePixelLength)
+            {
+                CheckAndSetLine(line.Substring(1));
+            }
+            else
+            {
+                _textInputLabel.Text.Text = line;
             }
         }
 
@@ -175,7 +205,8 @@ namespace ClientServices.UserInterface.Components
 
             if (e.Key == KeyboardKeys.Enter)
             {
-                if (TextSubmitted != null) TextSubmitted(this, _textInputLabel.Text.Text);
+                if (TextSubmitted != null) TextSubmitted(this, _currentInputText.ToString());
+                _currentInputText.Clear();
                 _textInputLabel.Text.Text = "";
                 Active = false;
                 return true;
@@ -183,18 +214,16 @@ namespace ClientServices.UserInterface.Components
 
             if (e.Key == KeyboardKeys.Back)
             {
-                if (_textInputLabel.Text.Text.Length > 0)
-                    _textInputLabel.Text.Text = _textInputLabel.Text.Text.Remove(_textInputLabel.Text.Text.Length - 1, 1);
+                if (_currentInputText.Length > 0)
+                    _currentInputText.Remove(_currentInputText.Length - 1, 1);
                 return true;
             }
 
-            if (char.IsLetterOrDigit(e.CharacterMapping.Character) || char.IsPunctuation(e.CharacterMapping.Character) || char.IsWhiteSpace(e.CharacterMapping.Character))
+            if (char.IsLetterOrDigit(e.CharacterMapping.Character) || char.IsPunctuation(e.CharacterMapping.Character) || char.IsWhiteSpace(e.CharacterMapping.Character) || char.IsSymbol(e.CharacterMapping.Character))
             {
-                if (e.Shift)
-                    _textInputLabel.Text.Text += e.CharacterMapping.Shifted;
-                else
-                    _textInputLabel.Text.Text += e.CharacterMapping.Character;
+                _currentInputText.Append(e.Shift ? e.CharacterMapping.Shifted : e.CharacterMapping.Character);
             }
+
             return true;
         }
 
@@ -220,13 +249,10 @@ namespace ClientServices.UserInterface.Components
 
         public override void Render()
         {
-            if (_disposing) return;
+            if (_disposing || !IsVisible()) return;
 
-            if (IsVisible())
-            {
-                _renderImage.Blit(ClientArea.X, ClientArea.Y);
-                DrawLines();
-            }
+            _renderImage.Blit(ClientArea.X, ClientArea.Y);
+            DrawLines();
         }
     }
 }

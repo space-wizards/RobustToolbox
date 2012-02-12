95d8fa148660e95f118a6f58e2bdadcb0c24c548
keelin <keelin@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2012-02-12 15:55:50 +0000

    Options menu stuff. Gorgon doesn't like changes to resolution or full screen while its running, gotta figure out a way to do that properly.

--- ClientServices/State/States/OptionsMenu.cs
+++ ClientServices/State/States/OptionsMenu.cs
@@ -20,7 +20,11 @@ namespace ClientServices.State.States
         private readonly Sprite _ticketbg;
 
         private readonly Label _mainmenubtt;
+        private readonly Label _applybtt;
+
         private readonly Listbox _reslistbox;
+        private readonly Checkbox _chkfullscreen;
+        private readonly Label _lblfullscreen;
 
         private Dictionary<string, VideoMode> vmList = new Dictionary<string, VideoMode>();
 
@@ -34,6 +38,10 @@ namespace ClientServices.State.States
         {
             _background = ResourceManager.GetSprite("mainbg");
             _background.Smoothing = Smoothing.Smooth;
+
+            _chkfullscreen = new Checkbox(ResourceManager);
+            _lblfullscreen = new Label("Fullscreen", "CALIBRI", ResourceManager);
+            _chkfullscreen.ValueChanged += new Checkbox.CheckboxChangedHandler(_chkfullscreen_ValueChanged);
 
             _reslistbox = new Listbox(new Size(150, 250), 150, ResourceManager);
             _reslistbox.ItemSelected += new Listbox.ListboxPressHandler(_reslistbox_ItemSelected);
@@ -60,6 +68,25 @@ namespace ClientServices.State.States
             _mainmenubtt = new Label("Main Menu", "CALIBRI", ResourceManager);
             _mainmenubtt.DrawBorder = true;
             _mainmenubtt.Clicked += new Label.LabelPressHandler(_mainmenubtt_Clicked);
+
+            _applybtt = new Label("Apply", "CALIBRI", ResourceManager);
+            _applybtt.DrawBorder = true;
+            _applybtt.Clicked += new Label.LabelPressHandler(_applybtt_Clicked);
+        }
+
+        void _applybtt_Clicked(Label sender)
+        {
+            ApplyVideoMode();
+        }
+
+        void _chkfullscreen_ValueChanged(bool newValue)
+        {
+            ConfigurationManager.SetFullscreen(newValue);
+        }
+
+        private void ApplyVideoMode()
+        {
+            Gorgon.SetMode(Gorgon.Screen.Owner, (int)ConfigurationManager.GetDisplayWidth(), (int)ConfigurationManager.GetDisplayHeight(), Gorgon.DesktopVideoMode.Format, ConfigurationManager.GetFullscreen(), false, false, Gorgon.DesktopVideoMode.RefreshRate);
         }
 
         void _reslistbox_ItemSelected(Label item)
@@ -100,6 +127,9 @@ namespace ClientServices.State.States
             NetworkManager.Disconnect();
             UserInterfaceManager.AddComponent(_mainmenubtt);
             UserInterfaceManager.AddComponent(_reslistbox);
+            UserInterfaceManager.AddComponent(_chkfullscreen);
+            UserInterfaceManager.AddComponent(_lblfullscreen);
+            UserInterfaceManager.AddComponent(_applybtt);
         }
 
 
@@ -107,13 +137,21 @@ namespace ClientServices.State.States
         {
             UserInterfaceManager.RemoveComponent(_mainmenubtt);
             UserInterfaceManager.RemoveComponent(_reslistbox);
+            UserInterfaceManager.RemoveComponent(_chkfullscreen);
+            UserInterfaceManager.RemoveComponent(_lblfullscreen);
+            UserInterfaceManager.RemoveComponent(_applybtt);
         }
 
         public void Update(FrameEventArgs e)
         {
             //_connectbtt.Position = new Point(_connecttxt.Position.X, _connecttxt.Position.Y + _connecttxt.ClientArea.Height + 2);
             _reslistbox.Position = new Point(45, (int)(Gorgon.Screen.Height / 2.5f));
+            _chkfullscreen.Position = new Point(_reslistbox.Position.X, _reslistbox.Position.Y + _reslistbox.ClientArea.Height + 5);
+            _lblfullscreen.Position = new Point(_chkfullscreen.Position.X + _chkfullscreen.ClientArea.Width + 3, _chkfullscreen.Position.Y + (int)(_chkfullscreen.ClientArea.Height / 2f) - (int)(_lblfullscreen.ClientArea.Height / 2f));
             _mainmenubtt.Position = new Point(_reslistbox.Position.X + 650, _reslistbox.Position.Y);
+            _applybtt.Position = new Point(_mainmenubtt.Position.X, _mainmenubtt.Position.Y + _mainmenubtt.ClientArea.Height + 5);
+
+            _chkfullscreen.Value = ConfigurationManager.GetFullscreen();
 
             UserInterfaceManager.Update();
         }

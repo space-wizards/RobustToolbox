29e3afccb0f665689490a59fa67abf6687871b0e
spoogemonster <spoogemonster@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2012-04-15 18:10:35 +0000

    Added event for detecting map changes. More methods for recalculating lights.

--- ClientInterfaces/Lighting/ILightManager.cs
+++ ClientInterfaces/Lighting/ILightManager.cs
@@ -14,5 +14,9 @@ namespace ClientInterfaces.Lighting
         void RemoveLight(ILight light);
         ILight[] lightsInRadius(Vector2D point, float radius);
         void RecalculateLights();
+        void RecalculateLightsInView(Vector2D point);
+        void RecalculateLightsInView(RectangleF rect);
+        ILight[] LightsIntersectingPoint(Vector2D point);
+        ILight[] LightsIntersectingRect(RectangleF rect);
     }
 }

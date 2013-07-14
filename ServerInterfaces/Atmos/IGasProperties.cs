698e2c2c7fc54699f9231186f9a92108f520a9a9
ostaf <ostaf@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2013-07-14 19:58:04 +0000

    Added gas properties to atmos manager (name, specific heat capacity)

---
+++ ServerInterfaces/Atmos/IGasProperties.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SS13_Shared;
+
+namespace ServerInterfaces.Atmos
+{
+    public interface IGasProperties
+    {
+        string Name { get;}
+        float SpecificHeatCapacity { get;}
+        GasType Type { get; }
+    }
+}

889bbb416f006ec30553657a219afbe0c481080e
keelin <keelin@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2013-07-18 16:29:17 +0000

    Added the rest of the layers to the main menu. If we could split up the cloud layer into several distinct clouds it would look even better.

--- ClientServices/UserInterface/Components/SimpleImage.cs
+++ ClientServices/UserInterface/Components/SimpleImage.cs
@@ -2,6 +2,7 @@
 using System.Drawing;
 using ClientInterfaces;
 using ClientInterfaces.Resource;
+using GorgonLibrary;
 using GorgonLibrary.Graphics;
 using GorgonLibrary.InputDevices;
 
@@ -13,16 +14,19 @@ namespace ClientServices.UserInterface.Components
 
         Sprite drawingSprite;
 
+        public Vector2D size;
+
         public SimpleImage(IResourceManager resourceManager, string spriteName)
         {
             _resourceManager = resourceManager;
             drawingSprite = _resourceManager.GetSprite(spriteName);
+            size = drawingSprite.Size;
             Update(0);
         }
 
         public override void Update(float frameTime)
         {
-            ClientArea = new Rectangle(this.Position, new Size((int)drawingSprite.Width, (int)drawingSprite.Height));
+            ClientArea = new Rectangle(this.Position, new Size((int)size.X, (int)size.Y));
         }
 
         public override void Render()

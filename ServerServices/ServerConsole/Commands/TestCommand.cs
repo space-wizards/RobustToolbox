9b79779a481c253dc7466dacf0e79a87b3470441
spoogemonster <spoogemonster@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2013-07-24 01:02:43 +0000

    Implemeted server-side command console processor

---
+++ ServerServices/ServerConsole/Commands/TestCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServerServices.ServerConsole.Commands
+{
+    public class TestCommand : ConsoleCommand
+    {
+        public override string GetCommand()
+        {
+            return "test";
+        }
+
+        public override void Execute(params object[] args)
+        {
+            Console.WriteLine("Test!");
+        }
+    }
+}

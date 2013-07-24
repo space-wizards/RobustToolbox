29805c579fbec2a4bde187f35264e3828136cf6a
spoogemonster <spoogemonster@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2013-07-24 22:13:44 +0000

    Whoops

---
+++ ServerServices/ServerConsole/Commands/SaveConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SS13.IoC;
+using ServerInterfaces.Configuration;
+
+namespace ServerServices.ServerConsole.Commands
+{
+    public class SaveConfig : ConsoleCommand
+    {
+        public override string Command
+        {
+            get { return "saveconfig"; }
+        }
+
+        public override string Description
+        {
+            get { return "Saves the server configuration to the config file"; }
+        }
+
+        public override string Help
+        {
+            get { return "No arguments required. Saves the server configuration to the config file."; }
+        }
+
+        public override void Execute(params string[] args)
+        {
+            IoCManager.Resolve<IConfigurationManager>().Save();
+        }
+    }
+}

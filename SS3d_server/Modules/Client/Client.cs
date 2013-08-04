4457c5561ff539679db65021a4d464af4e885393
spoogemonster <spoogemonster@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2013-08-04 00:46:11 +0000

    Ran resharper cleanup on Server

--- SS3d_server/Modules/Client/Client.cs
+++ SS3d_server/Modules/Client/Client.cs
@@ -1,24 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.IO;
-using Lidgren.Network;
+﻿using Lidgren.Network;
 using SS13_Shared;
 using ServerInterfaces;
 
 namespace SS13_Server.Modules.Client
 {
-    public class Client: IClient
+    public class Client : IClient
     {
+        public Client(NetConnection connection)
+        {
+            NetConnection = connection;
+        }
+
+        #region IClient Members
+
         public NetConnection NetConnection { get; private set; }
         public string PlayerName { get; set; }
         public ClientStatus Status { get; set; }
         public ushort MobID { get; set; }
 
-        public Client(NetConnection connection)
-        {
-            NetConnection = connection;
-        }
+        #endregion
     }
 }

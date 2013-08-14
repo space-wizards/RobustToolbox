11c27bb784a21f37777bc99d245129ede24c2f11
keelin <keelin@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2013-08-14 15:34:46 +0000

    Fuck gorgon and everything it stands for (it stands for shitty code).

--- ClientServices/UserInterface/LobbyState/JobTab.cs
+++ ClientServices/UserInterface/LobbyState/JobTab.cs
@@ -21,6 +21,9 @@ namespace ClientServices.UserInterface.Components
         private SimpleImage _imgJobDesc;
         public Label _lbljobDesc;
         public Label _lbljobName;
+        private SimpleImage _imgJobFluff;
+        private SimpleImage _imgDepGrad;
+        public SimpleImage _imgJobGrad;
 
         public JobTab(string uniqueName, Size size, IResourceManager resourceManager)
             : base(uniqueName, size, resourceManager)
@@ -55,8 +58,15 @@ namespace ClientServices.UserInterface.Components
                 SelectionBackground = "dep_glow",
                 ItemSpacing = 20
             };
-
             _shwDepa.Update(0);
+
+            _imgDepGrad = new SimpleImage()
+            {
+                Sprite = "lobby_depgrad",
+                Color = Color.FromArgb(180, Color.White)
+            };
+            _imgDepGrad.Update(0);
+            _imgDepGrad.Position = new Point(_shwDepa.ClientArea.X + (int)(_shwDepa.ClientArea.Width / 2f - _imgDepGrad.ClientArea.Width / 2f), _shwDepa.ClientArea.Top);
 
             _lblDep = new Label("DEPARTMENT", "MICROGBE", resourceManager)
             {
@@ -69,15 +79,29 @@ namespace ClientServices.UserInterface.Components
             _lblDep.Position = new Point((int)(size.Width / 2f - _lblDep.ClientArea.Width / 2f), _shwDepa.ClientArea.Bottom + 5);
             _lblDep.Update(0);
 
+            _imgJobFluff = new SimpleImage()
+            {
+                Sprite = "lobby_jobfluff"
+            };
+            _imgJobFluff.Position = new Point(_lblDep.ClientArea.X + (int)(_lblDep.ClientArea.Width / 2f - _imgJobFluff.ClientArea.Width / 2f), _lblDep.ClientArea.Bottom);
+
             _shwJobs = new LobbyShowcase
             {
-                Position = new Point(60, _lblDep.ClientArea.Bottom + 15),
+                Position = new Point(60, _lblDep.ClientArea.Bottom + 25),
                 Size = new Size(675, 80),
                 ButtonLeft = "job_arrowleft",
                 ButtonRight = "job_arrowright",
                 SelectionBackground = "job_glow"
             };
             _shwJobs.Update(0);
+
+            _imgJobGrad = new SimpleImage()
+            {
+                Sprite = "lobby_jobgrad",
+                Color = Color.FromArgb(180, Color.White)
+            };
+            _imgJobGrad.Update(0);
+            _imgJobGrad.Position = new Point(_shwJobs.ClientArea.X + (int)(_shwJobs.ClientArea.Width / 2f - _imgJobGrad.ClientArea.Width / 2f), _shwJobs.ClientArea.Top);
 
             _imgJobDesc.Update(0);
             _imgJobDesc.Position = new Point(0, _shwJobs.ClientArea.Bottom + 5);
@@ -97,12 +121,16 @@ namespace ClientServices.UserInterface.Components
             };
             _lbljobDesc.Position = new Point(3, _lbljobName.ClientArea.Bottom + 5);
 
+
             _bttReady.Position = new Point(ClientArea.Width - _bttReady.ClientArea.Width - 5, _imgJobDesc.ClientArea.Bottom + 10);
 
             components.Add(_lblDep);
             components.Add(_imgWhatDep);
+            components.Add(_imgDepGrad);
             components.Add(_shwDepa);
+            components.Add(_imgJobGrad);
             components.Add(_shwJobs);
+            components.Add(_imgJobFluff);
             components.Add(_imgJobDesc);
             components.Add(_lbljobDesc);
             components.Add(_lbljobName);
@@ -127,8 +155,12 @@ namespace ClientServices.UserInterface.Components
         {
             ClientArea = new Rectangle(Position, Size);
 
-            if(_lblDep != null)
-                _lblDep.Position = new Point((int)(Size.Width / 2f - _lblDep.ClientArea.Width / 2f), _shwDepa.ClientArea.Bottom + 10);
+            if (_lblDep != null)
+            {
+                _lblDep.Position = new Point((int) (Size.Width/2f - _lblDep.ClientArea.Width/2f), _shwDepa.ClientArea.Bottom + 10);
+                if(_imgJobFluff != null)
+                    _imgJobFluff.Position = new Point(_lblDep.ClientArea.X + (int) (_lblDep.ClientArea.Width/2f - _imgJobFluff.ClientArea.Width/2f), _lblDep.ClientArea.Bottom);
+            }
 
             //if(_shwJobs != null)
             //    _shwJobs.Position = new Point(Position.X + (int)(ClientArea.Width / 2f - _shwJobs.ClientArea.Width / 2f), 5);

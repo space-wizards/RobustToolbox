492f40f310805a6397786ff14fd3e71e2bc183be
keelin <keelin@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2013-09-06 18:35:52 +0000

    Ugh no, this is horrible. Gotta think about this a little.

--- CGO/EntitySystems/InventorySystem.cs
+++ CGO/EntitySystems/InventorySystem.cs
@@ -5,13 +5,15 @@ using EntityManager = GameObject.EntityManager;
 
 namespace CGO.EntitySystems
 {
-    public class TestSystem : EntitySystem
+    public class InventorySystem : EntitySystem
     {
-        public TestSystem(EntityManager em)
+        public InventorySystem(EntityManager em)
             : base(em)
         {
             EntityQuery = new EntityQuery();
-            EntityQuery.OneSet.Add(typeof (BasicItemComponent));
+            EntityQuery.OneSet.Add(typeof(InventoryComponent));
+            EntityQuery.OneSet.Add(typeof(EquipmentComponent));
+            EntityQuery.OneSet.Add(typeof(HumanHandsComponent));
         }
 
         public override void Update(float frametime)

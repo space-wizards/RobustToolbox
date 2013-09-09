30cf3cb273981079a2d3214a7b194fdad6d4894d
spoogemonster <spoogemonster@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2013-09-09 16:28:45 +0000

    Fixed more timing issues caused by slow-ass atmos code

--- SS3d_server/Program.cs
+++ SS3d_server/Program.cs
@@ -13,21 +13,28 @@ namespace SS13_Server
 
         private static void Main(string[] args)
         {
-            var main = new EntryPoint();
-            main._server = new SS13Server();
-            LogManager.Log("Server -> Starting");
-
-            if (main._server.Start())
+            try
             {
-                LogManager.Log("Server -> Can not start server", LogLevel.Fatal);
-                //Not like you'd see this, haha. Perhaps later for logging.
-                Environment.Exit(0);
+                var main = new EntryPoint();
+                main._server = new SS13Server();
+                LogManager.Log("Server -> Starting");
+
+                if (main._server.Start())
+                {
+                    LogManager.Log("Server -> Can not start server", LogLevel.Fatal);
+                    //Not like you'd see this, haha. Perhaps later for logging.
+                    Environment.Exit(0);
+                }
+
+                string strVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+                LogManager.Log("Server Version " + strVersion + " -> Ready");
+
+                main._server.MainLoop();
             }
-
-            string strVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
-            LogManager.Log("Server Version " + strVersion + " -> Ready");
-
-            main._server.MainLoop();
+            catch (Exception e)
+            {
+                LogManager.Log(e.ToString(), LogLevel.Error);
+            }
         }
     }
 }

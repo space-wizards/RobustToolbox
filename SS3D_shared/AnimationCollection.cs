a6b2505ce4b49d77ef52fb417e951bb0fbb376c4
spoogemonster <spoogemonster@f2b8f004-7ac4-4a4e-9beb-898cd48add4c>
2013-10-10 03:58:35 +0000

    - added some extra goodness to resource pack builder - added animations folder to textures and atlas builder - added animation metadata loader to resource manager

---
+++ SS3D_shared/AnimationCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SS13_Shared
+{
+    [Serializable]
+    public class AnimationCollection
+    {
+        public string Name { get; set; }
+        public List<AnimationInfo> Animations { get; set; } 
+
+        public AnimationCollection()
+        {
+            Animations = new List<AnimationInfo>();
+        }
+    }
+}
---
+++ SS3D_shared/AnimationInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SS13_Shared
+{
+    [Serializable]
+    public class AnimationInfo
+    {
+        public string Name { get; set; }
+        public string Track { get; set; }
+        public int Frames { get; set; }
+        public int FPS { get; set; }
+    }
+}

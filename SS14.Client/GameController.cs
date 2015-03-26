435f46f98759411e50d32103d8b86335f10548e0
Harik <github@harik.welp.gs>
2015-03-26 03:25:22 -0400

    Moved testing code into it's own class.
    Serves a few purposes, de-clutters GameController.cs, and tests that
    state management is working properly.
    
    Press any key to go from the test screen to the old main-menu.

--- SS14.Client/GameController.cs
+++ SS14.Client/GameController.cs
@@ -70,99 +70,25 @@ namespace SS14.Client
            
 
             //Setup
-            SetupCluwne();
-            SetupInput();
+            SetupCluwne ();
+            SetupInput ();
 
-            #region testing
-            CluwneLib.Clear(Color.Black);
 
-	    // Test a sprite extracted from the spritesheet
-            CluwneSprite _OneTile = _resourceManager.GetSprite("locker_closed");
-            _OneTile.Position = new SFML.System.Vector2f(0, 0);
-            _OneTile.Draw();
+            _stateManager.RequestStateChange<TestState> ();
 
-            TextSprite CluwneEngineText = new TextSprite("TEST", "CluwneEngine", _resourceManager.GetFont("CALIBRI"));
-            CluwneEngineText.Position = new Shared.Maths.Vector2(450,600);
-            CluwneEngineText.Color = Color.DarkRed;
-            CluwneEngineText.Text = " SS14: Running on CluwneEngine";
+            FrameEventArgs _frameEvent = new FrameEventArgs (
+                                      new TimingData (new SFML.System.Clock ()));
 
-            TextSprite VersionText = new TextSprite("TEST", "version", _resourceManager.GetFont("CALIBRI"));
-            VersionText.Position = new Shared.Maths.Vector2(500, 650);
-            VersionText.Color = Color.Gold;
-            VersionText.Text = "( Running SFML v2.0 ) " ;
-
-            TextSprite ProjNotDeadText = new TextSprite("TEST", "ProjNoDed", _resourceManager.GetFont("CALIBRI"));
-            ProjNotDeadText.Position = new Shared.Maths.Vector2(512, 700);
-            ProjNotDeadText.Color = Color.Gold;
-            ProjNotDeadText.Text = "  Project != Dead :)";
-            
-
-            //CluwneLib.drawHollowRectangle(100, 100, 100, 100, .6f, Color.Blue);
-            //CluwneLib.drawPoint(134, 223, Color.Beige);
-            //CluwneLib.drawCircle(121, 142, 20, Color.Crimson);
-
-            Texture Cluwnelogo = new Texture(_resourceManager.GetImage("Textures/CluwneLibLogo.png"));
-
-            CluwneSprite CluwneEngineLogo = new CluwneSprite(Cluwnelogo);
-            CluwneEngineLogo.Position = new SFML.System.Vector2f(150,100);
-
-            Texture _Tiles = new Texture(_resourceManager.GetImage("Textures/0_Tiles.png"));
-
-            CluwneSprite _TilesSprite = new CluwneSprite(_Tiles);
-            _TilesSprite.Position = new SFML.System.Vector2f(50, 50);
-
-            ImageButton _button = new ImageButton {
-                ImageNormal = "connect_norm",
-                ImageHover = "connect_hover",
-            };
-            _button.Update(0);
-            _button.Position = new System.Drawing.Point(0, 0);
-            _button.Render();
-
-            Texture _Items = new Texture(_resourceManager.GetImage("Textures/0_Items.png"));
-
-            CluwneSprite _TilesItems = new CluwneSprite(_Items);
-            _TilesItems.Position = new SFML.System.Vector2f(0, 500);
-
-            Texture _Objects = new Texture(_resourceManager.GetImage("Textures/0_Objects.png"));
-
-            CluwneSprite _TilesObjects = new CluwneSprite(_Objects);
-            _TilesObjects.Position = new SFML.System.Vector2f(970, 400);
-
-            Texture _Decals = new Texture(_resourceManager.GetImage("Textures/0_Decals.png"));
-
-            CluwneSprite _TilesDecals = new CluwneSprite(_Decals);
-            _TilesDecals.Position = new SFML.System.Vector2f(1000, 0);
-
-            _TilesObjects.Draw();
-            _TilesDecals.Draw();
-            _TilesItems.Draw();
-            _TilesSprite.Draw();
-            CluwneEngineText.Draw();
-            VersionText.Draw();
-            CluwneEngineLogo.Draw();
-            ProjNotDeadText.Draw();
-            CluwneLib.Screen.Display();
-
-            //States Testing
-            _stateManager.RequestStateChange<MainScreen>();
-	    System.Threading.Thread.Sleep(1000);
-
-	    FrameEventArgs _frameEvent = new FrameEventArgs(
-			    new TimingData(new SFML.System.Clock()));
-
-             while(CluwneLib.Screen.IsOpen == true)
-                {
-                    CluwneLib.Clear(Color.Black);
-                    CluwneLib.Screen.DispatchEvents();
-                    CluwneLibIdle(this, _frameEvent);
-                    CluwneLib.Screen.Display();
-                }
+            while (CluwneLib.Screen.IsOpen == true) {
+                CluwneLib.Clear (Color.Black);
+                CluwneLib.Screen.DispatchEvents ();
+                CluwneLibIdle (this, _frameEvent);
+                CluwneLib.Screen.Display ();
+            }
 
 
 
         }
-            #endregion
 
         #endregion
 

61cc1f7559113d8cc1f702715a07944b2bcdb3b1
RednoWCirabrab <rednowcirabrab@gmail.com>
2015-03-27 22:49:25 -0400

    Set up basic system of external and internal organs

---
+++ SS14.Server.GameObjects/Component/Damageable/Health/Organs/Internal/HumanInternal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using SS14.Shared;
+
+
+namespace SS14.Server.GameObjects.Organs.Human
+{
+
+    public class Brain : InternalOrgan
+    {
+        public Brain(HumanHealthComponent _owner, ExternalOrgan _organ)
+        {
+            Name = "Brain";
+            Damage = 0;
+            Parent = _organ;
+            
+        }
+    }
+
+
+    public class Heart : InternalOrgan
+    {
+        public Heart(HumanHealthComponent _owner, ExternalOrgan _organ)
+        {
+            Name = "Heart";
+            Damage = 0;
+            Parent = _organ;
+        }
+    }
+
+
+    public class Lungs : InternalOrgan
+    {
+        public Lungs(HumanHealthComponent _owner, ExternalOrgan _organ)
+        {
+            Name = "Lungs";
+            Damage = 0;
+            Parent = _organ;
+        }
+    }
+
+
+
+
+
+
+
+
+}

b36fcd755ea49456f2f7fe348fb2b4768f2c5e98
Silver <silvertorch5@gmail.com>
2015-09-02 23:25:22 -0600

    GLSLShaders, TechniqueList
    Added more SetParameter Overloads.
    Added a dictionary for the Techniquelist

--- SS14.Client.Graphics/Shader/GLSLShader.cs
+++ SS14.Client.Graphics/Shader/GLSLShader.cs
@@ -9,24 +9,34 @@ using SS14.Client.Graphics.Render;
 using SS14.Client.Graphics.Sprite;
 using SS14.Shared.Maths;
 using SFML.System;
+using System.Diagnostics;
+using Vector2 = SS14.Shared.Maths.Vector2;
+using Vector3 = SS14.Shared.Maths.Vector3;
+using Vector4 = SS14.Shared.Maths.Vector4;
+using OpenTK;
+
 
 namespace SS14.Client.Graphics.Shader
 {
+    [DebuggerDisplay("[GLSLShader] ResourceName = {_resourceName} | availible? {isAvalible}")]
     public class GLSLShader : ShaderClass
     {
+
         private string _resourceName;
-    
 
 
-        public GLSLShader(string vertexShaderFilename, string fragmentShaderFilename) : base(vertexShaderFilename, fragmentShaderFilename)
+
+        public GLSLShader(string vertexShaderFilename, string fragmentShaderFilename)
+            : base(vertexShaderFilename, fragmentShaderFilename)
         {
 
         }
 
-        public GLSLShader(Stream vertexShaderStream, Stream fragmentShaderStream) : base(vertexShaderStream, fragmentShaderStream)
+        public GLSLShader(Stream vertexShaderStream, Stream fragmentShaderStream)
+            : base(vertexShaderStream, fragmentShaderStream)
         {
 
-        }         
+        }
 
         public string ResourceName
         {
@@ -34,38 +44,46 @@ namespace SS14.Client.Graphics.Shader
             set { _resourceName = value; }
         }
 
-        public void setDuration(float duration) 
+        public void setAsCurrentShader()
         {
-            
+            CluwneLib.CurrentShader = this;
         }
 
-        public void SetParameter(string Parameter, RenderImage Image) 
+        public void ResetCurrentShader()
         {
-            base.SetParameter(Parameter, Image.Texture);
+            CluwneLib.CurrentShader = null;
+        }
+
+        public void setDuration(float duration)
+        {
+
+        }
+
+        public void SetParameter(string Parameter, RenderImage Image)
+        {
+            base.SetParameter(Parameter, GLSLShader.CurrentTexture);
         }
 
         public void SetParameter(string Parameter, Vector2 vec2)
         {
-           base.SetParameter(Parameter, vec2.X, vec2.Y);
+            base.SetParameter(Parameter, vec2.X, vec2.Y);
+            Debug.Write("Setting Parameter " + Parameter + " with value: " + vec2.X + " " + vec2.Y);
         }
 
         public void SetParameter(string Parameter, Vector3 vec3)
         {
             base.SetParameter(Parameter, vec3.X, vec3.Y, vec3.Z);
+            Debug.Write("Setting Parameter " + Parameter + " with value: " + vec3.X + " " + vec3.Y + " " + vec3.Z);
         }
         public void SetParameter(string Parameter, Vector4 vec4)
         {
             base.SetParameter(Parameter, vec4.X, vec4.Y, vec4.Z, vec4.W);
+            Debug.Write("Setting Parameter " + Parameter + " with value: " + vec4.X + " " + vec4.Y + " " + vec4.Z + " " + vec4.W);
         }
-        
+
         public void SetParameter(string Parameter, CluwneSprite Sprite)
         {
             base.SetParameter(Parameter, Sprite.Texture);
-        }
-
-        public void SetParameter(string Parameter, Transform mat4)
-        {            
-            base.SetParameter(Parameter,mat4);
         }
 
         public void SetParameter(string Parameter, Vector2[] vec2array)
@@ -88,13 +106,21 @@ namespace SS14.Client.Graphics.Shader
         {
             for (int i = 0; i < vec4array.Length; i++)
             {
-               this.SetParameter(Parameter + i, vec4array[i]);
+                this.SetParameter(Parameter + i, vec4array[i]);
             }
+
         }
 
         public void SetParameter(string Parameter, Texture texture)
         {
             base.SetParameter(Parameter, texture);
         }
+
+        public void SetParameter(string Parameter, CurrentTextureType texture)
+        {
+            base.SetParameter(Parameter, texture);
+        }
+
     }
 }
+ 

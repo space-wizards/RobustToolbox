5b1925345a3ba0cd70729860c4dd722814a55198
Silver <silvertorch5@gmail.com>
2015-11-08 01:04:52 -0700

    Added Setting handlers
    For Debugging and for Video settings

---
+++ SS14.Client.Graphics/DebugSettings.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SS14.Client.Graphics
+{
+    public class DebugSettings
+    {
+        public static bool DebugTextboxes { get; private set; }
+        public static bool DebugWalls     { get; private set; }
+        public static bool DebugAtmos     { get; private set; }
+        public static bool DebugEntities  { get; private set; }
+
+        public DebugSettings()
+        {
+            DebugAtmos = false;
+            DebugEntities = false;
+            DebugTextboxes = false;
+            DebugWalls = false;
+        }
+
+        public static void ToggleAtmosDebug()
+        {
+            DebugAtmos = true;
+        }
+
+        public static void ToggleWallDebug()
+        {
+            DebugWalls = true;
+        }
+
+        public static void ToggleTextboxDebug()
+        {
+            DebugTextboxes = true;
+        }
+
+        public static void ToggleEntitiesDebug()
+        {
+            DebugEntities= true;
+        }
+
+        
+
+
+
+    }
+}
---
+++ SS14.Client.Graphics/VideoSettings.cs
@@ -0,0 +1,68 @@
+﻿using SFML.Window;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SS14.Client.Graphics
+{
+    public class VideoSettings
+    {
+        private static Styles WindowStyle;
+        private static ContextSettings OpenGLSettings;
+        private static VideoMode WindowSettings;
+        private static uint RefreshRate;
+
+        public VideoSettings()
+        {
+            WindowStyle    = Styles.Default; // Titlebar + Resize + Close
+            WindowSettings = VideoMode.DesktopMode; 
+
+            OpenGLSettings = new ContextSettings();
+            RefreshRate    = 30;
+          
+        }
+
+
+        internal VideoMode getVideoMode()
+        {
+            if (WindowSettings.IsValid())
+                return WindowSettings;
+            else
+                return new VideoMode(800, 600);
+            //TODO logmanager show that the windowsettings were invalid
+        }
+
+        internal Styles getWindowStyle()
+        {
+            return WindowStyle; 
+        }
+
+        public static void SetWindowSize(uint width, uint height)
+        {
+            WindowSettings.Height = height;
+            WindowSettings.Width  = width;
+        }
+
+        public static void Fullscreen(bool active)
+        {
+            if (WindowSettings.IsValid())
+                WindowStyle = Styles.Fullscreen;
+            else
+                WindowStyle = Styles.Default;
+            // Logmanager Windowsettings are invalid
+        }
+
+        public static void SetRefreshRate(uint rate)
+        {
+            if ((rate >= 30) && (rate <= 144))
+                RefreshRate = rate;
+            else
+                RefreshRate = 30;
+                //Logmanager Rate is either above 144 or below 30
+
+        }
+
+    }
+}

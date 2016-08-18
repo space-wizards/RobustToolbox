2aa504350e5b175ea8b61ee70f4c4802bd170037
Silvertorch5 <silvertorch5@gmail.com>
2016-08-18 00:39:47 -0600

    F6 will now toggle lighting.

--- SS14.Client.Services/State/States/GameScreen.cs
+++ SS14.Client.Services/State/States/GameScreen.cs
@@ -442,7 +442,11 @@ namespace SS14.Client.Services.State.States
 
                 //Debug.DebugRendertarget(_sceneTarget);
 
-                LightScene();
+                if (bFullVision)
+                    _sceneTarget.Blit(0, 0, CluwneLib.Screen.Size.X, CluwneLib.Screen.Size.Y);
+                else
+                    LightScene();
+
 
                 RenderDebug(vp);
                 //Render the placement manager shit

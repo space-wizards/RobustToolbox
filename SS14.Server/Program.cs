1a318f9f23e8867e3d1c5a3c53587afc067268cd
Pieter-Jan Briers <pieterjan.briers@gmail.com>
2017-05-06 21:08:53 +0200

    Fix Windows build. Move signal handling to reflection.

--- SS14.Server/Program.cs
+++ SS14.Server/Program.cs
@@ -31,9 +31,7 @@ namespace SS14.Server
             string strVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
             LogManager.Log("Server Version " + strVersion + " -> Ready");
 
-            #if __MonoCS__
             SignalHander.InstallSignals();
-            #endif
 
             main._server.MainLoop();
         }

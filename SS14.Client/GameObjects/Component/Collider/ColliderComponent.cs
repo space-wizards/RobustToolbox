f3ac564235856351d41000646daaeff91a626d2b
Pieter-Jan Briers <pieterjan.briers@gmail.com>
2017-05-20 18:19:57 +0200

    Project restructure part one: compiles

---
+++ SS14.Client/GameObjects/Component/Collider/ColliderComponent.cs
@@ -0,0 +1,68 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using SS14.Client.Interfaces.Collision;
+using SS14.Client.Interfaces.GOC;
+using SS14.Shared;
+using SS14.Shared.GameObjects;
+using SS14.Shared.GO;
+using SS14.Shared.IoC;
+
+namespace SS14.Client.GameObjects
+{
+    public class ColliderComponent : Component
+    {
+        public SFML.Graphics.Color DebugColor { get; set; }
+
+        private FloatRect AABB
+        {
+            get
+            {
+                if (Owner.HasComponent(ComponentFamily.Hitbox))
+                    return Owner.GetComponent<HitboxComponent>(ComponentFamily.Hitbox).AABB;
+                else if (Owner.HasComponent(ComponentFamily.Renderable))
+                    return Owner.GetComponent<IRenderableComponent>(ComponentFamily.Renderable).AverageAABB;
+                else
+                    return new FloatRect();
+            }
+        }
+
+        public ColliderComponent()
+        {
+            Family = ComponentFamily.Collider;
+            DebugColor = Color.Blue;
+        }
+
+        public FloatRect WorldAABB
+        {
+            get
+            {
+                // Return tweaked AABB
+                var aabb = AABB;
+                var trans = Owner.GetComponent<TransformComponent>(ComponentFamily.Transform);
+                if (trans == null)
+                    return aabb;
+                else if (aabb != null)
+                    return new FloatRect(
+                        aabb.Left + trans.X,
+                        aabb.Top + trans.Y,
+                        aabb.Width,
+                        aabb.Height);
+                else
+                    return new FloatRect();
+            }
+        }
+
+        public bool TryCollision(Vector2f offset, bool bump = false)
+        {
+            return IoCManager.Resolve<ICollisionManager>().TryCollide(Owner, offset, bump);
+        }
+
+        public override void SetParameter(ComponentParameter parameter) {
+            switch (parameter.MemberName) {
+                case "DebugColor":
+                    DebugColor = ColorUtils.FromHex(parameter.GetValue<string>(), Color.Blue);
+                    break;
+            }
+        }
+    }
+}

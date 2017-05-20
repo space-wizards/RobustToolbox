cd032b6d2b9fa803074645655ff50a2e9d4de356
Pieter-Jan Briers <pieterjan.briers@gmail.com>
2017-05-20 22:12:11 +0200

    Rename the executables to SS14.Client.exe and SS14.Server.exe

--- SS14.Server/SS14Server.cs
+++ SS14.Server/SS14Server.cs
@@ -35,6 +35,7 @@ using MainLoopTimer = SS14.Server.Timing.MainLoopTimer;
 
 namespace SS14.Server
 {
+    [IoCTarget]
     public class SS14Server : ISS14Server
     {
         private const int GameCountdown = 15;

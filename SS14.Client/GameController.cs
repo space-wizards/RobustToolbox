e5a23b15295c673e5680b32e7a60ac2cb8ca9faf
Pieter-Jan Briers <pieterjan.briers@gmail.com>
2017-05-21 12:52:13 +0200

    New Splash screen

--- SS14.Client/GameController.cs
+++ SS14.Client/GameController.cs
@@ -1,7 +1,9 @@
 ﻿using SFML.Graphics;
 using SFML.Window;
+using SFML.System;
 using SS14.Client.Graphics;
 using SS14.Client.Graphics.Event;
+using SS14.Client.Graphics.Render;
 using SS14.Client.Interfaces.Configuration;
 using SS14.Client.Interfaces.Input;
 using SS14.Client.Interfaces.Network;
@@ -52,6 +54,7 @@ namespace SS14.Client
             LogManager.Log("Initialising GameController.", LogLevel.Debug);
 
             ShowSplashScreen();
+            System.Threading.Thread.Sleep(20000);
 
             var assemblies = new List<Assembly>();
             assemblies.Add(AppDomain.CurrentDomain.GetAssemblyByName("SS14.Shared"));
@@ -99,8 +102,36 @@ namespace SS14.Client
 
         private void ShowSplashScreen()
         {
-            string splashTexturePath = PathHelpers.ExecutableRelativeFile("logo.png");
-            CluwneLib.ShowSplashScreen(new VideoMode(600, 300), splashTexturePath);
+            const uint SIZE_X = 600;
+            const uint SIZE_Y = 300;
+            // Size of the NT logo in the bottom left.
+            const float NT_SIZE_X = SIZE_X / 10;
+            const float NT_SIZE_Y = SIZE_Y / 10;
+            CluwneWindow window = CluwneLib.ShowSplashScreen(new VideoMode(SIZE_X, SIZE_Y));
+
+            var assembly = Assembly.GetExecutingAssembly();
+
+            var logoTexture = new Texture(assembly.GetManifestResourceStream("SS14.Client._EmbeddedBaseResources.Logo.logo.png"));
+            var logo = new SFML.Graphics.Sprite(logoTexture);
+            var logoSize = logoTexture.Size;
+            logo.Position = new Vector2f(SIZE_X/2 - logoSize.X/2,SIZE_Y/2 - logoSize.Y/2);
+
+            var backgroundTexture = new Texture(assembly.GetManifestResourceStream("SS14.Client._EmbeddedBaseResources.Logo.background.png"));
+            var background = new SFML.Graphics.Sprite(backgroundTexture);
+            var backgroundSize = backgroundTexture.Size;
+            background.Scale = new Vector2f((float)SIZE_X / backgroundSize.X, (float)SIZE_Y / backgroundSize.Y);
+
+            var nanotrasenTexture = new Texture(assembly.GetManifestResourceStream("SS14.Client._EmbeddedBaseResources.Logo.nanotrasen.png"));
+            var nanotrasen = new SFML.Graphics.Sprite(nanotrasenTexture);
+            var nanotrasenSize = nanotrasenTexture.Size;
+            nanotrasen.Scale = new Vector2f(NT_SIZE_X / nanotrasenSize.X, NT_SIZE_Y / nanotrasenSize.Y);
+            nanotrasen.Position = new Vector2f(SIZE_X - NT_SIZE_X - 5, SIZE_Y - NT_SIZE_Y - 5);
+            nanotrasen.Color = new Color(255, 255, 255, 64);
+
+            window.Draw(background);
+            window.Draw(logo);
+            window.Draw(nanotrasen);
+            window.Display();
         }
 
         private void CleanupSplashScreen()

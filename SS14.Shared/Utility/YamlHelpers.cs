db0e0be2bb78c16387fdae1e7e406304e01b67f5
Pieter-Jan Briers <pieterjan.briers@gmail.com>
2017-06-14 17:13:48 +0200

    Fixes #225

--- SS14.Shared/Utility/YamlHelpers.cs
+++ SS14.Shared/Utility/YamlHelpers.cs
@@ -1,8 +1,9 @@
-using SFML.System;
+﻿using SFML.System;
 using SFML.Graphics;
 using SS14.Shared.Maths;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using YamlDotNet.RepresentationModel;
 
@@ -12,7 +13,7 @@ namespace SS14.Shared.Utility
     {
         public static int AsInt(this YamlNode node)
         {
-            return int.Parse(((YamlScalarNode)node).Value);
+            return int.Parse(((YamlScalarNode)node).Value, CultureInfo.InvariantCulture);
         }
 
         public static string AsString(this YamlNode node)
@@ -22,7 +23,7 @@ namespace SS14.Shared.Utility
 
         public static float AsFloat(this YamlNode node)
         {
-            return float.Parse(((YamlScalarNode)node).Value);
+            return float.Parse(((YamlScalarNode)node).Value, CultureInfo.InvariantCulture);
         }
 
         public static bool AsBool(this YamlNode node)
@@ -39,7 +40,8 @@ namespace SS14.Shared.Utility
                 throw new ArgumentException(string.Format("Could not parse {0}: '{1}'", nameof(Vector2f), raw));
             }
 
-            return new Vector2f(float.Parse(args[0]), float.Parse(args[1]));
+            return new Vector2f(float.Parse(args[0], CultureInfo.InvariantCulture),
+                                float.Parse(args[1], CultureInfo.InvariantCulture));
         }
 
         public static Vector2i AsVector2i(this YamlNode node)
@@ -51,7 +53,8 @@ namespace SS14.Shared.Utility
                 throw new ArgumentException(string.Format("Could not parse {0}: '{1}'", nameof(Vector2f), raw));
             }
 
-            return new Vector2i(int.Parse(args[0]), int.Parse(args[1]));
+            return new Vector2i(int.Parse(args[0], CultureInfo.InvariantCulture),
+                                int.Parse(args[1], CultureInfo.InvariantCulture));
         }
 
         public static Vector3f AsVector3f(this YamlNode node)
@@ -63,7 +66,9 @@ namespace SS14.Shared.Utility
                 throw new ArgumentException(string.Format("Could not parse {0}: '{1}'", nameof(Vector3f), raw));
             }
 
-            return new Vector3f(float.Parse(args[0]), float.Parse(args[1]), float.Parse(args[2]));
+            return new Vector3f(float.Parse(args[0], CultureInfo.InvariantCulture),
+                                float.Parse(args[1], CultureInfo.InvariantCulture),
+                                float.Parse(args[2], CultureInfo.InvariantCulture));
         }
 
         public static Vector4f AsVector4f(this YamlNode node)
@@ -75,7 +80,10 @@ namespace SS14.Shared.Utility
                 throw new ArgumentException(string.Format("Could not parse {0}: '{1}'", nameof(Vector4f), raw));
             }
 
-            return new Vector4f(float.Parse(args[0]), float.Parse(args[1]), float.Parse(args[2]), float.Parse(args[3]));
+            return new Vector4f(float.Parse(args[0], CultureInfo.InvariantCulture),
+                                float.Parse(args[1], CultureInfo.InvariantCulture),
+                                float.Parse(args[2], CultureInfo.InvariantCulture),
+                                float.Parse(args[3], CultureInfo.InvariantCulture));
         }
 
         public static T AsEnum<T>(this YamlNode node)
@@ -90,7 +98,7 @@ namespace SS14.Shared.Utility
 
         public static Dictionary<string, YamlNode> YamlMappingToDict(YamlMappingNode mapping)
         {
-            return mapping.ToDictionary<KeyValuePair<YamlNode, YamlNode>, string, YamlNode>(p => p.Key.AsString(), p => p.Value);
+            return mapping.ToDictionary(p => p.Key.AsString(), p => p.Value);
         }
     }
 }
---
+++ SS14.UnitTesting/SS14.Shared/Utility/YamlHelpers_Test.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using SS14.Shared.Utility;
+using System;
+using System.Globalization;
+using YamlDotNet.RepresentationModel;
+
+namespace SS14.UnitTesting.SS14.Shared.Utility
+{
+    [TestFixture]
+    public class YamlHelpers_Test : SS14UnitTest
+    {
+        [Test]
+        [SetCulture("fr-FR")]
+        public void Test_CultureInvariance()
+        {
+            // Make sure that we're on a locale in which the decimals would be messed up.
+            // French is one but I'd rather have false negatives than false positives.
+            Assert.That(CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator, Is.EqualTo(","));
+            Assert.That(float.Parse("10,5"), Is.EqualTo(10.5f));
+            Assert.That(() => float.Parse("10.5"), Throws.InstanceOf<FormatException>());
+
+            Assert.That(new YamlScalarNode("10.5").AsFloat(), Is.EqualTo(10.5f));
+        }
+    }
+}

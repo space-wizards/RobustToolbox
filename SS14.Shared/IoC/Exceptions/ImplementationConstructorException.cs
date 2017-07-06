831d4ec1da8ea4ec6922436c917a60dcf5093730
Pieter-Jan Briers <pieterjan.briers@gmail.com>
2017-07-07 00:42:30 +0200

    Whoops

--- SS14.Shared/IoC/Exceptions/ImplementationConstructorException.cs
+++ SS14.Shared/IoC/Exceptions/ImplementationConstructorException.cs
@@ -36,15 +36,4 @@ namespace SS14.Shared.IoC.Exceptions
             info.AddValue("typeName", typeName);
         }
     }
-
-    [System.Serializable]
-    public class MyException : System.Exception
-    {
-        public MyException() { }
-        public MyException( string message ) : base( message ) { }
-        public MyException( string message, System.Exception inner ) : base( message, inner ) { }
-        protected MyException(
-            System.Runtime.Serialization.SerializationInfo info,
-            System.Runtime.Serialization.StreamingContext context ) : base( info, context ) { }
-    }
 }

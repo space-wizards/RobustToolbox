22fa2c3f4068617b05a79ffdd1d60a6ca1f6c5b6
sood <gbasood@gmail.com>
2017-07-09 16:09:23 -0700

    Updates lidgren, adds ipv6 support (#204)
    * restart
    
    * saving work
    
    * IT LIVES
    
    * overload method instead
    
    * Solution file changes ala Visual Studio should make it build now

--- SS14.Server/Network/SS14NetServer.cs
+++ SS14.Server/Network/SS14NetServer.cs
@@ -3,6 +3,8 @@ using SS14.Server.Interfaces.Network;
 using SS14.Shared.Interfaces.Configuration;
 using SS14.Shared.IoC;
 using System.Collections.Generic;
+using System;
+using System.Threading;
 
 namespace SS14.Server.Network
 {
@@ -27,7 +29,7 @@ namespace SS14.Server.Network
 
         public void SendToMany(NetOutgoingMessage message, List<NetConnection> recipients)
         {
-            SendMessage(message, recipients, NetDeliveryMethod.ReliableOrdered, 0);
+             SendMessage(message, recipients, NetDeliveryMethod.ReliableOrdered, 0);
         }
 
         #endregion ISS13NetServer Members
@@ -43,6 +45,19 @@ namespace SS14.Server.Network
 #endif
 
             return _config;
+        }
+
+        public void SendMessage(NetOutgoingMessage message, List<NetConnection> recipients, NetDeliveryMethod method, int sequenceChannel)
+        {
+            foreach (NetConnection recipient in recipients)
+                SendMessage(message, recipient, NetDeliveryMethod.ReliableOrdered, sequenceChannel);
+        }
+
+        // acc to Lidgren code comment: Call this to register a callback for when a new message arrives
+        // we aren't currently using it, so it's left as this exception
+        public void RegisterReceivedCallback(SendOrPostCallback callback)
+        {
+            throw new NotImplementedException();
         }
     }
 }

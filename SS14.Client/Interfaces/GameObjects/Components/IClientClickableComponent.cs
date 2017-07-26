eca4042a1ea924f4d56fbfc920be2efd52d640cf
Pieter-Jan Briers <pieterjan.briers@gmail.com>
2017-07-26 10:25:27 +0200

    Update doc comments on IClientClickable

--- SS14.Client/Interfaces/GameObjects/Components/IClientClickableComponent.cs
+++ SS14.Client/Interfaces/GameObjects/Components/IClientClickableComponent.cs
@@ -1,4 +1,5 @@
 using SFML.System;
+using SS14.Client.State.States;
 using SS14.Shared.GameObjects;
 using SS14.Shared.Interfaces.GameObjects;
 using SS14.Shared.Interfaces.GameObjects.Components;
@@ -11,7 +12,10 @@ namespace SS14.Client.Interfaces.GameObjects.Components
         /// Used to check whether a click worked.
         /// </summary>
         /// <param name="worldPos">The world position that was clicked.</param>
-        /// <param name="drawdepth">The draw depth for the click. On what depth did the click go through.</param>
+        /// <param name="drawdepth">
+        /// The draw depth for the sprite that captured the click.
+        /// Used by <see cref="GameScreen" /> to sort and pick the highest successful one when multiple overlapping entities passed.
+        /// </param>
         /// <returns>True if the click worked, false otherwise.</returns>
         bool CheckClick(Vector2f worldPos, out int drawdepth);
 

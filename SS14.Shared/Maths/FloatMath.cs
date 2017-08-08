a43da12f9b072aa760cd1c455c5deb578e82532e
clusterfack <clusterfack@users.noreply.github.com>
2017-08-08 04:51:38 -0500

    Fixes a rare out of bounds exception (#313)
    * Fixes a rare crash
    
    Fixes a crash that occurs from the following
    >return AngleDirections[(int)Math.Floor(angle/45f)];
    
    System.IndexOutOfRangeException: 'Index was outside the bounds of the array.'
    
    That occurs most likely due to a float rounding error
    
    * Apparently this works fine

--- SS14.Shared/Maths/FloatMath.cs
+++ SS14.Shared/Maths/FloatMath.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 
 
 namespace SS14.Shared.Maths
@@ -57,10 +57,19 @@ namespace SS14.Shared.Maths
          {
              return radians/Pi*180;
          }
-         public static float ToRadians(float degrees)
-         {
-             return degrees/180*Pi;
-         }
-     }
- }
+        public static float ToRadians(float degrees)
+        {
+            return degrees / 180 * Pi;
+        }
+
+        public static T Clamp<T>(this T val, T min, T max) where T : IComparable<T>
+        {
+            if (val.CompareTo(min) < 0) return min;
+            else if (val.CompareTo(max) > 0) return max;
+            else return val;
+        }
+
+
+    }
+}
 

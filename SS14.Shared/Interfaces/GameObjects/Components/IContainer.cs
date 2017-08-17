5ecd3b072b80e1440aaa44ca7919a9a5fa049619
clusterfack <clusterfack@users.noreply.github.com>
2017-08-17 03:41:49 -0500

    And then readd the files, fuck you git

---
+++ SS14.Shared/Interfaces/GameObjects/Components/IContainer.cs
@@ -0,0 +1,19 @@
+﻿using SS14.Shared.Interfaces.GameObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SS14.Server.Interfaces.GameObjects
+{
+    public interface IContainer
+    {
+        bool CanInsert(IEntity toinsert);
+        bool Insert(IEntity toinsert);
+        bool CanRemove(IEntity toremove);
+        bool Remove(IEntity toremove);
+        bool Contains(IEntity contained);
+        void Shutdown();
+    }
+}
---
+++ SS14.Shared/Interfaces/GameObjects/Components/IContainerManager.cs
@@ -0,0 +1,15 @@
+﻿using SS14.Shared.Interfaces.GameObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SS14.Server.Interfaces.GameObjects
+{
+    public interface IContainerManager : IComponent
+    {
+        bool Remove(IEntity entity);
+        Dictionary<Type, IContainer> EntityContainers { get; }
+    }
+}

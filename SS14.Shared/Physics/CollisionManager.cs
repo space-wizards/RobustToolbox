space-wizards/RobustToolbox

commit 134957aae31a8ccee0863962c147271154581723

Replace SFML math types with OpenTK and custom ones. (#341)
* Attempt to messily convert the codebase to use OpenTK math.

* Vector2f is gone. It compiles!

* Fix unit tests.

* Goodbye FloatRect

* Goodbye IntRect, say hello to Box2i.

SS14.Shared/Physics/CollisionManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using OpenTK;
 using SFML.Graphics;
 using SFML.System;
 using SS14.Shared.Interfaces.GameObjects;
@@ -9,7 +10,7 @@ using SS14.Shared.Interfaces.Physics;
 using SS14.Shared.IoC;
 using SS14.Shared.Maths;
 using SS14.Shared.Utility;
-using Vector2i = SFML.System.Vector2i;
+using Vector2i = SS14.Shared.Maths.Vector2i;
 
 namespace SS14.Shared.Physics
 {
@@ -46,14 +47,14 @@ namespace SS14.Shared.Physics
         /// </summary>
         /// <param name="collider">Rectangle to check for collision</param>
         /// <returns></returns>
-        public bool IsColliding(FloatRect collider)
+        public bool IsColliding(Box2 collider)
         {
-            Vector2f[] points =
+            Vector2[] points =
             {
-                new Vector2f(collider.Left, collider.Top),
-                new Vector2f(collider.Right(), collider.Top),
-                new Vector2f(collider.Right(), collider.Bottom()),
-                new Vector2f(collider.Left, collider.Bottom())
+                new Vector2(collider.Left, collider.Top),
+                new Vector2(collider.Right, collider.Top),
+                new Vector2(collider.Right, collider.Bottom),
+                new Vector2(collider.Left, collider.Bottom)
             };
 
             //Get the buckets that correspond to the collider's points.
@@ -85,7 +86,7 @@ namespace SS14.Shared.Physics
         /// <returns></returns>
         public bool TryCollide(IEntity entity)
         {
-            return TryCollide(entity, new Vector2f());
+            return TryCollide(entity, new Vector2());
         }
 
         /// <summary>
@@ -93,24 +94,24 @@ namespace SS14.Shared.Physics
         /// </summary>
         /// <param name="collider">Rectangle to check for collision</param>
         /// <returns></returns>
-        public bool TryCollide(IEntity entity, Vector2f offset, bool bump = true)
+        public bool TryCollide(IEntity entity, Vector2 offset, bool bump = true)
         {
             var collider = entity.GetComponent<ICollidableComponent>();
             if (collider == null) return false;
 
             var ColliderAABB = collider.WorldAABB;
-            if (offset.LengthSquared() > 0)
+            if (offset.LengthSquared > 0)
             {
                 ColliderAABB.Left += offset.X;
                 ColliderAABB.Top += offset.Y;
             }
 
-            Vector2f[] points =
+            Vector2[] points =
             {
-                new Vector2f(ColliderAABB.Left, ColliderAABB.Top),
-                new Vector2f(SfmlExt.Right((FloatRect) ColliderAABB), ColliderAABB.Top),
-                new Vector2f(SfmlExt.Right((FloatRect) ColliderAABB), SfmlExt.Bottom((FloatRect) ColliderAABB)),
-                new Vector2f(ColliderAABB.Left, SfmlExt.Bottom((FloatRect) ColliderAABB))
+                new Vector2(ColliderAABB.Left, ColliderAABB.Top),
+                new Vector2(ColliderAABB.Right, ColliderAABB.Top),
+                new Vector2(ColliderAABB.Right, ColliderAABB.Bottom),
+                new Vector2(ColliderAABB.Left, ColliderAABB.Bottom)
             };
 
             var aabbs =
@@ -184,7 +185,7 @@ namespace SS14.Shared.Physics
         /// <param name="point"></param>
         private void AddPoint(CollidablePoint point)
         {
-            var b = GetBucket(point.Coordinates.Convert());
+            var b = GetBucket(point.Coordinates);
             b.AddPoint(point);
         }
 
@@ -194,7 +195,7 @@ namespace SS14.Shared.Physics
         /// <param name="point"></param>
         private void RemovePoint(CollidablePoint point)
         {
-            var b = GetBucket(point.Coordinates.Convert());
+            var b = GetBucket(point.Coordinates);
             b.RemovePoint(point);
         }
 
@@ -203,7 +204,7 @@ namespace SS14.Shared.Physics
         /// </summary>
         /// <param name="coordinate"></param>
         /// <returns></returns>
-        private CollidableBucket GetBucket(Vector2f coordinate)
+        private CollidableBucket GetBucket(Vector2 coordinate)
         {
             var key = GetBucketCoordinate(coordinate);
             return _bucketIndex.ContainsKey(key)
@@ -211,7 +212,7 @@ namespace SS14.Shared.Physics
                 : CreateBucket(key);
         }
 
-        private static Vector2i GetBucketCoordinate(Vector2f coordinate)
+        private static Vector2i GetBucketCoordinate(Vector2 coordinate)
         {
             var x = (int) Math.Floor(coordinate.X / BucketSize);
             var y = (int) Math.Floor(coordinate.Y / BucketSize);

commit 3b20d56163ae9711dfc91bfdbb19364599e28b7f

Fix collision when moving to the bottom or right.
The bug was caused by 134957aae31a8ccee0863962c147271154581723. Collision adds to the AABB's Left and Top properties to do offsets, but when moving to OpenTK, this stopped working, as Right and Bottom now had to be offset too. I missed this, and as such, your hitbox is smaller when moving to the bottom or right. This allows you to clip into walls, but when you're in you can't move the other direction, as your hitbox is also *larger* when moving to the top or left.

SS14.Shared/Physics/CollisionManager.cs
@@ -101,7 +101,9 @@ namespace SS14.Shared.Physics
             if (offset.LengthSquared > 0)
             {
                 colliderAABB.Left += offset.X;
+                colliderAABB.Right += offset.X;
                 colliderAABB.Top += offset.Y;
+                colliderAABB.Bottom += offset.Y;
             }
 
             Vector2[] points =
@@ -112,7 +114,7 @@ namespace SS14.Shared.Physics
                 new Vector2(colliderAABB.Left, colliderAABB.Bottom)
             };
 
-            var bounds = 
+            var bounds =
                 points
                     .Select(GetBucket) // Get the buckets that correspond to the collider's points.
                     .Distinct()
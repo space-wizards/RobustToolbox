space-wizards/RobustToolbox

commit 134957aae31a8ccee0863962c147271154581723

Replace SFML math types with OpenTK and custom ones. (#341)
* Attempt to messily convert the codebase to use OpenTK math.

* Vector2f is gone. It compiles!

* Fix unit tests.

* Goodbye FloatRect

* Goodbye IntRect, say hello to Box2i.

SS14.Client.Graphics/CluwneLib.cs
@@ -1,8 +1,8 @@
-﻿using OpenTK.Graphics;
+﻿using OpenTK;
+using OpenTK.Graphics;
 using SFML.Graphics;
 using SFML.System;
 using SFML.Window;
-using SS14.Client.Graphics.Event;
 using SS14.Client.Graphics.Render;
 using SS14.Client.Graphics.Settings;
 using SS14.Client.Graphics.Shader;
@@ -11,8 +11,9 @@ using System;
 using System.Reflection;
 using SS14.Shared.Network;
 using SS14.Shared.Timing;
-using Vector2i = SFML.System.Vector2i;
-
+using SS14.Shared.Maths;
+using Vector2i = SS14.Shared.Maths.Vector2i;
+using Vector2u = SS14.Shared.Maths.Vector2u;
 
 namespace SS14.Client.Graphics
 {
@@ -21,28 +22,28 @@ namespace SS14.Client.Graphics
         private static RenderTarget[] renderTargetArray;
 
         public static GameTiming Time { get; private set; }
-        public static event FrameEventHandler FrameEvent;
+        public static event EventHandler<FrameEventArgs> FrameEvent;
         public static Viewport CurrentClippingViewport;
 
         public delegate void EventHandler();
         public static event EventHandler RefreshVideoSettings;
 
         #region Accessors
-        public static Vector2f WorldCenter { get; set; }
+        public static Vector2 WorldCenter { get; set; }
         public static Vector2u ScreenViewportSize { get; set; }
         public static int TileSize { get; set; }
-        public static FloatRect WorldViewport
+        public static Box2 WorldViewport
         {
             get
             {
                 return ScreenToWorld(ScreenViewport);
             }
         }
-        public static IntRect ScreenViewport
+        public static Box2i ScreenViewport
         {
             get
             {
-                return new IntRect(0, 0, (int)ScreenViewportSize.X, (int)ScreenViewportSize.Y);
+                return Box2i.FromDimensions(0, 0, (int)ScreenViewportSize.X, (int)ScreenViewportSize.Y);
             }
         }
 
@@ -95,7 +96,7 @@ namespace SS14.Client.Graphics
             {
                 Initialize();
             }
-            
+
             if ((Screen != null) && (renderTargetArray == null))
                 throw new InvalidOperationException("Something has gone terribly wrong!");
 
@@ -148,7 +149,7 @@ namespace SS14.Client.Graphics
             Screen = new CluwneWindow(CluwneLib.Video.getVideoMode(), "Developer Station 14", CluwneLib.Video.getWindowStyle());
             Screen.SetVerticalSyncEnabled(true);
             Screen.SetFramerateLimit(300);
-            
+
             renderTargetArray = new RenderTarget[5];
             CurrentClippingViewport = new Viewport(0, 0, Screen.Size.X, Screen.Size.Y);
             IsInitialized = true;
@@ -342,7 +343,7 @@ namespace SS14.Client.Graphics
         /// <param name="radius"> Radius of Cirle </param>
         /// <param name="color"> Fill Color </param>
         /// <param name="vector2"></param>
-        public static void drawCircle(float posX, float posY, int radius, Color color, Vector2f vector2)
+        public static void drawCircle(float posX, float posY, int radius, Color color, Vector2 vector2)
         {
             CircleShape Circle = new CircleShape();
             Circle.Position = new Vector2f(posX, posY);
@@ -439,81 +440,70 @@ namespace SS14.Client.Graphics
         /// <summary>
         /// Transforms a point from the world (tile) space, to screen (pixel) space.
         /// </summary>
-        public static Vector2f WorldToScreen(Vector2f point)
+        public static Vector2 WorldToScreen(Vector2 point)
         {
             var center = WorldCenter;
-            return new Vector2f(
-               (point.X - center.X) * TileSize + ScreenViewportSize.X / 2,
-               (point.Y - center.Y) * TileSize + ScreenViewportSize.Y / 2
-               );
+            return (point - center) * TileSize + ScreenViewportSize / 2;
         }
 
         /// <summary>
         /// Transforms a rectangle from the world (tile) space, to screen (pixel) space.
         /// </summary>
-        public static FloatRect WorldToScreen(FloatRect rect)
+        public static Box2 WorldToScreen(Box2 rect)
         {
             var center = WorldCenter;
-            return new FloatRect(
-                (rect.Left - center.X) * TileSize + ScreenViewportSize.X / 2,
-                (rect.Top - center.Y) * TileSize + ScreenViewportSize.Y / 2,
-                rect.Width * TileSize,
-                rect.Height * TileSize
-                );
+            var topLeft = new Vector2(rect.Left, rect.Top);
+            var bottomRight = new Vector2(rect.Right, rect.Bottom);
+            return new Box2(
+                WorldToScreen(topLeft),
+                WorldToScreen(bottomRight)
+            );
         }
 
-        public static Vector2f WorldToTile(Vector2f point)
+        public static Vector2 WorldToTile(Vector2 point)
         {
-            return new Vector2f(
+            return new Vector2(
                 (float)Math.Floor(point.X),
                 (float)Math.Floor(point.Y)
-                );
+            );
         }
 
-        public static Vector2f TileToWorld(Vector2f point)
+        public static Vector2 TileToWorld(Vector2 point)
         {
-            return new Vector2f(
+            return new Vector2(
                 point.X + 0.5f,
                 point.Y + 0.5f
-                );
+            );
         }
 
         /// <summary>
         /// Transforms a point from the screen (pixel) space, to world (tile) space.
         /// </summary>
-        public static Vector2f ScreenToWorld(Vector2i point)
+        public static Vector2 ScreenToWorld(Vector2i point)
         {
-            return new Vector2f(
-                ((float)point.X - ScreenViewportSize.X / 2 ) / TileSize + WorldCenter.X,
-                ((float)point.Y - ScreenViewportSize.Y / 2 ) / TileSize + WorldCenter.Y
-                );
+            return ((Vector2)point - ScreenViewportSize / 2) / TileSize + WorldCenter;
         }
 
         /// <summary>
         /// Transforms a rectangle from the screen (pixel) space, to world (tile) space.
         /// </summary>
-        public static FloatRect ScreenToWorld(IntRect rect)
-       {
-           var center = WorldCenter;
-           return new FloatRect(
-               ((float)rect.Left - ScreenViewportSize.X / 2) / TileSize + center.X,
-               ((float)rect.Top  - ScreenViewportSize.Y / 2) / TileSize + center.Y,
-               rect.Width / TileSize,
-               rect.Height / TileSize
-               );
-       }
+        public static Box2 ScreenToWorld(Box2i rect)
+        {
+            var center = WorldCenter;
+            return new Box2(
+                ((Vector2)rect.TopLeft - ScreenViewportSize / 2) / TileSize + center,
+                ((Vector2)rect.BottomRight - ScreenViewportSize / 2) / TileSize + center
+            );
+        }
 
         /// <summary>
         /// Scales a vector from pixel coordinates to tile coordinates.
         /// </summary>
         /// <param name="size"></param>
         /// <returns></returns>
-        public static Vector2f PixelToTile(Vector2f vec)
+        public static Vector2 PixelToTile(Vector2 vec)
         {
-            return new Vector2f(
-                vec.X / TileSize,
-                vec.Y / TileSize
-                );
+            return vec / TileSize;
         }
 
         /// <summary>
@@ -521,25 +511,25 @@ namespace SS14.Client.Graphics
         /// </summary>
         /// <param name="size"></param>
         /// <returns></returns>
-        public static FloatRect PixelToTile(FloatRect rect)
-       {
-           return new FloatRect(
-               rect.Left / TileSize,
-               rect.Top / TileSize,
-               rect.Width / TileSize,
-               rect.Height / TileSize
-               );
-       }
+        public static Box2 PixelToTile(Box2 rect)
+        {
+            return new Box2(
+                rect.Left / TileSize,
+                rect.Top / TileSize,
+                rect.Right / TileSize,
+                rect.Bottom / TileSize
+            );
+        }
 
         /// <summary>
         /// Takes a point in world (tile) coordinates, and rounds it to the nearest pixel.
         /// </summary>
-        public static Vector2f GetNearestPixel(Vector2f worldPoint)
+        public static Vector2 GetNearestPixel(Vector2 worldPoint)
         {
-            return new Vector2f(
+            return new Vector2(
                 (float)Math.Round(worldPoint.X * TileSize) / TileSize,
                 (float)Math.Round(worldPoint.Y * TileSize) / TileSize
-                );
+            );
         }
 
         #endregion

commit ab468b288796e0c19589ca720d0089cbcdda9435

Fix F2 on Unix, collision bug and join crash. (#348)
* Make F2 not crash on Unix, fix join crash.

* Fix collision when moving to the bottom or right.

The bug was caused by 134957aae31a8ccee0863962c147271154581723. Collision adds to the AABB's Left and Top properties to do offsets, but when moving to OpenTK, this stopped working, as Right and Bottom now had to be offset too. I missed this, and as such, your hitbox is smaller when moving to the bottom or right. This allows you to clip into walls, but when you're in you can't move the other direction, as your hitbox is also *larger* when moving to the top or left.

SS14.Client.Graphics/CluwneLib.cs
@@ -34,7 +34,7 @@ namespace SS14.Client.Graphics
 
 
         /// <summary>
-        /// Viewport scaling 
+        /// Viewport scaling
         /// </summary>
         public static int TileSize { get; set; } = 32;
 
@@ -429,9 +429,9 @@ namespace SS14.Client.Graphics
         /// <param name="text"> Text to render </param>
         /// <param name="size"> Size of the font </param>
         /// <param name="textColor"> Color of the text </param>
-        public static void drawText(float posX, float posY, string text, uint size, Color textColor)
+        public static void drawText(float posX, float posY, string text, uint size, Color textColor, Font font)
         {
-            Text _text = new Text(text, new Font(@"..\..\Resources\Fonts\bluehigh.ttf"));
+            Text _text = new Text(text, font);
             _text.Position = new SFML.System.Vector2f(posX, posY);
             _text.Color = textColor;
             _text.CharacterSize = size;

SS14.Client/Player/PlayerManager.cs
@@ -60,7 +60,10 @@ namespace SS14.Client.Player
                 ControlledEntity.RemoveComponent<IMoverComponent>();
             }
             ControlledEntity.AddComponent(factory.GetComponent<PlayerInputMoverComponent>());
-            ControlledEntity.AddComponent(factory.GetComponent<CollidableComponent>());
+            if (!ControlledEntity.HasComponent<CollidableComponent>())
+            {
+                ControlledEntity.AddComponent(factory.GetComponent<CollidableComponent>());
+            }
 
             ControlledEntity.GetComponent<ITransformComponent>().OnMove += PlayerEntityMoved;
         }

SS14.Client/State/States/GameScreen.cs
@@ -18,6 +18,7 @@ using SS14.Client.Interfaces.State;
 using SS14.Client.Helpers;
 using SS14.Client.Lighting;
 using SS14.Client.UserInterface.Components;
+using SS14.Client.ResourceManagement;
 using SS14.Shared;
 using SS14.Shared.GameObjects;
 using SS14.Shared.GameStates;
@@ -31,7 +32,6 @@ using SS14.Shared.Serialization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using OpenTK;
 using SS14.Shared.Configuration;
 using SS14.Shared.Interfaces.Configuration;
 using SS14.Client.Map;
@@ -123,7 +123,7 @@ namespace SS14.Client.State.States
         private RenderImage screenShadows;
         private RenderImage shadowBlendIntermediate;
         private RenderImage shadowIntermediate;
-        
+
         private ShadowMapResolver shadowMapResolver;
 
         #endregion Lighting
@@ -355,7 +355,7 @@ namespace SS14.Client.State.States
 
                 //PreOcclusion
                 RenderTiles();
-                
+
                 RenderComponents((float)e.Time, vp);
 
                 RenderOverlay();
@@ -458,20 +458,21 @@ namespace SS14.Client.State.States
                 Vector2 playerWorldOffset = PlayerManager.ControlledEntity.GetComponent<ITransformComponent>().Position;
                 Vector2 playerTile = CluwneLib.WorldToTile(playerWorldOffset);
                 Vector2 playerScreen = CluwneLib.WorldToScreen(playerWorldOffset);
-                CluwneLib.drawText(15, 15, "Postioning Debug", 14, Color.White);
-                CluwneLib.drawText(15, 30, "Character Pos", 14, Color.White);
-                CluwneLib.drawText(15, 45, String.Format("Pixel: {0} / {1}", playerWorldOffset.X, playerWorldOffset.Y), 14, Color.White);
-                CluwneLib.drawText(15, 60, String.Format("World: {0} / {1}", playerTile.X, playerTile.Y), 14, Color.White);
-                CluwneLib.drawText(15, 75, String.Format("Screen: {0} / {1}", playerScreen.X, playerScreen.Y), 14, Color.White);
+                var font = ResourceCache.GetResource<FontResource>(@"Fonts/bluehigh.ttf").Font;
+                CluwneLib.drawText(15, 15, "Postioning Debug", 14, Color.White, font);
+                CluwneLib.drawText(15, 30, "Character Pos", 14, Color.White, font);
+                CluwneLib.drawText(15, 45, String.Format("Pixel: {0} / {1}", playerWorldOffset.X, playerWorldOffset.Y), 14, Color.White, font);
+                CluwneLib.drawText(15, 60, String.Format("World: {0} / {1}", playerTile.X, playerTile.Y), 14, Color.White, font);
+                CluwneLib.drawText(15, 75, String.Format("Screen: {0} / {1}", playerScreen.X, playerScreen.Y), 14, Color.White, font);
 
                 // Mouse position debug
                 Vector2i mouseScreenPos = MousePosScreen; // default to screen space
                 Vector2 mouseWorldOffset = CluwneLib.ScreenToWorld(MousePosScreen);
                 Vector2 mouseTile = CluwneLib.WorldToTile(mouseWorldOffset);
-                CluwneLib.drawText(15, 120, "Mouse Pos", 14, Color.White);
-                CluwneLib.drawText(15, 135, String.Format("Pixel: {0} / {1}", mouseWorldOffset.X, mouseWorldOffset.Y), 14, Color.White);
-                CluwneLib.drawText(15, 150, String.Format("World: {0} / {1}", mouseTile.X, mouseTile.Y), 14, Color.White);
-                CluwneLib.drawText(15, 165, String.Format("Screen: {0} / {1}", mouseScreenPos.X, mouseScreenPos.Y), 14, Color.White);
+                CluwneLib.drawText(15, 120, "Mouse Pos", 14, Color.White, font);
+                CluwneLib.drawText(15, 135, String.Format("Pixel: {0} / {1}", mouseWorldOffset.X, mouseWorldOffset.Y), 14, Color.White, font);
+                CluwneLib.drawText(15, 150, String.Format("World: {0} / {1}", mouseTile.X, mouseTile.Y), 14, Color.White, font);
+                CluwneLib.drawText(15, 165, String.Format("Screen: {0} / {1}", mouseScreenPos.X, mouseScreenPos.Y), 14, Color.White, font);
             }
         }
 
@@ -1190,12 +1191,12 @@ namespace SS14.Client.State.States
                 }
 
                 playerOcclusionTarget.BeginDrawing(); // Set to shadow rendertarget
-                
+
                 area.RenderTarget.BlendSettings.ColorSrcFactor = BlendMode.Factor.One;
                 area.RenderTarget.BlendSettings.ColorDstFactor = BlendMode.Factor.Zero;
 
                 area.RenderTarget.Blit((int)blitPos.X, (int)blitPos.Y, area.RenderTarget.Width, area.RenderTarget.Height, Color.White, BlitterSizeMode.Crop);
-                
+
                 area.RenderTarget.BlendSettings.ColorDstFactor = BlendMode.Factor.SrcAlpha;
                 area.RenderTarget.BlendSettings.ColorDstFactor = BlendMode.Factor.OneMinusSrcAlpha;
 

SS14.Shared/Physics/CollisionManager.cs
@@ -101,7 +101,9 @@ namespace SS14.Shared.Physics
             if (offset.LengthSquared > 0)
             {
                 colliderAABB.Left += offset.X;
+                colliderAABB.Right += offset.X;
                 colliderAABB.Top += offset.Y;
+                colliderAABB.Bottom += offset.Y;
             }
 
             Vector2[] points =
@@ -112,7 +114,7 @@ namespace SS14.Shared.Physics
                 new Vector2(colliderAABB.Left, colliderAABB.Bottom)
             };
 
-            var bounds = 
+            var bounds =
                 points
                     .Select(GetBucket) // Get the buckets that correspond to the collider's points.
                     .Distinct()
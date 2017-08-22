a9db7d4d827a8855aafa4dd7b5008055b2d9b350
Pieter-Jan Briers <pieterjan.briers@gmail.com>
2017-08-22 15:38:29 +0200

    ColorUtils extensions, fixes and tests. (#365)
    * Color4 extensions for byte color parts, fix WithAlpha with byte.
    
    * Unit tests and bug fix for FromHex.

--- SS14.Shared/ColorUtils.cs
+++ SS14.Shared/ColorUtils.cs
@@ -5,6 +5,13 @@ namespace SS14.Shared
 {
     public static class ColorUtils
     {
+        /// <summary>
+        /// Interpolate two colors with a lambda, AKA returning the two colors combined with a ratio of <paramref name="lambda" />.
+        /// </summary>
+        /// <param name="lambda">
+        /// A value ranging from 0-1. The higher the value the more is taken from <paramref name="endPoint1" />,
+        /// with 0.5 being 50% of both colors, 0.25 being 25% of <paramref name="endPoint1" /> and 75% <paramref name="endPoint2" />.
+        /// </param>
         public static Color4 InterpolateBetween(Color4 endPoint1, Color4 endPoint2, double lambda)
         {
             if (lambda < 0 || lambda > 1)
@@ -21,7 +28,7 @@ namespace SS14.Shared
 
         public static Color4 WithAlpha(this Color4 color, byte a)
         {
-            color.A = a;
+            color.A = a/255f;
             return color;
         }
 
@@ -50,7 +57,7 @@ namespace SS14.Shared
                                       Convert.ToByte(hexColor.Substring(5, 2), 16),
                                       255);
                 }
-                else if (hexColor.Length == 4)
+                else if (hexColor.Length == 5)
                 {
                     string r = hexColor[1].ToString();
                     string g = hexColor[2].ToString();
@@ -62,7 +69,7 @@ namespace SS14.Shared
                                       Convert.ToByte(b + b, 16),
                                       Convert.ToByte(a + a, 16));
                 }
-                else if (hexColor.Length == 3)
+                else if (hexColor.Length == 4)
                 {
                     string r = hexColor[1].ToString();
                     string g = hexColor[2].ToString();
@@ -78,7 +85,43 @@ namespace SS14.Shared
             if (fallback.HasValue)
                 return fallback.Value;
             else
-                throw new ArgumentException("Invalid color code.", "hexColor");
+                throw new ArgumentException("Invalid color code.", nameof(hexColor));
+        }
+
+        /// <summary>
+        /// Gets the red part of a color as byte from 0-255.
+        /// </summary>
+        /// <param name="color">The color to get the red part from.</param>
+        public static byte RByte(this Color4 color)
+        {
+            return (byte)(color.R * Byte.MaxValue);
+        }
+
+        /// <summary>
+        /// Gets the green part of a color as byte from 0-255.
+        /// </summary>
+        /// <param name="color">The color to get the green part from.</param>
+        public static byte GByte(this Color4 color)
+        {
+            return (byte)(color.G * Byte.MaxValue);
+        }
+
+        /// <summary>
+        /// Gets the blue part of a color as byte from 0-255.
+        /// </summary>
+        /// <param name="color">The color to get the blue part from.</param>
+        public static byte BByte(this Color4 color)
+        {
+            return (byte)(color.B * Byte.MaxValue);
+        }
+
+        /// <summary>
+        /// Gets the alpha part of a color as byte from 0-255.
+        /// </summary>
+        /// <param name="color">The color to get the alpha part from.</param>
+        public static byte AByte(this Color4 color)
+        {
+            return (byte)(color.A * Byte.MaxValue);
         }
     }
 }

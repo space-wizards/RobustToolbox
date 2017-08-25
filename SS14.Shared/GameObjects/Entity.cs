af3c90e2a8918f1992e7f0a8a2368cbe34324509
clusterfack <clusterfack@users.noreply.github.com>
2017-08-25 14:20:32 -0500

    Removes bad shutdown (#376)
    * Undoes this
    
    * Move it to the entity manager shutdown

--- SS14.Shared/GameObjects/Entity.cs
+++ SS14.Shared/GameObjects/Entity.cs
@@ -393,8 +393,6 @@ namespace SS14.Shared.GameObjects
             _components.Clear();
             _netIDs.Clear();
             _componentReferences.Clear();
-            var componentmanager = IoCManager.Resolve<IComponentManager>();
-            componentmanager.Cull();
         }
 
         public IEnumerable<IComponent> GetComponents()

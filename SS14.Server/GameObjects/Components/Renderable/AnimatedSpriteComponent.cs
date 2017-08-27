d66e09317bf97a2b9f18b84e2594024cfa9afbba
clusterfack <clusterfack@users.noreply.github.com>
2017-08-27 02:09:03 -0500

    Forgot to remove this

--- SS14.Server/GameObjects/Components/Renderable/AnimatedSpriteComponent.cs
+++ SS14.Server/GameObjects/Components/Renderable/AnimatedSpriteComponent.cs
@@ -66,16 +66,6 @@ namespace SS14.Server.GameObjects
                 DrawDepth = node.AsEnum<DrawDepth>();
             }
 
-            if (mapping.TryGetNode("color", out node))
-            {
-                Color = System.Drawing.Color.FromName(node.ToString());
-            }
-
-            if (mapping.TryGetNode("hexcolor", out node))
-            {
-                Color = node.AsHexColor();
-            }
-
             if (mapping.TryGetNode("sprite", out node))
             {
                 SpriteName = node.AsString();

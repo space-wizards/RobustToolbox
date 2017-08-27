90ad2c7b543290213325059f32bf3de195714f50
clusterfack <clusterfack@users.noreply.github.com>
2017-08-27 16:06:45 -0500

    Improve snap placement managers again Fix using prototype.name instead of prototype.ID

--- SS14.Client/Placement/Modes/AlignSnapgridBorder.cs
+++ SS14.Client/Placement/Modes/AlignSnapgridBorder.cs
@@ -30,8 +30,8 @@ namespace SS14.Client.Placement.Modes
             {
                 var position = CluwneLib.ScreenToWorld(new Vector2i(0,0));  //Find world coordinates closest to screen origin
                 var gridstart = CluwneLib.WorldToScreen(new Vector2( //Find snap grid closest to screen origin and convert back to screen coords
-                (float)Math.Round((position.X / (double)snapsize), MidpointRounding.AwayFromZero) * snapsize,
-                (float)Math.Round((position.Y / (double)snapsize), MidpointRounding.AwayFromZero) * snapsize));
+                (float)Math.Round((position.X / snapsize), MidpointRounding.AwayFromZero) * snapsize,
+                (float)Math.Round((position.Y / snapsize), MidpointRounding.AwayFromZero) * snapsize));
                 for (float a = gridstart.X; a < CluwneLib.ScreenViewportSize.X; a += snapsize * 32) //Iterate through screen creating gridlines
                 {
                     CluwneLib.drawLine(a, 0, CluwneLib.ScreenViewportSize.Y, 90, 0.5f, Color4.Blue);
@@ -57,15 +57,15 @@ namespace SS14.Client.Placement.Modes
             snapsize = currentgrid.SnapSize; //Find snap size.
             
             mouselocal = new Vector2( //Round local coordinates onto the snap grid
-                (float)Math.Round((mouselocal.X / (double)snapsize), MidpointRounding.AwayFromZero) * snapsize,
-                (float)Math.Round((mouselocal.Y / (double)snapsize), MidpointRounding.AwayFromZero) * snapsize);
+                (float)Math.Round((mouselocal.X / snapsize), MidpointRounding.AwayFromZero) * snapsize,
+                (float)Math.Round((mouselocal.Y / snapsize), MidpointRounding.AwayFromZero) * snapsize);
             
             //Convert back to original world and screen coordinates after applying offset
             mouseWorld = currentgrid.LocalToWorld(mouselocal) + new Vector2(pManager.CurrentPrototype.PlacementOffset.X, pManager.CurrentPrototype.PlacementOffset.Y);
             mouseScreen = (Vector2i)CluwneLib.WorldToScreen(mouseWorld);
 
             var protomanager = IoCManager.Resolve<IPrototypeManager>();
-            if (!protomanager.CanSpawnAt(pManager.CurrentPrototype.Name, currentgrid, mouselocal))
+            if (!protomanager.CanSpawnAt(pManager.CurrentPrototype.ID, currentgrid, mouselocal))
                 return false;
 
             if (!RangeCheck())

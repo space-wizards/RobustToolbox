24cdc9d540be923d1780ce636c956fda1d06afe5
Pieter-Jan Briers <pieterjan.briers@gmail.com>
2017-11-05 13:28:52 +0100

    Vector2u deserialization test.

---
+++ SS14.UnitTesting/Shared/Maths/Vector2u_Test.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using SS14.Shared.Maths;
+
+namespace SS14.UnitTesting.Shared.Maths
+{
+    [TestFixture]
+    [Parallelizable]
+    [TestOf(typeof(Vector2u))]
+    public class Vector2u_Test
+    {
+        [Test]
+        public void TestJsonDeserialization()
+        {
+            Assert.That(JsonConvert.DeserializeObject<Vector2u>("{\"x\": 10, \"y\": 10}"), Is.EqualTo(new Vector2u(10, 10)));
+        }
+    }
+}

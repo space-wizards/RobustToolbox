5268ed1e48b87f98a064f28317458d20cd2ec82a
PJB3005 <pieterjan.briers@gmail.com>
2018-01-04 09:38:07 +0100

    Oops forgot to stage these.

--- SS14.Client/UserInterface/CustomControls/DebugConsole.cs
+++ SS14.Client/UserInterface/CustomControls/DebugConsole.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using Lidgren.Network;
 using SS14.Client.Interfaces.Console;
 using SS14.Shared;
@@ -13,17 +12,6 @@ using SS14.Shared.Reflection;
 using SS14.Shared.Utility;
 using System;
 using System.Collections.Generic;
-=======
-﻿using System.Collections.Generic;
-using OpenTK.Graphics;
-using SS14.Client.Console;
-using SS14.Client.Graphics.Input;
-using SS14.Client.Interfaces.Console;
-using SS14.Client.UserInterface.Controls;
-using SS14.Shared.Console;
-using SS14.Shared.IoC;
-using Vector2i = SS14.Shared.Maths.Vector2i;
->>>>>>> upstream/master
 
 namespace SS14.Client.UserInterface
 {
@@ -31,7 +19,6 @@ namespace SS14.Client.UserInterface
     [Reflect(false)]
     public class DebugConsole : Control, IDebugConsole
     {
-<<<<<<< HEAD
         private LineEdit CommandBar;
         private RichTextLabel Contents;
 
@@ -59,67 +46,19 @@ namespace SS14.Client.UserInterface
         }
 
         public void Toggle()
-=======
-        private readonly IClientConsole _console;
-        private readonly Textbox _txtInput;
-        private readonly ListPanel _historyList;
-
-        private int _lastY;
-        
-        public DebugConsole(Vector2i size)
-            : base(size)
-        {
-            _console = IoCManager.Resolve<IClientConsole>();
-            _txtInput = new Textbox(size.X)
-            {
-                ClearFocusOnSubmit = false,
-                BackgroundColor = new Color4(64, 64, 64, 100),
-                ForegroundColor = new Color4(255, 250, 240, 255)
-            };
-            _txtInput.OnSubmit += TxtInputOnSubmit;
-
-            _historyList = new ListPanel();
-            Container.AddControl(_historyList);
-
-            BackgroundColor = new Color4(64, 64, 64, 200);
-            DrawBackground = true;
-            DrawBorder = true;
-            
-            _console.AddString += (sender, args) => AddLine(args.Text, args.Channel, args.Color);
-            _console.ClearText += (sender, args) => Clear();
-        }
-
-        public IReadOnlyDictionary<string, IConsoleCommand> Commands => _console.Commands;
-
-        public void AddLine(string text, ChatChannel channel, Color4 color)
->>>>>>> upstream/master
         {
             var focus = CommandBar.HasFocus();
             Visible = !Visible;
             if (Visible)
             {
-<<<<<<< HEAD
                 CommandBar.GrabFocus();
             }
             else if (focus)
-=======
-                Position = new Vector2i(5, _lastY),
-                ForegroundColor = color
-            };
-            
-            _lastY = newLabel.ClientArea.Bottom;
-            _historyList.AddControl(newLabel);
-            _historyList.DoLayout();
-
-
-            if (atBottom)
->>>>>>> upstream/master
             {
                 // We manually need to call this.
                 // See https://github.com/godotengine/godot/pull/15074
                 UserInterfaceManager.FocusExited(CommandBar);
             }
-<<<<<<< HEAD
 
             var netMgr = IoCManager.Resolve<IClientNetManager>();
             if (Visible)
@@ -299,85 +238,6 @@ namespace SS14.Client.UserInterface
         public bool Execute(IDebugConsole console, params string[] args)
         {
             return true;
-=======
-        }
-
-        public void Clear()
-        {
-            _historyList.DisposeAllChildren();
-            _historyList.DoLayout();
-            _lastY = 0;
-            ScrollbarV.Value = 0;
-        }
-
-        public override void DoLayout()
-        {
-            base.DoLayout();
-
-            _txtInput.LocalPosition = Position + new Vector2i(ClientArea.Left, ClientArea.Bottom);
-            _txtInput.DoLayout();
-        }
-        
-        public override void Update(float frameTime)
-        {
-            base.Update(frameTime);
-            _txtInput.Update(frameTime);
-        }
-
-        public override void Draw()
-        {
-            base.Draw();
-            _txtInput.Draw();
-        }
-
-        public override void Dispose()
-        {
-            _txtInput.Dispose();
-            _console.Dispose();
-            base.Dispose();
-        }
-
-        public override bool MouseDown(MouseButtonEventArgs e)
-        {
-            if (!base.MouseDown(e))
-                if (_txtInput.MouseDown(e))
-                    return true;
-            return false;
-        }
-
-        public override bool MouseUp(MouseButtonEventArgs e)
-        {
-            if (!base.MouseUp(e))
-                return _txtInput.MouseUp(e);
-            return false;
-        }
-
-        public override void MouseMove(MouseMoveEventArgs e)
-        {
-            base.MouseMove(e);
-            _txtInput.MouseMove(e);
-        }
-
-        public override bool KeyDown(KeyEventArgs e)
-        {
-            if (!base.KeyDown(e))
-                return _txtInput.KeyDown(e);
-            return false;
-        }
-
-        public override bool TextEntered(TextEventArgs e)
-        {
-            if (!base.TextEntered(e))
-                return _txtInput.TextEntered(e);
-            return false;
-        }
-
-        private void TxtInputOnSubmit(Textbox sender, string text)
-        {
-            // debugConsole input is not prefixed with slash
-            if(!string.IsNullOrWhiteSpace(text))
-                _console.ProcessCommand(text);
->>>>>>> upstream/master
         }
     }
 }

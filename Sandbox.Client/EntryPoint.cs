d3f4830e7e0db58ce46dcb27520ea9c5841c0d84
Acruid <shatter66@gmail.com>
2018-01-03 17:19:20 -0800

    Adds skeleton Sandbox Content projects to the solution.

---
+++ Sandbox.Client/EntryPoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SS14.Shared.ContentPack;
+
+namespace Sandbox.Client
+{
+    public class EntryPoint : GameClient
+    {
+        public override void Init()
+        {
+            base.Init();
+        }
+    }
+}
---
+++ Sandbox.Server/EntryPoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SS14.Shared.ContentPack;
+
+namespace Sandbox.Server
+{
+    public class EntryPoint : GameServer
+    {
+        public override void Init()
+        {
+            base.Init();
+        }
+    }
+}

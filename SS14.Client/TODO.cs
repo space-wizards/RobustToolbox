2bcaf8699649cc10b62c678acd7ee9721497f7af
Pieter-Jan Briers <pieterjan.briers@gmail.com>
2018-02-04 14:41:36 +0100

    Update TODO.

--- SS14.Client/TODO.cs
+++ SS14.Client/TODO.cs
@@ -5,8 +5,7 @@
  * Fix that HORRIBLE hack inside PathHelpers.ExecutableRelativeFile()
  * Resource Cache doesn't use VFS, so no ZIP support.
     Godot has a VFS internally (See FileAccess and PackedData), but it's not exposed to user code afaict.
- * Export of client AND automatic fetching of SS14.Shared.Bsdiff for development.
- *  Really just needs a Windows build of the bsdiffwrap.dll for the second point.
+ * Export of client.
  * Spawn Entities and Spawn Tiles.
  * Tilemaps don't seem to be clearing correctly on disconnect.
  */

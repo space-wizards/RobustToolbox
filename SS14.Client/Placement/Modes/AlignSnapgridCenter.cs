4ae308bd845655d855941e3e993df07de0381c8b
PJB3005 <pieterjan.briers@gmail.com>
2018-02-12 22:26:17 +0100

    Fix Center Grid Rendering.

--- SS14.Client/Placement/Modes/AlignSnapgridCenter.cs
+++ SS14.Client/Placement/Modes/AlignSnapgridCenter.cs
@@ -3,6 +3,7 @@ using SS14.Client.Graphics;
 using SS14.Client.Utility;
 using SS14.Shared.Map;
 using SS14.Shared.Maths;
+using SS14.Client.Graphics.ClientEye;
 
 namespace SS14.Client.Placement.Modes
 {
@@ -19,21 +20,22 @@ namespace SS14.Client.Placement.Modes
         {
             if (onGrid)
             {
+                const int ppm = EyeManager.PIXELSPERMETER;
                 var viewportSize = pManager.sceneTree.SceneTree.Root.Size.Convert();
                 var position = pManager.eyeManager.ScreenToWorld(Vector2.Zero);
                 var gridstart = pManager.eyeManager.WorldToScreen(new Vector2( //Find snap grid closest to screen origin and convert back to screen coords
-                    (float)(Math.Round(position.X/snapSize - 0.5f, MidpointRounding.AwayFromZero)+0.5f) * snapSize,
-                    (float)(Math.Round(position.Y/snapSize - 0.5f, MidpointRounding.AwayFromZero)+0.5f) * snapSize));
+                    (float)(Math.Round(position.X / snapSize - 0.5f, MidpointRounding.AwayFromZero) + 0.5f) * snapSize,
+                    (float)(Math.Round(position.Y / snapSize - 0.5f, MidpointRounding.AwayFromZero) + 0.5f) * snapSize));
                 for (var a = gridstart.X; a < viewportSize.X; a += snapSize * 32) //Iterate through screen creating gridlines
                 {
-                    var from = ScreenToWorld(new Vector2(a, 0)).Convert();
-                    var to = ScreenToWorld(new Vector2(a, viewportSize.Y)).Convert();
+                    var from = ScreenToWorld(new Vector2(a, 0)).Convert() * ppm;
+                    var to = ScreenToWorld(new Vector2(a, viewportSize.Y)).Convert() * ppm;
                     pManager.drawNode.DrawLine(from, to, new Godot.Color(0, 0, 1), 0.5f);
                 }
                 for (var a = gridstart.Y; a < viewportSize.Y; a += snapSize * 32)
                 {
-                    var from = ScreenToWorld(new Vector2(0, a)).Convert();
-                    var to = ScreenToWorld(new Vector2(viewportSize.X, a)).Convert();
+                    var from = ScreenToWorld(new Vector2(0, a)).Convert() * ppm;
+                    var to = ScreenToWorld(new Vector2(viewportSize.X, a)).Convert() * ppm;
                     pManager.drawNode.DrawLine(from, to, new Godot.Color(0, 0, 1), 0.5f);
                 }
             }
@@ -57,8 +59,8 @@ namespace SS14.Client.Placement.Modes
             onGrid = true;
 
             var mouseLocal = new Vector2( //Round local coordinates onto the snap grid
-                (float)(Math.Round((MouseCoords.Position.X/(double)snapSize - 0.5f), MidpointRounding.AwayFromZero)+0.5) * snapSize,
-                (float)(Math.Round((MouseCoords.Position.Y/(double)snapSize - 0.5f), MidpointRounding.AwayFromZero)+0.5) * snapSize);
+                (float)(Math.Round((MouseCoords.Position.X / (double)snapSize - 0.5f), MidpointRounding.AwayFromZero) + 0.5) * snapSize,
+                (float)(Math.Round((MouseCoords.Position.Y / (double)snapSize - 0.5f), MidpointRounding.AwayFromZero) + 0.5) * snapSize);
 
             //Adjust mouseCoords to new calculated position
             MouseCoords = new LocalCoordinates(mouseLocal + new Vector2(pManager.CurrentPrototype.PlacementOffset.X, pManager.CurrentPrototype.PlacementOffset.Y), MouseCoords.Grid);

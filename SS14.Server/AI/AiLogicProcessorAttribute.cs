b96dce07399de9265b36049c175261f62cd11dc0
Acruid <shatter66@gmail.com>
2018-03-09 10:56:36 -0800

    Moved AimShootLifeProcessor.cs to content.

--- SS14.Server/AI/AiLogicProcessorAttribute.cs
+++ SS14.Server/AI/AiLogicProcessorAttribute.cs
@@ -1,16 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SS14.Server.AI
 {
+    /// <summary>
+    ///     This attribute is used to mark a class as a LogicProcessor for the AI system.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
-    class AiLogicProcessorAttribute : Attribute
+    public class AiLogicProcessorAttribute : Attribute
     {
+        /// <summary>
+        ///     Name of this LogicProcessor in serialized files.
+        /// </summary>
         public string SerializeName { get; }
 
+        /// <summary>
+        ///     Creates an instance of this Attribute.
+        /// </summary>
+        /// <param name="serializeName">Name of this LogicProcessor in serialized files.</param>
         public AiLogicProcessorAttribute(string serializeName)
         {
             SerializeName = serializeName;

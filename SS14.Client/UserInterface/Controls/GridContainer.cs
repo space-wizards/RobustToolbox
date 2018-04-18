db21b9343b62e497a13a8964f18f47f36af91cc1
clusterfack <afleck1@alumni.nd.edu>
2018-04-18 08:03:24 -0500

    Gridcontainer Wrapper

---
+++ SS14.Client/UserInterface/Controls/GridContainer.cs
@@ -0,0 +1,23 @@
+﻿
+namespace SS14.Client.UserInterface.Controls
+{
+    public class GridContainer : Control
+    {
+        public GridContainer() : base() { }
+        public GridContainer(string name) : base(name) { }
+        public GridContainer(Godot.GridContainer sceneControl) : base(sceneControl) { }
+
+        new private Godot.GridContainer SceneControl;
+
+        protected override Godot.Control SpawnSceneControl()
+        {
+            return new Godot.GridContainer();
+        }
+
+        public int Columns
+        {
+            get => SceneControl.GetColumns();
+            set => SceneControl.SetColumns(value);
+        }
+    }
+}

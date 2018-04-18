320aa3f9c0aa3edb5fc83942cd0908fe16e4ebb5
clusterfack <clusterfack@users.noreply.github.com>
2018-04-18 14:44:30 -0500

    Gridcontainer Wrapper (#564)
    * Gridcontainer Wrapper
    
    * This is probably important too

---
+++ SS14.Client/UserInterface/Controls/GridContainer.cs
@@ -0,0 +1,29 @@
+﻿
+namespace SS14.Client.UserInterface.Controls
+{
+    public class GridContainer : Control
+    {
+        public GridContainer() : base() { }
+        public GridContainer(string name) : base(name) { }
+        public GridContainer(Godot.GridContainer sceneControl) : base(sceneControl) { }
+
+        new private Godot.GridContainer SceneControl;
+
+        protected override Godot.Control SpawnSceneControl()
+        {
+            return new Godot.GridContainer();
+        }
+
+        protected override void SetSceneControl(Godot.Control control)
+        {
+            base.SetSceneControl(control);
+            SceneControl = (Godot.GridContainer)control;
+        }
+
+        public int Columns
+        {
+            get => SceneControl.GetColumns();
+            set => SceneControl.SetColumns(value);
+        }
+    }
+}

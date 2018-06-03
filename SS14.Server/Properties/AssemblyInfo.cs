7c47d444e2526732c361985ede188a2880a6335f
Pieter-Jan Briers <pieterjan.briers@gmail.com>
2018-06-03 14:18:15 +0200

    Fix unit tests.

--- SS14.Server/Properties/AssemblyInfo.cs
+++ SS14.Server/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -37,3 +38,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("0.1.*")]
 [assembly: AssemblyFileVersion("0.1")]
+
+[assembly: InternalsVisibleTo("SS14.UnitTesting")]

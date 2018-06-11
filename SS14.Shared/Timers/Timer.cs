305f64e41579d6275a782793aedb78aa504604ea
Pieter-Jan Briers <pieterjan.briers@gmail.com>
2018-06-11 20:19:19 +0200

    Async Delay and Spawn helpers for Timer.

--- SS14.Shared/Timers/Timer.cs
+++ SS14.Shared/Timers/Timer.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Threading.Tasks;
+using SS14.Shared.Interfaces.Timers;
+using SS14.Shared.IoC;
+using SS14.Shared.Log;
 
 namespace SS14.Shared.Timers
 {
@@ -7,35 +11,33 @@ namespace SS14.Shared.Timers
         /// <summary>
         /// Counts the time (in milliseconds) before firing again.
         /// </summary>
-        private int _timeCounter { get; set; }
+        private int _timeCounter;
 
         /// <summary>
         /// Time (in milliseconds) between firings.
         /// </summary>
-        public int Time { get; set; }
+        public int Time { get; }
 
         /// <summary>
         /// Whether or not this timer should repeat.
         /// </summary>
-        public bool IsRepeating { get; set; }
+        public bool IsRepeating { get; }
 
         /// <summary>
         /// Whether or not this timer is still running.
         /// </summary>
-        public bool IsActive { get; set; }
+        public bool IsActive { get; private set; } = true;
 
         /// <summary>
         /// Called when the timer is fired.
         /// </summary>
-        public Action OnFired { get; set; }
+        public Action OnFired { get; }
 
-        public Timer(int time, bool isRepeating, Action onFired)
+        public Timer(int milliseconds, bool isRepeating, Action onFired)
         {
-            Time = time;
+            _timeCounter = Time = milliseconds;
             IsRepeating = isRepeating;
-            _timeCounter = Time;
             OnFired = onFired;
-            IsActive = true;
         }
 
         public void Update(float frameTime)
@@ -46,7 +48,14 @@ namespace SS14.Shared.Timers
 
                 if (_timeCounter <= 0)
                 {
-                    OnFired();
+                    try
+                    {
+                        OnFired();
+                    }
+                    catch (Exception e)
+                    {
+                        Logger.ErrorS("timer", "Caught exception in timer callback: {0}", e);
+                    }
 
                     if (IsRepeating)
                     {
@@ -58,6 +67,53 @@ namespace SS14.Shared.Timers
                     }
                 }
             }
+        }
+
+        /// <summary>
+        ///     Creates a task that will complete after a given delay.
+        ///     The task is resumed on the main game logic thread.
+        /// </summary>
+        /// <param name="milliseconds">The length of time, in milliseconds, to delay for.</param>
+        /// <returns>The task that can be awaited.</returns>
+        public static Task Delay(int milliseconds)
+        {
+            var tcs = new TaskCompletionSource<object>();
+            Spawn(milliseconds, () => tcs.SetResult(null));
+            return tcs.Task;
+        }
+
+        /// <summary>
+        ///     Creates a task that will complete after a given delay.
+        ///     The task is resumed on the main game logic thread.
+        /// </summary>
+        /// <param name="duration">The length of time to delay for.</param>
+        /// <returns>The task that can be awaited.</returns>
+        public static Task Delay(TimeSpan duration)
+        {
+            return Delay(duration.Milliseconds);
+        }
+
+        /// <summary>
+        ///     Schedule an action to be fired after a certain delay.
+        ///     The action will be resumed on the main game logic thread.
+        /// </summary>
+        /// <param name="milliseconds">The length of time, in milliseconds, to wait before firing the action.</param>
+        /// <param name="onFired">The action to fire.</param>
+        public static void Spawn(int milliseconds, Action onFired)
+        {
+            var timer = new Timer(milliseconds, false, onFired);
+            IoCManager.Resolve<ITimerManager>().AddTimer(timer);
+        }
+
+        /// <summary>
+        ///     Schedule an action to be fired after a certain delay.
+        ///     The action will be resumed on the main game logic thread.
+        /// </summary>
+        /// <param name="duration">The length of time, to wait before firing the action.</param>
+        /// <param name="onFired">The action to fire.</param>
+        public static void Spawn(TimeSpan duration, Action onFired)
+        {
+            Spawn(duration.Milliseconds, onFired);
         }
     }
 }

4fc8e930f51c1d5d1a61b7af918bcd1ecd643ce9
Acruid <shatter66@gmail.com>
2018-08-17 16:04:37 -0700

    Moves PlayerInputMover logic into an EntitySystem.

--- SS14.Server/GameObjects/Components/Mover/PlayerInputMoverComponent.cs
+++ SS14.Server/GameObjects/Components/Mover/PlayerInputMoverComponent.cs
@@ -1,13 +1,8 @@
 ﻿using SS14.Server.Interfaces.GameObjects;
-using SS14.Server.Interfaces.Player;
-using SS14.Shared.Enums;
 using SS14.Shared.GameObjects;
-using SS14.Shared.Input;
-using SS14.Shared.Interfaces.GameObjects;
-using SS14.Shared.Interfaces.GameObjects.Components;
-using SS14.Shared.Interfaces.Network;
 using SS14.Shared.Log;
 using SS14.Shared.Maths;
+using SS14.Shared.Serialization;
 
 namespace SS14.Server.GameObjects
 {
@@ -16,23 +11,10 @@ namespace SS14.Server.GameObjects
     /// </summary>
     public class PlayerInputMoverComponent : Component, IMoverComponent
     {
-        private const float BaseMoveSpeed = 4.0f;
-        private const float FastMoveSpeed = 10.0f;
-
-        private Direction LastDir = Direction.South;
-        private Vector2 _moveDir;
         private bool _movingUp;
         private bool _movingDown;
         private bool _movingLeft;
         private bool _movingRight;
-        private bool _run;
-
-        private InputCmdHandler _moveUpCmdHandler;
-        private InputCmdHandler _moveDownCmdHandler;
-        private InputCmdHandler _moveLeftCmdHandler;
-        private InputCmdHandler _moveRightCmdHandler;
-        private InputCmdHandler _runCmdHandler;
-
 
         /// <inheritdoc />
         public override string Name => "PlayerInputMover";
@@ -43,13 +25,29 @@ namespace SS14.Server.GameObjects
         /// <inheritdoc />
         public override bool NetworkSynchronizeExistence => false;
 
+        /// <summary>
+        ///     Movement speed (m/s) that the entity walks.
+        /// </summary>
+        public float WalkMoveSpeed { get; set; } = 4.0f;
+
+        /// <summary>
+        ///     Movement speed (m/s) that the entity sprints.
+        /// </summary>
+        public float SprintMoveSpeed { get; set; } = 10.0f;
+
+        /// <summary>
+        ///     Is the entity Sprinting (running)?
+        /// </summary>
+        public bool Sprinting { get; set; }
+
+        /// <summary>
+        ///     Calculated linear velocity direction of the entity.
+        /// </summary>
+        public Vector2 VelocityDir { get; private set; }
+
         /// <inheritdoc />
         public override void OnAdd()
         {
-            // This component requires that the entity has a KeyBindingInputComponent.
-            if (!Owner.HasComponent<IActorComponent>())
-                Logger.Error($"[ECS] {Owner.Prototype.Name} - {nameof(PlayerInputMoverComponent)} requires {nameof(IActorComponent)}. ");
-
             // This component requires that the entity has a PhysicsComponent.
             if (!Owner.HasComponent<PhysicsComponent>())
                 Logger.Error($"[ECS] {Owner.Prototype.Name} - {nameof(PlayerInputMoverComponent)} requires {nameof(PhysicsComponent)}. ");
@@ -57,52 +55,43 @@ namespace SS14.Server.GameObjects
             base.OnAdd();
         }
 
-        public override void HandleMessage(ComponentMessage message, INetChannel netChannel = null, IComponent component = null)
+        /// <inheritdoc />
+        public override void ExposeData(ObjectSerializer serializer)
         {
-            base.HandleMessage(message, netChannel, component);
+            base.ExposeData(serializer);
 
-            IPlayerInput input;
-            switch (message)
-            {
-                case PlayerAttachedMsg msg:
-                    InitInputCommands();
-                    input = msg.NewPlayer.Input;
-                    input.SetCommand(EngineKeyFunctions.MoveUp, _moveUpCmdHandler);
-                    input.SetCommand(EngineKeyFunctions.MoveLeft, _moveLeftCmdHandler);
-                    input.SetCommand(EngineKeyFunctions.MoveRight, _moveRightCmdHandler);
-                    input.SetCommand(EngineKeyFunctions.MoveDown, _moveDownCmdHandler);
-                    input.SetCommand(EngineKeyFunctions.Run, _runCmdHandler);
-                    break;
+            serializer.DataReadWriteFunction("wspd", 4.0f, value => WalkMoveSpeed = value, () => WalkMoveSpeed);
+            serializer.DataReadWriteFunction("rspd", 10.0f, value => SprintMoveSpeed = value, () => SprintMoveSpeed);
 
-                case PlayerDetachedMsg msg:
-                    input = msg.OldPlayer.Input;
-                    input.SetCommand(EngineKeyFunctions.MoveUp, null);
-                    input.SetCommand(EngineKeyFunctions.MoveLeft, null);
-                    input.SetCommand(EngineKeyFunctions.MoveRight, null);
-                    input.SetCommand(EngineKeyFunctions.MoveDown, null);
-                    input.SetCommand(EngineKeyFunctions.Run, null);
-                    break;
-            }
+            // The velocity and moving directions is usually set from player or AI input,
+            // so we don't want to save/load these derived fields.
         }
 
-        public void OnUpdate()
+        /// <summary>
+        ///     Toggles one of the four cardinal directions. Each of the four directions are
+        ///     composed into a single direction vector, <see cref="VelocityDir"/>. Enabling
+        ///     opposite directions will cancel each other out, resulting in no direction.
+        /// </summary>
+        /// <param name="direction">Direction to toggle.</param>
+        /// <param name="enabled">If the direction is active.</param>
+        public void SetVelocityDirection(Direction direction, bool enabled)
         {
-            var physics = Owner.GetComponent<PhysicsComponent>();
-
-            if (_moveDir.LengthSquared < 0.001)
+            switch (direction)
             {
-                if (physics.LinearVelocity != Vector2.Zero)
-                {
-                    physics.LinearVelocity = Vector2.Zero;
-                }
-                return;
+                case Direction.East:
+                    _movingRight = enabled;
+                    break;
+                case Direction.North:
+                    _movingUp = enabled;
+                    break;
+                case Direction.West:
+                    _movingLeft = enabled;
+                    break;
+                case Direction.South:
+                    _movingDown = enabled;
+                    break;
             }
-            physics.LinearVelocity = _moveDir * (_run ? FastMoveSpeed : BaseMoveSpeed);
-            Owner.Transform.LocalRotation = _moveDir.GetDir().ToAngle();
-        }
 
-        private void HandleKeyChange()
-        {
             // key directions are in screen coordinates
             // _moveDir is in world coordinates
             // if the camera is moved, this needs to be changed
@@ -115,40 +104,11 @@ namespace SS14.Server.GameObjects
             y += _movingDown ? 1 : 0;
             y -= _movingUp ? 1 : 0;
 
-            _moveDir = new Vector2(x, y);
+            VelocityDir = new Vector2(x, y);
 
             // can't normalize zero length vector
-            if (_moveDir.LengthSquared > 1.0e-6)
-                _moveDir = _moveDir.Normalized;
-
-            if (_moveDir.LengthSquared > 0.001)
-            {
-                LastDir = _moveDir.GetDir();
-            }
-        }
-
-        private void InitInputCommands()
-        {
-            if (_moveUpCmdHandler != null)
-            {
-                return;
-            }
-
-            _moveUpCmdHandler = InputCmdHandler.FromDelegate(
-                session => { _movingUp = true; HandleKeyChange(); },
-                session => { _movingUp = false; HandleKeyChange(); });
-            _moveLeftCmdHandler = InputCmdHandler.FromDelegate(
-                session => { _movingLeft = true; HandleKeyChange(); },
-                session => { _movingLeft = false; HandleKeyChange(); });
-            _moveRightCmdHandler = InputCmdHandler.FromDelegate(
-                session => { _movingRight = true; HandleKeyChange(); },
-                session => { _movingRight = false; HandleKeyChange(); });
-            _moveDownCmdHandler = InputCmdHandler.FromDelegate(
-                session => { _movingDown = true; HandleKeyChange(); },
-                session => { _movingDown = false; HandleKeyChange(); });
-            _runCmdHandler = InputCmdHandler.FromDelegate(
-                session => _run = true,
-                session => _run = false);
+            if (VelocityDir.LengthSquared > 1.0e-6)
+                VelocityDir = VelocityDir.Normalized;
         }
     }
 }
--- SS14.Server/GameObjects/EntitySystems/MoverSystem.cs
+++ SS14.Server/GameObjects/EntitySystems/MoverSystem.cs
@@ -1,23 +1,117 @@
-﻿using SS14.Shared.GameObjects;
+﻿using SS14.Server.Interfaces.Player;
+using SS14.Shared.GameObjects;
 using SS14.Shared.GameObjects.Systems;
 using SS14.Shared.Input;
+using SS14.Shared.Interfaces.GameObjects.Components;
+using SS14.Shared.Maths;
+using SS14.Shared.Players;
 
 namespace SS14.Server.GameObjects.EntitySystems
 {
     class MoverSystem : EntitySystem
     {
+        /// <inheritdoc />
         public override void Initialize()
         {
             EntityQuery = new TypeEntityQuery(typeof(PlayerInputMoverComponent));
+            
+            var moveUpCmdHandler = InputCmdHandler.FromDelegate(
+                session => HandleDirChange(session, Direction.North, true),
+                session => HandleDirChange(session, Direction.North, false));
+            var moveLeftCmdHandler = InputCmdHandler.FromDelegate(
+                session => HandleDirChange(session, Direction.West, true),
+                session => HandleDirChange(session, Direction.West, false));
+            var moveRightCmdHandler = InputCmdHandler.FromDelegate(
+                session => HandleDirChange(session, Direction.East, true),
+                session => HandleDirChange(session, Direction.East, false));
+            var moveDownCmdHandler = InputCmdHandler.FromDelegate(
+                session => HandleDirChange(session, Direction.South, true),
+                session => HandleDirChange(session, Direction.South, false));
+            var runCmdHandler = InputCmdHandler.FromDelegate(
+                session => HandleRunChange(session, true),
+                session => HandleRunChange(session, false));
+
+            var input = EntitySystemManager.GetEntitySystem<InputSystem>();
+
+            input.BindMap.BindFunction(EngineKeyFunctions.MoveUp, moveUpCmdHandler);
+            input.BindMap.BindFunction(EngineKeyFunctions.MoveLeft, moveLeftCmdHandler);
+            input.BindMap.BindFunction(EngineKeyFunctions.MoveRight, moveRightCmdHandler);
+            input.BindMap.BindFunction(EngineKeyFunctions.MoveDown, moveDownCmdHandler);
+            input.BindMap.BindFunction(EngineKeyFunctions.Run, runCmdHandler);
         }
 
+        /// <inheritdoc />
+        public override void Shutdown()
+        {
+            if (EntitySystemManager.TryGetEntitySystem(out InputSystem input))
+            {
+                input.BindMap.UnbindFunction(EngineKeyFunctions.MoveUp);
+                input.BindMap.UnbindFunction(EngineKeyFunctions.MoveLeft);
+                input.BindMap.UnbindFunction(EngineKeyFunctions.MoveRight);
+                input.BindMap.UnbindFunction(EngineKeyFunctions.MoveDown);
+                input.BindMap.UnbindFunction(EngineKeyFunctions.Run);
+            }
+
+            base.Shutdown();
+        }
+
+        /// <inheritdoc />
         public override void Update(float frameTime)
         {
             foreach (var entity in RelevantEntities)
             {
-                var comp = entity.GetComponent<PlayerInputMoverComponent>();
-                comp.OnUpdate();
+                var mover = entity.GetComponent<PlayerInputMoverComponent>();
+                var physics = entity.GetComponent<PhysicsComponent>();
+
+                UpdateKinematics(entity.Transform, mover, physics);
             }
+        }
+
+        private static void UpdateKinematics(ITransformComponent transform, PlayerInputMoverComponent mover, PhysicsComponent physics)
+        {
+            if (mover.VelocityDir.LengthSquared < 0.001)
+            {
+                if (physics.LinearVelocity != Vector2.Zero)
+                    physics.LinearVelocity = Vector2.Zero;
+            }
+            else
+            {
+                physics.LinearVelocity = mover.VelocityDir * (mover.Sprinting ? mover.SprintMoveSpeed : mover.WalkMoveSpeed);
+                transform.LocalRotation = mover.VelocityDir.GetDir().ToAngle();
+            }
+        }
+
+        private static void HandleDirChange(ICommonSession session, Direction dir, bool state)
+        {
+            if(!TryGetAttachedComponent(session as IPlayerSession, out PlayerInputMoverComponent moverComp))
+                return;
+
+            moverComp.SetVelocityDirection(dir, state);
+        }
+
+        private static void HandleRunChange(ICommonSession session, bool running)
+        {
+            if(!TryGetAttachedComponent(session as IPlayerSession, out PlayerInputMoverComponent moverComp))
+                return;
+
+            moverComp.Sprinting = running;
+        }
+
+        private static bool TryGetAttachedComponent<T>(IPlayerSession session, out T component)
+            where T: Component
+        {
+            component = default(T);
+            
+            var ent = session.AttachedEntity;
+
+            if (ent == null || !ent.IsValid())
+                return false;
+
+            if (!ent.TryGetComponent(out T comp))
+                return false;
+
+            component = comp;
+            return true;
         }
     }
 }

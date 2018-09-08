cc89b3af0dd9e67f5e3d41e07edce8947af79931
Remie Richards <remierichards@gmail.com>
2018-09-08 16:36:39 +0100

    Adds a var to forcibly show a godot transform. (#682)

--- SS14.Shared/Interfaces/GameObjects/Components/ITransformComponent.cs
+++ SS14.Shared/Interfaces/GameObjects/Components/ITransformComponent.cs
@@ -64,6 +64,11 @@ namespace SS14.Shared.Interfaces.GameObjects.Components
         bool IsMapTransform { get; }
 
         /// <summary>
+        /// Whether or not this entity is visible while parented to another entity.
+        /// </summary>
+        bool VisibleWhileParented { get; set; }
+
+        /// <summary>
         ///     Finds the transform located on the map or in nullspace
         /// </summary>
         ITransformComponent GetMapTransform();

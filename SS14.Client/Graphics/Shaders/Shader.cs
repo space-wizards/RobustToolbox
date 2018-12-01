c76f7917996891cbb9f0ded168beac3e0eb2a619
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2018-12-01 23:41:25 +0100

    Leak shaders to work around Godot issue #24108

--- SS14.Client/Graphics/Shaders/Shader.cs
+++ SS14.Client/Graphics/Shaders/Shader.cs
@@ -1,11 +1,18 @@
-﻿namespace SS14.Client.Graphics.Shaders
+﻿using System.Collections.Generic;
+
+namespace SS14.Client.Graphics.Shaders
 {
     public sealed class Shader
     {
         internal readonly Godot.Material GodotMaterial;
 
+        // We intentionally leak shaders to work around Godot issue #24108
+        // ReSharper disable once CollectionNeverQueried.Local
+        private static readonly List<Shader> LeakyLeaky = new List<Shader>();
+
         internal Shader(Godot.Material godotMaterial)
         {
+            LeakyLeaky.Add(this);
             GodotMaterial = godotMaterial;
         }
 

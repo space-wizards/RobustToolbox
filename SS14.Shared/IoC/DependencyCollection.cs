33bdef41a314c004a88d9200474132d02f89f989
Acruid <shatter66@gmail.com>
2018-12-15 16:57:16 -0800

    IoCManager functionality refactored into an instance-able object, called DependencyCollection. IoCManager now holds a DependencyCollection singleton, ad is used as a proxy.

---
+++ SS14.Shared/IoC/DependencyCollection.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Reflection;
+using SS14.Shared.IoC.Exceptions;
+using SS14.Shared.Utility;
+
+namespace SS14.Shared.IoC
+{
+    /// <inheritdoc />
+    internal class DependencyCollection : IDependencyCollection
+    {
+        /// <summary>
+        /// Dictionary that maps the types passed to <see cref="Resolve{T}"/> to their implementation.
+        /// </summary>
+        private readonly Dictionary<Type, object> _services = new Dictionary<Type, object>();
+
+        /// <summary>
+        /// The types interface types mapping to their registered implementations.
+        /// This is pulled from to make a service if it doesn't exist yet.
+        /// </summary>
+        private readonly Dictionary<Type, Type> _resolveTypes = new Dictionary<Type, Type>();
+
+        /// <inheritdoc />
+        public void Register<TInterface, TImplementation>(bool overwrite = false)
+            where TImplementation : class, TInterface, new()
+        {
+            var interfaceType = typeof(TInterface);
+            if (_resolveTypes.ContainsKey(interfaceType))
+            {
+                if (!overwrite)
+                {
+                    throw new InvalidOperationException
+                    (
+                        string.Format("Attempted to register already registered interface {0}. New implementation: {1}, Old implementation: {2}",
+                        interfaceType, typeof(TImplementation), _resolveTypes[interfaceType]
+                    ));
+                }
+
+                if (_services.ContainsKey(interfaceType))
+                {
+                    throw new InvalidOperationException($"Attempted to overwrite already instantiated interface {interfaceType}.");
+                }
+            }
+
+            _resolveTypes[interfaceType] = typeof(TImplementation);
+        }
+
+        /// <inheritdoc />
+        public void Clear()
+        {
+            foreach (var service in _services.Values.OfType<IDisposable>().Distinct())
+            {
+                try
+                {
+                    service.Dispose();
+                }
+                catch (Exception e)
+                {
+                    // DON'T use the logger since it might be dead already.
+                    System.Console.WriteLine($"Caught exception inside {service.GetType()} dispose! {e}");
+                }
+            }
+            _services.Clear();
+            _resolveTypes.Clear();
+        }
+
+        /// <inheritdoc />
+        [Pure]
+        public T Resolve<T>()
+        {
+            return (T)ResolveType(typeof(T));
+        }
+
+        /// <inheritdoc />
+        [Pure]
+        public object ResolveType(Type type)
+        {
+            if (_services.TryGetValue(type, out var value))
+            {
+                return value;
+            }
+
+            if (_resolveTypes.ContainsKey(type))
+            {
+                // If we have the type registered but not created that means we haven't been told to initialize the graph yet.
+                throw new InvalidOperationException($"Attempted to resolve type {type} before the object graph for it has been populated.");
+            }
+
+            throw new UnregisteredTypeException(type);
+
+        }
+
+        /// <inheritdoc />
+        public void BuildGraph()
+        {
+            // List of all objects we need to inject dependencies into.
+            var injectList = new List<object>();
+
+            // First we build every type we have registered but isn't yet built.
+            // This allows us to run this after the content assembly has been loaded.
+            foreach (KeyValuePair<Type, Type> currentType in _resolveTypes.Where(p => !_services.ContainsKey(p.Key)))
+            {
+                // Find a potential dupe by checking other registered types that have already been instantiated that have the same instance type.
+                // Can't catch ourselves because we're not instantiated.
+                // Ones that aren't yet instantiated are about to be and will find us instead.
+                KeyValuePair<Type, Type> dupeType = _resolveTypes.FirstOrDefault(p => _services.ContainsKey(p.Key) && p.Value == currentType.Value);
+
+                // Interface key can't be null so since KeyValuePair<> is a struct,
+                // this effectively checks whether we found something.
+                if (dupeType.Key != null)
+                {
+                    // We have something with the same instance type, use that.
+                    _services[currentType.Key] = _services[dupeType.Key];
+                    continue;
+                }
+
+                try
+                {
+                    var instance = Activator.CreateInstance(currentType.Value);
+                    _services[currentType.Key] = instance;
+                    injectList.Add(instance);
+                }
+                catch (TargetInvocationException e)
+                {
+                    throw new ImplementationConstructorException(currentType.Value, e.InnerException);
+                }
+            }
+
+            // Graph built, go over ones that need injection.
+            foreach (var implementation in injectList)
+            {
+                InjectDependencies(implementation);
+            }
+
+            foreach (var injectedItem in injectList.OfType<IPostInjectInit>())
+            {
+                injectedItem.PostInject();
+            }
+        }
+
+        /// <inheritdoc />
+        public void InjectDependencies(object obj)
+        {
+            foreach (var field in obj.GetType().GetAllFields()
+                            .Where(p => Attribute.GetCustomAttribute(p, typeof(DependencyAttribute)) != null))
+            {
+                // Not using Resolve<T>() because we're literally building it right now.
+                if (!_services.ContainsKey(field.FieldType))
+                {
+                    throw new UnregisteredDependencyException(obj.GetType(), field.FieldType, field.Name);
+                }
+
+                // Quick note: this DOES work with read only fields, though it may be a CLR implementation detail.
+                field.SetValue(obj, _services[field.FieldType]);
+            }
+        }
+    }
+}
--- SS14.Shared/IoC/IDependencyCollection.cs
+++ SS14.Shared/IoC/IDependencyCollection.cs
@@ -1,6 +1,5 @@
-﻿using SS14.Shared.IoC.Exceptions;
-using System;
-using System.Diagnostics.Contracts;
+﻿using System;
+using SS14.Shared.IoC.Exceptions;
 
 namespace SS14.Shared.IoC
 {
@@ -24,12 +23,10 @@ namespace SS14.Shared.IoC
     /// </para>
     /// </remarks>
     /// <seealso cref="Interfaces.Reflection.IReflectionManager"/>
-    public static class IoCManager
+    public interface IDependencyCollection
     {
-        private static readonly IDependencyCollection _container = new DependencyCollection();
-
         /// <summary>
-        /// Registers an interface to an implementation, to make it accessible to <see cref="Resolve{T}"/>
+        /// Registers an interface to an implementation, to make it accessible to <see cref="DependencyCollection.Resolve{T}"/>
         /// </summary>
         /// <typeparam name="TInterface">The type that will be resolvable.</typeparam>
         /// <typeparam name="TImplementation">The type that will be constructed as implementation.</typeparam>
@@ -39,23 +36,16 @@ namespace SS14.Shared.IoC
         /// </param>
         /// <exception cref="InvalidOperationException">
         /// Thrown if <paramref name="overwrite"/> is false and <typeparamref name="TInterface"/> has been registered before,
-        /// or if an already instantiated interface (by <see cref="BuildGraph"/>) is attempting to be overwriten.
+        /// or if an already instantiated interface (by <see cref="DependencyCollection.BuildGraph"/>) is attempting to be overwritten.
         /// </exception>
-        public static void Register<TInterface, TImplementation>(bool overwrite = false)
-            where TImplementation : class, TInterface, new()
-        {
-            _container.Register<TInterface, TImplementation>(overwrite);
-        }
+        void Register<TInterface, TImplementation>(bool overwrite = false) where TImplementation : class, TInterface, new();
 
         /// <summary>
         /// Clear all services and types.
         /// Use this between unit tests and on program shutdown.
         /// If a service implements <see cref="IDisposable"/>, <see cref="IDisposable.Dispose"/> will be called on it.
         /// </summary>
-        public static void Clear()
-        {
-            _container.Clear();
-        }
+        void Clear();
 
         /// <summary>
         /// Resolve a dependency manually.
@@ -65,11 +55,7 @@ namespace SS14.Shared.IoC
         /// Thrown if the resolved type hasn't been created yet
         /// because the object graph still needs to be constructed for it.
         /// </exception>
-        [Pure]
-        public static T Resolve<T>()
-        {
-            return (T)ResolveType(typeof(T));
-        }
+        T Resolve<T>();
 
         /// <summary>
         /// Resolve a dependency manually.
@@ -79,20 +65,13 @@ namespace SS14.Shared.IoC
         /// Thrown if the resolved type hasn't been created yet
         /// because the object graph still needs to be constructed for it.
         /// </exception>
-        [Pure]
-        public static object ResolveType(Type type)
-        {
-            return _container.ResolveType(type);
-        }
+        object ResolveType(Type type);
 
         /// <summary>
         /// Initializes the object graph by building every object and resolving all dependencies.
         /// </summary>
-        /// <seealso cref="InjectDependencies(object)"/>
-        public static void BuildGraph()
-        {
-            _container.BuildGraph();
-        }
+        /// <seealso cref="DependencyCollection.InjectDependencies"/>
+        void BuildGraph();
 
         /// <summary>
         ///     Injects dependencies into all fields with <see cref="DependencyAttribute"/> on the provided object.
@@ -105,10 +84,7 @@ namespace SS14.Shared.IoC
         /// <exception cref="UnregisteredDependencyException">
         ///     Thrown if a dependency field on the object is not registered.
         /// </exception>
-        /// <seealso cref="BuildGraph"/>
-        public static void InjectDependencies(object obj)
-        {
-            _container.InjectDependencies(obj);
-        }
+        /// <seealso cref="DependencyCollection.BuildGraph"/>
+        void InjectDependencies(object obj);
     }
 }

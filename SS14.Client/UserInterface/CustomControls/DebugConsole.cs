1d23df6d794c386cc5fa184a378d1467702588cc
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2019-02-23 23:28:11 +0100

    Make DebugConsole scene purely in code.

--- SS14.Client/UserInterface/CustomControls/DebugConsole.cs
+++ SS14.Client/UserInterface/CustomControls/DebugConsole.cs
@@ -5,10 +5,9 @@ using SS14.Client.Utility;
 using SS14.Shared.Console;
 using SS14.Shared.IoC;
 using SS14.Shared.Maths;
-using SS14.Shared.Reflection;
-using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using SS14.Client.Graphics.Drawing;
 using SS14.Client.Input;
 using SS14.Shared.Utility;
 
@@ -16,20 +15,15 @@ namespace SS14.Client.UserInterface.CustomControls
 {
     // Quick note on how thread safety works in here:
     // Messages from other threads are not actually immediately drawn. They're stored in a queue.
-    // Every frame OR the next time a message on the main Godot thread comes in, this queue is drained.
+    // Every frame OR the next time a message on the main thread comes in, this queue is drained.
     // This keeps thread safety while still making it so messages are ordered how they come in.
     // And also if Update() stops firing due to an exception loop the console will still work.
     // (At least from the main thread, which is what's throwing the exceptions..)
-    //
-    // Disable reflection so that we won't be looked at for scene translation.
-    [Reflect(false)]
     public class DebugConsole : Control, IDebugConsole
     {
         [Dependency] readonly IClientConsole console;
 
         private LineEdit CommandBar;
-
-        // This one is used outside Godot.
         private OutputPanel Output;
 
         public IReadOnlyDictionary<string, IConsoleCommand> Commands => console.Commands;
@@ -39,25 +33,35 @@ namespace SS14.Client.UserInterface.CustomControls
         private int _historyPosition;
         private bool _currentCommandEdited;
 
-        protected override ResourcePath ScenePath => new ResourcePath("/Scenes/DebugConsole/DebugConsole.tscn");
-
         protected override void Initialize()
         {
             IoCManager.InjectDependencies(this);
 
             Visible = false;
 
-            CommandBar = GetChild<LineEdit>("CommandBar");
+            AnchorRight = 1f;
+            AnchorBottom = 0.35f;
+
+            var boxContainer = new VBoxContainer {SeparationOverride = 0};
+            boxContainer.SetAnchorPreset(LayoutPreset.Wide);
+            AddChild(boxContainer);
+            var styleBox = new StyleBoxFlat
+            {
+                BackgroundColor = Color.Gray.WithAlpha(0.5f),
+            };
+            styleBox.SetContentMarginOverride(StyleBox.Margin.All, 3);
+            var outputWrapPanel = new PanelContainer
+            {
+                PanelOverride = styleBox,
+                SizeFlagsVertical = SizeFlags.FillExpand
+            };
+            boxContainer.AddChild(outputWrapPanel);
+            Output = new OutputPanel();
+            outputWrapPanel.AddChild(Output);
+
+            CommandBar = new LineEdit {PlaceHolder = "Command Here"};
+            boxContainer.AddChild(CommandBar);
             CommandBar.OnKeyDown += CommandBarOnOnKeyDown;
-
-            GetChild<RichTextLabel>("Contents").Dispose();
-
-            var output = new OutputPanel {Name = "Contents"};
-            output.SetAnchorAndMarginPreset(LayoutPreset.Wide, margin: 5);
-            output.MarginBottom = -30;
-            Output = output;
-            AddChild(output);
-
             CommandBar.OnTextEntered += CommandEntered;
             CommandBar.OnTextChanged += CommandBarOnOnTextChanged;
 

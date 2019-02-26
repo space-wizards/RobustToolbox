45efe4f29f54d368924e3245bcec6442ba6b0df9
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2019-02-26 15:39:44 +0100

    Implement scroll bars.

--- SS14.Client/UserInterface/Controls/Range.cs
+++ SS14.Client/UserInterface/Controls/Range.cs
@@ -1,15 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
+using SS14.Shared.Maths;
 
 namespace SS14.Client.UserInterface.Controls
 {
     [ControlWrap(typeof(Godot.Range))]
     public abstract class Range : Control
     {
-        public Range() : base()
+        private float _maxValue;
+        private float _minValue;
+        private float _value;
+        private float _page;
+
+        public Range()
         {
         }
 
@@ -21,57 +24,104 @@ namespace SS14.Client.UserInterface.Controls
         {
         }
 
+        public event Action<Range> OnValueChanged;
+
         public float GetAsRatio()
         {
-            return GameController.OnGodot ? (float)SceneControl.Call("get_as_ratio") : default;
+            if (GameController.OnGodot)
+            {
+                return (float)SceneControl.Call("get_as_ratio");
+            }
+
+            return (_value - _minValue) / (_maxValue - _minValue);
         }
 
         public float Page
         {
-            get => GameController.OnGodot ? (float)SceneControl.Get("page") : default;
+            get => GameController.OnGodot ? (float)SceneControl.Get("page") : _page;
             set
             {
                 if (GameController.OnGodot)
                 {
                     SceneControl.Set("page", value);
                 }
+                else
+                {
+                    _page = value;
+                    _ensureValueClamped();
+                }
             }
         }
 
         public float MaxValue
         {
-            get => GameController.OnGodot ? (float)SceneControl.Get("max_value") : default;
+            get => GameController.OnGodot ? (float)SceneControl.Get("max_value") : _maxValue;
             set
             {
                 if (GameController.OnGodot)
                 {
                     SceneControl.Set("max_value", value);
                 }
+                else
+                {
+                    _maxValue = value;
+                    _ensureValueClamped();
+                }
             }
         }
 
         public float MinValue
         {
-            get => GameController.OnGodot ? (float)SceneControl.Get("min_value") : default;
+            get => GameController.OnGodot ? (float)SceneControl.Get("min_value") : _minValue;
             set
             {
                 if (GameController.OnGodot)
                 {
                     SceneControl.Set("min_value", value);
                 }
+                else
+                {
+                    _minValue = value;
+                    _ensureValueClamped();
+                }
             }
         }
 
         public float Value
         {
-            get => GameController.OnGodot ? (float)SceneControl.Get("value") : default;
+            get => GameController.OnGodot ? (float) SceneControl.Get("value") : _value;
             set
             {
                 if (GameController.OnGodot)
                 {
                     SceneControl.Set("value", value);
                 }
+                else
+                {
+                    var newValue = ClampValue(value);
+                    if (!FloatMath.CloseTo(newValue, _value))
+                    {
+                        _value = newValue;
+                        OnValueChanged?.Invoke(this);
+                    }
+                }
             }
+        }
+
+        private void _ensureValueClamped()
+        {
+            var newValue = ClampValue(_value);
+            if (!FloatMath.CloseTo(newValue, _value))
+            {
+                _value = newValue;
+                OnValueChanged?.Invoke(this);
+            }
+        }
+
+        [Pure]
+        protected float ClampValue(float value)
+        {
+            return value.Clamp(_minValue, _maxValue-_page);
         }
     }
 }
--- SS14.Client/UserInterface/Controls/VScrollBar.cs
+++ SS14.Client/UserInterface/Controls/VScrollBar.cs
@@ -1,20 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SS14.Shared.Log;
-
-namespace SS14.Client.UserInterface.Controls
+﻿namespace SS14.Client.UserInterface.Controls
 {
     [ControlWrap(typeof(Godot.VScrollBar))]
     public class VScrollBar : ScrollBar
     {
-        public VScrollBar() : base()
-        {
-        }
-
-        public VScrollBar(string name) : base(name)
+        public VScrollBar() : base(OrientationMode.Vertical)
         {
         }
 
@@ -26,9 +15,5 @@ namespace SS14.Client.UserInterface.Controls
         {
             return new Godot.VScrollBar();
         }
-
-        // The Value of a scrollbar is the position where the "bar" begins,
-        //  so since Page is the "size" of the bar, page + value is the max.
-        public bool IsAtBottom => Value + Page == MaxValue;
     }
 }

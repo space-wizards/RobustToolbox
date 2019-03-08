ec314a64b2614ff8d687737dc2eaed94a4edaa2b
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2019-03-08 17:14:01 +0100

    Implement ProgressBar

--- SS14.Client/UserInterface/Controls/ProgressBar.cs
+++ SS14.Client/UserInterface/Controls/ProgressBar.cs
@@ -1,10 +1,16 @@
 using System;
+using System.Diagnostics.Contracts;
+using SS14.Client.Graphics.Drawing;
+using SS14.Shared.Maths;
 
 namespace SS14.Client.UserInterface.Controls
 {
     [ControlWrap(typeof(Godot.ProgressBar))]
     public class ProgressBar : Range
     {
+        public const string StylePropertyBackground = "background";
+        public const string StylePropertyForeground = "foreground";
+
         public ProgressBar()
         {
         }
@@ -27,6 +33,58 @@ namespace SS14.Client.UserInterface.Controls
             {
                 if (GameController.OnGodot) SceneControl.Set("percent_visible", value);
             }
+        }
+
+        [Pure]
+        private StyleBox _getBackground()
+        {
+            TryGetStyleProperty(StylePropertyBackground, out StyleBox ret);
+            return ret;
+        }
+
+        [Pure]
+        private StyleBox _getForeground()
+        {
+            TryGetStyleProperty(StylePropertyForeground, out StyleBox ret);
+            return ret;
+        }
+
+        protected internal override void Draw(DrawingHandleScreen handle)
+        {
+            base.Draw(handle);
+
+            if (GameController.OnGodot)
+            {
+                return;
+            }
+
+            var bg = _getBackground();
+            bg?.Draw(handle, SizeBox);
+
+            var fg = _getForeground();
+            if (fg == null)
+            {
+                return;
+            }
+            var minSize = fg.MinimumSize;
+            var size = Width * GetAsRatio() - minSize.X;
+            if (size > 0)
+            {
+                fg.Draw(handle, UIBox2.FromDimensions(0, 0, minSize.X + size, Height));
+            }
+        }
+
+        protected override Vector2 CalculateMinimumSize()
+        {
+            if (GameController.OnGodot)
+            {
+                return Vector2.Zero;
+            }
+
+            var bgSize = _getBackground()?.MinimumSize ?? Vector2.Zero;
+            var fgSize = _getForeground()?.MinimumSize ?? Vector2.Zero;
+
+            return Vector2.ComponentMax(bgSize, fgSize);
         }
 
         private protected override Godot.Control SpawnSceneControl()

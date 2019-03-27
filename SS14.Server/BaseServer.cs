78b285a359ead7ffca30fa8251dbed3f33b9f5ea
Nirnael <none>
2019-03-27 04:32:06 +0100

    Standardize and future-proof the server

--- SS14.Server/BaseServer.cs
+++ SS14.Server/BaseServer.cs
@@ -291,7 +291,7 @@ namespace SS14.Server
 
             // Wrtie down exception log
             var logPath = _config.GetCVar<string>("log.path");
-            var pathToWrite = Path.Combine(PathHelpers.ExecutableRelativeFile(logPath), "Runtime-" + DateTime.Now.ToShortDateString() + ".txt");
+            var pathToWrite = Path.Combine(PathHelpers.ExecutableRelativeFile(logPath), "Runtime_" + DateTime.Now.ToString("yyyyy_MM_dd") + ".txt");
             File.WriteAllText(pathToWrite, runtimeLog.Display(), Encoding.UTF8);
 
             //TODO: This should prob shutdown all managers in a loop.

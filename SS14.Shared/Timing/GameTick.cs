29ac4fb19083947e79aabad162d17747a31aced1
Acruid <shatter66@gmail.com>
2019-03-31 11:50:17 -0700

    add a new GameTick struct.

---
+++ SS14.Shared/Timing/GameTick.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace SS14.Shared.Timing
+{
+    /// <summary>
+    ///     Wraps a game tick value.
+    /// </summary>
+    [Serializable]
+    public readonly struct GameTick : IEquatable<GameTick>, IComparable<GameTick>
+    {
+        public readonly uint Value;
+
+        /// <summary>
+        ///     Constructs a new instance of <c>GameTick</c>.
+        /// </summary>
+        /// <param name="value"></param>
+        public GameTick(uint value)
+        {
+            Value = value;
+        }
+
+        /// <inheritdoc />
+        public bool Equals(GameTick other)
+        {
+            return Value == other.Value;
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is GameTick other && Equals(other);
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            return (int) Value;
+        }
+
+        /// <summary>
+        ///     Check for equality by value between two objects.
+        /// </summary>
+        public static bool operator ==(GameTick a, GameTick b)
+        {
+            return a.Value == b.Value;
+        }
+
+        /// <summary>
+        ///     Check for inequality by value between two objects.
+        /// </summary>
+        public static bool operator !=(GameTick a, GameTick b)
+        {
+            return a.Value != b.Value;
+        }
+
+        /// <inheritdoc />
+        public int CompareTo(GameTick other)
+        {
+            return Value.CompareTo(other.Value);
+        }
+    }
+}

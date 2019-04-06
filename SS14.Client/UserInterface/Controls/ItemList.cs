a890957d3088631f87f3df6ee5cef9d5acd0b43f
Víctor Aguilera Puerto <6766154+Zumorica@users.noreply.github.com>
2019-04-06 17:04:32 +0200

    Implement ItemLists. (#781)
    Resolves #756
    Style in https://github.com/space-wizards/space-station-14/pull/183
    
    It works, but I'm sure it can be improved further.

--- SS14.Client/UserInterface/Controls/ItemList.cs
+++ SS14.Client/UserInterface/Controls/ItemList.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using SS14.Client.Graphics;
+using SS14.Client.Graphics.Drawing;
+using SS14.Client.Input;
 using SS14.Client.Utility;
 using SS14.Shared.Maths;
 
@@ -8,7 +11,18 @@ namespace SS14.Client.UserInterface.Controls
     [ControlWrap(typeof(Godot.ItemList))]
     public class ItemList : Control
     {
-        public int ItemCount => GameController.OnGodot ? (int)SceneControl.Call("get_item_count") : default;
+        private readonly List<Item> _itemList = new List<Item>();
+        public event Action<ItemListSelectedEventArgs> OnItemSelected;
+        public event Action<ItemListDeselectedEventArgs> OnItemDeselected;
+
+        public const string StylePropertyBackground = "itemlist-background";
+        public const string StylePropertyItemBackground = "item-background";
+        public const string StylePropertySelectedItemBackground = "selected-item-background";
+        public const string StylePropertyDisabledItemBackground = "disabled-item-background";
+
+        public int ItemCount => GameController.OnGodot ? (int)SceneControl.Call("get_item_count") : _itemList.Count;
+
+        public ItemListSelectMode SelectMode { get; set; } = ItemListSelectMode.Single;
 
         public ItemList()
         {
@@ -22,6 +36,11 @@ namespace SS14.Client.UserInterface.Controls
         {
         }
 
+        protected override void SetDefaults()
+        {
+            RectClipContent = true;
+        }
+
         private protected override Godot.Control SpawnSceneControl()
         {
             return new Godot.ItemList();
@@ -33,6 +52,11 @@ namespace SS14.Client.UserInterface.Controls
             {
                 SceneControl.Call("add_item", text, icon, selectable);
             }
+            else
+            {
+                var i = new Item() {Text = text, Icon = icon, Selectable = selectable};
+                _itemList.Add(i);
+            }
         }
 
         public void AddIconItem(Texture icon, bool selectable = true)
@@ -41,6 +65,10 @@ namespace SS14.Client.UserInterface.Controls
             {
                 SceneControl.Call("add_icon_item", icon, selectable);
             }
+            else
+            {
+                AddItem(null, icon, selectable);
+            }
         }
 
         public void Clear()
@@ -48,6 +76,10 @@ namespace SS14.Client.UserInterface.Controls
             if (GameController.OnGodot)
             {
                 SceneControl.Call("clear");
+            }
+            else
+            {
+                _itemList.Clear();
             }
         }
 
@@ -66,7 +98,7 @@ namespace SS14.Client.UserInterface.Controls
 
         public bool IsSelected(int idx)
         {
-            return GameController.OnGodot ? (bool)SceneControl.Call("is_selected", idx) : default;
+            return GameController.OnGodot ? (bool)SceneControl.Call("is_selected", idx) : _itemList[idx].Selected;
         }
 
         public void RemoveItem(int idx)
@@ -74,6 +106,10 @@ namespace SS14.Client.UserInterface.Controls
             if (GameController.OnGodot)
             {
                 SceneControl.Call("remove_item", idx);
+            }
+            else
+            {
+                _itemList.RemoveAt(idx);
             }
         }
 
@@ -83,6 +119,38 @@ namespace SS14.Client.UserInterface.Controls
             {
                 SceneControl.Call("select", idx, single);
             }
+            else
+            {
+                if (single)
+                {
+                    for (var jdx = 0; jdx < _itemList.Count; jdx++)
+                    {
+                        Unselect(jdx);
+                    }
+                }
+
+                var i = _itemList[idx];
+
+                if (i.Selectable)
+                {
+                    i.Selected = true;
+                    OnItemSelected?.Invoke(new ItemListSelectedEventArgs(idx, this));
+                }
+            }
+        }
+
+        public void Select(Item item, bool single = true)
+        {
+            if (GameController.OnGodot)
+            {
+                return;
+            }
+            else
+            {
+                var idx = _itemList.IndexOf(item);
+                if (idx != -1)
+                    Select(idx, single);
+            }
         }
 
         public void SetItemCustomBgColor(int idx, Color color)
@@ -90,6 +158,10 @@ namespace SS14.Client.UserInterface.Controls
             if (GameController.OnGodot)
             {
                 SceneControl.Call("set_icon_custom_bg_color", idx, color.Convert());
+            }
+            else
+            {
+                //_itemList[idx].CustomBg = color;
             }
         }
 
@@ -99,6 +171,12 @@ namespace SS14.Client.UserInterface.Controls
             {
                 SceneControl.Call("set_item_disabled", idx, disabled);
             }
+            else
+            {
+                Unselect(idx);
+                var i = _itemList[idx];
+                i.Disabled = disabled;
+            }
         }
 
         public void SetItemIcon(int idx, Texture icon)
@@ -106,6 +184,10 @@ namespace SS14.Client.UserInterface.Controls
             if (GameController.OnGodot)
             {
                 SceneControl.Call("set_item_icon", idx, icon);
+            }
+            else
+            {
+                _itemList[idx].Icon = icon;
             }
         }
 
@@ -115,6 +197,10 @@ namespace SS14.Client.UserInterface.Controls
             {
                 SceneControl.Call("set_item_icon_region", idx, region.Convert());
             }
+            else
+            {
+                _itemList[idx].IconRegion = region;
+            }
         }
 
         public void SetItemSelectable(int idx, bool selectable)
@@ -122,6 +208,10 @@ namespace SS14.Client.UserInterface.Controls
             if (GameController.OnGodot)
             {
                 SceneControl.Call("set_item_selectable", idx, selectable);
+            }
+            else
+            {
+                _itemList[idx].Selectable = selectable;
             }
         }
 
@@ -131,6 +221,10 @@ namespace SS14.Client.UserInterface.Controls
             {
                 SceneControl.Call("set_item_text", idx, text);
             }
+            else
+            {
+                _itemList[idx].Text = text;
+            }
         }
 
         public void SetItemTooltip(int idx, string tooltip)
@@ -138,6 +232,10 @@ namespace SS14.Client.UserInterface.Controls
             if (GameController.OnGodot)
             {
                 SceneControl.Call("set_item_tooltip", idx, tooltip);
+            }
+            else
+            {
+                _itemList[idx].TooltipText = tooltip;
             }
         }
 
@@ -147,6 +245,10 @@ namespace SS14.Client.UserInterface.Controls
             {
                 SceneControl.Call("set_item_tooltip_enabled", idx, enabled);
             }
+            else
+            {
+                _itemList[idx].TooltipEnabled = true;
+            }
         }
 
         public void SortItemsByText()
@@ -154,6 +256,10 @@ namespace SS14.Client.UserInterface.Controls
             if (GameController.OnGodot)
             {
                 SceneControl.Call("sort_items_by_text");
+            }
+            else
+            {
+                _itemList.Sort((p, q) => string.Compare(p.Text, q.Text, StringComparison.Ordinal));
             }
         }
 
@@ -163,6 +269,282 @@ namespace SS14.Client.UserInterface.Controls
             {
                 SceneControl.Call("unselect", idx);
             }
+            else
+            {
+                var i = _itemList[idx];
+                if (!i.Selected) return;
+                i.Selected = false;
+                OnItemDeselected?.Invoke(new ItemListDeselectedEventArgs(idx, this));
+            }
+        }
+
+        public void Unselect(Item item)
+        {
+            if (GameController.OnGodot)
+            {
+                return;
+            }
+            else
+            {
+                var idx = _itemList.IndexOf(item);
+                if (idx == -1) return;
+                Unselect(idx);
+            }
+        }
+
+        public Font ActualFont
+        {
+            get
+            {
+                if (TryGetStyleProperty("font", out Font font))
+                {
+                    return font;
+                }
+
+                return UserInterfaceManager.ThemeDefaults.DefaultFont;
+            }
+        }
+
+        public Color ActualFontColor
+        {
+            get
+            {
+                if (TryGetStyleProperty("font-color", out Color fontColor))
+                {
+                    return fontColor;
+                }
+
+                return Color.White;
+            }
+        }
+
+        public StyleBox ActualBackground
+        {
+            get
+            {
+                if (TryGetStyleProperty(StylePropertyBackground, out StyleBox bg))
+                {
+                    return bg;
+                }
+
+                return new StyleBoxFlat();
+            }
+        }
+        public StyleBox ActualItemBackground
+        {
+            get
+            {
+                if (TryGetStyleProperty(StylePropertyItemBackground, out StyleBox bg))
+                {
+                    return bg;
+                }
+
+                return new StyleBoxFlat();
+            }
+        }
+
+        public StyleBox ActualSelectedItemBackground
+        {
+            get
+            {
+                if (TryGetStyleProperty(StylePropertySelectedItemBackground, out StyleBox bg))
+                {
+                    return bg;
+                }
+
+                return new StyleBoxFlat();
+            }
+        }
+
+        public StyleBox ActualDisabledItemBackground
+        {
+            get
+            {
+                if (TryGetStyleProperty(StylePropertyDisabledItemBackground, out StyleBox bg))
+                {
+                    return bg;
+                }
+
+                return new StyleBoxFlat();
+            }
+        }
+
+        protected internal override void Draw(DrawingHandleScreen handle)
+        {
+            base.Draw(handle);
+
+            if (GameController.OnGodot)
+            {
+                return;
+            }
+
+            var font = ActualFont;
+            var listBg = ActualBackground;
+            var iconBg = ActualItemBackground;
+            var iconSelectedBg = ActualSelectedItemBackground;
+            var iconDisabledBg = ActualDisabledItemBackground;
+
+            Vector2 separation = (0, 0);
+
+            listBg.Draw(handle, SizeBox);
+
+            foreach (var item in _itemList)
+            {
+                var bg = iconBg;
+
+                if (item.Disabled)
+                    bg = iconDisabledBg;
+
+                if (item.Selected)
+                    bg = iconSelectedBg;
+
+                var itemHeight = item.Icon != null
+                    ? Math.Max(item.IconSize.Y, font.Height) + bg.MinimumSize.Y
+                    : font.Height + bg.MinimumSize.Y;
+
+                item.Region = UIBox2.FromDimensions(separation, (SizeBox.Width, itemHeight));
+
+                bg.Draw(handle, item.Region.Value);
+
+                if (item.Icon != null)
+                {
+                    if (item.IconRegion.Size == Vector2.Zero)
+                    {
+                        handle.DrawTextureRect(item.Icon, UIBox2.FromDimensions(separation, item.Icon.Size), false, item.IconModulate, item.IconTranspose);
+                    }
+                    else
+                    {
+                        handle.DrawTextureRectRegion(item.Icon, UIBox2.FromDimensions(separation, item.Icon.Size), item.IconRegion, item.IconModulate);
+                    }
+                }
+
+                if (item.Text != null)
+                {
+                    DrawTextInternal(handle, item.Text,
+                        UIBox2.FromDimensions((item.IconSize.X, separation.Y), (SizeBox.Width-item.IconSize.X,font.Height*2))
+                        );
+                }
+
+                separation += (0, itemHeight);
+            }
+        }
+
+        protected void DrawTextInternal(DrawingHandleScreen handle, string text, UIBox2 box)
+        {
+            var font = ActualFont;
+
+            var color = ActualFontColor;
+            var offsetY = (int) (box.Height - font.Height) / 2;
+            var baseLine = new Vector2i(0, offsetY + font.Ascent) + box.TopLeft;
+
+            foreach (var chr in text)
+            {
+                if (!font.TryGetCharMetrics(chr, out var metrics))
+                {
+                    continue;
+                }
+
+                if (!(baseLine.X < box.Left || baseLine.X + metrics.Advance > box.Right))
+                {
+                    font.DrawChar(handle, chr, baseLine, color);
+                }
+
+                baseLine += (metrics.Advance, 0);
+            }
+        }
+
+        protected internal override void MouseDown(GUIMouseButtonEventArgs args)
+        {
+            base.MouseDown(args);
+
+            if (GameController.OnGodot || SelectMode == ItemListSelectMode.None || args.Button != Mouse.Button.Left)
+            {
+                return;
+            }
+
+            foreach (var item in _itemList)
+            {
+                if (item.Region == null) continue;
+                if (!item.Region.Value.Contains(args.RelativePosition)) continue;
+                if (item.Selectable && !item.Disabled)
+                    if (item.Selected)
+                        Unselect(item);
+                    else
+                        Select(item, SelectMode == ItemListSelectMode.Single);
+                break;
+            }
+        }
+
+        public sealed class Item
+        {
+            public string Text = null;
+            public string TooltipText = null;
+            public Texture Icon = null;
+            public UIBox2 IconRegion = new UIBox2();
+            public Color IconModulate = Color.White;
+            public bool IconTranspose = false;
+            public bool Selected = false;
+            public bool Selectable = true;
+            public bool TooltipEnabled = true;
+            public bool Disabled = false;
+
+            public UIBox2? Region = null;
+
+            public Vector2 IconSize
+            {
+                get
+                {
+                    if (Icon == null)
+                        return Vector2.Zero;
+                    return IconRegion.Size != Vector2.Zero ? IconRegion.Size : Icon.Size;
+                }
+            }
+        }
+
+        public class ItemListEventArgs : EventArgs
+        {
+            /// <summary>
+            ///     The ItemList this event originated from.
+            /// </summary>
+            public ItemList ItemList { get; }
+
+            public ItemListEventArgs(ItemList list)
+            {
+                ItemList = list;
+            }
+        }
+
+        public class ItemListSelectedEventArgs : ItemListEventArgs
+        {
+            /// <summary>
+            ///     The index of the item that was selected.
+            /// </summary>
+            public int ItemIndex;
+
+            public ItemListSelectedEventArgs(int itemIndex, ItemList list) : base(list)
+            {
+                ItemIndex = itemIndex;
+            }
+        }
+
+        public class ItemListDeselectedEventArgs : ItemListEventArgs
+        {
+            /// <summary>
+            ///     The index of the item that was selected.
+            /// </summary>
+            public int ItemIndex;
+
+            public ItemListDeselectedEventArgs(int itemIndex, ItemList list) : base(list)
+            {
+                ItemIndex = itemIndex;
+            }
+        }
+
+        public enum ItemListSelectMode
+        {
+            None,
+            Single,
+            Multiple,
         }
     }
 }

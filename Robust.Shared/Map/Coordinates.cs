a5fce220b605b80308ff3fd1c382e00045c7630b
Acruid <shatter66@gmail.com>
2019-04-12 23:30:05 -0700

    Modified ScreenCoordinates: Filled out doc comments. Added the PublicApi attribute. Implemented IEquatable.

--- Robust.Shared/Map/Coordinates.cs
+++ Robust.Shared/Map/Coordinates.cs
@@ -192,28 +192,87 @@ namespace Robust.Shared.Map
         }
     }
 
+    /// <summary>
+    ///     Contains the coordinates of a position on the rendering screen.
+    /// </summary>
+    [PublicAPI]
     [Serializable, NetSerializable]
-    public readonly struct ScreenCoordinates
+    public readonly struct ScreenCoordinates : IEquatable<ScreenCoordinates>
     {
+        /// <summary>
+        ///     Position on the rendering screen.
+        /// </summary>
         public readonly Vector2 Position;
 
+        /// <summary>
+        ///     Screen position on the X axis.
+        /// </summary>
         public float X => Position.X;
 
+        /// <summary>
+        ///     Screen position on the Y axis.
+        /// </summary>
         public float Y => Position.Y;
 
-        public ScreenCoordinates(Vector2 argPosition)
+        /// <summary>
+        ///     Constructs a new instance of <c>ScreenCoordinates</c>.
+        /// </summary>
+        /// <param name="position">Position on the rendering screen.</param>
+        public ScreenCoordinates(Vector2 position)
         {
-            Position = argPosition;
+            Position = position;
         }
 
+        /// <summary>
+        ///     Constructs a new instance of <c>ScreenCoordinates</c>.
+        /// </summary>
+        /// <param name="x">X axis of a position on the screen.</param>
+        /// <param name="y">Y axis of a position on the screen.</param>
         public ScreenCoordinates(float x, float y)
         {
             Position = new Vector2(x, y);
         }
 
+        /// <inheritdoc />
         public override string ToString()
         {
             return Position.ToString();
+        }
+
+        /// <inheritdoc />
+        public bool Equals(ScreenCoordinates other)
+        {
+            return Position.Equals(other.Position);
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+            return obj is ScreenCoordinates other && Equals(other);
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            return Position.GetHashCode();
+        }
+
+        /// <summary>
+        ///     Check for equality by value between two objects.
+        /// </summary>
+        public static bool operator ==(ScreenCoordinates a, ScreenCoordinates b)
+        {
+            return a.Equals(b);
+        }
+
+        /// <summary>
+        ///     Check for inequality by value between two objects.
+        /// </summary>
+        public static bool operator !=(ScreenCoordinates a, ScreenCoordinates b)
+        {
+            return !a.Equals(b);
         }
     }
 

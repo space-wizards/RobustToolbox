8ae7f33fdcf6c7fc8e0e4150f918274adf62352e
Acruid <shatter66@gmail.com>
2019-04-12 23:24:37 -0700

    Modified MapCoordinates: Added PublicApi attribute. Implemented IEquatable. Filled out doc comments. Removed Map property.

--- Robust.Shared/Map/Coordinates.cs
+++ Robust.Shared/Map/Coordinates.cs
@@ -222,28 +222,91 @@ namespace Robust.Shared.Map
     /// </summary>
     [PublicAPI]
     [Serializable, NetSerializable]
-    public readonly struct MapCoordinates
+    public readonly struct MapCoordinates : IEquatable<MapCoordinates>
     {
+        /// <summary>
+        ///     World Position coordinates.
+        /// </summary>
         public readonly Vector2 Position;
+
+        /// <summary>
+        ///     Map identifier relevant to this position.
+        /// </summary>
         public readonly MapId MapId;
 
+        /// <summary>
+        ///     World position on the X axis.
+        /// </summary>
         public float X => Position.X;
-        public float Y => Position.Y;
-        public IMap Map => IoCManager.Resolve<IMapManager>().GetMap(MapId);
 
+        /// <summary>
+        ///     World position on the Y axis.
+        /// </summary>
+        public float Y => Position.Y;
+
+        /// <summary>
+        ///     Constructs a new instance of <c>MapCoordinates</c>.
+        /// </summary>
+        /// <param name="position">World position coordinates.</param>
+        /// <param name="mapId">Map identifier relevant to this position.</param>
         public MapCoordinates(Vector2 position, MapId mapId)
         {
             Position = position;
             MapId = mapId;
         }
 
-        public MapCoordinates(float x, float y, MapId mapId) : this(new Vector2(x, y), mapId)
-        {
-        }
+        /// <summary>
+        ///     Constructs a new instance of <c>MapCoordinates</c>.
+        /// </summary>
+        /// <param name="x">World position coordinate on the X axis.</param>
+        /// <param name="y">World position coordinate on the Y axis.</param>
+        /// <param name="mapId">Map identifier relevant to this position.</param>
+        public MapCoordinates(float x, float y, MapId mapId)
+            : this(new Vector2(x, y), mapId) { }
 
+        /// <inheritdoc />
         public override string ToString()
         {
-            return $"({X}, {Y}, map: {MapId})";
+            return $"({Position.X}, {Position.Y}, map: {MapId})";
+        }
+
+        /// <inheritdoc />
+        public bool Equals(MapCoordinates other)
+        {
+            return Position.Equals(other.Position) && MapId.Equals(other.MapId);
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+            return obj is MapCoordinates other && Equals(other);
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Position.GetHashCode() * 397) ^ MapId.GetHashCode();
+            }
+        }
+
+        /// <summary>
+        ///     Check for equality by value between two objects.
+        /// </summary>
+        public static bool operator ==(MapCoordinates a, MapCoordinates b)
+        {
+            return a.Equals(b);
+        }
+
+        /// <summary>
+        ///     Check for inequality by value between two objects.
+        /// </summary>
+        public static bool operator !=(MapCoordinates a, MapCoordinates b)
+        {
+            return !a.Equals(b);
         }
     }
 }

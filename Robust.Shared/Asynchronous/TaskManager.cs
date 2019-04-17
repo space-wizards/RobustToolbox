ce052d4db365d1e301a33d90319609fbb5e39b9a
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2019-04-17 23:23:35 +0200

    Add method to post a delegate to the main thread.
    Helps for writing threaded code.

--- Robust.Shared/Asynchronous/TaskManager.cs
+++ Robust.Shared/Asynchronous/TaskManager.cs
@@ -1,8 +1,9 @@
+using System;
 using System.Threading;
 
 namespace Robust.Shared.Asynchronous
 {
-    internal class TaskManager : ITaskManager
+    internal sealed class TaskManager : ITaskManager
     {
         private SS14SynchronizationContext _mainThreadContext;
 
@@ -16,11 +17,31 @@ namespace Robust.Shared.Asynchronous
         {
             _mainThreadContext.ProcessPendingTasks();
         }
+
+        public void RunOnMainThread(Action callback)
+        {
+            _mainThreadContext.Post(_runCallback, callback);
+        }
+
+        private static readonly SendOrPostCallback _runCallback = o =>
+        {
+            ((Action)o)();
+        };
     }
 
     public interface ITaskManager
     {
         void Initialize();
         void ProcessPendingTasks();
+
+        /// <summary>
+        ///     Run a delegate on the main thread sometime later.
+        ///     Thread safe.
+        /// </summary>
+        /// <remarks>
+        ///     Useful if you want to run a callback from a separate thread.
+        /// </remarks>
+        /// <param name="callback">The callback that will be invoked on the main thread.</param>
+        void RunOnMainThread(Action callback);
     }
 }

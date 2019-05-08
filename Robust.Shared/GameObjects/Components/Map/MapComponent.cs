7d417b2e567199d6cd8cf1ec7d102bd1650527fa
Acruid <shatter66@gmail.com>
2019-05-07 18:01:47 -0700

    Adds Map components to the ECS system.

---
+++ Robust.Shared/GameObjects/Components/Map/MapComponent.cs
@@ -0,0 +1,37 @@
+﻿using Robust.Shared.Interfaces.Map;
+using Robust.Shared.IoC;
+using Robust.Shared.Map;
+using Robust.Shared.Serialization;
+
+namespace Robust.Shared.GameObjects.Components.Map
+{
+    /// <summary>
+    ///     Represents a world map inside the ECS system.
+    /// </summary>
+    public interface IMapComponent
+    {
+        IMap WorldMap { get; }
+    }
+
+    /// <inheritdoc cref="IMapComponent"/>
+    public class MapComponent : Component, IMapComponent
+    {
+        [Dependency] private readonly IMapManager _mapManager;
+
+        private MapId _mapIndex;
+
+        /// <inheritdoc />
+        public override string Name => "Map";
+
+        /// <inheritdoc />
+        public IMap WorldMap => _mapManager.GetMap(_mapIndex);
+        
+        /// <inheritdoc />
+        public override void ExposeData(ObjectSerializer serializer)
+        {
+            base.ExposeData(serializer);
+
+            serializer.DataField(ref _mapIndex, "index", MapId.Nullspace);
+        }
+    }
+}
---
+++ Robust.Shared/GameObjects/Components/Map/MapGridComponent.cs
@@ -0,0 +1,37 @@
+﻿using Robust.Shared.Interfaces.Map;
+using Robust.Shared.IoC;
+using Robust.Shared.Map;
+using Robust.Shared.Serialization;
+
+namespace Robust.Shared.GameObjects.Components.Map
+{
+    /// <summary>
+    ///     Represents a map grid inside the ECS system.
+    /// </summary>
+    internal interface IMapGridComponent
+    {
+        IMapGrid Grid { get; }
+    }
+
+    /// <inheritdoc cref="IMapGridComponent"/>
+    public class MapGridComponent : Component, IMapGridComponent
+    {
+        [Dependency] private readonly IMapManager _mapManager;
+
+        private GridId _gridIndex;
+
+        /// <inheritdoc />
+        public override string Name => "MapGrid";
+
+        /// <inheritdoc />
+        public IMapGrid Grid => _mapManager.GetGrid(_gridIndex);
+
+        /// <inheritdoc />
+        public override void ExposeData(ObjectSerializer serializer)
+        {
+            base.ExposeData(serializer);
+
+            serializer.DataField(ref _gridIndex, "index", GridId.Nullspace);
+        }
+    }
+}

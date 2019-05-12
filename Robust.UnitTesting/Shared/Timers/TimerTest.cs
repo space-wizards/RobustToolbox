9977d64e2feb16859e1afc6a4629544c7509ab93
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2019-05-12 11:47:20 +0200

    Remove exception-catch tests from timers.
    These tests were ensuring that exceptions were logged,
    we decided these should bubble up instead.

--- Robust.UnitTesting/Shared/Timers/TimerTest.cs
+++ Robust.UnitTesting/Shared/Timers/TimerTest.cs
@@ -46,42 +46,6 @@ namespace Robust.UnitTesting.Shared.Timers
         }
 
         [Test]
-        public void TestSpawnedException()
-        {
-            var timerManager = IoCManager.Resolve<ITimerManager>();
-            _catcher.Flush();
-
-            bool DidThrow()
-            {
-                // TODO: Probably not perfect to hard code the reliance on logging.
-                // Logging should be kinda user-facing purely.
-                // Some sorta structured logging for exceptions like we have in SS13 would be good.
-                return _catcher.CaughtLogs.Any(l => l.Level == LogLevel.Error);
-            }
-
-            var threw = false;
-
-            Timer.Spawn(TimeSpan.FromMilliseconds(500), () =>
-            {
-                threw = true;
-                throw new InvalidOperationException();
-            });
-
-            Assert.That(threw, Is.False);
-            Assert.That(DidThrow(), Is.False);
-
-            // Set timers ahead 250 ms
-            timerManager.UpdateTimers(0.25f);
-            Assert.That(threw, Is.False);
-            Assert.That(DidThrow(), Is.False);
-
-            // Another 300ms should do it.
-            timerManager.UpdateTimers(0.30f);
-            Assert.That(threw, Is.True);
-            Assert.That(DidThrow(), Is.True);
-        }
-
-        [Test]
         public void TestAsyncDelay()
         {
             var timerManager = IoCManager.Resolve<ITimerManager>();
@@ -103,48 +67,6 @@ namespace Robust.UnitTesting.Shared.Timers
             // Another 300ms should do it.
             timerManager.UpdateTimers(0.30f);
             Assert.That(ran, Is.True);
-        }
-
-        [Test]
-        public void TestAsyncDelayException()
-        {
-            var timerManager = IoCManager.Resolve<ITimerManager>();
-            var taskManager = IoCManager.Resolve<ITaskManager>();
-
-            taskManager.Initialize();
-            _catcher.Flush();
-
-            bool DidThrow()
-            {
-                // TODO: Probably not perfect to hard code the reliance on logging.
-                // Logging should be kinda user-facing purely.
-                // Some sorta structured logging for exceptions like we have in SS13 would be good.
-                return _catcher.CaughtLogs.Any(l => l.Level == LogLevel.Error);
-            }
-
-            var threw = false;
-
-            async void Run()
-            {
-                await Timer.Delay(TimeSpan.FromMilliseconds(500));
-                threw = true;
-                throw new InvalidOperationException();
-            }
-
-            Run();
-            Assert.That(threw, Is.False);
-            Assert.That(DidThrow(), Is.False);
-
-            // Set timers ahead 250 ms
-            timerManager.UpdateTimers(0.25f);
-            Assert.That(threw, Is.False);
-            Assert.That(DidThrow(), Is.False);
-
-            // Another 300ms should do it.
-            timerManager.UpdateTimers(0.30f);
-            taskManager.ProcessPendingTasks();
-            Assert.That(threw, Is.True);
-            Assert.That(DidThrow(), Is.True);
         }
 
         [Test]

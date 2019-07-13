c504e58226affd1ab34fe38548baa3b26e2ae257
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2019-07-13 21:55:47 +0200

    Remove a bunch of cruft from RobustUnitTest.

--- Robust.UnitTesting/RobustUnitTest.cs
+++ Robust.UnitTesting/RobustUnitTest.cs
@@ -5,7 +5,6 @@ using System.Reflection;
 using NUnit.Framework;
 using Robust.Shared.ContentPack;
 using Robust.Shared.GameObjects;
-using Robust.Shared.Interfaces.Configuration;
 using Robust.Shared.Interfaces.GameObjects;
 using Robust.Shared.Interfaces.Reflection;
 using Robust.Shared.IoC;
@@ -21,78 +20,33 @@ namespace Robust.UnitTesting
     [Parallelizable]
     public abstract partial class RobustUnitTest
     {
-        #region Options
-
-        // TODO: make this figured out at runtime so we don't have to pass a compiler flag.
-#if HEADLESS
-        public const bool Headless = true;
-#else
-        public const bool Headless = false;
-#endif
-
-        // These properties are meant to be overriden to disable certain parts
-        // Like loading resource packs, which isn't always needed.
-
-        /// <summary>
-        /// Whether the client resource pack should be loaded or not.
-        /// </summary>
-        public virtual bool NeedsResourcePack => false;
-
-        /// <summary>
-        /// Whether the client config should be loaded or not.
-        /// </summary>
-        public virtual bool NeedsClientConfig => false;
-
         public virtual UnitTestProject Project => UnitTestProject.Server;
-
-        #endregion Options
-
-        #region Accessors
-
-        public IConfigurationManager GetConfigurationManager { get; private set; }
-
-        #endregion Accessors
 
         [OneTimeSetUp]
         public void BaseSetup()
         {
-            TestFixtureAttribute a = Attribute.GetCustomAttribute(GetType(), typeof(TestFixtureAttribute)) as TestFixtureAttribute;
-            if (NeedsResourcePack && Headless)
-            {
-                // Disable the test automatically.
-                a.Explicit = true;
-                return;
-            }
-
             // Clear state across tests.
             IoCManager.InitThread();
             IoCManager.Clear();
             RegisterIoC();
 
-            var Assemblies = new List<Assembly>(4);
+            var assemblies = new List<Assembly>(4);
             switch (Project)
             {
                 case UnitTestProject.Client:
-                    Assemblies.Add(AppDomain.CurrentDomain.GetAssemblyByName("Robust.Client"));
+                    assemblies.Add(AppDomain.CurrentDomain.GetAssemblyByName("Robust.Client"));
                     break;
                 case UnitTestProject.Server:
-                    Assemblies.Add(AppDomain.CurrentDomain.GetAssemblyByName("Robust.Server"));
+                    assemblies.Add(AppDomain.CurrentDomain.GetAssemblyByName("Robust.Server"));
                     break;
                 default:
                     throw new NotSupportedException($"Unknown testing project: {Project}");
             }
 
-            Assemblies.Add(AppDomain.CurrentDomain.GetAssemblyByName("Robust.Shared"));
-            Assemblies.Add(Assembly.GetExecutingAssembly());
+            assemblies.Add(AppDomain.CurrentDomain.GetAssemblyByName("Robust.Shared"));
+            assemblies.Add(Assembly.GetExecutingAssembly());
 
-            IoCManager.Resolve<IReflectionManager>().LoadAssemblies(Assemblies);
-
-            if (NeedsClientConfig)
-            {
-                //ConfigurationManager setup
-                GetConfigurationManager = IoCManager.Resolve<IConfigurationManager>();
-                GetConfigurationManager.LoadFromFile(PathHelpers.ExecutableRelativeFile("./client_config.toml"));
-            }
+            IoCManager.Resolve<IReflectionManager>().LoadAssemblies(assemblies);
 
             // Required components for the engine to work
             var compFactory = IoCManager.Resolve<IComponentFactory>();
@@ -101,15 +55,6 @@ namespace Robust.UnitTesting
                 compFactory.Register<MetaDataComponent>();
                 compFactory.RegisterReference<MetaDataComponent, IMetaDataComponent>();
             }
-
-
-            /*
-            if (NeedsResourcePack)
-            {
-                GetResourceCache = IoCManager.Resolve<IResourceCache>();
-                InitializeResources();
-            }
-            */
         }
 
         /// <summary>

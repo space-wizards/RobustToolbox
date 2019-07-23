414bd95eb2b9e4bfa4a7722bade8182cbb41b85b
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2019-07-23 21:08:01 +0200

    DynamicTypeFactory improvements:
    Can now make instances with constructor arguments.
    Adds some helpers.

--- Robust.Shared/IoC/DynamicTypeFactory.cs
+++ Robust.Shared/IoC/DynamicTypeFactory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using JetBrains.Annotations;
+using Robust.Shared.Utility;
 
 namespace Robust.Shared.IoC
 {
@@ -8,6 +9,7 @@ namespace Robust.Shared.IoC
     ///     dependencies resolved. If you think you need Activator.CreateInstance(), use this
     ///     factory instead.
     /// </summary>
+    /// <seealso cref="DynamicTypeFactoryExt"/>
     [PublicAPI]
     public interface IDynamicTypeFactory
     {
@@ -22,10 +24,51 @@ namespace Robust.Shared.IoC
         /// <summary>
         ///     Constructs a new instance of the given type with Dependencies resolved.
         /// </summary>
+        /// <param name="type">Type of object to instantiate.</param>
+        /// <param name="args">The arguments to be passed to the constructor.</param>
+        /// <returns>Newly created object.</returns>
+        object CreateInstance(Type type, object[] args);
+
+        /// <summary>
+        ///     Constructs a new instance of the given type with Dependencies resolved.
+        /// </summary>
         /// <typeparam name="T">Type of object to instantiate.</typeparam>
         /// <returns>Newly created object.</returns>
         T CreateInstance<T>()
             where T : new();
+    }
+
+    /// <summary>
+    ///     Extension methods for <see cref="IDynamicTypeFactory"/>.
+    /// </summary>
+    public static class DynamicTypeFactoryExt
+    {
+        /// <summary>
+        ///     Constructs a new instance of the given type, and return it cast to the specified type.
+        /// </summary>
+        /// <param name="dynamicTypeFactory">The dynamic type factory to use.</param>
+        /// <param name="type">The type to instantiate.</param>
+        /// <typeparam name="T">The type that the instance will be cast to.</typeparam>
+        /// <returns>Newly created object, cast to <typeparamref name="T"/>.</returns>
+        public static T CreateInstance<T>(this IDynamicTypeFactory dynamicTypeFactory, Type type)
+        {
+            DebugTools.Assert(typeof(T).IsAssignableFrom(type), "type must be subtype of T");
+            return (T) dynamicTypeFactory.CreateInstance(type);
+        }
+
+        /// <summary>
+        ///     Constructs a new instance of the given type, and return it cast to the specified type.
+        /// </summary>
+        /// <param name="dynamicTypeFactory">The dynamic type factory to use.</param>
+        /// <param name="type">The type to instantiate.</param>
+        /// <param name="args">The arguments to pass to the constructor.</param>
+        /// <typeparam name="T">The type that the instance will be cast to.</typeparam>
+        /// <returns>Newly created object, cast to <typeparamref name="T"/>.</returns>
+        public static T CreateInstance<T>(this IDynamicTypeFactory dynamicTypeFactory, Type type, object[] args)
+        {
+            DebugTools.Assert(typeof(T).IsAssignableFrom(type), "type must be subtype of T");
+            return (T) dynamicTypeFactory.CreateInstance(type, args);
+        }
     }
 
     /// <inheritdoc />
@@ -41,10 +84,20 @@ namespace Robust.Shared.IoC
         /// <inheritdoc />
         public object CreateInstance(Type type)
         {
-            if(type == null)
+            if (type == null)
                 throw new ArgumentNullException(nameof(type));
 
             var instance = Activator.CreateInstance(type);
+            _dependencies.InjectDependencies(instance);
+            return instance;
+        }
+
+        public object CreateInstance(Type type, object[] args)
+        {
+            if (type == null)
+                throw new ArgumentNullException(nameof(type));
+
+            var instance = Activator.CreateInstance(type, args);
             _dependencies.InjectDependencies(instance);
             return instance;
         }

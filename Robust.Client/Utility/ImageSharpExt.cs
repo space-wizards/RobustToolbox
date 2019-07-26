6f26f77ce61648758fd4cbcb88f58738c15a0379
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2019-07-26 12:20:59 +0200

    Add efficient ImageSharp blitting method.

--- Robust.Client/Utility/ImageSharpExt.cs
+++ Robust.Client/Utility/ImageSharpExt.cs
@@ -1,5 +1,7 @@
 using SixLabors.ImageSharp.PixelFormats;
 using Robust.Shared.Maths;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Advanced;
 
 namespace Robust.Client.Utility
 {
@@ -13,6 +15,40 @@ namespace Robust.Client.Utility
         public static Rgba32 ConvertImgSharp(this Color color)
         {
             return new Rgba32(color.R, color.G, color.B, color.A);
+        }
+
+        /// <summary>
+        ///     Blit an image into another, with the specified offset.
+        /// </summary>
+        /// <param name="source">The image to copy data from.</param>
+        /// <param name="sourceRect">The sub section of <see cref="source"/> that will be copied.</param>
+        /// <param name="destinationOffset">
+        ///     The offset into <see cref="destination"/> that data will be copied into.
+        /// </param>
+        /// <param name="destination">The image to copy to.</param>
+        /// <typeparam name="T">The type of pixel stored in the images.</typeparam>
+        public static void Blit<T>(this Image<T> source, UIBox2i sourceRect,
+                Image<T> destination, Vector2i destinationOffset)
+            where T : struct, IPixel<T>
+        {
+            // TODO: Bounds checks.
+
+            var srcSpan = source.GetPixelSpan();
+            var dstSpan = destination.GetPixelSpan();
+
+            var srcWidth = source.Width;
+            var dstWidth = destination.Width;
+
+            var (ox, oy) = destinationOffset;
+
+            for (var x = 0; x < sourceRect.Width; x++)
+            {
+                for (var y = 0; y < sourceRect.Height; y++)
+                {
+                    var pixel = srcSpan[x + sourceRect.Left + srcWidth * (y + sourceRect.Top)];
+                    dstSpan[x + ox + dstWidth * (y + oy)] = pixel;
+                }
+            }
         }
     }
 }

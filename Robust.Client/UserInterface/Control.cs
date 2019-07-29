39a2c9db86f89097a294bd6ec6c94c5f234780da
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2019-07-29 23:04:37 +0200

    Fix parenting a control not correctly setting its layout.

--- Robust.Client/UserInterface/Control.cs
+++ Robust.Client/UserInterface/Control.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -1343,6 +1343,7 @@ namespace Robust.Client.UserInterface
         protected virtual void Parented(Control newParent)
         {
             Restyle();
+            DoUpdateLayout();
         }
 
         /// <summary>

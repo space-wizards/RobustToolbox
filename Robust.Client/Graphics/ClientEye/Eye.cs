96432bd7c3f5ecab88087fd69a30ca8f6ca38a93
Acruid <shatter66@gmail.com>
2019-09-05 16:21:43 -0700

    Removes EyeComponent's reliance on TransformComponent.OnMove. Now is uses an EntitySystem that is ran every frame. Refactored some functions off of Eye, removed the IEyeManager dependence from it.

--- Robust.Client/Graphics/ClientEye/Eye.cs
+++ Robust.Client/Graphics/ClientEye/Eye.cs
@@ -1,92 +1,28 @@
-﻿using System;
-using Robust.Client.Interfaces.Graphics.ClientEye;
-using Robust.Client.Utility;
-using Robust.Shared.IoC;
+﻿using Robust.Client.Interfaces.Graphics.ClientEye;
 using Robust.Shared.Map;
 using Robust.Shared.Maths;
 
 namespace Robust.Client.Graphics.ClientEye
 {
-    public class Eye : IEye, IDisposable
+    /// <inheritdoc />
+    public class Eye : IEye
     {
-        protected IEyeManager eyeManager;
-        private bool disposed = false;
+        //TODO: This is literally a Transformation matrix, might as well add Rotation to it as well.
 
-        public bool Current
-        {
-            get => eyeManager.CurrentEye == this;
-            set
-            {
-                if (Current == value)
-                {
-                    return;
-                }
-
-                if (value)
-                {
-                    eyeManager.CurrentEye = this;
-                }
-                else
-                {
-                    eyeManager.CurrentEye = null;
-                }
-            }
-        }
-
+        /// <inheritdoc />
         public Vector2 Zoom { get; set; } = Vector2.One;
 
-        private MapCoordinates _position;
-
-        public virtual MapCoordinates Position
-        {
-            get => _position;
-            internal set => _position = value;
-        }
-
-        public MapId MapId => _position.MapId;
-
-        public Eye()
-        {
-            eyeManager = IoCManager.Resolve<IEyeManager>();
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            disposed = true;
-
-            if (disposing)
-            {
-                Current = false;
-                eyeManager = null;
-            }
-        }
-
-        public void Dispose()
-        {
-            if (disposed)
-            {
-                return;
-            }
-
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        ~Eye()
-        {
-            Dispose(false);
-        }
-    }
-
-    public static class EyeExtensions
-    {
-        public static Matrix3 GetMatrix(this IEye eye)
+        /// <inheritdoc />
+        public virtual MapCoordinates Position { get; internal set; }
+        
+        /// <inheritdoc />
+        public Matrix3 GetViewMatrix()
         {
             var matrix = Matrix3.Identity;
-            matrix.R0C0 = 1 / eye.Zoom.X;
-            matrix.R1C1 = 1 / eye.Zoom.Y;
-            matrix.R0C2 = -eye.Position.X / eye.Zoom.X;
-            matrix.R1C2 = -eye.Position.Y / eye.Zoom.Y;
+            matrix.R0C0 = 1 / Zoom.X;
+            matrix.R1C1 = 1 / Zoom.Y;
+            matrix.R0C2 = -Position.X / Zoom.X;
+            matrix.R1C2 = -Position.Y / Zoom.Y;
             return matrix;
         }
     }

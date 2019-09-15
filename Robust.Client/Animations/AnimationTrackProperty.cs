88d837fe75281a010e4d8dc27234dc373d328915
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2019-09-15 21:39:59 +0200

    Use better lerp for Angle property animations.

--- Robust.Client/Animations/AnimationTrackProperty.cs
+++ Robust.Client/Animations/AnimationTrackProperty.cs
@@ -93,7 +93,7 @@ namespace Robust.Client.Animations
                 case double d:
                     return FloatMath.Lerp(d, (double) b, t);
                 case Angle angle:
-                    return Angle.Lerp(angle, (Angle) b, t);
+                    return (Angle)FloatMath.Lerp(angle, (Angle) b, t);
                 case Color color:
                     return Color.InterpolateBetween(color, (Color) b, t);
                 case int i:

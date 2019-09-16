3d2228e64ef89ad510bbce6cfb3c2042efc58878
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2019-09-16 20:57:00 +0200

    Add a command to spawn entities client side.

---
+++ Robust.Client/Console/Commands/ClientSpawnCommand.cs
@@ -0,0 +1,30 @@
+using JetBrains.Annotations;
+using Robust.Client.Interfaces.Console;
+using Robust.Client.Player;
+using Robust.Shared.Interfaces.GameObjects;
+using Robust.Shared.IoC;
+
+namespace Robust.Client.Console.Commands
+{
+    [UsedImplicitly]
+    internal sealed class ClientSpawnCommand : IConsoleCommand
+    {
+        public string Command => "cspawn";
+        public string Description => "Spawns a client-side entity with specific type at your feet.";
+        public string Help => "cspawn <entity type>";
+
+        public bool Execute(IDebugConsole console, params string[] args)
+        {
+            var player = IoCManager.Resolve<IPlayerManager>().LocalPlayer;
+            if (player.ControlledEntity == null)
+            {
+                console.AddLine("You don't have an attached entity.");
+                return false;
+            }
+
+            var entityManager = IoCManager.Resolve<IEntityManager>();
+            entityManager.SpawnEntityAt(args[0], player.ControlledEntity.Transform.GridPosition);
+            return false;
+        }
+    }
+}

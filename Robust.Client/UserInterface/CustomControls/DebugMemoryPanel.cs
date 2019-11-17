50251782039c97e12169967a5a0e29d40523b8d9
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2019-11-17 22:18:30 +0100

    Very shitty allocation rate tracker.

--- Robust.Client/UserInterface/CustomControls/DebugMemoryPanel.cs
+++ Robust.Client/UserInterface/CustomControls/DebugMemoryPanel.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Robust.Client.Graphics.Drawing;
 using Robust.Client.UserInterface.Controls;
 using Robust.Shared.Maths;
@@ -9,6 +11,10 @@ namespace Robust.Client.UserInterface.CustomControls
     internal sealed class DebugMemoryPanel : PanelContainer
     {
         private readonly Label _label;
+
+        private readonly long[] _allocDeltas = new long[60];
+        private long _lastAllocated;
+        private int _allocDeltaIndex = 0;
 
         public DebugMemoryPanel()
         {
@@ -47,13 +53,16 @@ namespace Robust.Client.UserInterface.CustomControls
             _label.Text = GetMemoryInfo();
         }
 
-        private static string GetMemoryInfo()
+        private string GetMemoryInfo()
         {
 #if NETCOREAPP
+            var allocated = GC.GetTotalMemory(false);
+            LogAllocSize(allocated);
             var info = GC.GetGCMemoryInfo();
             return $@"Heap Size: {FormatBytes(info.HeapSizeBytes)}
-Total Allocated: {FormatBytes(GC.GetTotalMemory(false))}
-Collections: {GC.CollectionCount(0)} {GC.CollectionCount(1)} {GC.CollectionCount(2)}";
+Total Allocated: {FormatBytes(allocated)}
+Collections: {GC.CollectionCount(0)} {GC.CollectionCount(1)} {GC.CollectionCount(2)}
+Alloc Rate: {FormatBytes(CalculateAllocRate())} / frame";
 #else
             return "Memory information needs .NET Core";
 #endif
@@ -65,5 +74,20 @@ Collections: {GC.CollectionCount(0)} {GC.CollectionCount(1)} {GC.CollectionCount
             return $"{bytes / 1024} KiB";
         }
 #endif
+
+        private void LogAllocSize(long allocated)
+        {
+            var delta = allocated - _lastAllocated;
+            _lastAllocated = allocated;
+
+            // delta is < 0 if the GC ran a collection so it dropped.
+            // In that case, treat it as a dud by writing write -1.
+            _allocDeltas[_allocDeltaIndex++ % _allocDeltas.Length] = Math.Max(-1, delta);
+        }
+
+        private long CalculateAllocRate()
+        {
+            return (long) _allocDeltas.Where(x => x >= 0).Average();
+        }
     }
 }

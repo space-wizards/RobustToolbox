c8d3d00478966cc1447fe54fe27183d9e2fe73f4
Damian <damian@autistici.org>
2019-11-20 17:06:29 +0100

    Public YamlMappingFix

---
+++ Robust.Shared/Serialization/YamlMappingFix.cs
@@ -0,0 +1,28 @@
+using YamlDotNet.Core;
+using YamlDotNet.Core.Events;
+
+namespace Robust.Shared.Serialization
+{
+    /// <summary>
+    /// TODO: This fix exists because of https://github.com/aaubry/YamlDotNet/issues/409.
+    /// Credit: https://stackoverflow.com/a/56452440
+    /// </summary>
+    public class YamlMappingFix : IEmitter
+    {
+        private readonly IEmitter _next;
+
+        public YamlMappingFix(IEmitter next)
+        {
+            _next = next;
+        }
+
+        public void Emit(ParsingEvent @event)
+        {
+            if (@event is MappingStart mapping)
+            {
+                @event = new MappingStart(mapping.Anchor, mapping.Tag, false, mapping.Style, mapping.Start, mapping.End);
+            }
+            _next.Emit(@event);
+        }
+    }
+}

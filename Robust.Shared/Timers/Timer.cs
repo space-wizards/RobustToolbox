dc775523b596f0575898ba1cecb4ed05ae5d19ac
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2019-11-21 13:29:13 -0800

    Update doc comments

--- Robust.Shared/Timers/Timer.cs
+++ Robust.Shared/Timers/Timer.cs
@@ -128,12 +128,11 @@ namespace Robust.Shared.Timers
         }
 
         /// <summary>
-        ///     Schedule an action to be fired after a certain delay.
-        ///     The action will be resumed on the main game logic thread.
+        ///     Schedule an action that repeatedly fires after a delay specified in milliseconds.
         /// </summary>
-        /// <param name="milliseconds">The length of time, in milliseconds, to wait before firing the action.</param>
+        /// <param name="milliseconds">The length of time, in milliseconds, to delay before firing the repeated action.</param>
         /// <param name="onFired">The action to fire.</param>
-        /// <param name="cancellationToken"></param>
+        /// <param name="cancellationToken">The CancellationToken for stopping the Timer.</param>
         public static void SpawnRepeating(int milliseconds, Action onFired, CancellationToken cancellationToken)
         {
             var timer = new Timer(milliseconds, true, onFired);
@@ -141,12 +140,11 @@ namespace Robust.Shared.Timers
         }
 
         /// <summary>
-        ///     Schedule an action to be fired after a certain delay.
-        ///     The action will be resumed on the main game logic thread.
+        ///     Schedule an action that repeatedly fires after a delay.
         /// </summary>
-        /// <param name="duration">The length of time, to wait before firing the action.</param>
+        /// <param name="duration">The length of time to delay before firing the repeated action.</param>
         /// <param name="onFired">The action to fire.</param>
-        /// <param name="cancellationToken"></param>
+        /// <param name="cancellationToken">The CancellationToken for stopping the Timer.</param>
         public static void SpawnRepeating(TimeSpan duration, Action onFired, CancellationToken cancellationToken)
         {
             SpawnRepeating((int)duration.TotalMilliseconds, onFired, cancellationToken);

134141feaf81b65fdbe52d8433f32e62e582d2c2
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2019-11-21 14:21:14 -0800

    Implement SpawnRepeating (#898)
    * Implement SpawnRepeating
    
    Allows repeating timer events to be used.
    
    * Update doc comments

--- Robust.Shared/Timers/Timer.cs
+++ Robust.Shared/Timers/Timer.cs
@@ -126,5 +126,28 @@ namespace Robust.Shared.Timers
         {
             Spawn((int)duration.TotalMilliseconds, onFired, cancellationToken);
         }
+
+        /// <summary>
+        ///     Schedule an action that repeatedly fires after a delay specified in milliseconds.
+        /// </summary>
+        /// <param name="milliseconds">The length of time, in milliseconds, to delay before firing the repeated action.</param>
+        /// <param name="onFired">The action to fire.</param>
+        /// <param name="cancellationToken">The CancellationToken for stopping the Timer.</param>
+        public static void SpawnRepeating(int milliseconds, Action onFired, CancellationToken cancellationToken)
+        {
+            var timer = new Timer(milliseconds, true, onFired);
+            IoCManager.Resolve<ITimerManager>().AddTimer(timer, cancellationToken);
+        }
+
+        /// <summary>
+        ///     Schedule an action that repeatedly fires after a delay.
+        /// </summary>
+        /// <param name="duration">The length of time to delay before firing the repeated action.</param>
+        /// <param name="onFired">The action to fire.</param>
+        /// <param name="cancellationToken">The CancellationToken for stopping the Timer.</param>
+        public static void SpawnRepeating(TimeSpan duration, Action onFired, CancellationToken cancellationToken)
+        {
+            SpawnRepeating((int)duration.TotalMilliseconds, onFired, cancellationToken);
+        }
     }
 }

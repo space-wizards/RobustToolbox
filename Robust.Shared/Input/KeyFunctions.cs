3326297b383eb9e9a3348cff962f92308d138b47
moneyl <8206401+Moneyl@users.noreply.github.com>
2020-01-20 21:33:36 -0500

    Add delete key behavior to LineEdit
    Pressing delete will now delete the character in front of your cursor like usual.

--- Robust.Shared/Input/KeyFunctions.cs
+++ Robust.Shared/Input/KeyFunctions.cs
@@ -42,6 +42,7 @@ namespace Robust.Shared.Input
         public static readonly BoundKeyFunction TextHistoryNext = "TextHistoryNext";
         public static readonly BoundKeyFunction TextReleaseFocus = "TextReleaseFocus";
         public static readonly BoundKeyFunction TextScrollToBottom = "TextScrollToBottom";
+        public static readonly BoundKeyFunction TextDelete = "TextDelete";
     }
 
     [Serializable, NetSerializable]

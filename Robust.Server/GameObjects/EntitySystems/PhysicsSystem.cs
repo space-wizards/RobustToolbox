5c2b244d2d90948aa8219ba56a65c8683e7d4077
Tyler Young <tyler.young@impromptu.ninja>
2020-02-07 11:47:28 -0500

    Fix containers containing containers being destroyed/deleted and deleting their contents causing unexpected attempts to access deleted child entities

--- Robust.Server/GameObjects/EntitySystems/PhysicsSystem.cs
+++ Robust.Server/GameObjects/EntitySystems/PhysicsSystem.cs
@@ -37,8 +37,14 @@ namespace Robust.Server.GameObjects.EntitySystems
 
         private void SimulateWorld(float frameTime, IEnumerable<IEntity> entities)
         {
+            // simulation can introduce deleted entities into the query results
             foreach (var entity in entities)
             {
+                if (entity.Deleted)
+                {
+                    continue;
+                }
+
                 if (_pauseManager.IsEntityPaused(entity))
                 {
                     continue;
@@ -49,6 +55,11 @@ namespace Robust.Server.GameObjects.EntitySystems
 
             foreach (var entity in entities)
             {
+                if (entity.Deleted)
+                {
+                    continue;
+                }
+
                 DoMovement(entity, frameTime);
             }
         }

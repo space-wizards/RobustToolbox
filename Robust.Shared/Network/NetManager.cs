56bda966ba045a608d334b39e2c0cbaa981979d7
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2020-02-09 01:26:24 -0800

    Fix client reconnect after getting disconnected (#965)
    The _clientConnectionState wasn't reset to NotConnecting when disconnected by the server (compared to the client disconnecting from the server).

--- Robust.Shared/Network/NetManager.cs
+++ Robust.Shared/Network/NetManager.cs
@@ -559,7 +559,14 @@ namespace Robust.Shared.Network
             _channels.Remove(connection);
 
             if (IsClient)
+            {
+                connection.Peer.Shutdown(reason);
+                _toCleanNetPeers.Add(connection.Peer);
                 _strings.Reset();
+
+                _cancelConnectTokenSource?.Cancel();
+                _clientConnectionState = ClientConnectionState.NotConnecting;
+            }
         }
 
         /// <inheritdoc />

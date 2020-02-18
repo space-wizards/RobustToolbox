5ef80a9b14b4a6c1a6b9434a3da8ba28665fba7c
GlassEclipse <tsymall5@gmail.com>
2020-02-17 18:49:31 -0600

    Added StringHelper

---
+++ Robust.Shared/Utility/StringHelpers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Robust.Shared.Utility {
+    public static class StringHelpers {
+
+        /// <summary>
+        ///    Reads data from YAML
+        /// </summary>
+        public static string CapitalizeAllWords(string input) {
+            char[] chars = input.ToCharArray();
+            if (chars.Length > 0) {
+                chars[0] = Char.ToUpper(chars[0]);
+                for (int i = 1; i < input.Length; i++) {
+                    if (chars[i-1] == ' ')
+                        chars[i] = Char.ToUpper(chars[i]);
+                }
+            }
+
+            return new string(chars);
+        }
+    }
+}

a8f615d64b04d821186b646c5232953ba4498482
Tyler Young <tyler.young@impromptu.ninja>
2020-02-26 01:31:46 -0500

    Fix lockers making items uninteractable (#1004)
    * remove setter for MapPosition
    
    change DetachParent to set WorldPosition after re-parenting to correct Map entity or MapGrid entity
    
    * workaround until default grid not needed due to items can't be picked up if map ent parent

--- Robust.Shared/GameObjects/Components/Transform/TransformComponent.cs
+++ Robust.Shared/GameObjects/Components/Transform/TransformComponent.cs
@@ -297,12 +297,6 @@ namespace Robust.Shared.GameObjects.Components.Transform
         public MapCoordinates MapPosition
         {
             get => new MapCoordinates(WorldPosition, MapID);
-            set
-            {
-                AttachParent(_mapManager.GetMapEntity(value.MapId));
-
-                WorldPosition = value.Position;
-            }
         }
 
         [ViewVariables(VVAccess.ReadWrite)]
@@ -389,7 +383,14 @@ namespace Robust.Shared.GameObjects.Components.Transform
             }
 
             var mapPos = MapPosition;
-            var newMapEntity = _mapManager.GetMapEntity(mapPos.MapId);
+            var mapGrid = _mapManager.FindGridAt(mapPos.MapId, mapPos.Position);
+            var newMapEntity = _entityManager.GetEntity(mapGrid.GridEntityId);
+            /* // TODO: reinstate this once entities can be picked up from the map
+            var newMapEntity = mapGrid.IsDefaultGrid
+                ? _mapManager.GetMapEntity(mapPos.MapId)
+                : _entityManager.GetEntity(mapGrid.GridEntityId);
+            */
+
 
             // this would be a no-op
             var oldParentEnt = oldParent.Owner;
@@ -400,7 +401,7 @@ namespace Robust.Shared.GameObjects.Components.Transform
 
             AttachParent(newMapEntity);
 
-            MapPosition = mapPos;
+            WorldPosition = mapPos.Position;
 
             Dirty();
         }

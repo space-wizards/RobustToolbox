f7e4bf46fce2b6b0000406d0f85064dc247329d6
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2020-02-29 15:07:49 +0100

    InputSystem: don't send state changes to server if there wasn't an actual change.
    This fixes the flood of up bind spam in the console when typing into LineEdit and such.

--- Robust.Client/GameObjects/EntitySystems/InputSystem.cs
+++ Robust.Client/GameObjects/EntitySystems/InputSystem.cs
@@ -60,6 +60,11 @@ namespace Robust.Client.GameObjects.EntitySystems
             #endif
 
             // set state, state change is updated regardless if it is locally bound
+            if (_cmdStates.GetState(function) == message.State)
+            {
+                return;
+            }
+
             _cmdStates.SetState(function, message.State);
 
             // handle local binds before sending off

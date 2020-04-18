ea9a1e0f7e3066ec14e115997596617ffa1ab8ed
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2020-04-18 17:23:38 +0200

    Add net.verbose config option for Lidgren verbose logging.

--- Robust.Shared/Network/NetManager.cs
+++ Robust.Shared/Network/NetManager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -170,6 +170,7 @@ namespace Robust.Shared.Network
 
             _config.RegisterCVar("net.sendbuffersize", 131071, CVar.ARCHIVE);
             _config.RegisterCVar("net.receivebuffersize", 131071, CVar.ARCHIVE);
+            _config.RegisterCVar("net.verbose", false, CVar.ARCHIVE, NetVerboseChanged);
 
             if (!isServer)
             {
@@ -194,6 +195,14 @@ namespace Robust.Shared.Network
             _strings.Initialize(this, () => { OnConnected(ServerChannel); });
 
             _initialized = true;
+        }
+
+        private void NetVerboseChanged(bool on)
+        {
+            foreach (var peer in _netPeers)
+            {
+                peer.Configuration.SetMessageTypeEnabled(NetIncomingMessageType.VerboseDebugMessage, on);
+            }
         }
 
         public void StartServer()
@@ -354,6 +363,9 @@ namespace Robust.Shared.Network
             netConfig.SendBufferSize = _config.GetCVar<int>("net.sendbuffersize");
             netConfig.ReceiveBufferSize = _config.GetCVar<int>("net.receivebuffersize");
             netConfig.MaximumHandshakeAttempts = 1;
+
+            var verbose = _config.GetCVar<bool>("net.verbose");
+            netConfig.SetMessageTypeEnabled(NetIncomingMessageType.VerboseDebugMessage, verbose);
 
             if (IsServer)
             {

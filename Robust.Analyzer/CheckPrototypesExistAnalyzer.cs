7e904dfa6e16e2d95276010b254b3ef2652655ee
ComicIronic <comicironic@gmail.com>
2020-05-02 16:03:21 -0700

    Add Roslyn analyzer for checking prototype names
    Based on space-wizards/RobustToolbox#899, this makes a first attempt at
    introducing some static analysis to catch mistakes in the prototype name
    passed to `SpawnEntity` invocations.

---
+++ Robust.Analyzer/CheckPrototypesExistAnalyzer.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+using Robust.Shared.GameObjects;
+using Robust.Shared.IoC;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Utility;
+
+namespace Robust.Analyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class CheckPrototypesExistAnalyzer : DiagnosticAnalyzer
+    {
+        // Robust Toolbox Error #1
+        public const string DiagnosticId = "RTE001";
+
+        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+        // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Usage";
+
+        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+        
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze|GeneratedCodeAnalysisFlags.ReportDiagnostics);
+
+            // So long as the compilation start action runs exactly once, then this is safe
+            // TODO: should double-check that, the API docs don't seem to guarantee it
+            context.EnableConcurrentExecution();
+
+            context.RegisterCompilationStartAction(compilationContext =>
+            {
+                var entityManagerInterfaces = compilationContext.Compilation.References
+                    .Select(compilationContext.Compilation.GetAssemblyOrModuleSymbol)
+                    .OfType<IAssemblySymbol>()
+                    .Select(assemblySymbol => assemblySymbol.GetTypeByMetadataName("Robust.Shared.Interfaces.GameObjects.IEntityManager"))
+                    .Where(t => t != null);
+                INamedTypeSymbol entityManagerInterface = entityManagerInterfaces.FirstOrDefault();
+                if (entityManagerInterface == null)
+                {
+                    throw new Exception("Could not load the IEntityManager interface.");
+                }
+
+                // Load the prototypes fed in to AdditionalFiles at compile-time
+                // TODO: think about if this is the best design choice
+                var prototypeManager = new PrototypeManager();
+                foreach (var file in compilationContext.Options.AdditionalFiles)
+                {
+                    if (Path.GetExtension(file.Path) != ".yaml")
+                    {
+                        continue;
+                    }
+                    prototypeManager.LoadFromStream(new StringReader(file.GetText(compilationContext.CancellationToken).ToString()));
+                }
+                prototypeManager.Resync();
+
+                compilationContext.RegisterOperationAction(operationContext => AnalyzeSpawnEntity(operationContext, entityManagerInterface, prototypeManager), new OperationKind[] { OperationKind.Invocation });
+            });
+        }
+
+        private static bool IsPrototype(string prototypeName, IPrototypeManager prototypeManager)
+        {
+            try
+            {
+                return prototypeManager.HasIndex<EntityPrototype>(prototypeName);
+            }
+            catch (UnknownPrototypeException e)
+            {
+                throw new Exception(String.Format("Exception prevented querying of entity prototypes for existence of \"{0}\".", prototypeName), e);
+            }
+        }
+
+        private static void AnalyzeSpawnEntity(OperationAnalysisContext context, INamedTypeSymbol entityManagerInterface, IPrototypeManager prototypeManager)
+        {
+            // Safe cast, we only target method calls
+            var invocation = (IInvocationOperation)context.Operation;
+            SemanticModel model = invocation.SemanticModel;
+
+            // Ignore everything that isn't this method call
+            if (invocation.TargetMethod.Name != "SpawnEntity")
+            {
+                return;
+            }
+
+            // Try and figure out if it's being called on an entity manager
+            if (!(invocation.TargetMethod.ReceiverType is INamedTypeSymbol receiverType))
+            {
+                return;
+            }
+
+            if (!receiverType.AllInterfaces.Contains(entityManagerInterface))
+            {
+                return;
+            }
+
+            // Fish out the first argument - if it doesn't exist, ignore the call
+            var firstArgument = invocation.Arguments.FirstOrDefault();
+            if (firstArgument == null)
+            {
+                return;
+            }
+
+            var argLiteral = firstArgument.ConstantValue;
+            if (!argLiteral.HasValue || !(argLiteral.Value is string prototypeName))
+            {
+                return;
+            }
+
+            // Now we finally have a string which should refer to a prototype - so
+            // check it is one
+            if (!IsPrototype(prototypeName, prototypeManager))
+            {
+                context.ReportDiagnostic(Diagnostic.Create(Rule, invocation.Syntax.GetLocation(), prototypeName));
+            }
+        }
+    }
+}
---
+++ Robust.Analyzer/Robust.Analyzer.Test/CheckPrototypeExistsAnalyzerTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TestHelper;
+
+using Robust.Analyzer;
+
+namespace Robust.Analyzer.Test
+{
+    [TestClass]
+    public class UnitTest : DiagnosticVerifier
+    {
+
+        //No diagnostics expected to show up
+        [TestMethod]
+        public void TestEmpty()
+        {
+            var test = @"";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        //Diagnostic should trigger
+        [TestMethod]
+        public void TestMissingPrototype()
+        {
+            var test = @"
+    using System;
+
+    using Robust.Shared.Interfaces.GameObjects;
+    using Robust.Shared.Map;
+
+    namespace SomeGameCode
+    {
+        class SomeGameCodeClass
+        {
+
+            public static void DoSomeGameStuff()
+            {
+                IEntityManager entityManager = null;
+                _ = entityManager.SpawnEntity(""some_entity"", MapCoordinates.Nullspace);
+            }
+        }
+    }";
+            var expected = new DiagnosticResult
+            {
+                Id = "RTE001",
+                Message = "Could not find entity prototype with name '\"some_entity\"'.",
+                Severity = DiagnosticSeverity.Error,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 14, 17)
+                        }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new CheckPrototypesExistAnalyzer();
+        }
+    }
+}

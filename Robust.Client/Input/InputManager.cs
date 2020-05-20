a9d272da620e34fa42976aa5b281c2aa9b6c90c3
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2020-05-20 11:57:28 +0200

    More KeyBinding improvements: 1. Fixed handling of repeats so that they don't fire stuff incorrectly anymore. 2. Add Priority system to keybinds because apparently C# Sort isn't stable like I thought.

--- Robust.Client/Input/InputManager.cs
+++ Robust.Client/Input/InputManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -162,7 +163,7 @@ namespace Robust.Client.Input
 
             foreach (var binding in bindsDown)
             {
-                if (DownBind(binding, uiOnly))
+                if (DownBind(binding, uiOnly, args.IsRepeat))
                 {
                     break;
                 }
@@ -193,17 +194,23 @@ namespace Robust.Client.Input
             _keysPressed[(int) args.Key] = false;
         }
 
-        private bool DownBind(KeyBinding binding, bool uiOnly)
+        private bool DownBind(KeyBinding binding, bool uiOnly, bool isRepeat)
         {
             if (binding.State == BoundKeyState.Down)
             {
+                if (isRepeat)
+                {
+                    if (binding.CanRepeat)
+                    {
+                        return SetBindState(binding, BoundKeyState.Down, uiOnly);
+                    }
+
+                    return true;
+                }
+
                 if (binding.BindingType == KeyBindingType.Toggle)
                 {
                     return SetBindState(binding, BoundKeyState.Up);
-                }
-                else if (binding.CanRepeat)
-                {
-                    return SetBindState(binding, BoundKeyState.Down, uiOnly);
                 }
             }
             else
@@ -348,9 +355,16 @@ namespace Robust.Client.Input
                     mod3 = mod3Name.AsEnum<Key>();
                 }
 
+                var priority = 0;
+                if (keyMapping.TryGetNode("priority", out var priorityValue))
+                {
+                    priority = priorityValue.AsInt();
+                }
+
                 var type = keyMapping.GetNode("type").AsEnum<KeyBindingType>();
 
-                var binding = new KeyBinding(this, function, type, key, canFocus, canRepeat, mod1, mod2, mod3);
+                var binding = new KeyBinding(this, function, type, key, canFocus, canRepeat, priority, mod1, mod2,
+                    mod3);
                 RegisterBinding(binding);
             }
         }
@@ -360,18 +374,22 @@ namespace Robust.Client.Input
             Key baseKey, Key? mod1, Key? mod2, Key? mod3)
         {
             var binding = new KeyBinding(this, function, bindingType, baseKey, false, false,
-                mod1 ?? Key.Unknown, mod2 ?? Key.Unknown, mod3 ?? Key.Unknown);
+                0, mod1 ?? Key.Unknown, mod2 ?? Key.Unknown, mod3 ?? Key.Unknown);
 
             RegisterBinding(binding);
         }
 
         private void RegisterBinding(KeyBinding binding)
         {
-            _bindings.Add(binding);
+            // we sort larger combos first so they take priority over smaller (single key) combos,
+            // so they get processed first in KeyDown and such.
+            var pos = _bindings.BinarySearch(binding, KeyBinding.ProcessPriorityComparer);
+            if (pos < 0)
+            {
+                pos = ~pos;
+            }
 
-            // reversed a,b for descending order
-            // we sort larger combos first so they take priority over smaller (single key) combos
-            _bindings.Sort((a, b) => b.PackedKeyCombo.Packed.CompareTo(a.PackedKeyCombo.Packed));
+            _bindings.Insert(pos, binding);
         }
 
         /// <inheritdoc />
@@ -409,6 +427,7 @@ namespace Robust.Client.Input
             _commands[function] = cmdHandler;
         }
 
+        [DebuggerDisplay("KeyBinding {" + nameof(Function) + "}")]
         private class KeyBinding : IKeyBinding
         {
             private readonly InputManager _inputManager;
@@ -428,10 +447,12 @@ namespace Robust.Client.Input
             /// </summary>
             public bool CanRepeat { get; internal set; }
 
+            public int Priority { get; internal set; }
+
             public KeyBinding(InputManager inputManager, BoundKeyFunction function,
                 KeyBindingType bindingType,
                 Key baseKey,
-                bool canFocus, bool canRepeat, Key mod1 = Key.Unknown,
+                bool canFocus, bool canRepeat, int priority, Key mod1 = Key.Unknown,
                 Key mod2 = Key.Unknown,
                 Key mod3 = Key.Unknown)
             {
@@ -439,6 +460,7 @@ namespace Robust.Client.Input
                 BindingType = bindingType;
                 CanFocus = canFocus;
                 CanRepeat = canRepeat;
+                Priority = priority;
                 _inputManager = inputManager;
 
                 PackedKeyCombo = new PackedKeyCombo(baseKey, mod1, mod2, mod3);
@@ -469,6 +491,21 @@ namespace Robust.Client.Input
 
                 return sb.ToString();
             }
+
+            private sealed class ProcessPriorityRelationalComparer : IComparer<KeyBinding>
+            {
+                public int Compare(KeyBinding x, KeyBinding y)
+                {
+                    if (ReferenceEquals(x, y)) return 0;
+                    if (ReferenceEquals(null, y)) return 1;
+                    if (ReferenceEquals(null, x)) return -1;
+                    var cmp = y.PackedKeyCombo.Packed.CompareTo(x.PackedKeyCombo.Packed);
+                    // Higher priority is first in the list so gets to go first.
+                    return cmp != 0 ? cmp : y.Priority.CompareTo(x.Priority);
+                }
+            }
+
+            public static IComparer<KeyBinding> ProcessPriorityComparer { get; } = new ProcessPriorityRelationalComparer();
         }
 
         [StructLayout(LayoutKind.Explicit)]

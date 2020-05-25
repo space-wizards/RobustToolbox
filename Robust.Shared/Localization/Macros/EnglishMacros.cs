b4eac8526a33490feff59967abb092df1aa11618
Hugal31 <hugo.laloge@gmail.com>
2020-05-25 20:47:51 +0200

    Implement text macros (#1058)

---
+++ Robust.Shared/Localization/Macros/EnglishMacros.cs
@@ -0,0 +1,77 @@
+﻿namespace Robust.Shared.Localization.Macros
+{
+    [RegisterTextMacro("they", "en")]
+    public class They : ITextMacro
+    {
+        public string Format(object argument)
+        {
+            return IGenderable.GetGenderOrEpicene(argument) switch
+            {
+                Gender.Female => "she",
+                Gender.Male => "he",
+                Gender.Neuter => "it",
+                _ => "they",
+            };
+        }
+    }
+
+    [RegisterTextMacro("their", "en")]
+    public class Their : ITextMacro
+    {
+        public string Format(object argument)
+        {
+            return IGenderable.GetGenderOrEpicene(argument) switch
+            {
+                Gender.Female => "her",
+                Gender.Male => "his",
+                Gender.Neuter => "its",
+                _ => "their",
+            };
+        }
+    }
+
+    [RegisterTextMacro("theirs", "en")]
+    public class Theirs : ITextMacro
+    {
+        public string Format(object argument)
+        {
+            return IGenderable.GetGenderOrEpicene(argument) switch
+            {
+                Gender.Female => "hers",
+                Gender.Male => "his",
+                Gender.Neuter => "its",
+                _ => "theirs",
+            };
+        }
+    }
+
+    [RegisterTextMacro("them", "en")]
+    public class Them : ITextMacro
+    {
+        public string Format(object argument)
+        {
+            return IGenderable.GetGenderOrEpicene(argument) switch
+            {
+                Gender.Female => "her",
+                Gender.Male => "him",
+                Gender.Neuter => "it",
+                _ => "them",
+            };
+        }
+    }
+
+    [RegisterTextMacro("themself", "en")]
+    public class Themself : ITextMacro
+    {
+        public string Format(object argument)
+        {
+            return IGenderable.GetGenderOrEpicene(argument) switch
+            {
+                Gender.Female => "herself",
+                Gender.Male => "himself",
+                Gender.Neuter => "itself",
+                _ => "themself",
+            };
+        }
+    }
+}

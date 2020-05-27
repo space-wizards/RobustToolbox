7c5c952d5d3b1c5e8c903ac0f180e9912d327285
Víctor Aguilera Puerto <zddm@outlook.es>
2020-05-27 11:52:15 +0200

    Add distance to AudioSystem

--- Robust.Server/GameObjects/EntitySystems/AudioSystem.cs
+++ Robust.Server/GameObjects/EntitySystems/AudioSystem.cs
@@ -1,26 +1,43 @@
-﻿using Robust.Shared.Audio;
+﻿using System;
+using Robust.Server.Interfaces.Player;
+using Robust.Shared.Audio;
 using Robust.Shared.GameObjects;
 using Robust.Shared.GameObjects.Systems;
 using Robust.Shared.Interfaces.GameObjects;
+using Robust.Shared.IoC;
+using Robust.Shared.Log;
 using Robust.Shared.Map;
 
 namespace Robust.Server.GameObjects.EntitySystems
 {
     public class AudioSystem : EntitySystem
     {
+        [Dependency] private IPlayerManager _playerManager;
+
+
+        public const int AudioDistanceRange = 20;
+
         /// <summary>
         ///     Play an audio file globally, without position.
         /// </summary>
         /// <param name="filename">The resource path to the OGG Vorbis file to play.</param>
         /// <param name="audioParams"></param>
-        public void Play(string filename, AudioParams? audioParams = null)
+        /// <param name="predicate">The predicate that will be used to send the audio to players, or null to send to everyone.</param>
+        public void Play(string filename, AudioParams? audioParams = null, Func<IPlayerSession, bool> predicate = null)
         {
             var msg = new PlayAudioGlobalMessage
             {
                 FileName = filename,
                 AudioParams = audioParams ?? AudioParams.Default
             };
-            RaiseNetworkEvent(msg);
+
+            if(predicate == null)
+                RaiseNetworkEvent(msg);
+            else
+                foreach(var player in _playerManager.GetPlayersBy(predicate))
+                {
+                    RaiseNetworkEvent(msg, player.ConnectedClient);
+                }
         }
 
         /// <summary>
@@ -29,7 +46,8 @@ namespace Robust.Server.GameObjects.EntitySystems
         /// <param name="filename">The resource path to the OGG Vorbis file to play.</param>
         /// <param name="entity">The entity "emitting" the audio.</param>
         /// <param name="audioParams"></param>
-        public void Play(string filename, IEntity entity, AudioParams? audioParams = null)
+        /// <param name="range">The max range at which the audio will be heard. Less than or equal to 0 to send to every player.</param>
+        public void Play(string filename, IEntity entity, AudioParams? audioParams = null, int range = AudioDistanceRange)
         {
             var msg = new PlayAudioEntityMessage
             {
@@ -37,7 +55,14 @@ namespace Robust.Server.GameObjects.EntitySystems
                 EntityUid = entity.Uid,
                 AudioParams = audioParams ?? AudioParams.Default
             };
-            RaiseNetworkEvent(msg);
+
+            if (range <= 0)
+                RaiseNetworkEvent(msg);
+            else
+                foreach(var player in _playerManager.GetPlayersInRange(entity.Transform.GridPosition, range))
+                {
+                    RaiseNetworkEvent(msg, player.ConnectedClient);
+                }
         }
 
         /// <summary>
@@ -46,7 +71,8 @@ namespace Robust.Server.GameObjects.EntitySystems
         /// <param name="filename">The resource path to the OGG Vorbis file to play.</param>
         /// <param name="coordinates">The coordinates at which to play the audio.</param>
         /// <param name="audioParams"></param>
-        public void Play(string filename, GridCoordinates coordinates, AudioParams? audioParams = null)
+        /// <param name="range">The max range at which the audio will be heard. Less than or equal to 0 to send to every player.</param>
+        public void Play(string filename, GridCoordinates coordinates, AudioParams? audioParams = null, int range = AudioDistanceRange)
         {
             var msg = new PlayAudioPositionalMessage
             {
@@ -54,7 +80,14 @@ namespace Robust.Server.GameObjects.EntitySystems
                 Coordinates = coordinates,
                 AudioParams = audioParams ?? AudioParams.Default
             };
-            RaiseNetworkEvent(msg);
+
+            if (range <= 0)
+                RaiseNetworkEvent(msg);
+            else
+                foreach(var player in _playerManager.GetPlayersInRange(coordinates, range))
+                {
+                    RaiseNetworkEvent(msg, player.ConnectedClient);
+                }
         }
     }
 }

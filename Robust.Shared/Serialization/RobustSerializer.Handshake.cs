2babf606236aa0df07637922fbc3e71bef1c1eee
Tyler Young <tyler.young@impromptu.ninja>
2020-06-11 09:31:54 -0400

    more docs

--- Robust.Shared/Serialization/RobustSerializer.Handshake.cs
+++ Robust.Shared/Serialization/RobustSerializer.Handshake.cs
@@ -8,9 +8,22 @@ namespace Robust.Shared.Serialization
 
     public partial class RobustSerializer
     {
+        /// <summary>
+        /// Initiates any sequence of handshake extensions that
+        /// need to occur before the serializer is initialized
+        /// for a given client.
+        /// </summary>
+        /// <param name="channel"></param>
+        /// <returns></returns>
         public Task Handshake(INetChannel channel)
             => MappedStringSerializer.Handshake(channel);
 
+        /// <summary>
+        /// An event that occurs once all handshake extensions have
+        /// completed for the client.
+        ///
+        /// Note: This should not occur on the server.
+        /// </summary>
         public event Action ClientHandshakeComplete
         {
             add => MappedStringSerializer.ClientHandshakeComplete += value;

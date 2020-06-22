f5878724d78abeb8b9617cf3c3fa263dd32fa13d
Tyler Young <tyler.young@impromptu.ninja>
2020-06-22 09:44:07 -0400

    add some doc comments to unique index

--- Robust.Shared/Utility/UniqueIndex.cs
+++ Robust.Shared/Utility/UniqueIndex.cs
@@ -8,15 +8,30 @@ using JetBrains.Annotations;
 
 namespace Robust.Shared.Utility
 {
-
+    /// <summary>
+    /// An immutable dictionary of mutable sets for use as an index of unique values related to another collection.
+    /// Imitates the behavior of a read-focused index in a RDBMS.
+    /// Use when the index's keys don't change that rapidly or when fast lookup is preferred over creation time.
+    /// </summary>
+    /// <typeparam name="TKey">The type of key.</typeparam>
+    /// <typeparam name="TValue">The type of value.</typeparam>
     [PublicAPI]
     public struct UniqueIndex<TKey, TValue> : IEnumerable<KeyValuePair<TKey, ISet<TValue>>>
     {
 
         private ImmutableDictionary<TKey, ISet<TValue>> _index;
 
+        /// <summary>
+        /// The count of keys (and thus sets) in this index.
+        /// </summary>
         public int KeyCount => _index.Count;
 
+        /// <summary>
+        /// Adds a value.
+        /// </summary>
+        /// <param name="key">The associated key.</param>
+        /// <param name="value">A value to be added.</param>
+        /// <returns><c>true</c> upon success, otherwise <c>false</c>.</returns>
         public bool Add(TKey key, TValue value)
         {
             _index ??= ImmutableDictionary<TKey, ISet<TValue>>.Empty;
@@ -30,6 +45,12 @@ namespace Robust.Shared.Utility
             return true;
         }
 
+        /// <summary>
+        /// Adds a collection of values to a set.
+        /// </summary>
+        /// <param name="key">The associated key.</param>
+        /// <param name="values">A collection of values.</param>
+        /// <returns>The count of values that were added to the set.</returns>
         public int AddRange(TKey key, IEnumerable<TValue> values)
         {
             _index ??= ImmutableDictionary<TKey, ISet<TValue>>.Empty;
@@ -48,6 +69,11 @@ namespace Robust.Shared.Utility
             return set.Count;
         }
 
+        /// <summary>
+        /// Removes a set.
+        /// </summary>
+        /// <param name="key">The associated key.</param>
+        /// <returns><c>true</c> upon success, otherwise <c>false</c>.</returns>
         public bool Remove(TKey key)
         {
             if (_index == null)
@@ -67,6 +93,12 @@ namespace Robust.Shared.Utility
             return true;
         }
 
+        /// <summary>
+        /// Removes a value.
+        /// </summary>
+        /// <param name="key">The associated key.</param>
+        /// <param name="value">A value to be removed.</param>
+        /// <returns><c>true</c> upon success, otherwise <c>false</c>.</returns>
         public bool Remove(TKey key, TValue value)
         {
             // ReSharper disable once InvertIf
@@ -80,6 +112,12 @@ namespace Robust.Shared.Utility
                 && set.Remove(value);
         }
 
+        /// <summary>
+        /// Removes a collection of values from a set.
+        /// </summary>
+        /// <param name="key">The associated key.</param>
+        /// <param name="values">A collection of values.</param>
+        /// <returns>The count of values that were removed from the set.</returns>
         public int RemoveRange(TKey key, IEnumerable<TValue> values)
         {
             if (_index == null)
@@ -100,6 +138,13 @@ namespace Robust.Shared.Utility
             return c - set.Count;
         }
 
+        /// <summary>
+        /// Replaces a old value with a new value.
+        /// </summary>
+        /// <param name="key">The associated key.</param>
+        /// <param name="oldValue">A value to be replaced.</param>
+        /// <param name="newValue">A value to replace with.</param>
+        /// <returns><c>true</c> upon success, otherwise <c>false</c>.</returns>
         public bool Replace(TKey key, TValue oldValue, TValue newValue)
         {
             if (_index == null)
@@ -117,6 +162,10 @@ namespace Robust.Shared.Utility
                 && set.Add(newValue);
         }
 
+        /// <summary>
+        /// Creates ensures an empty mutable set for a given key.
+        /// </summary>
+        /// <param name="key">A given key.</param>
         public void Touch(TKey key)
         {
             _index ??= ImmutableDictionary<TKey, ISet<TValue>>.Empty;
@@ -126,10 +175,18 @@ namespace Robust.Shared.Utility
             _index = _index.Add(key, new HashSet<TValue>());
         }
 
+        /// <summary>
+        /// Initializes the index from a collection of keys.
+        /// </summary>
+        /// <param name="keys">A collection of keys.</param>
         /// <exception cref="InvalidOperationException">Already initialized.</exception>
         public void Initialize(IEnumerable<TKey> keys)
             => Initialize(keys.Select(k => new KeyValuePair<TKey, ISet<TValue>>(k, new HashSet<TValue>())));
 
+        /// <summary>
+        /// Initializes the index from an equivalent collection.
+        /// </summary>
+        /// <param name="index">An equivalent collection.</param>
         /// <exception cref="InvalidOperationException">Already initialized.</exception>
         public void Initialize(IEnumerable<KeyValuePair<TKey, ISet<TValue>>> index)
         {
@@ -138,6 +195,10 @@ namespace Robust.Shared.Utility
             _index = ImmutableDictionary.CreateRange(index);
         }
 
+        /// <summary>
+        /// Accesses the mutable set for a given key.
+        /// </summary>
+        /// <param name="key">A given key.</param>
         public ISet<TValue> this[TKey key]
         {
             get
@@ -152,6 +213,7 @@ namespace Robust.Shared.Utility
             }
         }
 
+        /// <inheritdoc/>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public IEnumerator<KeyValuePair<TKey, ISet<TValue>>> GetEnumerator() => _index.GetEnumerator();
 

ded0b6b71094c1a85443b49742da46b112c952e5
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2020-07-06 23:22:12 +0200

    PlayerCommandStates.SetState

--- Robust.Shared/Input/PlayerCommandStates.cs
+++ Robust.Shared/Input/PlayerCommandStates.cs
@@ -48,10 +48,7 @@ namespace Robust.Shared.Input
         /// <inheritdoc />
         public void SetState(BoundKeyFunction function, BoundKeyState state)
         {
-            if (_functionStates.ContainsKey(function))
-                _functionStates[function] = state;
-            else
-                _functionStates.Add(function, state);
+            _functionStates[function] = state;
         }
     }
 }

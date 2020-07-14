ce89b7c93dd3fece7949b26db2248ec62eac5e7e
Víctor Aguilera Puerto <zddm@outlook.es>
2020-07-14 23:40:44 +0200

    Overlay's FrameUpdate is now public.

--- Robust.Client/Graphics/Overlays/Overlay.cs
+++ Robust.Client/Graphics/Overlays/Overlay.cs
@@ -76,7 +76,7 @@ namespace Robust.Client.Graphics.Overlays
             _isDirty = true;
         }
 
-        internal virtual void FrameUpdate(FrameEventArgs args)
+        public virtual void FrameUpdate(FrameEventArgs args)
         {
         }
 

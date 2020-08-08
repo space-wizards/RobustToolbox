a7fb98a020b11ca547a2aabfbc3fea4666503b6e
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2020-08-07 20:39:57 -0700

    Change BaseWindow and SS14Window to open where they previously closed
    First open will be top left, centered, or left vertically centered. The subsequent calls to open will open the window at the position the window was closed at.

--- Robust.Client/UserInterface/CustomControls/BaseWindow.cs
+++ Robust.Client/UserInterface/CustomControls/BaseWindow.cs
@@ -14,6 +14,9 @@ namespace Robust.Client.UserInterface.CustomControls
         private DragMode CurrentDrag = DragMode.None;
         private Vector2 DragOffsetTopLeft;
         private Vector2 DragOffsetBottomRight;
+
+        protected bool _firstTimeOpened = true;
+
         public bool Resizable { get; set; } = true;
         public bool IsOpen => Parent != null;
 
@@ -210,15 +213,32 @@ namespace Robust.Client.UserInterface.CustomControls
 
         public void OpenCentered()
         {
-            LayoutContainer.SetSize(this, CombinedMinimumSize);
-            Open();
-            LayoutContainer.SetPosition(this, (Parent!.Size - Size) / 2);
+            if (_firstTimeOpened)
+            {
+                LayoutContainer.SetSize(this, CombinedMinimumSize);
+                Open();
+                LayoutContainer.SetPosition(this, (Parent!.Size - Size) / 2);
+                _firstTimeOpened = false;
+            }
+            else
+            {
+                Open();
+            }
         }
 
         public void OpenToLeft()
         {
-            Open();
-            LayoutContainer.SetPosition(this, (0, (Parent!.Size.Y - Size.Y) / 2));
+            if (_firstTimeOpened)
+            {
+                LayoutContainer.SetSize(this, CombinedMinimumSize);
+                Open();
+                LayoutContainer.SetPosition(this, (0, (Parent!.Size.Y - Size.Y) / 2));
+                _firstTimeOpened = false;
+            }
+            else
+            {
+                Open();
+            }
         }
 
         protected virtual void Opened()

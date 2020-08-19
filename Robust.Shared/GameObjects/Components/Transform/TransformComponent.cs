850dc6c14b2c95e22af2791b8dbba8be616c0166
Víctor Aguilera Puerto <zddm@outlook.es>
2020-08-19 19:52:11 +0200

    Revert changes to Transform

--- Robust.Shared/GameObjects/Components/Transform/TransformComponent.cs
+++ Robust.Shared/GameObjects/Components/Transform/TransformComponent.cs
@@ -185,7 +185,19 @@ namespace Robust.Shared.GameObjects.Components.Transform
         [ViewVariables(VVAccess.ReadWrite)]
         public GridCoordinates GridPosition
         {
-            get => new GridCoordinates(_localPosition, GridID);
+            get
+            {
+                if (_parent.IsValid())
+                {
+                    // transform _position from parent coords to world coords
+                    var worldPos = Parent!.WorldMatrix.Transform(_localPosition);
+                    return new GridCoordinates(worldPos, GridID);
+                }
+                else
+                {
+                    return new GridCoordinates(_localPosition, GridID);
+                }
+            }
             set
             {
                 if (!_parent.IsValid())

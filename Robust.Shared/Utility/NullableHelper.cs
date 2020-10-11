e110b999deb4963070a1ab38bec2f7fa21c1de4f
Paul <ritter.paul1@googlemail.com>
2020-10-11 23:45:18 +0200

    nullablehelper now done

--- Robust.Shared/Utility/NullableHelper.cs
+++ Robust.Shared/Utility/NullableHelper.cs
@@ -4,17 +4,18 @@ using System.Reflection;
 using System.Reflection.Emit;
 using System.Runtime.CompilerServices;
 using Mono.CompilerServices.SymbolWriter;
+using Robust.Shared.Log;
 
 namespace Robust.Shared.Utility
 {
     public static class NullableHelper
     {
-        private delegate byte[] GetNullableFlagDelegate(Attribute t);
         private static Dictionary<Assembly, (Type, FieldInfo)> _nullableAttributeTypeCache = new Dictionary<Assembly, (Type, FieldInfo)>();
+
+        private static Dictionary<Assembly, (Type, FieldInfo)> _nullableContextAttributeTypeCache = new Dictionary<Assembly, (Type, FieldInfo)>();
 
         /// <summary>
         /// Checks if the field has a nullable annotation [?]
-        /// DOES NOT WORK WITH Object? AND object?
         /// </summary>
         /// <param name="field"></param>
         /// <returns></returns>
@@ -23,8 +24,12 @@ namespace Robust.Shared.Utility
             if (Nullable.GetUnderlyingType(field.FieldType) != null) return true;
 
             var flags = GetNullableFlags(field);
-            if (flags.Length == 0) return false;
-            return flags[0] == 2;
+            if (flags.Length != 0 && flags[0] == 2) return true;
+
+            if (field.DeclaringType == null) return false;
+
+            var cflag = GetNullableContextFlag(field.DeclaringType);
+            return cflag == 2;
         }
 
         private static byte[] GetNullableFlags(FieldInfo field)
@@ -32,7 +37,15 @@ namespace Robust.Shared.Utility
             Assembly assembly = field.Module.Assembly;
             if (!_nullableAttributeTypeCache.TryGetValue(assembly, out var assemblyNullableEntry))
             {
-                CacheNullableFlagFieldInfo(assembly);
+                try
+                {
+                    CacheNullableFieldInfo(assembly);
+                }
+                catch (Exception e)
+                {
+                    Logger.Error($"Error while trying to get NullableFieldInfo for Assembly {assembly}. Error: {e.Message}");
+                    return new byte[0];
+                }
             }
             assemblyNullableEntry = _nullableAttributeTypeCache[assembly];
 
@@ -45,7 +58,33 @@ namespace Robust.Shared.Utility
             return assemblyNullableEntry.Item2.GetValue(nullableAttribute) as byte[] ?? new byte[0];
         }
 
-        private static void CacheNullableFlagFieldInfo(Assembly assembly)
+        private static byte GetNullableContextFlag(Type type)
+        {
+            Assembly assembly = type.Assembly;
+            if (!_nullableContextAttributeTypeCache.TryGetValue(assembly, out var assemblyNullableEntry))
+            {
+                try
+                {
+                    CacheNullableContextFieldInfo(assembly);
+                }
+                catch (Exception e)
+                {
+                    Logger.Error($"Error while trying to get NullableContextFieldInfo for Assembly {assembly}. Error: {e.Message}");
+                    return 0;
+                }
+            }
+            assemblyNullableEntry = _nullableContextAttributeTypeCache[assembly];
+
+            var nullableAttribute = type.GetCustomAttribute(assemblyNullableEntry.Item1);
+            if (nullableAttribute == null)
+            {
+                return 0;
+            }
+
+            return (byte) (assemblyNullableEntry.Item2.GetValue(nullableAttribute) ?? 0);
+        }
+
+        private static void CacheNullableFieldInfo(Assembly assembly)
         {
             var nullableAttributeType = assembly.GetType("System.Runtime.CompilerServices.NullableAttribute");
             if (nullableAttributeType == null)
@@ -59,6 +98,22 @@ namespace Robust.Shared.Utility
                 throw new Exception("NullableFlags field not found");
             }
             _nullableAttributeTypeCache.Add(assembly, (nullableAttributeType, field));
+        }
+
+        private static void CacheNullableContextFieldInfo(Assembly assembly)
+        {
+            var nullableContextAttributeType = assembly.GetType("System.Runtime.CompilerServices.NullableContextAttribute");
+            if (nullableContextAttributeType == null)
+            {
+                throw new Exception($"No System.Runtime.CompilerServices.NullableContext found in Assembly {assembly}");
+            }
+
+            var field = nullableContextAttributeType.GetField("Flag");
+            if (field == null)
+            {
+                throw new Exception("Flag field not found");
+            }
+            _nullableContextAttributeTypeCache.Add(assembly, (nullableContextAttributeType, field));
         }
     }
 }
--- Robust.UnitTesting/Shared/Utility/NullableHelper_Test.cs
+++ Robust.UnitTesting/Shared/Utility/NullableHelper_Test.cs
@@ -1,14 +1,24 @@
-﻿using System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
+using Robust.Shared.Interfaces.Log;
+using Robust.Shared.IoC;
+using Robust.Shared.Log;
 using Robust.Shared.Utility;
 
 namespace Robust.UnitTesting.Shared.Utility
 {
     [TestFixture]
     [TestOf(typeof(NullableHelper))]
-
     public class NullableHelper_Test
     {
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            //initializing logmanager so it wont error out if nullablehelper logs an error
+            IoCManager.InitThread();
+            IoCManager.Register<ILogManager, LogManager>();
+            IoCManager.BuildGraph();
+        }
+
         [Test]
         public void IsNullableTest()
         {
@@ -28,27 +38,27 @@ namespace Robust.UnitTesting.Shared.Utility
                 Assert.That(!NullableHelper.IsMarkedAsNullable(field), Is.True, $"{field}");
             }
         }
-
-        private class NullableTestClass
-        {
-            private int? i;
-            private double? d;
-            private object? o;
-            private ITestInterace? Itest;
-            private NullableTestClass? nTc;
-            private char? c;
-        }
-
-        private class NotNullableTestClass
-        {
-            private int i;
-            private double d;
-            private object o = null!;
-            private ITestInterace Itest = null!;
-            private NullableTestClass nTc = null!;
-            private char c;
-        }
-
-        private interface ITestInterace{}
     }
+
+    public class NullableTestClass
+    {
+        private int? i;
+        private double? d;
+        public object? o;
+        public INullableTestInterface? Itest;
+        public NullableTestClass? nTc;
+        private char? c;
+    }
+
+    public class NotNullableTestClass
+    {
+        private int i;
+        private double d;
+        private object o = null!;
+        private INullableTestInterface Itest = null!;
+        private NullableTestClass nTc = null!;
+        private char c;
+    }
+
+    public interface INullableTestInterface{}
 }

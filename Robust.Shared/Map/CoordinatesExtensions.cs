40cab593b6eed69652870af482251fd54c79b3c7
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2020-10-11 15:11:57 +0200

    Add back Vector2i.ToEntityCoordinates

---
+++ Robust.Shared/Map/CoordinatesExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Robust.Shared.Interfaces.Map;
+using Robust.Shared.IoC;
+using Robust.Shared.Maths;
+
+namespace Robust.Shared.Map
+{
+    public static class CoordinatesExtensions
+    {
+        public static EntityCoordinates ToEntityCoordinates(this Vector2i vector, GridId gridId, IMapManager? mapManager = null)
+        {
+            mapManager ??= IoCManager.Resolve<IMapManager>();
+
+            var grid = mapManager.GetGrid(gridId);
+            var tile = grid.TileSize;
+            
+            return new EntityCoordinates(grid.GridEntityId, (vector.X * tile, vector.Y * tile));
+        }
+    }
+}

21972c7cf22b19cf3eac12883eb90ec92cfd1e40
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2020-10-14 11:59:33 +0200

    Move player.name to CVars

--- Robust.Shared/CVars.cs
+++ Robust.Shared/CVars.cs
@@ -63,6 +63,9 @@ namespace Robust.Shared
         public static readonly CVarDef<string> AuthServer =
             CVarDef.Create("auth.server", "http://localhost:5000/", CVar.SECURE);
 
+        public static readonly CVarDef<string> PlayerName =
+            CVarDef.Create("player.name", "JoeGenero", CVar.ARCHIVE);
+
 #if DEBUG
         public static readonly CVarDef<float> NetFakeLoss = CVarDef.Create("net.fakeloss", 0f, CVar.CHEAT);
         public static readonly CVarDef<float> NetFakeLagMin = CVarDef.Create("net.fakelagmin", 0f, CVar.CHEAT);

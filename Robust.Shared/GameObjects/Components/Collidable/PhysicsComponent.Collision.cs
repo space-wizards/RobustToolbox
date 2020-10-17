4a8c84d9485789958f6ab816f6302e27e6793042
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2020-10-18 02:42:22 +1100

    Fix even more physics jank (#1338)
    * Fix even more physics jank
    
    * Minor tweaks
    
    Co-authored-by: Metal Gear Sloth <metalgearsloth@gmail.com>
    Co-authored-by: Víctor Aguilera Puerto <6766154+Zumorica@users.noreply.github.com>

--- Robust.Shared/GameObjects/Components/Collidable/PhysicsComponent.Collision.cs
+++ Robust.Shared/GameObjects/Components/Collidable/PhysicsComponent.Collision.cs
@@ -130,7 +130,7 @@ namespace Robust.Shared.GameObjects.Components
             serializer.DataField(ref _bodyType, "bodyType", BodyType.Static);
             serializer.DataField(ref _physShapes, "shapes", new List<IPhysShape> {new PhysShapeAabb()});
             serializer.DataField(ref _anchored, "anchored", true);
-            serializer.DataField(ref _mass, "mass", 1f);
+            serializer.DataField(ref _mass, "mass", 1.0f);
         }
 
         /// <inheritdoc />

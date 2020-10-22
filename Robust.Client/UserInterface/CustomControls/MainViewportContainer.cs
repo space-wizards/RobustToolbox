565c7855e64b52856feaae8230719f41705489d2
20kdc <asdd2808@gmail.com>
2020-10-22 16:49:51 +0100

    Re-add main viewport in a different place

---
+++ Robust.Client/UserInterface/CustomControls/MainViewportContainer.cs
@@ -0,0 +1,28 @@
+﻿using Robust.Shared.Maths;
+using Robust.Shared.Utility;
+﻿using Robust.Client.Graphics.Drawing;
+using Robust.Client.Interfaces.Graphics;
+using Robust.Client.Interfaces.Graphics.ClientEye;
+
+namespace Robust.Client.UserInterface.CustomControls
+{
+    /// <summary>
+    ///     A viewport container shows a viewport.
+    ///     This one is particularly gnarly because it has the code for the main viewport stuff.
+    /// </summary>
+    public class MainViewportContainer : ViewportContainer
+    {
+        private readonly IEyeManager _eyeManager;
+
+        public MainViewportContainer(IEyeManager eyeManager) : base(true)
+        {
+            _eyeManager = eyeManager;
+        }
+
+        protected internal override void Draw(DrawingHandleScreen handle)
+        {
+            Viewport.Eye = _eyeManager.CurrentEye;
+            base.Draw(handle);
+        }
+    }
+}
---
+++ Robust.Client/UserInterface/CustomControls/ViewportContainer.cs
@@ -0,0 +1,56 @@
+﻿using Robust.Shared.Maths;
+using Robust.Shared.Utility;
+﻿using Robust.Client.Graphics.Drawing;
+using Robust.Client.Interfaces.Graphics;
+using Robust.Shared.IoC;
+using Robust.Shared.Maths;
+
+namespace Robust.Client.UserInterface.CustomControls
+{
+    /// <summary>
+    ///     A viewport container shows a viewport.
+    /// </summary>
+    public class ViewportContainer : Control
+    {
+        private readonly IClyde _displayManager;
+        public IClydeViewport? Viewport;
+        public readonly bool OwnsViewport;
+
+        public ViewportContainer(bool owns)
+        {
+            _displayManager = IoCManager.Resolve<IClyde>();
+            OwnsViewport = owns;
+            if (owns)
+            {
+                Viewport = _displayManager.CreateViewport((1, 1), "ViewportContainerOwnedViewport");
+            }
+        }
+
+        protected internal override void Draw(DrawingHandleScreen handle)
+        {
+            base.Draw(handle);
+            if (OwnsViewport)
+            {
+                Viewport.Render();
+            }
+            if (Viewport == null)
+            {
+                handle.DrawRect(UIBox2.FromDimensions((0, 0), Size * UserInterfaceManager.UIScale), Color.Red);
+            }
+            else
+            {
+                handle.DrawTextureRect(Viewport.RenderTarget.Texture, UIBox2.FromDimensions((0, 0), PixelSize));
+            }
+        }
+
+        protected override void Resized()
+        {
+            if (OwnsViewport)
+            {
+                Viewport?.Dispose();
+                Viewport = _displayManager.CreateViewport(Vector2i.ComponentMax((1, 1), PixelSize), "ViewportContainerOwnedViewport");
+            }
+        }
+
+    }
+}

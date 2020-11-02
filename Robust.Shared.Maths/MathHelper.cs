6299302025f2171ea45a805cb94bd27861274754
Víctor Aguilera Puerto <6766154+Zumorica@users.noreply.github.com>
2020-11-02 17:04:10 +0100

    Adds math helper to get the next closest multiple of a number given a value. (#1378)

--- Robust.Shared.Maths/MathHelper.cs
+++ Robust.Shared.Maths/MathHelper.cs
@@ -137,6 +137,58 @@ namespace Robust.Shared.Maths
 
         #endregion NextPowerOfTwo
 
+        #region NextMultipleOf
+
+        /// <summary>
+        ///     Returns the next closest multiple of a number.
+        /// </summary>
+        /// <param name="value">Closest value</param>
+        /// <param name="of">Returns the multiple of this number.</param>
+        /// <returns>The next closest multiple of a number.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static double NextMultipleOf(double value, double of)
+        {
+            return Math.Ceiling(value / of) * of;
+        }
+
+        /// <summary>
+        ///     Returns the next closest multiple of a number.
+        /// </summary>
+        /// <param name="value">Closest value</param>
+        /// <param name="of">Returns the multiple of this number.</param>
+        /// <returns>The next closest multiple of a number.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float NextMultipleOf(float value, float of)
+        {
+            return MathF.Ceiling(value / of) * of;
+        }
+
+        /// <summary>
+        ///     Returns the next closest multiple of a number.
+        /// </summary>
+        /// <param name="value">Closest value</param>
+        /// <param name="of">Returns the multiple of this number.</param>
+        /// <returns>The next closest multiple of a number.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static long NextMultipleOf(long value, long of)
+        {
+            return (long)NextMultipleOf((double)value, of);
+        }
+
+        /// <summary>
+        ///     Returns the next closest multiple of a number.
+        /// </summary>
+        /// <param name="value">Closest value</param>
+        /// <param name="of">Returns the multiple of this number.</param>
+        /// <returns>The next closest multiple of a number.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int NextMultipleOf(int value, int of)
+        {
+            return (int)NextMultipleOf((float)value, of);
+        }
+
+        #endregion
+
         #region Factorial
 
         /// <summary>Calculates the factorial of a given natural number.

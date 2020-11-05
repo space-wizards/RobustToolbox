b20bbd071c9d4cc0940d7e839b5dd102dc1ccc4a
Víctor Aguilera Puerto <zddm@outlook.es>
2020-11-05 17:46:34 +0100

    Address review

--- Robust.Shared/Map/CoordinatesExtensions.cs
+++ Robust.Shared/Map/CoordinatesExtensions.cs
@@ -25,9 +25,7 @@ namespace Robust.Shared.Map
 
             var gridId = coords.GetGridId(entityManager);
 
-            IMapGrid? mapGrid = null;
-
-            if (!gridId.IsValid() || !mapManager.TryGetGrid(gridId, out mapGrid))
+            if (!gridId.IsValid() || !mapManager.TryGetGrid(gridId, out _))
             {
                 // create a box around the cursor
                 var gridSearchBox = Box2.UnitCentered.Scale(searchBoxSize).Translated(coords.Position);

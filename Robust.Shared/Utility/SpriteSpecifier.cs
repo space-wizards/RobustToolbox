3d4564be39076e548807e767e1f58ede5a41b182
Paul <ritter.paul1+git@googlemail.com>
2020-11-12 12:12:08 +0100

    prototypeicon

--- Robust.Shared/Utility/SpriteSpecifier.cs
+++ Robust.Shared/Utility/SpriteSpecifier.cs
@@ -73,5 +73,28 @@ namespace Robust.Shared.Utility
                 return TexturePath.GetHashCode();
             }
         }
+
+        [Serializable, NetSerializable]
+        public sealed class PrototypeIcon : SpriteSpecifier
+        {
+            public readonly string PrototypeId;
+
+            public PrototypeIcon(string prototypeId)
+            {
+                PrototypeId = prototypeId;
+            }
+
+            public override bool Equals(object? obj)
+            {
+                if (obj is PrototypeIcon prototypeIcon)
+                    return PrototypeId == prototypeIcon.PrototypeId;
+                return false;
+            }
+
+            public override int GetHashCode()
+            {
+                return PrototypeId.GetHashCode();
+            }
+        }
     }
 }

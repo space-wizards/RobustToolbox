04ae4c8d0d98de5cf2f00059f9b1c626cc76fd6e
Paul <ritter.paul1@googlemail.com>
2020-11-15 21:38:41 +0100

    removes bb-overrides

--- Robust.Shared/Physics/PhysShapeRect.cs
+++ Robust.Shared/Physics/PhysShapeRect.cs
@@ -18,17 +18,6 @@ namespace Robust.Shared.Physics
         private int _collisionLayer;
         private int _collisionMask;
 
-        [ViewVariables(VVAccess.ReadWrite)]
-        private Dictionary<Angle, Box2> boundingBoxOverrides = new Dictionary<Angle, Box2>();
-
-        //this will result in a inconsistency of up to 0,05729578°, which is acceptable imo since we cant expect someone to write down all 100000 decimal places of something in radians
-        private static Angle GetOverrideIndexAngle(Angle angle) => new Angle(Math.Round(angle.Theta, 3));
-        private Box2? GetBoxOverride(Angle angle)
-        {
-            var roundedAngle = GetOverrideIndexAngle(angle);
-            return boundingBoxOverrides.ContainsKey(roundedAngle) ? boundingBoxOverrides[roundedAngle] : (Box2?)null;
-        }
-
         private Box2 _rectangle = Box2.UnitCentered;
         [ViewVariables(VVAccess.ReadWrite)]
         public Box2 Rectangle
@@ -39,13 +28,6 @@ namespace Robust.Shared.Physics
                 _rectangle = value;
                 OnDataChanged?.Invoke();
             }
-        }
-
-        public Box2Rotated GetBox2Rotated(Angle angle)
-        {
-            var roundedAngle = GetOverrideIndexAngle(angle);
-            var overrideBB = GetBoxOverride(roundedAngle);
-            return overrideBB == null ? new Box2Rotated(_rectangle, angle, Vector2.Zero) : new Box2Rotated(overrideBB.Value, new Angle(), Vector2.Zero);
         }
 
         /// <inheritdoc />
@@ -92,14 +74,6 @@ namespace Robust.Shared.Physics
             serializer.DataField(ref _collisionLayer, "layer", 0, WithFormat.Flags<CollisionLayer>());
             serializer.DataField(ref _collisionMask, "mask", 0, WithFormat.Flags<CollisionMask>());
             serializer.DataField(ref _rectangle, "bounds", Box2.UnitCentered);
-
-            var overrides = new List<BoundingBoxOverridePrototype>();
-            serializer.DataField(ref overrides, "overrides", new List<BoundingBoxOverridePrototype>());
-            foreach (var boundingBoxOverridePrototype in overrides!)
-            {
-                var roundedAngle = GetOverrideIndexAngle(boundingBoxOverridePrototype.Angle);
-                boundingBoxOverrides.Add(roundedAngle, boundingBoxOverridePrototype.Override);
-            }
         }
 
         [field: NonSerialized]
@@ -107,30 +81,7 @@ namespace Robust.Shared.Physics
 
         public Box2 CalculateLocalBounds(Angle rotation)
         {
-            return GetBox2Rotated(rotation.Opposite()).CalcBoundingBox();
-        }
-
-        private struct BoundingBoxOverridePrototype : IExposeData
-        {
-            public Box2 Override { get; private set; }
-            public Angle Angle { get; private set; }
-            public void ExposeData(ObjectSerializer serializer)
-            {
-                if (!serializer.TryReadDataField("angle", out double theta))
-                {
-                    var dir = Direction.East;
-                    serializer.DataField(ref dir, "dir", Direction.East);
-                    Angle = dir.ToAngle();
-                }
-                else
-                {
-                    Angle = GetOverrideIndexAngle(new Angle(theta));
-                }
-
-                var bounds = Box2.UnitCentered;
-                serializer.DataField(ref bounds, "bounds", Box2.UnitCentered);
-                Override = bounds;
-            }
+            return new Box2Rotated(_rectangle, rotation.Opposite(), Vector2.Zero).CalcBoundingBox();
         }
     }
 }

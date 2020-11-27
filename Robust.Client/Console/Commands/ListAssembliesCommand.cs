08f9aaa8a892b48c421273458a5d2d6a7bce8e80
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2020-11-27 16:39:30 +0100

    Fix lsasm crashing clyde due to huge message.

--- Robust.Client/Console/Commands/ListAssembliesCommand.cs
+++ Robust.Client/Console/Commands/ListAssembliesCommand.cs
@@ -15,17 +15,15 @@ namespace Robust.Client.Console.Commands
 
         public bool Execute(IDebugConsole console, string[] args)
         {
-            var sb = new StringBuilder();
             foreach (var context in AssemblyLoadContext.All)
             {
-                sb.AppendFormat("{0}:\n", context.Name);
+                console.AddLine($"{context.Name}:");
                 foreach (var assembly in context.Assemblies.OrderBy(a => a.FullName))
                 {
-                    sb.AppendFormat("  {0}\n", assembly.FullName);
+                    console.AddLine($"  {assembly.FullName}");
                 }
             }
 
-            console.AddLine(sb.ToString());
             return false;
         }
     }

9353c53ed110792b6b12b2a1f61f3eb3ec2deefc
Paul <ritter.paul1+git@googlemail.com>
2020-12-04 17:31:21 +0100

    adds unit test

---
+++ Robust.UnitTesting/Shared/Maths/Box2Rotated_Test.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using Robust.Shared.Maths;
+
+namespace Robust.UnitTesting.Shared.Maths
+{
+    [TestFixture]
+    [TestOf(typeof(Box2Rotated))]
+    public class Box2Rotated_Test
+    {
+        public Box2[] Boxes1 = {
+            new Box2(0.5f, -0.2f, 0.5f, 0.5f),
+            new Box2(-0.2f, 0.5f, 0.5f, 0.5f),
+            new Box2(0.5f, 0.5f, 0.5f, -0.2f),
+            new Box2(0.5f, 0.5f, -0.2f, 0.5f)
+        };
+
+        [Test]
+        public void FullRotationTest()
+        {
+            var angle = Angle.FromDegrees(0);
+            for (int i = 0; i < Boxes1.Length; i++)
+            {
+                var next = i + 1;
+                if (next == Boxes1.Length)
+                    next = 0;
+                var box = Boxes1[i];
+                var nextbox = Boxes1[next];
+
+                var rotatedBox = new Box2Rotated(box, angle);
+                Assert.Equals(rotatedBox.CalcBoundingBox(), nextbox);
+                angle += Angle.FromDegrees(90);
+            }
+        }
+    }
+}

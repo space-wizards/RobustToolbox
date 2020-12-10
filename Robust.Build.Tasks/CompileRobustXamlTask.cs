8234dbb84fbb61c1e44774b714a754bcc5dabf19
Paul <ritter.paul1@googlemail.com>
2020-12-10 23:50:02 +0100

    adds robust.build.tasks for postcompilation dll weaving

---
+++ Robust.Build.Tasks/CompileRobustXamlTask.cs
@@ -0,0 +1,72 @@
+﻿using System.IO;
+using System.Linq;
+using Microsoft.Build.Framework;
+
+namespace Robust.Build.Tasks
+{
+    public class CompileRobustXamlTask : ITask
+    {
+        public bool Execute()
+        {
+            OutputPath = OutputPath ?? AssemblyFile;
+            var outputPdb = GetPdbPath(OutputPath);
+            var input = AssemblyFile;
+            var inputPdb = GetPdbPath(input);
+            /*// Make a copy and delete the original file to prevent MSBuild from thinking that everything is OK
+            if (OriginalCopyPath != null)
+            {
+                File.Copy(AssemblyFile, OriginalCopyPath, true);
+                input = OriginalCopyPath;
+                File.Delete(AssemblyFile);l
+
+                if (File.Exists(inputPdb))
+                {
+                    var copyPdb = GetPdbPath(OriginalCopyPath);
+                    File.Copy(inputPdb, copyPdb, true);
+                    File.Delete(inputPdb);
+                    inputPdb = copyPdb;
+                }
+            }*/
+
+            var msg = $"CompileRobustXamlTask -> AssemblyFile:{AssemblyFile}, ProjectDirectory:{ProjectDirectory}, OutputPath:{OutputPath}";
+            BuildEngine.LogMessage(msg, MessageImportance.Low);
+
+            var res = XamlCompilerTaskExecutor.Compile(BuildEngine, input,
+                File.ReadAllLines(ReferencesFilePath).Where(l => !string.IsNullOrWhiteSpace(l)).ToArray(),
+                ProjectDirectory, OutputPath, VerifyIl, outputImportance,
+                (SignAssembly && !DelaySign) ? AssemblyOriginatorKeyFile : null,
+                EnableComInteropPatching);
+            if (!res.Success)
+                return false;
+            if (!res.WrittenFile)
+            {
+                File.Copy(input, OutputPath, true);
+                if(File.Exists(inputPdb))
+                    File.Copy(inputPdb, outputPdb, true);
+            }
+            return true;
+        }
+
+        public string ReferencesFilePath { get; set; }
+
+        public string ProjectDirectory { get; set; }
+
+        public string AssemblyFile { get; set; }
+
+        public string OutputPath { get; set; }
+
+        // shamelessly copied from avalonia
+        string GetPdbPath(string p)
+        {
+            var d = Path.GetDirectoryName(p);
+            var f = Path.GetFileNameWithoutExtension(p);
+            var rv = f + ".pdb";
+            if (d != null)
+                rv = Path.Combine(d, rv);
+            return rv;
+        }
+
+        public IBuildEngine BuildEngine { get; set; }
+        public ITaskHost HostObject { get; set; }
+    }
+}
---
+++ Robust.Build.Tasks/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Build.Framework;
+
+namespace Robust.Build.Tasks
+{
+    public static class Extensions
+    {
+        //shamefully copied from avalonia
+        public static void LogMessage(this IBuildEngine engine, string message, MessageImportance imp)
+        {
+            engine.LogMessageEvent(new BuildMessageEventArgs(message, "", "Avalonia", imp));
+        }
+    }
+}

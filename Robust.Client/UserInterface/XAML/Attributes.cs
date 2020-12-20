d94f70260172b9d08da42f8f20653717796738b1
Paul Ritter <ritter.paul1@googlemail.com>
2020-12-20 23:52:36 +0100

    Xaml UI (#1446)
    Co-authored-by: Paul <ritter.paul1+git@googlemail.com>
    Co-authored-by: Pieter-Jan Briers <pieterjan.briers+git@gmail.com>

---
+++ Robust.Client/UserInterface/XAML/Attributes.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Robust.Client.UserInterface.XAML
+{
+    public class ContentAttribute : Attribute
+    {
+    }
+
+    public class XmlnsDefinitionAttribute : Attribute
+    {
+        public XmlnsDefinitionAttribute(string xmlNamespace, string clrNamespace)
+        {
+        }
+    }
+
+    public class UsableDuringInitializationAttribute : Attribute
+    {
+        public UsableDuringInitializationAttribute(bool usable)
+        {
+        }
+    }
+
+    public class DeferredContentAttribute : Attribute
+    {
+    }
+
+    public interface ITestRootObjectProvider
+    {
+        object RootObject { get; }
+    }
+
+    public interface ITestProvideValueTarget
+    {
+        object TargetObject { get; }
+        object TargetProperty { get; }
+    }
+
+    public interface ITestUriContext
+    {
+        Uri BaseUri { get; set; }
+    }
+}

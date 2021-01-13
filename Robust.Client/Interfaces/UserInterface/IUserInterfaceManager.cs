c4062bcae95e965301c22768653b581287253763
chairbender <kwhipke1@gmail.com>
2021-01-13 14:18:45 -0800

    #1449 new ControlFocusExited override for allowing controls to know (#1467)
    when they lost control focus, separate from keyboard focus

--- Robust.Client/Interfaces/UserInterface/IUserInterfaceManager.cs
+++ Robust.Client/Interfaces/UserInterface/IUserInterfaceManager.cs
@@ -21,7 +21,21 @@ namespace Robust.Client.Interfaces.UserInterface
         /// </summary>
         Stylesheet? Stylesheet { get; set; }
 
+        /// <summary>
+        /// A control can have "keyboard focus" separate from ControlFocused, obtained when calling
+        /// Control.GrabKeyboardFocus. Corresponding events in Control are KeyboardFocusEntered/Exited
+        /// </summary>
         Control? KeyboardFocused { get; }
+
+        /// <summary>
+        /// A control gets "ControlFocused" when a mouse button (or any KeyBinding which has CanFocus = true) is
+        /// pressed down on the control. While it is focused, it will receive mouse hover events and the corresponding
+        /// keyup event if it still has focus when that occurs (it will NOT receive the keyup if focus has
+        /// been taken by another control). Focus is removed when a different control takes focus
+        /// (such as by pressing a different mouse button down over a different control) or when the keyup event
+        /// happens. When focus is lost on a control, it always fires Control.ControlFocusExited.
+        /// </summary>
+        Control? ControlFocused { get; }
 
         LayoutContainer StateRoot { get; }
 

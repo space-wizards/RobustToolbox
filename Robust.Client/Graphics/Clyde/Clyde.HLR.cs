849601591155518b4b5e8939baa0272a0233554d
Vera Aguilera Puerto <zddm@outlook.es>
2021-01-20 16:03:51 +0100

    Fix postfx, add WorldToScreen to Viewport

--- Robust.Client/Graphics/Clyde/Clyde.HLR.cs
+++ Robust.Client/Graphics/Clyde/Clyde.HLR.cs
@@ -187,7 +187,7 @@ namespace Robust.Client.Graphics.Clyde
                     // which is necessary for light application,
                     // but it's ACTUALLY drawing into the center of the render target.
                     var spritePos = entry.sprite.Owner.Transform.WorldPosition;
-                    var screenPos = _eyeManager.WorldToScreen(spritePos);
+                    var screenPos = viewport.WorldToScreen(spritePos);
                     var (roundedX, roundedY) = roundedPos = (Vector2i) screenPos;
                     var flippedPos = new Vector2i(roundedX, screenSize.Y - roundedY);
                     flippedPos -= EntityPostRenderTarget.Size / 2;

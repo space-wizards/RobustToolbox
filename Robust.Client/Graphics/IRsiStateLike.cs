e1110eadb4efb92052b6fc2e021817dfbbd34693
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2021-01-24 22:59:47 +0100

    Better unified handling of SpriteSpecifiers
    Add more utilities, most notably IRsiStateLike, to make working with SpriteSpecifiers easier. Especially when animated.

---
+++ Robust.Client/Graphics/IRsiStateLike.cs
@@ -0,0 +1,12 @@
+﻿namespace Robust.Client.Graphics
+{
+    public interface IRsiStateLike : IDirectionalTextureProvider
+    {
+        RSI.State.DirectionType Directions { get; }
+        bool IsAnimated { get; }
+        int AnimationFrameCount { get; }
+
+        float GetDelay(int frame);
+        Texture GetFrame(RSI.State.Direction dir, int frame);
+    }
+}

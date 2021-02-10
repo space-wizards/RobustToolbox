cc39352e3cb37d64718ca2dcd72c1974c35dbb4d
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-02-11 00:05:39 +0100

    Added test for data classes being parsed correctly

--- Robust.Generators.UnitTesting/DataClassTests.cs
+++ Robust.Generators.UnitTesting/DataClassTests.cs
@@ -1,4 +1,6 @@
+using System.Linq;
 using NUnit.Framework;
+using Robust.Shared.Prototypes;
 
 namespace Robust.Generators.UnitTesting
 {
@@ -37,7 +39,43 @@ namespace Test{
             var (newcomp, generatorDiags) = RunGenerators(comp, new DataClassGenerator());
 
             Assert.IsEmpty(generatorDiags);
-            Assert.NotNull(newcomp.GetTypeByMetadataName("Test.TestClass_AUTODATA"));
+
+            var type = newcomp.GetTypeByMetadataName("Test.TestClass_AUTODATA");
+            Assert.NotNull(type);
+
+            var memberNames = type.MemberNames.ToArray();
+
+            // 3 properties
+            Assert.That(memberNames, Has.Length.EqualTo(3));
+            Assert.That(memberNames, Contains.Item("testList"));
+            Assert.That(memberNames, Contains.Item("abc"));
+            Assert.That(memberNames, Contains.Item("test"));
+
+            var members = type.GetMembers();
+
+            // 3 properties + constructor
+            Assert.That(members, Has.Length.EqualTo(4));
+
+            var memberDictionary = members.ToDictionary(m => m.Name, m => m);
+            var yamlFieldNamespace = typeof(YamlFieldAttribute).FullName;
+
+            Assert.NotNull(yamlFieldNamespace);
+
+            var yamlFieldAttribute = comp.GetTypeByMetadataName(yamlFieldNamespace);
+
+            var testListYamlAttribute = memberDictionary["testList"].GetAttribute(yamlFieldAttribute);
+            Assert.NotNull(testListYamlAttribute);
+            Assert.That(testListYamlAttribute.ConstructorArguments[0].Value, Is.EqualTo("myList"));
+
+            var abcYamlAttribute = memberDictionary["abc"].GetAttribute(yamlFieldAttribute);
+            Assert.NotNull(abcYamlAttribute);
+            Assert.That(abcYamlAttribute.ConstructorArguments[0].Value, Is.EqualTo("myList"));
+
+            var testYamlAttribute = memberDictionary["test"].GetAttribute(yamlFieldAttribute);
+            Assert.NotNull(testYamlAttribute);
+            Assert.That(testYamlAttribute.ConstructorArguments[0].Value, Is.EqualTo("drawdepth"));
+            Assert.NotNull(testYamlAttribute.ConstructorArguments[3].Value);
+            Assert.That(testYamlAttribute.ConstructorArguments[3].Value.ToString(), Is.EqualTo("Test.TestClass"));
 
             //TODO Check for dataclass & if its correct
         }

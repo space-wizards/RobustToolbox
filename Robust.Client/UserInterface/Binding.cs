29a39f8e0a928280a6a37534fa61d184a2cdca3d
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2021-02-13 15:13:05 +0100

    Adds LocExtension markup extension for XAML.
    Does Loc.GetString().

---
+++ Robust.Client/UserInterface/Binding.cs
@@ -0,0 +1,10 @@
+﻿namespace Avalonia.Data
+{
+    // This type has to exist for Rider to recognize markup extensions like LocExtension.
+    // I am not kidding. This is the actual requirement.
+    // I decompiled Rider with Rider.
+    public class Binding
+    {
+
+    }
+}
---
+++ Robust.Client/UserInterface/LocExtension.cs
@@ -0,0 +1,19 @@
+﻿using Robust.Shared.Localization;
+
+namespace Robust.Client.UserInterface
+{
+    public class LocExtension
+    {
+        public string Key { get; }
+
+        public LocExtension(string key)
+        {
+            Key = key;
+        }
+
+        public object ProvideValue()
+        {
+            return Loc.GetString(Key);
+        }
+    }
+}

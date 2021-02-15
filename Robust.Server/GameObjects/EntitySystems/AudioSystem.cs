4485b0af74517de4e21b79981aa4a830d978fbeb
Acruid <shatter66@gmail.com>
2021-02-14 22:34:31 -0800

    Adds the SoundSystem static proxy class for the AudioSystem. Added a shared IAudioSystem interface for the future.

--- Robust.Server/GameObjects/EntitySystems/AudioSystem.cs
+++ Robust.Server/GameObjects/EntitySystems/AudioSystem.cs
@@ -1,15 +1,16 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Robust.Server.Player;
 using Robust.Shared.Audio;
 using Robust.Shared.GameObjects;
 using Robust.Shared.IoC;
 using Robust.Shared.Map;
-using static Robust.Server.GameObjects.AudioSystem;
+using Robust.Shared.Player;
+using Robust.Shared.Players;
 
 namespace Robust.Server.GameObjects
 {
-    public class AudioSystem : EntitySystem
+    public class AudioSystem : EntitySystem, IAudioSystem
     {
         [Dependency] private readonly IPlayerManager _playerManager = default!;
 
@@ -17,7 +18,7 @@ namespace Robust.Server.GameObjects
 
         private uint _streamIndex;
 
-        public class AudioSourceServer
+        public class AudioSourceServer : IPlayingAudioStream
         {
             private readonly uint _id;
             private readonly AudioSystem _audioSystem;
@@ -33,6 +34,12 @@ namespace Robust.Server.GameObjects
             {
                 _audioSystem.InternalStop(_id, _sessions);
             }
+        }
+
+        /// <inheritdoc />
+        public override void Initialize()
+        {
+            SubscribeLocalEvent<SoundSystem.QueryAudioSystem>((ev => ev.Audio = this));
         }
 
         private void InternalStop(uint id, IEnumerable<IPlayerSession>? sessions = null)
@@ -65,7 +72,8 @@ namespace Robust.Server.GameObjects
         /// <param name="audioParams"></param>
         /// <param name="predicate">The predicate that will be used to send the audio to players, or null to send to everyone.</param>
         /// <param name="excludedSession">Session that won't receive the audio message.</param>
-        public AudioSourceServer PlayGlobal(string filename, AudioParams? audioParams = null, Func<IPlayerSession, bool>? predicate = null, IPlayerSession? excludedSession = null)
+        /// <param name="recipients"></param>
+        public IPlayingAudioStream PlayGlobal(string filename, AudioParams? audioParams = null, Func<IPlayerSession, bool>? predicate = null, IPlayerSession? excludedSession = null)
         {
             var id = CacheIdentifier();
             var msg = new PlayAudioGlobalMessage
@@ -81,7 +89,7 @@ namespace Robust.Server.GameObjects
                 return new AudioSourceServer(this, id);
             }
 
-            var players = predicate != null ? _playerManager.GetPlayersBy(predicate) : _playerManager.GetAllPlayers();
+            IList<IPlayerSession> players = predicate != null ? _playerManager.GetPlayersBy(predicate) : _playerManager.GetAllPlayers();
 
             for (var i = players.Count - 1; i >= 0; i--)
             {
@@ -107,7 +115,7 @@ namespace Robust.Server.GameObjects
         /// <param name="audioParams"></param>
         /// <param name="range">The max range at which the audio will be heard. Less than or equal to 0 to send to every player.</param>
         /// <param name="excludedSession">Sessions that won't receive the audio message.</param>
-        public AudioSourceServer PlayFromEntity(string filename, IEntity entity, AudioParams? audioParams = null, int range = AudioDistanceRange, IPlayerSession? excludedSession = null)
+        public IPlayingAudioStream PlayFromEntity(string filename, IEntity entity, AudioParams? audioParams = null, int range = AudioDistanceRange, IPlayerSession? excludedSession = null)
         {
             var id = CacheIdentifier();
 
@@ -120,13 +128,19 @@ namespace Robust.Server.GameObjects
                 Identifier = id,
             };
 
+            // send to every player
             if (range <= 0 && excludedSession == null)
             {
                 RaiseNetworkEvent(msg);
                 return new AudioSourceServer(this, id);
             }
 
-            var players = range > 0.0f ? _playerManager.GetPlayersInRange(entity.Transform.Coordinates, range) : _playerManager.GetAllPlayers();
+            List<IPlayerSession> players;
+
+            if (range > 0.0f)
+                players = _playerManager.GetPlayersInRange(entity.Transform.Coordinates, range);
+            else
+                players = _playerManager.GetAllPlayers();
 
             for (var i = players.Count - 1; i >= 0; i--)
             {
@@ -151,7 +165,7 @@ namespace Robust.Server.GameObjects
         /// <param name="audioParams"></param>
         /// <param name="range">The max range at which the audio will be heard. Less than or equal to 0 to send to every player.</param>
         /// <param name="excludedSession">Session that won't receive the audio message.</param>
-        public AudioSourceServer PlayAtCoords(string filename, EntityCoordinates coordinates, AudioParams? audioParams = null, int range = AudioDistanceRange, IPlayerSession? excludedSession = null)
+        public IPlayingAudioStream PlayAtCoords(string filename, EntityCoordinates coordinates, AudioParams? audioParams = null, int range = AudioDistanceRange, IPlayerSession? excludedSession = null)
         {
             var id = CacheIdentifier();
             var msg = new PlayAudioPositionalMessage
@@ -168,7 +182,12 @@ namespace Robust.Server.GameObjects
                 return new AudioSourceServer(this, id);
             }
 
-            var players = range > 0.0f ? _playerManager.GetPlayersInRange(coordinates, range) : _playerManager.GetAllPlayers();
+            List<IPlayerSession> players;
+
+            if (range > 0.0f)
+                players = _playerManager.GetPlayersInRange(coordinates, range);
+            else
+                players = _playerManager.GetAllPlayers();
 
             for (var i = players.Count - 1; i >= 0; i--)
             {
@@ -185,88 +204,28 @@ namespace Robust.Server.GameObjects
             return new AudioSourceServer(this, id, players);
         }
 
-        #region DEPRECATED
-        /// <summary>
-        ///     Play an audio file globally, without position.
-        /// </summary>
-        /// <param name="filename">The resource path to the OGG Vorbis file to play.</param>
-        /// <param name="audioParams"></param>
-        [Obsolete("Deprecated. Use PlayGlobal instead.")]
-        public void Play(string filename, AudioParams? audioParams = null)
+        /// <inheritdoc />
+        public int DefaultSoundRange => AudioDistanceRange;
+
+        /// <inheritdoc />
+        public int OcclusionCollisionMask { get; set; }
+
+        /// <inheritdoc />
+        public IPlayingAudioStream? Play(Filter playerFilter, string filename, AudioParams? audioParams = null)
         {
-            PlayGlobal(filename, audioParams);
+            throw new NotImplementedException();
         }
 
-
-        /// <summary>
-        ///     Play an audio file following an entity.
-        /// </summary>
-        /// <param name="filename">The resource path to the OGG Vorbis file to play.</param>
-        /// <param name="entity">The entity "emitting" the audio.</param>
-        /// <param name="audioParams"></param>
-        [Obsolete("Deprecated. Use PlayFromEntity instead.")]
-        public void Play(string filename, IEntity entity, AudioParams? audioParams = null)
+        /// <inheritdoc />
+        public IPlayingAudioStream? Play(Filter playerFilter, string filename, IEntity entity, AudioParams? audioParams = null)
         {
-            PlayFromEntity(filename, entity, audioParams);
+            throw new NotImplementedException();
         }
 
-        /// <summary>
-        ///     Play an audio file at a static position.
-        /// </summary>
-        /// <param name="filename">The resource path to the OGG Vorbis file to play.</param>
-        /// <param name="coordinates">The coordinates at which to play the audio.</param>
-        /// <param name="audioParams"></param>
-        [Obsolete("Deprecated. Use PlayAtCoords instead.")]
-        public void Play(string filename, EntityCoordinates coordinates, AudioParams? audioParams = null)
+        /// <inheritdoc />
+        public IPlayingAudioStream? Play(Filter playerFilter, string filename, EntityCoordinates coordinates, AudioParams? audioParams = null)
         {
-            PlayAtCoords(filename, coordinates, audioParams);
-        }
-
-        #endregion
-    }
-
-    public static class AudioSystemExtensions
-    {
-        /// <summary>
-        ///     Play an audio file following an entity.
-        /// </summary>
-        /// <param name="filename">The resource path to the OGG Vorbis file to play.</param>
-        /// <param name="entity">The entity "emitting" the audio.</param>
-        /// <param name="audioParams"></param>
-        /// <param name="range">The max range at which the audio will be heard. Less than or equal to 0 to send to every player.</param>
-        /// <param name="excludedSession">Sessions that won't receive the audio message.</param>
-        /// <param name="audioSystem">A pre-fetched instance of <see cref="AudioSystem"/> to use, can be null.</param>
-        public static void PlaySoundFrom(
-            this IEntity entity,
-            string filename,
-            AudioParams? audioParams = null,
-            int range = AudioDistanceRange,
-            IPlayerSession? excludedSession = null,
-            AudioSystem? audioSystem = null)
-        {
-            audioSystem ??= EntitySystem.Get<AudioSystem>();
-            audioSystem.PlayFromEntity(filename, entity, audioParams, range, excludedSession);
-        }
-
-        /// <summary>
-        ///     Play an audio file at a static position.
-        /// </summary>
-        /// <param name="filename">The resource path to the OGG Vorbis file to play.</param>
-        /// <param name="coordinates">The coordinates at which to play the audio.</param>
-        /// <param name="audioParams"></param>
-        /// <param name="range">The max range at which the audio will be heard. Less than or equal to 0 to send to every player.</param>
-        /// <param name="excludedSession">Session that won't receive the audio message.</param>
-        /// <param name="audioSystem">A pre-fetched instance of <see cref="AudioSystem"/> to use, can be null.</param>
-        public static void PlaySoundFrom(
-            this EntityCoordinates coordinates,
-            string filename,
-            AudioParams? audioParams = null,
-            int range = AudioDistanceRange,
-            IPlayerSession? excludedSession = null,
-            AudioSystem? audioSystem = null)
-        {
-            audioSystem ??= EntitySystem.Get<AudioSystem>();
-            audioSystem.PlayAtCoords(filename, coordinates, audioParams, range, excludedSession);
+            throw new NotImplementedException();
         }
     }
 }

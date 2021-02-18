ff2420ebb7c51ed60d74157d23ff207e17af94e3
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-02-18 13:48:26 +0100

    Merge fixes

--- Robust.Server/Console/Commands/ReloadCommand.cs
+++ Robust.Server/Console/Commands/ReloadCommand.cs
@@ -1,24 +1,18 @@
 using JetBrains.Annotations;
-using Robust.Server.Interfaces.Console;
-using Robust.Server.Interfaces.Player;
-using Robust.Shared.GameObjects;
-using Robust.Shared.Interfaces.GameObjects;
+using Robust.Shared.Console;
 using Robust.Shared.IoC;
 using Robust.Shared.Prototypes;
-using Robust.Shared.Utility;
 
 namespace Robust.Server.Console.Commands
 {
     [UsedImplicitly]
-    public class ReloadCommand: IClientCommand
+    public class ReloadCommand : IConsoleCommand
     {
         public string Command => "reload";
-
         public string Description => "Reloads all entity prototypes and updates entities in-game accordingly";
-
         public string Help => "";
 
-        public void Execute(IConsoleShell shell, IPlayerSession player, string[] args)
+        public void Execute(IConsoleShell shell, string argStr, string[] args)
         {
             var prototypeManager = IoCManager.Resolve<IPrototypeManager>();
 

17fe05e17cc04faf24bfab7f1fba4bc0bf6a3526
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-02-18 13:04:02 +0100

    Fix up names

--- Robust.Server/Console/Commands/AddComponentCommand.cs
+++ Robust.Server/Console/Commands/AddComponentCommand.cs
@@ -1,5 +1,4 @@
 ﻿using JetBrains.Annotations;
-using Robust.Server.Interfaces.Player;
 using Robust.Shared.Console;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Interfaces.GameObjects;
@@ -8,7 +7,7 @@ using Robust.Shared.IoC;
 namespace Robust.Server.Console.Commands
 {
     [UsedImplicitly]
-    internal sealed class AddCompCommand : IConsoleCommand
+    internal sealed class AddComponentCommand : IConsoleCommand
     {
         public string Command => "addcomp";
         public string Description => "Adds a component to an entity";
@@ -42,37 +41,6 @@ namespace Robust.Server.Console.Commands
             component.Owner = entity;
 
             compManager.AddComponent(entity, component);
-        }
-    }
-
-    [UsedImplicitly]
-    internal sealed class RemoveCompCommand : IConsoleCommand
-    {
-        public string Command => "rmcomp";
-        public string Description => "Removes a component from an entity.";
-        public string Help => $"{Command} <uid> <componentName>";
-
-        public void Execute(IConsoleShell shell, string argStr, string[] args)
-        {
-            if (args.Length != 2)
-            {
-                shell.WriteLine("Wrong number of arguments");
-                return;
-            }
-
-            var entityUid = EntityUid.Parse(args[0]);
-            var componentName = args[1];
-
-            var compManager = IoCManager.Resolve<IComponentManager>();
-            var compFactory = IoCManager.Resolve<IComponentFactory>();
-
-            if (!compFactory.TryGetRegistration(componentName, out var registration, true))
-            {
-                shell.WriteLine($"No component found with name {componentName}");
-                return;
-            }
-
-            compManager.RemoveComponent(entityUid, registration.Type);
         }
     }
 }
---
+++ Robust.Server/Console/Commands/RemoveComponentCommand.cs
@@ -0,0 +1,39 @@
+﻿using JetBrains.Annotations;
+using Robust.Shared.Console;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Interfaces.GameObjects;
+using Robust.Shared.IoC;
+
+namespace Robust.Server.Console.Commands
+{
+    [UsedImplicitly]
+    internal sealed class RemoveComponentCommand : IConsoleCommand
+    {
+        public string Command => "rmcomp";
+        public string Description => "Removes a component from an entity.";
+        public string Help => $"{Command} <uid> <componentName>";
+
+        public void Execute(IConsoleShell shell, string argStr, string[] args)
+        {
+            if (args.Length != 2)
+            {
+                shell.WriteLine("Wrong number of arguments");
+                return;
+            }
+
+            var entityUid = EntityUid.Parse(args[0]);
+            var componentName = args[1];
+
+            var compManager = IoCManager.Resolve<IComponentManager>();
+            var compFactory = IoCManager.Resolve<IComponentFactory>();
+
+            if (!compFactory.TryGetRegistration(componentName, out var registration, true))
+            {
+                shell.WriteLine($"No component found with name {componentName}");
+                return;
+            }
+
+            compManager.RemoveComponent(entityUid, registration.Type);
+        }
+    }
+}

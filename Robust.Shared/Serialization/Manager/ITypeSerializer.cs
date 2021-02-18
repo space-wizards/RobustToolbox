6634ebaf1cf6ac59cab6a93343f43299547fc1ef
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-02-18 16:38:43 +0100

    Separate ITypeSerializer into reader and writer

--- Robust.Shared/Serialization/Manager/ITypeSerializer.cs
+++ Robust.Shared/Serialization/Manager/ITypeSerializer.cs
@@ -1,12 +1,20 @@
-using Robust.Shared.IoC;
 using Robust.Shared.Serialization.Markdown;
 
 namespace Robust.Shared.Serialization.Manager
 {
-    public interface ITypeSerializer<TType, TNode> where TNode : DataNode
+    public interface ITypeSerializer<TType, TNode> : ITypeReader<TType, TNode>, ITypeWriter<TType>
+        where TType : notnull where TNode : DataNode
     {
-        TType NodeToType(DataNode node, ISerializationContext? context = null);
-        DataNode TypeToNode(TType value, bool alwaysWrite = false,
+    }
+
+    public interface ITypeReader<TType, TNode> where TType : notnull where TNode : DataNode
+    {
+        TType Read(TNode node, ISerializationContext? context = null);
+    }
+
+    public interface ITypeWriter<TType> where TType : notnull
+    {
+        DataNode Write(TType value, bool alwaysWrite = false,
             ISerializationContext? context = null);
     }
 }

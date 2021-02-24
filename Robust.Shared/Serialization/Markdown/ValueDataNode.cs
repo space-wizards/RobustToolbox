6a40b367b311737e52cee43b894dd0bc5083de41
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-02-24 12:25:32 +0100

    Fix the tag not being copied in data nodes

--- Robust.Shared/Serialization/Markdown/ValueDataNode.cs
+++ Robust.Shared/Serialization/Markdown/ValueDataNode.cs
@@ -1,5 +1,3 @@
-using System;
-using Robust.Shared.Serialization.Markdown.YAML;
 using YamlDotNet.RepresentationModel;
 
 namespace Robust.Shared.Serialization.Markdown
@@ -21,7 +19,7 @@ namespace Robust.Shared.Serialization.Markdown
 
         public override DataNode Copy()
         {
-            return new ValueDataNode(Value);
+            return new ValueDataNode(Value) {Tag = Tag};
         }
 
         public override bool Equals(object? obj)

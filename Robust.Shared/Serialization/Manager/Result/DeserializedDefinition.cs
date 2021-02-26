c28036643aa00efaad04e0fecd9974cfa0d4601c
Paul <ritter.paul1+git@googlemail.com>
2021-02-26 15:20:03 +0100

    finishes pushinheritance FRAMEWORK

--- Robust.Shared/Serialization/Manager/Result/DeserializedDefinition.cs
+++ Robust.Shared/Serialization/Manager/Result/DeserializedDefinition.cs
@@ -1,16 +1,18 @@
 ﻿using System;
+using Robust.Shared.IoC;
 
 namespace Robust.Shared.Serialization.Manager.Result
 {
-    public class DeserializedDefinition<T> : DeserializationResult
+    public class DeserializedDefinition<T> : DeserializationResult<T> where T : new()
     {
         public DeserializedDefinition(T value, DeserializedFieldEntry[] mapping)
         {
-            Value = value;
+            _value = value;
             Mapping = mapping;
         }
 
-        public T Value { get; }
+        public override T Value => _value;
+        private T _value;
 
         public DeserializedFieldEntry[] Mapping { get; }
 
@@ -22,14 +24,34 @@ namespace Robust.Shared.Serialization.Manager.Result
             if (dataDef.Mapping.Length != Mapping.Length)
                 throw new ArgumentException($"Mappinglength mismatch in PushInheritanceFrom ({typeof(T)})");
 
-            //todo paul split populate into serializing into deserializeddatadef & populating obj
-            var
+            var newMapping = new DeserializedFieldEntry[Mapping.Length];
 
             for (int i = 0; i < dataDef.Mapping.Length; i++)
             {
-                if(Mapping[i].Mapped) continue;
-
+                if(Mapping[i].Mapped)
+                {
+                    newMapping[i] = Mapping[i].Copy();
+                }
+                else
+                {
+                    newMapping[i] = dataDef.Mapping[i].Copy();
+                }
             }
+
+            return IoCManager.Resolve<ISerializationManager>().PopulateDataDefinition<T>(newMapping);
         }
+
+        public override DeserializationResult Copy()
+        {
+            var newMapping = new DeserializedFieldEntry[Mapping.Length];
+
+            for(var i = 0; i < Mapping.Length; i++)
+            {
+                newMapping[i] = Mapping[i].Copy();
+            }
+
+            return IoCManager.Resolve<ISerializationManager>().PopulateDataDefinition<T>(newMapping);
+        }
+
     }
 }

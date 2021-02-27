08288660f0af8b35e259cdf60711ab82f2bb4f60
Acruid <shatter66@gmail.com>
2021-02-26 21:42:39 -0800

    Pulled all members of client IPlayerSession into ICommonSession. Marked client IPlayerSession as obsolete, use the base ICommonSession.

--- Robust.Shared/Players/ICommonSession.cs
+++ Robust.Shared/Players/ICommonSession.cs
@@ -1,29 +1,34 @@
-﻿using Robust.Shared.Enums;
+using Robust.Shared.Enums;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Network;
 
 namespace Robust.Shared.Players
 {
     /// <summary>
-    ///     Common info between client and server sessions.
+    /// Common info between client and server sessions.
     /// </summary>
     public interface ICommonSession
     {
         /// <summary>
-        ///     Status of the session.
+        /// Status of the session.
         /// </summary>
         SessionStatus Status { get; set; }
 
         IEntity? AttachedEntity { get; }
 
         /// <summary>
-        ///     The UID of this session.
+        /// The UID of this session.
         /// </summary>
         NetUserId UserId { get; }
 
         /// <summary>
-        ///     Current name of this player.
+        /// Current name of this player.
         /// </summary>
-        string Name { get; }
+         string Name { get; set; }
+
+        /// <summary>
+        /// Current connection latency of this session from the server to their client.
+        /// </summary>
+        short Ping { get; set; }
     }
 }

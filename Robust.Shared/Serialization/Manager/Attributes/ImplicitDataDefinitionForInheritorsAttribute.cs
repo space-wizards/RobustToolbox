5907e8039b4af26bba2d210b653f7b24169cf8fd
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-03-01 14:04:07 +0100

    Add base type required and usage for MeansDataDefinition and ImplicitDataDefinitionForInheritorsAttribute

--- Robust.Shared/Serialization/Manager/Attributes/ImplicitDataDefinitionForInheritorsAttribute.cs
+++ Robust.Shared/Serialization/Manager/Attributes/ImplicitDataDefinitionForInheritorsAttribute.cs
@@ -2,5 +2,6 @@ using System;
 
 namespace Robust.Shared.Serialization.Manager.Attributes
 {
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface)]
     public class ImplicitDataDefinitionForInheritorsAttribute : Attribute { }
 }

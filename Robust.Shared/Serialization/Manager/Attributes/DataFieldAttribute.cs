b586034b55e80f8d01f0ed75ad29f29ecc7d12cd
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-03-01 14:35:01 +0100

    Update data field required summary with the new forbidden docs

--- Robust.Shared/Serialization/Manager/Attributes/DataFieldAttribute.cs
+++ Robust.Shared/Serialization/Manager/Attributes/DataFieldAttribute.cs
@@ -8,7 +8,14 @@ namespace Robust.Shared.Serialization.Manager.Attributes
     public class DataFieldAttribute : BaseDataFieldAttribute
     {
         public readonly bool ReadOnly;
+
+        /// <summary>
+        ///     Whether or not this field being mapped is required for the component to function.
+        ///     This will not guarantee that the field is mapped when the program is run,
+        ///     it is meant to be used as metadata information.
+        /// </summary>
         public readonly bool Required;
+
         public readonly bool ServerOnly;
 
         public DataFieldAttribute([NotNull] string tag, bool readOnly = false, int priority = 1, bool required = false, bool serverOnly = false) : base(tag, priority)

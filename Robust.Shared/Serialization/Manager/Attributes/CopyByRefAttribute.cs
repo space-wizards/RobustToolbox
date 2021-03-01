44e93991bd31d85a6670e69100e17d3d8a5549dc
Paul <ritter.paul1+git@googlemail.com>
2021-03-01 14:09:59 +0100

    copy by ref

--- Robust.Shared/Serialization/Manager/Attributes/CopyByRefAttribute.cs
+++ Robust.Shared/Serialization/Manager/Attributes/CopyByRefAttribute.cs
@@ -2,5 +2,6 @@ using System;
 
 namespace Robust.Shared.Serialization.Manager.Attributes
 {
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum, Inherited = false)]
     public class CopyByRefAttribute : Attribute {}
 }

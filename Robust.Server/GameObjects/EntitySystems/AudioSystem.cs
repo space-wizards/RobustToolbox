0a8a94aa00b3f5f353759746088e887bf74651ef
Acruid <shatter66@gmail.com>
2021-03-01 19:24:50 -0800

    Marked the old serverside functions to play sound obsolete, use the new ones from the IAudioSystem.

--- Robust.Server/GameObjects/EntitySystems/AudioSystem.cs
+++ Robust.Server/GameObjects/EntitySystems/AudioSystem.cs
@@ -74,6 +74,7 @@ namespace Robust.Server.GameObjects
         /// <param name="predicate">The predicate that will be used to send the audio to players, or null to send to everyone.</param>
         /// <param name="excludedSession">Session that won't receive the audio message.</param>
         /// <param name="recipients"></param>
+        [Obsolete("Use the Play() overload.")]
         public IPlayingAudioStream PlayGlobal(string filename, AudioParams? audioParams = null, Func<IPlayerSession, bool>? predicate = null, IPlayerSession? excludedSession = null)
         {
             var id = CacheIdentifier();
@@ -115,6 +116,7 @@ namespace Robust.Server.GameObjects
         /// <param name="audioParams"></param>
         /// <param name="range">The max range at which the audio will be heard. Less than or equal to 0 to send to every player.</param>
         /// <param name="excludedSession">Sessions that won't receive the audio message.</param>
+        [Obsolete("Use the Play() overload.")]
         public IPlayingAudioStream PlayFromEntity(string filename, IEntity entity, AudioParams? audioParams = null, int range = AudioDistanceRange, IPlayerSession? excludedSession = null)
         {
             var id = CacheIdentifier();
@@ -165,6 +167,7 @@ namespace Robust.Server.GameObjects
         /// <param name="audioParams"></param>
         /// <param name="range">The max range at which the audio will be heard. Less than or equal to 0 to send to every player.</param>
         /// <param name="excludedSession">Session that won't receive the audio message.</param>
+        [Obsolete("Use the Play() overload.")]
         public IPlayingAudioStream PlayAtCoords(string filename, EntityCoordinates coordinates, AudioParams? audioParams = null, int range = AudioDistanceRange, IPlayerSession? excludedSession = null)
         {
             var id = CacheIdentifier();

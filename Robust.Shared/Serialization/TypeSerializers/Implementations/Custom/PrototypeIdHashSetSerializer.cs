fbd0df0a5fbe7c76d7f51c715701d4f9e378930a
Vera Aguilera Puerto <zddm@outlook.es>
2021-03-25 14:23:32 +0100

    cleanup, better stuff

--- Robust.Shared/Serialization/TypeSerializers/Implementations/Custom/PrototypeIdHashSetSerializer.cs
+++ Robust.Shared/Serialization/TypeSerializers/Implementations/Custom/PrototypeIdHashSetSerializer.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using Robust.Shared.IoC;
 using Robust.Shared.Prototypes;
@@ -14,31 +15,34 @@ namespace Robust.Shared.Serialization.TypeSerializers.Implementations.Custom
     {
         private readonly PrototypeIdSerializer<TPrototype> _prototypeSerializer = new();
 
-        public ValidationNode Validate(ISerializationManager serializationManager, SequenceDataNode node,
-            IDependencyCollection dependencies, ISerializationContext? context = null)
+        public ValidationNode Validate(ISerializationManager serializationManager, SequenceDataNode node, IDependencyCollection dependencies, ISerializationContext? context = null)
         {
             var list = new List<ValidationNode>();
 
             foreach (var dataNode in node.Sequence)
             {
-                var value = (ValueDataNode) dataNode;
+                if (dataNode is not ValueDataNode value)
+                {
+                    list.Add(new ErrorNode(dataNode, $"Cannot cast node {dataNode} to ValueDataNode."));
+                    continue;
+                }
+
                 list.Add(_prototypeSerializer.Validate(serializationManager, value, dependencies, context));
             }
 
             return new ValidatedSequenceNode(list);
         }
 
-        public DeserializationResult Read(ISerializationManager serializationManager, SequenceDataNode node,
-            IDependencyCollection dependencies, bool skipHook, ISerializationContext? context = null)
+        public DeserializationResult Read(ISerializationManager serializationManager, SequenceDataNode node, IDependencyCollection dependencies, bool skipHook, ISerializationContext? context = null)
         {
             var set = new HashSet<string>();
             var mappings = new List<DeserializationResult>();
 
             foreach (var dataNode in node.Sequence)
             {
-                var (value, result) = serializationManager.ReadWithValueOrThrow<string>(dataNode, context, skipHook);
+                var result = _prototypeSerializer.Read(serializationManager, (ValueDataNode)dataNode, dependencies, skipHook, context);
 
-                set.Add(value);
+                set.Add((string)result.RawValue!);
                 mappings.Add(result);
             }
 
@@ -46,23 +50,21 @@ namespace Robust.Shared.Serialization.TypeSerializers.Implementations.Custom
                 elements => new HashSet<string>(elements));
         }
 
-        public DataNode Write(ISerializationManager serializationManager, HashSet<string> value, bool alwaysWrite = false,
-                ISerializationContext? context = null)
+        public DataNode Write(ISerializationManager serializationManager, HashSet<string> value, bool alwaysWrite = false, ISerializationContext? context = null)
+        {
+            var list = new List<DataNode>();
+
+            foreach (var str in value)
             {
-                var list = new List<DataNode>();
-
-                foreach (var str in value)
-                {
-                    list.Add(_prototypeSerializer.Write(serializationManager, str, alwaysWrite, context));
-                }
-
-                return new SequenceDataNode(list);
+                list.Add(_prototypeSerializer.Write(serializationManager, str, alwaysWrite, context));
             }
 
-            public HashSet<string> Copy(ISerializationManager serializationManager, HashSet<string> source, HashSet<string> target, bool skipHook,
-                ISerializationContext? context = null)
-            {
-                return new (source);
-            }
+            return new SequenceDataNode(list);
+        }
+
+        public HashSet<string> Copy(ISerializationManager serializationManager, HashSet<string> source, HashSet<string> target, bool skipHook, ISerializationContext? context = null)
+        {
+            return new (source);
+        }
     }
 }

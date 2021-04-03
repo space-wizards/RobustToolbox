741240bcad464b6f3a7decccbe6d7f006cd17224
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-04-03 16:23:34 +0200

    Remove testing code

--- Robust.Benchmarks/Program.cs
+++ Robust.Benchmarks/Program.cs
@@ -6,13 +6,6 @@ namespace Robust.Benchmarks
     {
         public static void Main(string[] args)
         {
-            // var b = new SerializationReadBenchmark();
-            //
-            // for (var i = 0; i < 100000; i++)
-            // {
-            //     b.ReadSeedDataDefinition();
-            // }
-
             BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run();
         }
     }

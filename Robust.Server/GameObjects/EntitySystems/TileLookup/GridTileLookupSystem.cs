ccf212e9cbd4dd89c543ddf60410fbbbdde9fe8c
Vera Aguilera Puerto <zddm@outlook.es>
2021-04-09 13:51:12 +0200

    GridTileLookupSystem unsubscribes from events on shutdown.

--- Robust.Server/GameObjects/EntitySystems/TileLookup/GridTileLookupSystem.cs
+++ Robust.Server/GameObjects/EntitySystems/TileLookup/GridTileLookupSystem.cs
@@ -219,6 +219,9 @@ namespace Robust.Server.GameObjects
         public override void Shutdown()
         {
             base.Shutdown();
+            UnsubscribeLocalEvent<MoveEvent>();
+            UnsubscribeLocalEvent<EntityInitializedMessage>();
+            UnsubscribeLocalEvent<EntityDeletedMessage>();
             _mapManager.OnGridCreated -= HandleGridCreated;
             _mapManager.OnGridRemoved -= HandleGridRemoval;
             _mapManager.TileChanged -= HandleTileChanged;

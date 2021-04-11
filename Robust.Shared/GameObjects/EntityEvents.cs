4d5dc10aa5f90710017c3c2d9113c1e9e28f9573
Vera Aguilera Puerto <zddm@outlook.es>
2021-04-11 16:34:47 +0200

    Adds abstract class for entity events that can be cancelled.

--- Robust.Shared/GameObjects/EntityEvents.cs
+++ Robust.Shared/GameObjects/EntityEvents.cs
@@ -17,9 +17,28 @@ namespace Robust.Shared.GameObjects
     public abstract class HandledEntityEventArgs : EntityEventArgs
     {
         /// <summary>
-        /// If this message has already been "handled" by a previous system.
+        ///     If this message has already been "handled" by a previous system.
         /// </summary>
         public bool Handled { get; set; }
+    }
+
+    [Serializable, NetSerializable]
+    public abstract class CancellableEntityEventArgs : EntityEventArgs
+    {
+        /// <summary>
+        ///     Whether this even has been cancelled.
+        /// </summary>
+        public bool Cancelled { get; private set; }
+
+        /// <summary>
+        ///     Cancels the event.
+        /// </summary>
+        public void Cancel() => Cancelled = true;
+
+        /// <summary>
+        ///     Uncancels the event. Don't call this if you don't know what you're doing.
+        /// </summary>
+        public void Uncancel() => Cancelled = false;
     }
 
     public readonly struct EntitySessionEventArgs

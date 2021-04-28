1fdaba5647b66c208be5ea78d5f51ac0953cad56
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2021-04-28 20:37:06 +0200

    WindowId struct in preparation for multi-window.

---
+++ Robust.Shared/Map/WindowId.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Robust.Shared.Serialization;
+
+namespace Robust.Shared.Map
+{
+    [Serializable, NetSerializable]
+    public readonly struct WindowId : IEquatable<WindowId>
+    {
+        internal readonly int Value;
+
+        public WindowId(int value)
+        {
+            Value = value;
+        }
+
+        public bool Equals(WindowId other)
+        {
+            return Value == other.Value;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is WindowId id && Equals(id);
+        }
+
+        public override int GetHashCode()
+        {
+            return Value;
+        }
+
+        public static bool operator ==(WindowId a, WindowId b)
+        {
+            return a.Value == b.Value;
+        }
+
+        public static bool operator !=(WindowId a, WindowId b)
+        {
+            return !(a == b);
+        }
+
+        public static explicit operator int(WindowId self)
+        {
+            return self.Value;
+        }
+
+        public override string ToString()
+        {
+            return $"Window {Value}";
+        }
+    }
+}

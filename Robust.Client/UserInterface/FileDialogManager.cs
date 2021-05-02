7c008e857d305b8d6e617113cebdfc77d0ab52b9
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2021-05-02 09:35:38 +0200

    Rewrite FileDialogManager to remove #if from it.

--- Robust.Client/UserInterface/FileDialogManager.cs
+++ Robust.Client/UserInterface/FileDialogManager.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Robust.Client.Graphics;
 using Robust.Shared;
+using Robust.Shared.Asynchronous;
 using Robust.Shared.IoC;
 using Robust.Shared.Log;
 using Robust.Shared.Utility;
@@ -23,18 +24,11 @@ namespace Robust.Client.UserInterface
         // Uses nativefiledialog to open the file dialogs cross platform.
         // On Linux, if the kdialog command is found, it will be used instead.
         // TODO: Should we maybe try to avoid running kdialog if the DE isn't KDE?
-#if LINUX
         [Dependency] private readonly IClydeInternal _clyde = default!;
-#endif
+        [Dependency] private readonly ITaskManager _taskManager = default!;
 
-#if MACOS
-        [Dependency] private readonly Shared.Asynchronous.ITaskManager _taskManager = default!;
-#endif
-
-#if LINUX
         private bool _kDialogAvailable;
         private bool _checkedKDialogAvailable;
-#endif
 
         static FileDialogManager()
         {
@@ -54,17 +48,11 @@ namespace Robust.Client.UserInterface
 
         private async Task<string?> GetOpenFileName(FileDialogFilters? filters)
         {
-#if LINUX
-            if (filters == null)
-            {
-                return null;
-            }
-
             if (await IsKDialogAvailable())
             {
                 return await OpenFileKDialog(filters);
             }
-#endif
+
             return await OpenFileNfd(filters);
         }
 
@@ -88,27 +76,13 @@ namespace Robust.Client.UserInterface
 
         private async Task<string?> GetSaveFileName()
         {
-#if LINUX
             if (await IsKDialogAvailable())
             {
                 return await SaveFileKDialog();
             }
-#endif
+
             return await SaveFileNfd();
         }
-
-        /*
-        public async Task<string?> OpenFolder()
-        {
-#if LINUX
-            if (await IsKDialogAvailable())
-            {
-                return await OpenFolderKDialog();
-            }
-#endif
-            return await OpenFolderNfd();
-        }
-        */
 
         private unsafe Task<string?> OpenFileNfd(FileDialogFilters? filters)
         {
@@ -156,6 +130,7 @@ namespace Robust.Client.UserInterface
             });
         }
 
+        /*
         private unsafe Task<string?> OpenFolderNfd()
         {
             // Have to run it in the thread pool to avoid blocking the main thread.
@@ -168,33 +143,38 @@ namespace Robust.Client.UserInterface
                 return HandleNfdResult(result, outPath);
             });
         }
+        */
 
         // ReSharper disable once MemberCanBeMadeStatic.Local
         private Task<string?> RunAsyncMaybe(Func<string?> action)
         {
-#if MACOS
-            // macOS seems pretty annoying about having the file dialog opened from the main thread.
-            // So we are forced to execute this synchronously on the main thread.
-            // Also I'm calling RunOnMainThread here to provide safety in case this is ran from a different thread.
-            // nativefiledialog doesn't provide any form of async API, so this WILL lock up the client.
-            var tcs = new TaskCompletionSource<string>();
-            _taskManager.RunOnMainThread(() => tcs.SetResult(action()));
-
-            return tcs.Task;
-#else
-            // Luckily, GTK Linux and COM Windows are both happily threaded. Yay!
-            // * Actual attempts to have multiple file dialogs up at the same time, and the resulting crashes,
-            // have shown that at least for GTK+ (Linux), just because it can handle being on any thread doesn't mean it handle being on two at the same time.
-            // Testing system was Ubuntu 20.04.
-            // COM on Windows might handle this, but honestly, who exactly wants to risk it?
-            // In particular this could very well be an swnfd issue.
-            return Task.Run(() =>
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
             {
-                lock (this) {
-                    return action();
-                }
-            });
-#endif
+                // macOS seems pretty annoying about having the file dialog opened from the main thread.
+                // So we are forced to execute this synchronously on the main thread.
+                // Also I'm calling RunOnMainThread here to provide safety in case this is ran from a different thread.
+                // nativefiledialog doesn't provide any form of async API, so this WILL lock up the client.
+                var tcs = new TaskCompletionSource<string?>();
+                _taskManager.RunOnMainThread(() => tcs.SetResult(action()));
+
+                return tcs.Task;
+            }
+            else
+            {
+                // Luckily, GTK Linux and COM Windows are both happily threaded. Yay!
+                // * Actual attempts to have multiple file dialogs up at the same time, and the resulting crashes,
+                // have shown that at least for GTK+ (Linux), just because it can handle being on any thread doesn't mean it handle being on two at the same time.
+                // Testing system was Ubuntu 20.04.
+                // COM on Windows might handle this, but honestly, who exactly wants to risk it?
+                // In particular this could very well be an swnfd issue.
+                return Task.Run(() =>
+                {
+                    lock (this)
+                    {
+                        return action();
+                    }
+                });
+            }
         }
 
         private static unsafe string? HandleNfdResult(sw_nfdresult result, byte* outPath)
@@ -219,7 +199,6 @@ namespace Robust.Client.UserInterface
             }
         }
 
-#if LINUX
         private async Task CheckKDialogSupport()
         {
             var currentDesktop = Environment.GetEnvironmentVariable("XDG_CURRENT_DESKTOP");
@@ -347,6 +326,9 @@ namespace Robust.Client.UserInterface
 
         private async Task<bool> IsKDialogAvailable()
         {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                return false;
+
             if (!_checkedKDialogAvailable)
             {
                 await CheckKDialogSupport();
@@ -355,7 +337,6 @@ namespace Robust.Client.UserInterface
 
             return _kDialogAvailable;
         }
-#endif
 
         [DllImport("swnfd.dll")]
         private static extern unsafe byte* sw_NFD_GetError();
@@ -368,9 +349,11 @@ namespace Robust.Client.UserInterface
         private static extern unsafe sw_nfdresult
             sw_NFD_SaveDialog(byte* filterList, byte* defaultPath, byte** outPath);
 
+        /*
         [DllImport("swnfd.dll")]
         private static extern unsafe sw_nfdresult
             sw_NFD_PickFolder(byte* defaultPath, byte** outPath);
+            */
 
         [DllImport("swnfd.dll")]
         private static extern unsafe void sw_NFD_Free(void* ptr);

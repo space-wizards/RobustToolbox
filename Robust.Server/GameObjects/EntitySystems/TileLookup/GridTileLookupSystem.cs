f0da64f1948ccace70fdda826836428b02734dc1
metalgearsloth <comedian_vs_clown@hotmail.com>
2021-05-04 13:12:01 +1000

    gucci 100

--- Robust.Server/GameObjects/EntitySystems/TileLookup/GridTileLookupSystem.cs
+++ Robust.Server/GameObjects/EntitySystems/TileLookup/GridTileLookupSystem.cs
@@ -320,7 +320,12 @@ namespace Robust.Server.GameObjects
         /// <param name="moveEvent"></param>
         private void HandleEntityMove(MoveEvent moveEvent)
         {
-            if (moveEvent.Sender.Deleted || moveEvent.NewPosition.GetGridId(EntityManager) == GridId.Invalid || !moveEvent.NewPosition.IsValid(EntityManager))
+            // TODO: When Acruid does TileEntities we may actually be able to delete this system if tile lookups become fast enough
+            var gridId = moveEvent.NewPosition.GetGridId(EntityManager);
+
+            if (moveEvent.Sender.Deleted ||
+                gridId == GridId.Invalid ||
+                !moveEvent.NewPosition.IsValid(EntityManager))
             {
                 HandleEntityRemove(moveEvent.Sender);
                 return;
@@ -332,7 +337,7 @@ namespace Robust.Server.GameObjects
             }
 
             // Memory leak protection
-            var gridBounds = _mapManager.GetGrid(moveEvent.Sender.Transform.GridID).WorldBounds;
+            var gridBounds = _mapManager.GetGrid(gridId).WorldBounds;
             if (!gridBounds.Contains(moveEvent.Sender.Transform.WorldPosition))
             {
                 HandleEntityRemove(moveEvent.Sender);
--- Robust.Shared/GameObjects/Systems/SharedGridTraversalSystem.cs
+++ Robust.Shared/GameObjects/Systems/SharedGridTraversalSystem.cs
@@ -29,58 +29,43 @@ namespace Robust.Shared.GameObjects
             UnsubscribeLocalEvent<MoveEvent>();
         }
 
-        public override void FrameUpdate(float frameTime)
-        {
-            base.FrameUpdate(frameTime);
-            while (_queuedMoveEvents.TryPop(out var moveEvent))
-            {
-                var entity = moveEvent.Sender;
-
-                if (_handledThisTick.Contains(entity.Uid)) continue;
-
-                _handledThisTick.Add(entity.Uid);
-
-                if (entity.Deleted ||
-                    entity.HasComponent<IMapComponent>() ||
-                    entity.HasComponent<IMapGridComponent>() ||
-                    entity.IsInContainer())
-                {
-                    continue;
-                }
-
-                var transform = entity.Transform;
-
-                // Change parent if necessary
-                if (_mapManager.TryFindGridAt(transform.MapID, moveEvent.NewPosition.ToMapPos(EntityManager), out var grid) &&
-                    grid.GridEntityId.IsValid() &&
-                    grid.GridEntityId != entity.Uid)
-                {
-                    // Some minor duplication here with AttachParent but only happens when going on/off grid so not a big deal ATM.
-                    if (grid.Index != transform.GridID)
-                    {
-                        transform.AttachParent(EntityManager.GetEntity(grid.GridEntityId));
-                        RaiseLocalEvent(new ChangedGridMessage(entity, transform.GridID, grid.Index));
-                    }
-                }
-                else
-                {
-                    var oldGridId = transform.GridID;
-
-                    // Attach them to map / they are on an invalid grid
-                    if (oldGridId != GridId.Invalid)
-                    {
-                        transform.AttachParent(_mapManager.GetMapEntity(transform.MapID));
-                        RaiseLocalEvent(new ChangedGridMessage(entity, oldGridId, GridId.Invalid));
-                    }
-                }
-            }
-
-            _handledThisTick.Clear();
-        }
-
         private void QueueMoveEvent(MoveEvent moveEvent)
         {
-            _queuedMoveEvents.Push(moveEvent);
+            var entity = moveEvent.Sender;
+
+            if (entity.Deleted ||
+                entity.HasComponent<IMapComponent>() ||
+                entity.HasComponent<IMapGridComponent>() ||
+                entity.IsInContainer())
+            {
+                return;
+            }
+
+            var transform = entity.Transform;
+
+            // Change parent if necessary
+            if (_mapManager.TryFindGridAt(transform.MapID, moveEvent.NewPosition.ToMapPos(EntityManager), out var grid) &&
+                grid.GridEntityId.IsValid() &&
+                grid.GridEntityId != entity.Uid)
+            {
+                // Some minor duplication here with AttachParent but only happens when going on/off grid so not a big deal ATM.
+                if (grid.Index != transform.GridID)
+                {
+                    transform.AttachParent(EntityManager.GetEntity(grid.GridEntityId));
+                    RaiseLocalEvent(new ChangedGridMessage(entity, transform.GridID, grid.Index));
+                }
+            }
+            else
+            {
+                var oldGridId = transform.GridID;
+
+                // Attach them to map / they are on an invalid grid
+                if (oldGridId != GridId.Invalid)
+                {
+                    transform.AttachParent(_mapManager.GetMapEntity(transform.MapID));
+                    RaiseLocalEvent(new ChangedGridMessage(entity, oldGridId, GridId.Invalid));
+                }
+            }
         }
     }
 

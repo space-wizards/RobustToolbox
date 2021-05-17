1d657619f05f3cd739dff9249f342a20513276b3
metalgearsloth <comedian_vs_clown@hotmail.com>
2021-05-17 18:32:31 +1000

    RenderingTreeSystem cleanup
    10% less bad but 100% still boilerplate

--- Robust.Client/GameObjects/Components/Renderable/SpriteComponent.cs
+++ Robust.Client/GameObjects/Components/Renderable/SpriteComponent.cs
@@ -125,6 +125,18 @@ namespace Robust.Client.GameObjects
         [DataField("directional")]
         private bool _directional = true;
 
+        /// <summary>
+        /// What MapId we are intersecting for RenderingTreeSystem.
+        /// </summary>
+        [ViewVariables]
+        internal MapId IntersectingMapId { get; set; } = MapId.Nullspace;
+
+        /// <summary>
+        /// What grids we're on for RenderingTreeSystem.
+        /// </summary>
+        [ViewVariables]
+        internal List<GridId> IntersectingGrids { get; } = new();
+
         [DataField("layerDatums")]
         private List<PrototypeLayerData> LayerDatums
         {
@@ -1355,7 +1367,7 @@ namespace Robust.Client.GameObjects
             if (map != MapId.Nullspace)
             {
                 Owner.EntityManager.EventBus.RaiseEvent(EventSource.Local,
-                    new RenderTreeRemoveSpriteMessage(this, map));
+                    new RenderTreeRemoveSpriteEvent(this, map));
             }
         }
 

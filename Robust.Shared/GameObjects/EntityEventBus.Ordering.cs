9deda1df0d23a6831259ab959f73036e33eddd4d
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2021-06-12 00:20:27 +0200

    Implement subscription ordering into EventBus.

---
+++ Robust.Shared/GameObjects/EntityEventBus.Ordering.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Robust.Shared.Utility;
+
+namespace Robust.Shared.GameObjects
+{
+    internal partial class EntityEventBus
+    {
+        // TODO: Topological sort is currently done every time an event is emitted.
+        // This should be fine for low-volume stuff like interactions, but definitely not for anything high volume.
+        // Not sure if we could pre-cache the topological sort, here.
+
+        // Ordered event raising is slow so if this event has any ordering dependencies we use a slower path.
+        private readonly HashSet<Type> _orderedEvents = new();
+
+        private void ProcessSingleEventOrdered(EventSource source, object eventArgs, Type eventType)
+        {
+            var found = new List<(EventHandler, OrderingData?)>();
+
+            CollectBroadcastOrdered(source, eventType, found);
+
+            DispatchOrderedEvents(eventArgs, found);
+        }
+
+        private void CollectBroadcastOrdered(
+            EventSource source,
+            Type eventType,
+            List<(EventHandler, OrderingData?)> found)
+        {
+            if (!_eventSubscriptions.TryGetValue(eventType, out var subs))
+                return;
+
+            foreach (var handler in subs)
+            {
+                if ((handler.Mask & source) != 0)
+                    found.Add((handler.Handler, handler.Ordering));
+            }
+        }
+
+        private void RaiseLocalOrdered<TEvent>(
+            EntityUid uid,
+            TEvent args,
+            bool broadcast)
+            where TEvent : EntityEventArgs
+        {
+            var found = new List<(EventHandler, OrderingData?)>();
+
+            if (broadcast)
+                CollectBroadcastOrdered(EventSource.Local, typeof(TEvent), found);
+
+            _eventTables.CollectOrdered(uid, typeof(TEvent), found);
+
+            DispatchOrderedEvents(args, found);
+
+            if (broadcast)
+                ProcessAwaitingMessages(EventSource.Local, args, typeof(TEvent));
+        }
+
+        private static void DispatchOrderedEvents(object eventArgs, List<(EventHandler, OrderingData?)> found)
+        {
+            var dict = new Dictionary<Type, (EventHandler handler, TopologicalSort.GraphNode<Type> node)>();
+
+            foreach (var (handler, orderData) in found)
+            {
+                if (orderData != null)
+                {
+                    var node = new TopologicalSort.GraphNode<Type>(orderData.OrderType);
+                    dict.Add(orderData.OrderType, (handler, node));
+                }
+            }
+
+            foreach (var (_, orderData) in found)
+            {
+                if (orderData == null)
+                    continue;
+
+                var (type, before, after) = orderData;
+
+                var thisNode = dict[type];
+
+                if (before != null)
+                {
+                    foreach (var bef in before)
+                    {
+                        if (dict.TryGetValue(bef, out var befNode))
+                            thisNode.node.Dependant.Add(befNode.node);
+                    }
+                }
+
+                if (after != null)
+                {
+                    foreach (var aft in after)
+                    {
+                        if (dict.TryGetValue(aft, out var aftNode))
+                            aftNode.node.Dependant.Add(thisNode.node);
+                    }
+                }
+            }
+
+            foreach (var type in TopologicalSort.Sort(dict.Values.Select(c => c.node)))
+            {
+                var handler = dict[type].handler;
+
+                handler(eventArgs);
+            }
+        }
+
+        private void HandleOrderRegistration(Type eventType, OrderingData? data)
+        {
+            if (data == null)
+                return;
+
+            if (data.Before != null || data.After != null)
+                _orderedEvents.Add(eventType);
+        }
+
+        private sealed record OrderingData(Type OrderType, Type[]? Before, Type[]? After);
+
+    }
+}
---
+++ Robust.Shared/Utility/TopologicalSort.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Robust.Shared.Utility
+{
+    public sealed class TopologicalSort
+    {
+        public static IEnumerable<T> Sort<T>(IEnumerable<GraphNode<T>> nodes)
+        {
+            var totalVerts = 0;
+            var empty = new Queue<GraphNode<T>>();
+
+            var nodesArray = nodes.ToArray();
+
+            foreach (var node in nodesArray)
+            {
+                totalVerts += 1;
+
+                foreach (var dep in node.Dependant)
+                {
+                    dep.DependsOnCount += 1;
+                }
+            }
+
+            foreach (var node in nodesArray)
+            {
+                if (node.DependsOnCount == 0)
+                    empty.Enqueue(node);
+            }
+
+            while (empty.TryDequeue(out var node))
+            {
+                yield return node.Value;
+                totalVerts -= 1;
+
+                foreach (var dep in node.Dependant)
+                {
+                    dep.DependsOnCount -= 1;
+                    if (dep.DependsOnCount == 0)
+                        empty.Enqueue(dep);
+                }
+            }
+
+            if (totalVerts != 0)
+                throw new InvalidOperationException("Graph contained cycle(s).");
+        }
+
+        [DebuggerDisplay("GraphNode: {" + nameof(System) + "}")]
+        public sealed class GraphNode<T>
+        {
+            public readonly T Value;
+            public readonly List<GraphNode<T>> Dependant = new();
+            public int DependsOnCount;
+
+            public GraphNode(T value)
+            {
+                Value = value;
+            }
+        }
+    }
+}

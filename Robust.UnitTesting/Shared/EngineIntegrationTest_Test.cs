0e9a0bec4d9526aa216698aafd1aff554fa19474
Vera Aguilera Puerto <gradientvera@outlook.com>
2021-07-03 14:06:05 +0200

    Add integration test for engine integration tests working correctly

---
+++ Robust.UnitTesting/Shared/EngineIntegrationTest_Test.cs
@@ -0,0 +1,75 @@
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Robust.Shared.Enums;
+using Robust.Shared.IoC;
+using Robust.Shared.Network;
+using IPlayerManager = Robust.Server.Player.IPlayerManager;
+
+namespace Robust.UnitTesting.Shared
+{
+    [TestFixture]
+    public class EngineIntegrationTest_Test : RobustIntegrationTest
+    {
+        [Test]
+        public void ServerStartsCorrectlyTest()
+        {
+            ServerIntegrationInstance? server = null;
+            Assert.DoesNotThrow(() => server = StartServer());
+            Assert.That(server, Is.Not.Null);
+        }
+
+        [Test]
+        public void ClientStartsCorrectlyTest()
+        {
+            ClientIntegrationInstance? client = null;
+            Assert.DoesNotThrow(() => client = StartClient());
+            Assert.That(client, Is.Not.Null);
+        }
+
+        [Test]
+        public async Task ServerClientPairConnectCorrectlyTest()
+        {
+            var server = StartServer();
+            var client = StartClient();
+
+            Assert.That(server, Is.Not.Null);
+            Assert.That(client, Is.Not.Null);
+
+            await Task.WhenAll(client.WaitIdleAsync(), server.WaitIdleAsync());
+
+            // Connect client to the server...
+            Assert.DoesNotThrow(() => client.SetConnectTarget(server));
+            client.Post(() => IoCManager.Resolve<IClientNetManager>().ClientConnect(null!, 0, null!));
+
+            // Run 10 synced ticks...
+            for (int i = 0; i < 10; i++)
+            {
+                await server.WaitRunTicks(1);
+                await client.WaitRunTicks(1);
+            }
+
+            await server.WaitAssertion(() =>
+            {
+                var playerManager = IoCManager.Resolve<IPlayerManager>();
+
+                // There must be a player connected.
+                Assert.That(playerManager.PlayerCount, Is.EqualTo(1));
+
+                // Get the only player...
+                var player = playerManager.GetAllPlayers()[0];
+
+                Assert.That(player.Status, Is.EqualTo(SessionStatus.Connected));
+                Assert.That(player.ConnectedClient.IsConnected, Is.True);
+            });
+
+            await client.WaitAssertion(() =>
+            {
+                var netManager = IoCManager.Resolve<IClientNetManager>();
+
+                Assert.That(netManager.IsConnected, Is.True);
+                Assert.That(netManager.ServerChannel, Is.Not.Null);
+                Assert.That(netManager.ServerChannel!.IsConnected, Is.True);
+            });
+        }
+    }
+}

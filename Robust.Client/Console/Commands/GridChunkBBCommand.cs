ee019f2821c9793de84c8457f85da7ae2c974005
metalgearsloth <comedian_vs_clown@hotmail.com>
2021-07-22 22:07:28 +1000

    spellcheck

--- Robust.Client/Console/Commands/GridChunkBBCommand.cs
+++ Robust.Client/Console/Commands/GridChunkBBCommand.cs
@@ -7,7 +7,7 @@ namespace Robust.Client.Console.Commands
     public class GridChunkBBCommand : IConsoleCommand
     {
         public string Command => "showchunkbb";
-        public string Description => "Dispalys chunk bounds for the purposes of rendering";
+        public string Description => "Displays chunk bounds for the purposes of rendering";
         public string Help => $"{Command}";
         public void Execute(IConsoleShell shell, string argStr, string[] args)
         {

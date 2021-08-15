11ad41c150540baf06ae11a2e725bb46a288609d
metalgearsloth <comedian_vs_clown@hotmail.com>
2021-08-15 13:51:39 +1000

    Optimise GetMapChunks
    Don't need to check every chunk's bounds when you can just do dictionary lookups. Should be an okay bonus for PVS and grid rendering on larger grids. Didn't make tests yet because all of the existing ones are mocked and painful to change.

--- Robust.Shared/Map/IMapGridInternal.cs
+++ Robust.Shared/Map/IMapGridInternal.cs
@@ -48,5 +48,10 @@ namespace Robust.Shared.Map
         /// </summary>
         /// <returns>All chunks in the grid.</returns>
         IReadOnlyDictionary<Vector2i, IMapChunkInternal> GetMapChunks();
+
+        /// <summary>
+        ///     Returns all the <see cref="IMapChunkInternal"/> intersecting the worldAABB.
+        /// </summary>
+        IEnumerable<IMapChunkInternal> GetMapChunks(Box2 worldAABB);
     }
 }

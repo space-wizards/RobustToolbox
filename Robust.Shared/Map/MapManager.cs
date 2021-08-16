f97ad4a03794880c147654f9bae4e3107db96c05
metalgearsloth <comedian_vs_clown@hotmail.com>
2021-08-17 00:59:35 +1000

    Remove some of the jank imports

--- Robust.Shared/Map/MapManager.cs
+++ Robust.Shared/Map/MapManager.cs
@@ -7,8 +7,6 @@ using Robust.Shared.IoC;
 using Robust.Shared.Log;
 using Robust.Shared.Maths;
 using Robust.Shared.Physics;
-using Robust.Shared.Physics.Broadphase;
-using Robust.Shared.Physics.Collision;
 using Robust.Shared.Timing;
 using Robust.Shared.Utility;
 

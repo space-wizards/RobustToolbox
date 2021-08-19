820d988e0ab661ef0da835c3d91ba4088e3c81bd
Acruid <shatter66@gmail.com>
2021-08-19 04:48:44 -0700

    PVS is now aware of entity anchoring. (#1845)
    * PVS is now aware of entity anchoring.
    Misc PVS perf improvements.
    
    * Fix master merge
    
    * Optimise chunks intersecting
    
    * Ensure anchored entity chunks are always sent
    
    * Remove silly pool by me
    
    Co-authored-by: metalgearsloth <comedian_vs_clown@hotmail.com>

--- Robust.Shared/Map/IMapGridInternal.cs
+++ Robust.Shared/Map/IMapGridInternal.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using Robust.Shared.Maths;
 using Robust.Shared.Timing;
 
@@ -6,7 +6,7 @@ namespace Robust.Shared.Map
 {
     internal interface IMapGridInternal : IMapGrid
     {
-        GameTick LastModifiedTick { get; }
+        GameTick LastTileModifiedTick { get; }
 
         GameTick CurTick { get; }
 
@@ -17,7 +17,15 @@ namespace Robust.Shared.Map
         /// </summary>
         int ChunkCount { get; }
 
+        GameTick LastAnchoredModifiedTick { get; }
+
         void NotifyTileChanged(in TileRef tileRef, in Tile oldTile);
+
+        /// <summary>
+        /// Notifies the grid that an anchored entity is dirty.
+        /// </summary>
+        /// <param name="pos">Position of the entity in local tile indices.</param>
+        void AnchoredEntDirty(Vector2i pos);
 
         /// <summary>
         ///     Regenerates anything that is based on chunk collision data.

726bfd58f68aba04f45017915ef1214e1c763381
Paul Ritter <ritter.paul1@googlemail.com>
2021-08-23 20:32:51 +0200

    adds some helpers to robustrandom (#1969)
    Co-authored-by: Paul <ritter.paul1+git@googlemail.com>

--- Robust.Shared/Random/IRobustRandom.cs
+++ Robust.Shared/Random/IRobustRandom.cs
@@ -1,14 +1,23 @@
 using System.Collections.Generic;
+using Robust.Shared.Maths;
 
 namespace Robust.Shared.Random
 {
     public interface IRobustRandom
     {
+        float NextFloat();
+        public float NextFloat(float minValue, float maxValue)
+            => NextFloat() * (maxValue - minValue) + minValue;
+        public float NextFloat(float maxValue) => NextFloat() * maxValue;
         int Next();
         int Next(int minValue, int maxValue);
         int Next(int maxValue);
         double NextDouble();
         void NextBytes(byte[] buffer);
+
+        public Angle NextAngle() => NextFloat() * MathHelper.Pi * 2;
+        public Angle NextAngle(Angle minValue, Angle maxValue) => NextFloat() * (maxValue - minValue) + minValue;
+        public Angle NextAngle(Angle maxValue) => NextFloat() * maxValue;
 
         void Shuffle<T>(IList<T> list)
         {

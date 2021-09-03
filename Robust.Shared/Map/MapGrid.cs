accd68ab354ee0e6adf3c2408572e494e014612c
metalgearsloth <comedian_vs_clown@hotmail.com>
2021-09-03 12:22:17 +1000

    More optimisations

--- Robust.Shared/Map/MapGrid.cs
+++ Robust.Shared/Map/MapGrid.cs
@@ -285,7 +285,9 @@ namespace Robust.Shared.Map
         /// <inheritdoc />
         public IEnumerable<TileRef> GetTilesIntersecting(Box2 worldArea, bool ignoreEmpty = true, Predicate<TileRef>? predicate = null)
         {
-            var localArea = new Box2(WorldToLocal(worldArea.BottomLeft), WorldToLocal(worldArea.TopRight));
+            var matrix = InvWorldMatrix;
+
+            var localArea = new Box2(matrix.Transform(worldArea.BottomLeft), matrix.Transform(worldArea.TopRight));
             var gridTileLb = new Vector2i((int)Math.Floor(localArea.Left), (int)Math.Floor(localArea.Bottom));
             var gridTileRt = new Vector2i((int)Math.Floor(localArea.Right), (int)Math.Floor(localArea.Top));
 
@@ -677,7 +679,13 @@ namespace Robust.Shared.Map
         /// </summary>
         public Vector2i LocalToChunkIndices(EntityCoordinates gridPos)
         {
-            var local = WorldToLocal(gridPos.ToMapPos(_entityManager));
+            Vector2 local;
+
+            if (gridPos.EntityId == GridEntityId)
+                local = gridPos.Position;
+            else
+                local = WorldToLocal(gridPos.ToMapPos(_entityManager));
+
             var x = (int)Math.Floor(local.X / (TileSize * ChunkSize));
             var y = (int)Math.Floor(local.Y / (TileSize * ChunkSize));
             return new Vector2i(x, y);

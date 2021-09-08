d294ac892bee7dd62f4273ebc0d476fc9dab85a7
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-09-08 13:56:32 +0200

    Fix serialization manager shutdown

---
+++ Robust.UnitTesting/Shared/Serialization/SerializationShutdownTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Robust.UnitTesting.Shared.Serialization
+{
+    [TestFixture]
+    public class SerializationShutdownTest : SerializationTest
+    {
+        [Test]
+        public void SerializationInitializeShutdownInitializeTest()
+        {
+            Assert.DoesNotThrow(() =>
+            {
+                // First initialize is done in the parent class
+                Serialization.Shutdown();
+                Serialization.Initialize();
+            });
+        }
+    }
+}

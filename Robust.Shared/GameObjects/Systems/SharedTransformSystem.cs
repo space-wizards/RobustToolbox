1ca203636b7ce0eae1cebe4409cc9c02c98c7a77
metalgearsloth <comedian_vs_clown@hotmail.com>
2021-09-18 15:14:10 +1000

    Re-parent unanchored entities

--- Robust.Shared/GameObjects/Systems/SharedTransformSystem.cs
+++ Robust.Shared/GameObjects/Systems/SharedTransformSystem.cs
@@ -38,13 +38,18 @@ namespace Robust.Shared.GameObjects
 
         private void UnanchorAllEntsOnTile(IMapGrid grid, Vector2i tileIndices)
         {
-            var anchoredEnts = grid.GetAnchoredEntities(tileIndices);
+            var anchoredEnts = grid.GetAnchoredEntities(tileIndices).Where(e => EntityManager.EntityExists(e)).ToList();
 
-            foreach (var ent in anchoredEnts.ToList()) // changing anchored modifies this set
+            if (anchoredEnts.Count == 0) return;
+
+            var mapEnt = _mapManager.GetMapEntity(grid.ParentMapId);
+
+            foreach (var ent in anchoredEnts) // changing anchored modifies this set
             {
-                if (!EntityManager.EntityExists(ent)) continue;
-
-                ComponentManager.GetComponent<TransformComponent>(ent).Anchored = false;
+                var transform = ComponentManager.GetComponent<TransformComponent>(ent);
+                transform.Anchored = false;
+                // If the tile was nuked than that means no longer intersecting the grid hence parent to the map
+                transform.AttachParent(mapEnt);
             }
         }
 

b18283245b990588a5be908536d7916ee2bcc869
Vera Aguilera Puerto <gradientvera@outlook.com>
2021-09-20 10:53:39 +0200

    Fix assert

--- Robust.Shared/GameObjects/EntitySystem.Resolve.cs
+++ Robust.Shared/GameObjects/EntitySystem.Resolve.cs
@@ -6,14 +6,30 @@ namespace Robust.Shared.GameObjects
 {
     public abstract partial class EntitySystem
     {
+        /// <summary>
+        ///     Resolves the component on the entity but only if the component instance is null.
+        /// </summary>
+        /// <param name="uid">The entity where to query the components.</param>
+        /// <param name="component">A reference to the variable storing the component, or null if it has to be resolved.</param>
+        /// <typeparam name="TComp">The component type to resolve.</typeparam>
+        /// <returns>True if the component is not null or was resolved correctly, false if the component couldn't be resolved.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         protected bool Resolve<TComp>(EntityUid uid, [NotNullWhen(true)] ref TComp? component)
             where TComp : IComponent
         {
-            DebugTools.Assert(uid == component?.Owner.Uid, "Specified Entity is not the component's Owner!");
+            DebugTools.Assert(component == null || uid == component.Owner.Uid, "Specified Entity is not the component's Owner!");
             return component != null || ComponentManager.TryGetComponent(uid, out component);
         }
 
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="uid"></param>
+        /// <param name="comp1"></param>
+        /// <param name="comp2"></param>
+        /// <typeparam name="TComp1"></typeparam>
+        /// <typeparam name="TComp2"></typeparam>
+        /// <returns></returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         protected bool Resolve<TComp1, TComp2>(EntityUid uid, [NotNullWhen(true)] ref TComp1? comp1, [NotNullWhen(true)] ref TComp2? comp2)
             where TComp1 : IComponent

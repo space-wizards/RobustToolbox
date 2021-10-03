243f405baba6497b44c51a21ee6780d0512d1c44
Vera Aguilera Puerto <gradientvera@outlook.com>
2021-10-03 11:56:41 +0200

    Add Filter for getting players from entities. - Also adds method to merge two filters.

---
+++ Robust.Client/Player/FilterSystem.cs
@@ -0,0 +1,26 @@
+using Robust.Shared.GameObjects;
+using Robust.Shared.IoC;
+using Robust.Shared.Player;
+
+namespace Robust.Client.Player
+{
+    internal class FilterSystem : SharedFilterSystem
+    {
+        [Dependency] private readonly IPlayerManager _playerManager = default!;
+
+        public override Filter FromEntities(Filter filter, params EntityUid[] entities)
+        {
+            if (_playerManager.LocalPlayer is not { } localPlayer
+                || localPlayer.Session.AttachedEntityUid is not {} attachedUid)
+                return filter;
+
+            foreach (var uid in entities)
+            {
+                if (uid == attachedUid)
+                    filter.AddPlayer(localPlayer.Session);
+            }
+
+            return filter;
+        }
+    }
+}

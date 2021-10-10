034c392cbe26f1a34eeb0924e4571376485c7674
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2021-10-11 01:37:19 +0200

    Add GetComponentOrNull extension method.

---
+++ Robust.Shared/GameObjects/EntityManagerExt.cs
@@ -0,0 +1,14 @@
+﻿namespace Robust.Shared.GameObjects
+{
+    public static class EntityManagerExt
+    {
+        public static T? GetComponentOrNull<T>(this IEntityManager entityManager, EntityUid entityUid)
+            where T : class, IComponent
+        {
+            if (entityManager.TryGetComponent(entityUid, out T? component))
+                return component;
+
+            return null;
+        }
+    }
+}

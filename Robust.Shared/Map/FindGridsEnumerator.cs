6491e51f3d6ed6fc107fdb08aa1ef52a2a374a51
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2021-10-19 13:30:52 +1100

    Reduce FindGridsIntersecting allocations (#2119)
    * Reduce FindGridsIntersecting allocations
    
    I love struct enumerators
    
    * Do slightly smarter pooling in EntityViewCulling
    
    Even more alloc reductions
    
    * Forgot to re-add this pooling
    
    * Fix missing chunks

---
+++ Robust.Shared/Map/FindGridsEnumerator.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Maths;
+using Robust.Shared.Physics;
+
+namespace Robust.Shared.Map
+{
+    public struct FindGridsEnumerator : IDisposable
+    {
+        private IEntityManager _entityManager;
+
+        private Dictionary<GridId, MapGrid>.Enumerator _enumerator;
+
+        private MapId _mapId;
+        private Box2 _worldAABB;
+        private bool _approx;
+
+        internal FindGridsEnumerator(IEntityManager entityManager, Dictionary<GridId, MapGrid>.Enumerator enumerator, MapId mapId, Box2 worldAABB, bool approx)
+        {
+            _entityManager = entityManager;
+            _enumerator = enumerator;
+            _mapId = mapId;
+            _worldAABB = worldAABB;
+            _approx = approx;
+        }
+
+        public bool MoveNext([NotNullWhen(true)] out IMapGrid? grid)
+        {
+            while (true)
+            {
+                if (!_enumerator.MoveNext())
+                {
+                    grid = null;
+                    return false;
+                }
+
+                var (_, nextGrid) = _enumerator.Current;
+
+                if (nextGrid.ParentMapId != _mapId)
+                {
+                    continue;
+                }
+
+                var xformComp = _entityManager.GetComponent<TransformComponent>(nextGrid.GridEntityId);
+                var invMatrix3 = xformComp.InvWorldMatrix;
+                var localAABB = invMatrix3.TransformBox(_worldAABB);
+
+                if (!localAABB.Intersects(nextGrid.LocalBounds)) continue;
+
+                var intersects = false;
+
+                if (_entityManager.HasComponent<PhysicsComponent>(nextGrid.GridEntityId))
+                {
+                    nextGrid.GetLocalMapChunks(localAABB, out var enumerator);
+
+                    if (!_approx)
+                    {
+                        var transform = new Transform(xformComp.WorldPosition, xformComp.WorldRotation);
+
+                        while (!intersects && enumerator.MoveNext(out var chunk))
+                        {
+                            foreach (var fixture in chunk.Fixtures)
+                            {
+                                for (var i = 0; i < fixture.Shape.ChildCount; i++)
+                                {
+                                    if (!fixture.Shape.ComputeAABB(transform, i).Intersects(_worldAABB)) continue;
+
+                                    intersects = true;
+                                    break;
+                                }
+
+                                if (intersects) break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        intersects = enumerator.MoveNext(out _);
+                    }
+                }
+
+                if (!intersects && nextGrid.ChunkCount == 0 && !_worldAABB.Contains(xformComp.WorldPosition))
+                {
+                    continue;
+                }
+
+                grid = nextGrid;
+                return true;
+            }
+        }
+
+        public void Dispose()
+        {
+            _enumerator.Dispose();
+        }
+    }
+}

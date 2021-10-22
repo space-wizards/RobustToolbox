819ae087f434ce72ccb208404b949effb72575cf
Visne <vincefvanwijk@gmail.com>
2021-10-22 18:04:25 +0200

    Make access level settable

--- Robust.Client/UserInterface/Control.cs
+++ Robust.Client/UserInterface/Control.cs
@@ -40,6 +40,8 @@ namespace Robust.Client.UserInterface
         [ViewVariables]
         public string? Name { get; set; }
 
+        public AccessLevel? Access { get; set; }
+
         /// <summary>
         ///     If true, this control will always be rendered, even if other UI rendering is disabled.
         /// </summary>
@@ -994,5 +996,12 @@ namespace Robust.Client.UserInterface
         public readonly Control Control;
         public readonly int OldIndex;
         public readonly int NewIndex;
+    }
+
+    public enum AccessLevel
+    {
+        Private,
+        Protected,
+        Public,
     }
 }

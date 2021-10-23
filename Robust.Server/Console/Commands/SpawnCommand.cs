85b795632ab42125a90ee43a5c27a66c43a17966
moonheart08 <moonheart08@users.noreply.github.com>
2021-10-23 17:37:58 -0500

    better spawn command

--- Robust.Server/Console/Commands/SpawnCommand.cs
+++ Robust.Server/Console/Commands/SpawnCommand.cs
@@ -1,23 +1,40 @@
 using Robust.Server.GameObjects;
 using Robust.Server.Player;
 using Robust.Shared.Console;
+using Robust.Shared.GameObjects;
 using Robust.Shared.IoC;
+using Robust.Shared.Map;
 
 namespace Robust.Server.Console.Commands
 {
     public class SpawnCommand : IConsoleCommand
     {
         public string Command => "spawn";
-        public string Description => "Spawns an entity with specific type at your feet.";
-        public string Help => "spawn <entity type>";
+        public string Description => "Spawns an entity with specific type.";
+        public string Help => "spawn <prototype> OR spawn <prototype> <relative entity ID> OR spawn <prototype> <x> <y>";
 
         public void Execute(IConsoleShell shell, string argStr, string[] args)
         {
             var player = shell.Player as IPlayerSession;
             var ent = IoCManager.Resolve<IServerEntityManager>();
-            if (player?.AttachedEntity != null)
+            if (args.Length is < 1 or > 3)
+            {
+                shell.WriteError("Incorrect number of arguments");
+            }
+
+            if (player?.AttachedEntity != null && args.Length == 1)
             {
                 ent.SpawnEntity(args[0], player.AttachedEntity.Transform.Coordinates);
+            }
+            else if (args.Length == 2)
+            {
+                ent.SpawnEntity(args[0], ent.GetEntity(EntityUid.Parse(args[1])).Transform.Coordinates);
+            }
+            else if (player?.AttachedEntity != null)
+            {
+                var coords = new MapCoordinates( float.Parse(args[1]),
+                    float.Parse(args[2]), player.AttachedEntity.Transform.MapID);
+                ent.SpawnEntity(args[0], coords);
             }
         }
     }

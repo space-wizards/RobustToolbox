884fade25af06add7691aabdabb016f29806e8dc
E F R <602406+Efruit@users.noreply.github.com>
2021-10-30 11:26:23 +0000

    Add tab "completion" for the server-side C# console (#2176)

--- Robust.Client/UserInterface/CustomControls/ScriptConsole.cs
+++ Robust.Client/UserInterface/CustomControls/ScriptConsole.cs
@@ -1,6 +1,7 @@
 using Robust.Client.Graphics;
 using Robust.Client.UserInterface.Controls;
 using Robust.Shared.Maths;
+using Robust.Client.Console;
 using static Robust.Client.UserInterface.Controls.BoxContainer;
 
 namespace Robust.Client.UserInterface.CustomControls
@@ -10,6 +11,7 @@ namespace Robust.Client.UserInterface.CustomControls
         protected OutputPanel OutputPanel { get; }
         protected HistoryLineEdit InputBar { get; }
         protected Button RunButton { get; }
+        protected Completions Suggestions { get; }
 
         protected ScriptConsole()
         {
@@ -47,10 +49,15 @@ namespace Robust.Client.UserInterface.CustomControls
                 }
             });
 
+            Suggestions = new Completions(InputBar);
+            InputBar.OnTabComplete += _ => Complete();
+            InputBar.OnTextChanged += _ => Suggestions.TextChanged();
             InputBar.OnTextEntered += _ => Run();
             RunButton.OnPressed += _ => Run();
             MinSize = (550, 300);
         }
+
+        protected abstract void Complete();
 
         protected abstract void Run();
 

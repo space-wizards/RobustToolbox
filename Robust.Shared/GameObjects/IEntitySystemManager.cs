93670fe65522cc002e652f021512d4b8d80f22b0
Vera Aguilera Puerto <gradientvera@outlook.com>
2021-11-14 12:10:50 +0100

    Add Resolves to EntitySystemManager

--- Robust.Shared/GameObjects/IEntitySystemManager.cs
+++ Robust.Shared/GameObjects/IEntitySystemManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using Robust.Shared.IoC.Exceptions;
 
 namespace Robust.Shared.GameObjects
 {
@@ -38,6 +39,44 @@ namespace Robust.Shared.GameObjects
         /// <typeparam name="T">The type of entity system to find.</typeparam>
         /// <returns>The <see cref="IEntitySystem"/> instance matching the specified type.</returns>
         T GetEntitySystem<T>() where T : IEntitySystem;
+
+        /// <summary>
+        /// Resolves an entity system.
+        /// </summary>
+        /// <exception cref="UnregisteredTypeException">Thrown if the provided type is not registered.</exception>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown if the resolved type hasn't been created yet
+        /// because the dependency collection object graph still needs to be constructed for it.
+        /// </exception>
+        void Resolve<T>([NotNull] ref T? instance)
+            where T : IEntitySystem;
+
+        /// <inheritdoc cref="Resolve{T}(ref T?)"/>
+        /// <summary>
+        /// Resolve two entity systems.
+        /// </summary>
+        void Resolve<T1, T2>([NotNull] ref T1? instance1, [NotNull] ref T2? instance2)
+            where T1 : IEntitySystem
+            where T2 : IEntitySystem;
+
+        /// <inheritdoc cref="Resolve{T1, T2}(ref T1?, ref T2?)"/>
+        /// <summary>
+        /// Resolve three entity systems.
+        /// </summary>
+        void Resolve<T1, T2, T3>([NotNull] ref T1? instance1, [NotNull] ref T2? instance2, [NotNull] ref T3? instance3)
+            where T1 : IEntitySystem
+            where T2 : IEntitySystem
+            where T3 : IEntitySystem;
+
+        /// <inheritdoc cref="Resolve{T1, T2, T3}(ref T1?, ref T2?, ref T3?)"/>
+        /// <summary>
+        /// Resolve four entity systems.
+        /// </summary>
+        void Resolve<T1, T2, T3, T4>([NotNull] ref T1? instance1, [NotNull] ref T2? instance2, [NotNull] ref T3? instance3, [NotNull] ref T4? instance4)
+            where T1 : IEntitySystem
+            where T2 : IEntitySystem
+            where T3 : IEntitySystem
+            where T4 : IEntitySystem;
 
         /// <summary>
         /// Tries to get an entity system of the specified type.

e1b9327ec0c28641502177cfe2bd3f5704f7d8d5
Acruid <shatter66@gmail.com>
2021-11-17 00:39:18 -0800

    Misc Engine Tweaks (#2238)

---
+++ Robust.Shared/GameObjects/ComponentProtoNameAttribute.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Robust.Shared.GameObjects
+{
+    /// <summary>
+    /// Defines Name that this component is represented with in prototypes.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ComponentProtoNameAttribute : Attribute
+    {
+        public string PrototypeName { get; }
+
+        public ComponentProtoNameAttribute(string prototypeName)
+        {
+            PrototypeName = prototypeName;
+        }
+    }
+}

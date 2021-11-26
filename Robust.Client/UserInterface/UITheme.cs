e8ed5416bb8431a1000d02c753efbcfb3609d825
Efruit <602406+Efruit@users.noreply.github.com>
2021-11-26 07:36:57 -0500

    UserInterface/UITheme: Move to FontLibraries

--- Robust.Client/UserInterface/UITheme.cs
+++ Robust.Client/UserInterface/UITheme.cs
@@ -10,19 +10,39 @@ namespace Robust.Client.UserInterface
     /// </summary>
     public abstract class UITheme
     {
-        public abstract Font DefaultFont { get; }
-        public abstract Font LabelFont { get; }
+        public abstract IFontLibrary DefaultFontLibrary { get; }
+        public abstract IFontLibrary LabelFontLibrary { get; }
+        public Font DefaultFont { get => DefaultFontLibrary.StartFont().Current; }
+        public Font LabelFont { get => LabelFontLibrary.StartFont().Current; }
         public abstract StyleBox PanelPanel { get; }
         public abstract StyleBox ButtonStyle { get; }
         public abstract StyleBox LineEditBox { get; }
     }
 
+
     public sealed class UIThemeDummy : UITheme
     {
-        public override Font DefaultFont { get; } = new DummyFont();
-        public override Font LabelFont { get; } = new DummyFont();
+        private static readonly FontClass _defaultFontClass = new FontClass ( Id: "dummy", Size: default, Style: default );
+        public override IFontLibrary DefaultFontLibrary { get; } = new FontLibrary(_defaultFontClass);
+        public override IFontLibrary LabelFontLibrary { get; } = new FontLibrary(_defaultFontClass);
         public override StyleBox PanelPanel { get; } = new StyleBoxFlat();
         public override StyleBox ButtonStyle { get; } = new StyleBoxFlat();
         public override StyleBox LineEditBox { get; } = new StyleBoxFlat();
+
+        public UIThemeDummy() : base()
+        {
+            DefaultFontLibrary.AddFont("dummy",
+                new []
+                {
+                        new DummyVariant (default)
+                }
+            );
+            LabelFontLibrary.AddFont("dummy",
+                new []
+                {
+                    new DummyVariant (default)
+                }
+            );
+        }
     }
 }

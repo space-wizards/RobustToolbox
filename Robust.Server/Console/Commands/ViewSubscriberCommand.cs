5f1cbda66eac9718a18c56cf7e7487f6008f9ec9
metalgearsloth <comedian_vs_clown@hotmail.com>
2021-11-28 16:52:49 +1100

    Add remove command

--- Robust.Server/Console/Commands/ViewSubscriberCommand.cs
+++ Robust.Server/Console/Commands/ViewSubscriberCommand.cs
@@ -7,7 +7,7 @@ using Robust.Shared.Players;
 
 namespace Robust.Server.Console.Commands
 {
-    public sealed class ViewSubscriberCommand : IConsoleCommand
+    public sealed class AddViewSubscriberCommand : IConsoleCommand
     {
         public string Command => "addview";
         public string Description => $"Allows you to subscribe to an entity's view for debugging purposes";
@@ -44,5 +44,43 @@ namespace Robust.Server.Console.Commands
 
             EntitySystem.Get<ViewSubscriberSystem>().AddViewSubscriber(uid, playerSession);
         }
+
+        public sealed class RemoveViewSubscriberCommand : IConsoleCommand
+        {
+            public string Command => "removeview";
+            public string Description => $"Allows you to unsubscribe to an entity's view for debugging purposes";
+            public string Help => $"{Command} <entityUid>";
+            public void Execute(IConsoleShell shell, string argStr, string[] args)
+            {
+                var session = shell.Player;
+
+                if (session is not IPlayerSession playerSession)
+                {
+                    shell.WriteError($"Unable to find {nameof(ICommonSession)} for shell");
+                    return;
+                }
+
+                if (args.Length != 1)
+                {
+                    shell.WriteError($"Only 1 arg valid for {Command}, received {args.Length}");
+                    return;
+                }
+
+                if (!EntityUid.TryParse(args[0], out var uid))
+                {
+                    shell.WriteError($"Unable to parse {args[0]} as a {nameof(EntityUid)}");
+                    return;
+                }
+
+                var entManager = IoCManager.Resolve<IEntityManager>();
+
+                if (!entManager.EntityExists(uid))
+                {
+                    shell.WriteError($"Unable to find entity {uid}");
+                    return;
+                }
+
+                EntitySystem.Get<ViewSubscriberSystem>().RemoveViewSubscriber(uid, playerSession);
+            }
     }
 }

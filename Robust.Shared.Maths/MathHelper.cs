e22d6ea65c13842bdbf4408794dc6c2416edc4f2
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2021-12-23 01:21:57 +0100

    Use float.IsFinite in some appropriate places previously using IsNan || IsInfinity

--- Robust.Shared.Maths/MathHelper.cs
+++ Robust.Shared.Maths/MathHelper.cs
@@ -112,7 +112,7 @@ namespace Robust.Shared.Maths
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static float NextPowerOfTwo(float n)
         {
-            if (float.IsNaN(n) || float.IsInfinity(n))
+            if (!float.IsFinite(n))
                 throw new ArgumentOutOfRangeException(nameof(n), "Must be a number.");
             if (n <= 0) throw new ArgumentOutOfRangeException(nameof(n), "Must be positive.");
             return (float) NextPowerOfTwo((double) n);
@@ -126,7 +126,7 @@ namespace Robust.Shared.Maths
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static double NextPowerOfTwo(double n)
         {
-            if (double.IsNaN(n) || double.IsInfinity(n))
+            if (!double.IsFinite(n))
                 throw new ArgumentOutOfRangeException(nameof(n), "Must be a number.");
             if (n <= 0) throw new ArgumentOutOfRangeException(nameof(n), "Must be positive.");
 

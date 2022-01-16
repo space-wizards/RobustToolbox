847f8d1fee343a52007b1da53bf56bee4fd36c9f
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2022-01-16 15:08:10 +0100

    Document most engine CVars.c

--- Robust.Shared/Network/AuthMode.cs
+++ Robust.Shared/Network/AuthMode.cs
@@ -1,9 +1,34 @@
 ﻿namespace Robust.Shared.Network
 {
+    /// <summary>
+    /// Possible modes for the authentication support on the server.
+    /// </summary>
     public enum AuthMode : byte
     {
+        /// <summary>
+        /// Authentication is allowed but not required.
+        /// </summary>
+        /// <remarks>
+        /// Unauthenticated clients get a "guest@" or "localhost@" prefix to avoid conflict with authenticated clients.
+        /// </remarks>
         Optional = 0,
+
+        /// <summary>
+        /// Authenticated is required to join the server.
+        /// </summary>
+        /// <remarks>
+        /// Unauthenticated clients are still allowed for localhost connections,
+        /// but only if CVar <c>auth.allowlocal</c> is true.
+        /// </remarks>
         Required = 1,
+
+        /// <summary>
+        /// Authentication is fully disabled, and even clients capable of authenticating will not authenticate.
+        /// </summary>
+        /// <remarks>
+        /// This disables any sort of "guest@" or "localhost@" prefix for unauthenticated users.
+        /// This may result in confusing mingling of database entries, if actively switched between on the same server.
+        /// </remarks>
         Disabled = 2
     }
 }

7b5678fd0feec9768c6f2bf2738d601121fa0bf0
metalgearsloth <comedian_vs_clown@hotmail.com>
2022-01-31 17:34:49 +1100

    woops

--- Robust.Shared/GameObjects/Systems/SharedPhysicsSystem.cs
+++ Robust.Shared/GameObjects/Systems/SharedPhysicsSystem.cs
@@ -82,6 +82,7 @@ namespace Robust.Shared.GameObjects
         {
             IoCManager.InjectDependencies(component);
             component.BroadphaseSystem = _broadphaseSystem;
+            component._physics = this;
             component.ContactManager = new();
             component.ContactManager.Initialize();
             component.ContactManager.MapId = component.MapId;

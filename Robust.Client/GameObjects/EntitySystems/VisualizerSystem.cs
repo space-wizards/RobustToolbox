cf5463cb2a5d5aefb95fe8eb681864557f968c11
Kara D <lunarautomaton6@gmail.com>
2022-02-08 15:54:53 -0700

    Abstract VisualizerSystem

---
+++ Robust.Client/GameObjects/EntitySystems/VisualizerSystem.cs
@@ -0,0 +1,22 @@
+﻿using Robust.Shared.GameObjects;
+
+namespace Robust.Client.GameObjects;
+
+/// <summary>
+///     An abstract entity system inheritor for systems that deal with
+///     appearance data, replacing <see cref="AppearanceVisualizer"/>.
+/// </summary>
+public abstract class VisualizerSystem<T> : EntitySystem
+    where T: Component
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<T, ComponentInit>(OnComponentInit);
+        SubscribeLocalEvent<T, AppearanceChangeEvent>(OnAppearanceChange);
+    }
+
+    protected abstract void OnComponentInit(EntityUid uid, T component, ComponentInit args);
+    protected abstract void OnAppearanceChange(EntityUid uid, T component, AppearanceChangeEvent args);
+}

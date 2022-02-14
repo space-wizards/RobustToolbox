f61ae5da6bec812725ee4c2d02d1945ffe65f8fd
Ygg01 <y.laughing.man.y@gmail.com>
2022-02-14 01:49:15 +0100

    Update Linguini and line info in errors

--- Robust.Shared/Localization/LocHelper.cs
+++ Robust.Shared/Localization/LocHelper.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Text;
 using Linguini.Bundle.Errors;
 using Linguini.Syntax.Parser.Error;
@@ -7,7 +8,7 @@ namespace Robust.Shared.Localization;
 
 internal static class LocHelper
 {
-    public static string FormatCompileErrors(this ParseError self, ReadOnlyMemory<char> resource, 
+    public static string FormatCompileErrors(this ParseError self, ReadOnlyMemory<char> resource,
         string? newLine = null)
     {
         ErrorSpan span = new(self.Row, self.Slice!.Value.Start.Value, self.Slice.Value.End.Value,
@@ -19,10 +20,9 @@ internal static class LocHelper
     {
         var sb = new StringBuilder();
         var errContext = resource.Slice(span.StartSpan, span.EndSpan - span.StartSpan).ToString();
-        sb.Append(newLine ?? Environment.NewLine);
-        sb.Append(errContext);
-
+        var lines = new List<ReadOnlyMemory<char>>(5);
         var currLineOffset = 0;
+        var lastStart = 0;
         for (var i = 0; i < span.StartMark - span.StartSpan; i++)
         {
             switch (errContext[i])
@@ -30,6 +30,16 @@ internal static class LocHelper
                 // Reset current line so that mark aligns with the reported error
                 // We cheat here a bit, since we both `\r\n` and `\n` end with '\n'
                 case '\n':
+                    if (i > 0 && errContext[i - 1] == '\r')
+                    {
+                        lines.Add(resource.Slice(lastStart, currLineOffset - 1));
+                    }
+                    else
+                    {
+                        lines.Add(resource.Slice(lastStart, currLineOffset));
+                    }
+
+                    lastStart = currLineOffset + 1;
                     currLineOffset = 0;
                     break;
                 default:
@@ -38,8 +48,20 @@ internal static class LocHelper
             }
         }
 
+        lines.Add(resource.Slice(lastStart, resource.Length - lastStart));
+
+
+        var lastLine = $"{span.Row + lines.Count - 1}".Length;
+        for (var index = 0; index < lines.Count; index++)
+        {
+            var line = lines[index];
+
+            sb.Append(newLine ?? Environment.NewLine).Append(' ').Append($"{span.Row + index}".PadLeft(lastLine))
+                .Append(" |").Append(line);
+        }
+
         sb.Append(newLine ?? Environment.NewLine)
-            .Append(' ', currLineOffset)
+            .Append(' ', currLineOffset + lastLine + 3)
             .Append('^', span.EndMark - span.StartMark)
             .Append($" {message}");
         return sb.ToString();
--- Robust.UnitTesting/Shared/Localization/FormatErrorTest.cs
+++ Robust.UnitTesting/Shared/Localization/FormatErrorTest.cs
@@ -1,57 +1,98 @@
 ﻿using System;
-using System.Linq;
 using Linguini.Syntax.Parser.Error;
 using NUnit.Framework;
-using Pidgin;
 using Robust.Shared.Localization;
 
 namespace Robust.UnitTesting.Shared.Localization;
 
 [TestFixture]
 [Parallelizable]
-public class TestFormatErrors
+public sealed class TestFormatErrors
 {
-    #region SingleLineExample
-
     private const string Res1 = "err1 = $user)";
-
-    private const string Expect1Wide1 = "\r\nerr1 = $user)\r\n        ^ Unbalanced closing brace";
-
-    private const string Expect2Wide1 = "\r\nerr1 = $user)\r\n^ Unbalanced closing brace";
-
-    private const string Expect1Wide4 = "\r\nerr1 = $user)\r\n        ^^^^ Expected a message field for \"x\"";
-
-    private const string Expect2Wide4 = "\r\nerr1 = $user)\r\n^^^^ Expected a message field for \"x\"";
-
-    #endregion
-
-    #region MultiLineExample
-
     private const string Res2 = "a = b {{\r\n  err = x";
+    private const string Res2Lf = "a = b {{\n  err = x";
 
-    private const string ExpectMulti1Wide1 = "\r\na = b {{\r\n  err = x\r\n  ^ Unbalanced closing brace";
+    #region ExpectedCrlf
 
-    private const string ExpectMulti1Wide3 = "\r\na = b {{\r\n  err = x\r\n  ^^^ Expected a message field for \"x\"";
+    private const string Expect1Wide1Crlf = "\r\n 99 |err1 = $user)\r\n            ^ Unbalanced closing brace";
+
+    private const string Expect2Wide1Crlf = "\r\n 99 |err1 = $user)\r\n     ^ Unbalanced closing brace";
+
+    private const string Expect1Wide4Crlf =
+        "\r\n 99 |err1 = $user)\r\n             ^^^^ Expected a message field for \"x\"";
+
+    private const string Expect2Wide4Crlf = "\r\n 99 |err1 = $user)\r\n     ^^^^ Expected a message field for \"x\"";
+
+
+    private const string ExpectMulti1Wide1Crlf =
+        "\r\n  99 |a = b {{\r\n 100 |  err = x\r\n        ^ Unbalanced closing brace";
+
+    private const string ExpectMulti1Wide3Crlf =
+        "\r\n  99 |a = b {{\r\n 100 |  err = x\r\n        ^^^ Expected a message field for \"x\"";
 
     #endregion
 
     [Test]
-    [TestCase(Expect1Wide1, Res1, 8)]
-    [TestCase(Expect2Wide1, Res1, 0)]
-    [TestCase(Expect1Wide4, Res1, 8, 12)]
-    [TestCase(Expect2Wide4, Res1, 0, 4)]
-    [TestCase(ExpectMulti1Wide1, Res2, 12)]
-    [TestCase(ExpectMulti1Wide3, Res2, 12, 15)]
-    public void TestSingleLineTest(string expected, string resource, int start, int? end = null)
+    [TestCase(Expect1Wide1Crlf, Res1, 7)]
+    [TestCase(Expect2Wide1Crlf, Res1, 0)]
+    [TestCase(Expect1Wide4Crlf, Res1, 8, 12)]
+    [TestCase(Expect2Wide4Crlf, Res1, 0, 4)]
+    [TestCase(ExpectMulti1Wide1Crlf, Res2, 12)]
+    [TestCase(ExpectMulti1Wide3Crlf, Res2, 12, 15)]
+    [TestCase(ExpectMulti1Wide1Crlf, Res2Lf, 11)]
+    [TestCase(ExpectMulti1Wide3Crlf, Res2Lf, 11, 14)]
+    public void TestSingleLineTestCrlf(string expected, string resource, int start, int? end = null)
     {
-        var err = ParseError.UnbalancedClosingBrace(start);
+        var err = ParseError.UnbalancedClosingBrace(start, 99);
         if (end != null)
         {
-            err = ParseError.ExpectedMessageField("x".AsMemory(), start, end.Value);
+            err = ParseError.ExpectedMessageField("x".AsMemory(), start, end.Value, 99);
         }
 
         err.Slice = new Range(0, resource.Length);
         var actual = err.FormatCompileErrors(resource.AsMemory(), "\r\n");
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    #region ExpectedLf
+
+    private const string Expect1Wide1Lf = "\n 99 |err1 = $user)\n            ^ Unbalanced closing brace";
+
+    private const string Expect2Wide1Lf = "\n 99 |err1 = $user)\n     ^ Unbalanced closing brace";
+
+    private const string Expect1Wide4Lf =
+        "\n 99 |err1 = $user)\n             ^^^^ Expected a message field for \"x\"";
+
+    private const string Expect2Wide4Lf = "\n 99 |err1 = $user)\n     ^^^^ Expected a message field for \"x\"";
+
+
+    private const string ExpectMulti1Wide1Lf = "\n  99 |a = b {{\n 100 |  err = x\n        ^ Unbalanced closing brace";
+
+    private const string ExpectMulti1Wide3Lf =
+        "\n  99 |a = b {{\n 100 |  err = x\n        ^^^ Expected a message field for \"x\"";
+
+    #endregion
+
+    [Test]
+    [TestCase(Expect1Wide1Lf, Res1, 7)]
+    [TestCase(Expect2Wide1Lf, Res1, 0)]
+    [TestCase(Expect1Wide4Lf, Res1, 8, 12)]
+    [TestCase(Expect2Wide4Lf, Res1, 0, 4)]
+    [TestCase(ExpectMulti1Wide1Lf, Res2, 12)]
+    [TestCase(ExpectMulti1Wide3Lf, Res2, 12, 15)]
+    [TestCase(ExpectMulti1Wide1Lf, Res2Lf, 11)]
+    [TestCase(ExpectMulti1Wide3Lf, Res2Lf, 11, 14)]
+    public void TestSingleLineTestLf(string expected, string resource, int start, int? end = null)
+    {
+        var err = ParseError.UnbalancedClosingBrace(start, 99);
+        if (end != null)
+        {
+            err = ParseError.ExpectedMessageField("x".AsMemory(), start, end.Value, 99);
+        }
+
+        err.Slice = new Range(0, resource.Length);
+        var actual = err.FormatCompileErrors(resource.AsMemory(), "\n");
         Assert.That(actual, Is.EqualTo(expected));
     }
 }

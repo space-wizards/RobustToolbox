2bbfdce8f164ec0061636f47f8ff9c563f96d21d
Paul <ritter.paul1@googlemail.com>
2022-03-28 16:28:09 +0200

    starting

---
+++ Robust.Benchmarks/Exporters/SQLExporter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Reports;
+
+namespace Robust.Benchmarks.Exporters;
+
+public sealed class SQLExporter : IExporter
+{
+    public static readonly IExporter Default = new SQLExporter();
+
+    private SQLExporter(){}
+
+    public void ExportToLog(Summary summary, ILogger logger)
+    {
+        Export(summary, logger);
+    }
+
+    public IEnumerable<string> ExportToFiles(Summary summary, ILogger consoleLogger)
+    {
+        Export(summary, consoleLogger);
+        return Array.Empty<string>();
+    }
+
+    private void Export(Summary summary, ILogger logger)
+    {
+
+    }
+
+    public string Name => "sql";
+}

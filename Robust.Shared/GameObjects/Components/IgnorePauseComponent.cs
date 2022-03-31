7c98087ce7437dbc4586d52b2a05e49030eeb152
ElectroJr <leonsfriedrich@gmail.com>
2022-03-31 15:35:13 +1300

    ah pause is already being removed

--- Robust.Shared/GameObjects/Components/IgnorePauseComponent.cs
+++ Robust.Shared/GameObjects/Components/IgnorePauseComponent.cs
@@ -1,4 +1,25 @@
-namespace Robust.Shared.GameObjects;
+using Robust.Shared.IoC;
+using Robust.Shared.Map;
 
-[RegisterComponent]
-public sealed class IgnorePauseComponent : Component { }
+namespace Robust.Shared.GameObjects
+{
+    [RegisterComponent]
+    public sealed class IgnorePauseComponent : Component
+    {
+        protected override void OnAdd()
+        {
+            base.OnAdd();
+            IoCManager.Resolve<IEntityManager>().GetComponent<MetaDataComponent>(Owner).EntityPaused = false;
+        }
+
+        protected override void OnRemove()
+        {
+            base.OnRemove();
+            var entMan = IoCManager.Resolve<IEntityManager>();
+            if (IoCManager.Resolve<IMapManager>().IsMapPaused(entMan.GetComponent<TransformComponent>(Owner).MapID))
+            {
+                entMan.GetComponent<MetaDataComponent>(Owner).EntityPaused = true;
+            }
+        }
+    }
+}

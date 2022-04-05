a1dc24bff4c4ede84172f8a285ef6afd39e02b88
Kara D <lunarautomaton6@gmail.com>
2022-04-04 22:12:26 -0700

    Open/close event

--- Robust.Client/GameObjects/Components/UserInterface/ClientUserInterfaceComponent.cs
+++ Robust.Client/GameObjects/Components/UserInterface/ClientUserInterfaceComponent.cs
@@ -75,6 +75,10 @@ namespace Robust.Client.GameObjects
                 (BoundUserInterface) _dynamicTypeFactory.CreateInstance(type, new[] {this, wrapped.UiKey});
             boundInterface.Open();
             _openInterfaces[wrapped.UiKey] = boundInterface;
+
+            var playerSession = _playerManager.LocalPlayer?.Session;
+            if(playerSession != null)
+                _entityManager.EventBus.RaiseLocalEvent(Owner, new BoundUIOpenedEvent(wrapped.UiKey, Owner, playerSession));
         }
 
         internal void Close(object uiKey, bool remoteCall)

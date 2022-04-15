7cb0978468acdb1c844c76fde570431c4e1b55cc
Vera Aguilera Puerto <gradientvera@outlook.com>
2022-04-15 12:46:18 +0200

    Revert "Adds custom MIDI soundfont support (#2720)"
    This reverts commit 9ff46b9aadf3973d66e6a107a75a7265217a1b48.
    
    Sandbox violation, oops.

--- Robust.Shared/CVars.cs
+++ Robust.Shared/CVars.cs
@@ -1052,9 +1052,6 @@ namespace Robust.Shared
         public static readonly CVarDef<float> MidiVolume =
             CVarDef.Create("midi.volume", 0f, CVar.CLIENTONLY | CVar.ARCHIVE);
 
-        public static readonly CVarDef<string> MidiSoundfont =
-            CVarDef.Create("midi.soundfont", string.Empty, CVar.CLIENTONLY | CVar.ARCHIVE);
-
         /*
          * HUB
          * CVars related to public master server hub

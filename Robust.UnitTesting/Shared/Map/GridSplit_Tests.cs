22b37473cf0c1b7c615b4c0b03017c34dfe1fc96
metalgearsloth <comedian_vs_clown@hotmail.com>
2022-04-18 17:56:07 +1000

    2nd test

--- Robust.UnitTesting/Shared/Map/GridSplit_Tests.cs
+++ Robust.UnitTesting/Shared/Map/GridSplit_Tests.cs
@@ -17,15 +17,43 @@ public sealed class GridSplit_Tests
         var mapId = mapManager.CreateMap();
         var grid = mapManager.CreateGrid(mapId);
 
-        for (var i = 0; i < 3; i++)
+        for (var x = 0; x < 3; x++)
         {
-            grid.SetTile(new Vector2i(i, 0), new Tile(1));
+            grid.SetTile(new Vector2i(x, 0), new Tile(1));
         }
 
         Assert.That(mapManager.GetAllMapGrids(mapId).Count(), Is.EqualTo(1));
 
         grid.SetTile(new Vector2i(1, 0), Tile.Empty);
 
+        Assert.That(mapManager.GetAllMapGrids(mapId).Count(), Is.EqualTo(2));
+    }
+
+    [Test]
+    public void DonutSplit()
+    {
+        var sim = RobustServerSimulation.NewSimulation().InitializeInstance();
+        var mapManager = sim.Resolve<IMapManager>();
+        var mapId = mapManager.CreateMap();
+        var grid = mapManager.CreateGrid(mapId);
+
+        for (var x = 0; x < 3; x++)
+        {
+            for (var y = 0; y < 3; y++)
+            {
+                grid.SetTile(new Vector2i(x, y), new Tile(1));
+            }
+        }
+
+        Assert.That(mapManager.GetAllMapGrids(mapId).Count(), Is.EqualTo(1));
+
+        grid.SetTile(Vector2i.One, Tile.Empty);
+        Assert.That(mapManager.GetAllMapGrids(mapId).Count(), Is.EqualTo(1));
+
+        grid.SetTile(new Vector2i(1, 2), Tile.Empty);
+        Assert.That(mapManager.GetAllMapGrids(mapId).Count(), Is.EqualTo(1));
+
+        grid.SetTile(new Vector2i(1, 0), Tile.Empty);
         Assert.That(mapManager.GetAllMapGrids(mapId).Count(), Is.EqualTo(2));
     }
 }

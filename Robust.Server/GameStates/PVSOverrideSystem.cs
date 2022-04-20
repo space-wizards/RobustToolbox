7f87033ae13f51b08770501bbadc5dc826c63b7b
ElectroJr <leonsfriedrich@gmail.com>
2022-04-20 18:54:34 +1200

    add session and clear functions

--- Robust.Server/GameStates/PVSOverrideSystem.cs
+++ Robust.Server/GameStates/PVSOverrideSystem.cs
@@ -1,4 +1,5 @@
 using Robust.Shared.GameObjects;
+using Robust.Shared.Players;
 
 namespace Robust.Server.GameStates;
 
@@ -10,8 +11,31 @@ public sealed partial class PVSOverrideSystem : EntitySystem
 {
     [Shared.IoC.Dependency] private readonly PVSSystem _pvs = default!;
 
+    /// <summary>
+    ///     Used to ensure that an entity is always sent to every client. Overrides any client-specific overrides.
+    /// </summary>
     public void AddGlobalOverride(EntityUid uid)
     {
         _pvs.EntityPVSCollection.UpdateIndex(uid, true);
+    }
+
+    /// <summary>
+    ///     Used to ensure that an entity is always sent to a specific client. Overrides any global or pre-existing
+    ///     client-specific overrides.
+    /// </summary>
+    public void AddSessionOverride(EntityUid uid, ICommonSession session)
+    {
+        _pvs.EntityPVSCollection.UpdateIndex(uid, session, true);
+    }
+
+    /// <summary>
+    ///     Removes any global or client-specific overrides.
+    /// </summary>
+    public void ClearOverride(EntityUid uid, TransformComponent? xform = null)
+    {
+        if (!Resolve(uid, ref xform))
+            return;
+
+        _pvs.EntityPVSCollection.UpdateIndex(uid, xform.Coordinates, true);
     }
 }

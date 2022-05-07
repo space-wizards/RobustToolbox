1695bafffc393ff2de57d456ab52a3559ebf8d35
Vera Aguilera Puerto <gradientvera@outlook.com>
2022-05-07 17:33:11 +0200

    Add BestFriend attribute.

--- Robust.Analyzers/FriendAnalyzer.cs
+++ Robust.Analyzers/FriendAnalyzer.cs
@@ -12,25 +12,94 @@ namespace Robust.Analyzers
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
     public class FriendAnalyzer : DiagnosticAnalyzer
     {
-        const string FriendAttribute = "Robust.Shared.Analyzers.FriendAttribute";
+        public const string BestFriendAttribute = "Robust.Shared.Analyzers.BestFriendAttribute";
+        public const string FriendAttribute = "Robust.Shared.Analyzers.FriendAttribute";
 
         [SuppressMessage("ReSharper", "RS2008")]
-        private static readonly DiagnosticDescriptor Rule = new (
+        private static readonly DiagnosticDescriptor FriendRule = new (
             Diagnostics.IdFriend,
             "Tried to access friend-only member",
-            "Tried to access member \"{0}\" in class \"{1}\" which can only be accessed by friend classes",
+            "Tried to access member \"{0}\" in type \"{1}\" which can only be accessed by friend types",
             "Usage",
             DiagnosticSeverity.Error,
             true,
-            "Make sure to specify the accessing class in the friends attribute.");
+            "Make sure to specify the accessing type in the friends attribute.");
 
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+        [SuppressMessage("ReSharper", "RS2008")]
+        private static readonly DiagnosticDescriptor BestFriendRule = new (
+            Diagnostics.IdBestFriend,
+            "Tried to access best-friends-only member",
+            "Tried to access member \"{0}\" in type \"{1}\" which can only be accessed by best-friend types",
+            "Usage",
+            DiagnosticSeverity.Error,
+            true,
+            "Make sure to specify the accessing type in the best-friends attribute.");
+
+        [SuppressMessage("ReSharper", "RS2008")]
+        private static readonly DiagnosticDescriptor MutuallyExclusiveFriendAttributesRule = new (
+            Diagnostics.IdMutuallyExclusiveFriendAttributes,
+            "Tried to define both the Friend and BestFriend attributes on the same type",
+            "Tried to define the mutually-exclusive \"Friend\" and \"BestFriend\" attributes in the same type \"{0}\"",
+            "Usage",
+            DiagnosticSeverity.Error,
+            true,
+            "Make sure to only specify one of these two attributes.");
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(FriendRule, BestFriendRule, MutuallyExclusiveFriendAttributesRule);
 
         public override void Initialize(AnalysisContext context)
         {
             context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
             context.EnableConcurrentExecution();
             context.RegisterSyntaxNodeAction(CheckFriendship, SyntaxKind.SimpleMemberAccessExpression);
+            context.RegisterSyntaxNodeAction(CheckFriendshipAttributes,
+                SyntaxKind.ClassDeclaration,
+                SyntaxKind.InterfaceDeclaration,
+                SyntaxKind.StructDeclaration);
+        }
+
+        private void CheckFriendshipAttributes(SyntaxNodeAnalysisContext context)
+        {
+            if (context.Node is not TypeDeclarationSyntax typeDeclaration)
+                return;
+
+            // Get the attributes
+            var friendAttr = context.Compilation.GetTypeByMetadataName(FriendAttribute);
+            var bestFriendAttr = context.Compilation.GetTypeByMetadataName(BestFriendAttribute);
+
+            var friendFound = false;
+            var bestFriendFound = false;
+
+            foreach (var attributeList in typeDeclaration.AttributeLists)
+            {
+                foreach (var attribute in attributeList.Attributes)
+                {
+                    if (context.SemanticModel.GetTypeInfo(attribute).ConvertedType is not {} type)
+                        continue;
+
+                    if (SymbolEqualityComparer.Default.Equals(type, friendAttr))
+                    {
+                        friendFound = true;
+                    }
+
+                    if (SymbolEqualityComparer.Default.Equals(type, bestFriendAttr))
+                    {
+                        bestFriendFound = true;
+                    }
+
+                    if (!friendFound || !bestFriendFound)
+                        continue;
+
+                    // Mutually exclusive attributes! Report error.
+                    context.ReportDiagnostic(
+                        Diagnostic.Create(MutuallyExclusiveFriendAttributesRule, attribute.GetLocation(),
+                            $"{typeDeclaration.Identifier.Text}"));
+
+                    return;
+                }
+            }
+
+
         }
 
         private void CheckFriendship(SyntaxNodeAnalysisContext context)
@@ -38,28 +107,9 @@ namespace Robust.Analyzers
             if (context.Node is not MemberAccessExpressionSyntax memberAccess)
                 return;
 
-            // We only do something if our parent is one of a few types.
-            switch (context.Node.Parent)
-            {
-                // If we're being assigned...
-                case AssignmentExpressionSyntax assignParent:
-                {
-                    if (assignParent.Left != memberAccess)
-                        return;
-                    break;
-                }
-
-                // If we're being invoked...
-                case InvocationExpressionSyntax:
-                    break;
-
-                // Otherwise, do nothing.
-                default:
-                    return;
-            }
-
-            // Get the friend attribute
+            // Get the attributes
             var friendAttr = context.Compilation.GetTypeByMetadataName(FriendAttribute);
+            var bestFriendAttr = context.Compilation.GetTypeByMetadataName(BestFriendAttribute);
 
             // Get the type that is containing this expression, or, the class where this is happening.
             if (context.ContainingSymbol?.ContainingType is not { } containingType)
@@ -68,7 +118,8 @@ namespace Robust.Analyzers
             // We check all of our children and get only the identifiers.
             foreach (var identifier in memberAccess.ChildNodes().Select(node => node as IdentifierNameSyntax))
             {
-                if (identifier == null) continue;
+                if (identifier == null)
+                    continue;
 
                 // Get the type info of the identifier, so we can check the attributes...
                 if (context.SemanticModel.GetTypeInfo(identifier).ConvertedType is not { } type)
@@ -78,12 +129,44 @@ namespace Robust.Analyzers
                 if (SymbolEqualityComparer.Default.Equals(type, containingType))
                     continue;
 
-                // Finally, get all attributes of the type, to check if we have any friend classes.
+                // Finally, get all attributes of the type, to check if we have any friend types.
                 foreach (var attribute in type.GetAttributes())
                 {
+                    var bestFriend = false;
+
                     // If the attribute isn't the friend attribute, continue.
                     if (!SymbolEqualityComparer.Default.Equals(attribute.AttributeClass, friendAttr))
-                        continue;
+                    {
+                        if(!SymbolEqualityComparer.Default.Equals(attribute.AttributeClass, bestFriendAttr))
+                            continue;
+
+                        // This is the best friend attribute instead!
+                        bestFriend = true;
+                    }
+
+                    // If we're working with a Friend attribute, we only care about write/execute.
+                    if (!bestFriend)
+                    {
+                        // We only do something if our parent is one of a few types.
+                        switch (context.Node.Parent)
+                        {
+                            // If we're being assigned...
+                            case AssignmentExpressionSyntax assignParent:
+                            {
+                                if (assignParent.Left != memberAccess)
+                                    return;
+                                break;
+                            }
+
+                            // If we're being invoked...
+                            case InvocationExpressionSyntax:
+                                break;
+
+                            // Otherwise, do nothing.
+                            default:
+                                continue;
+                        }
+                    }
 
                     // Check all types allowed in the friend attribute. (We assume there's only one constructor arg.)
                     foreach (var constant in attribute.ConstructorArguments[0].Values)
@@ -99,7 +182,7 @@ namespace Robust.Analyzers
 
                     // Not in a friend class! Report an error.
                     context.ReportDiagnostic(
-                        Diagnostic.Create(Rule, context.Node.GetLocation(),
+                        Diagnostic.Create(bestFriend ? BestFriendRule : FriendRule, context.Node.GetLocation(),
                             $"{context.Node.ToString().Split('.').LastOrDefault()}", $"{type.Name}"));
                 }
             }
--- Robust.Shared/Analyzers/FriendAttribute.cs
+++ Robust.Shared/Analyzers/FriendAttribute.cs
@@ -2,14 +2,23 @@ using System;
 
 namespace Robust.Shared.Analyzers
 {
+    [Virtual]
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface | AttributeTargets.Struct)]
-    public sealed class FriendAttribute : Attribute
+    public class FriendAttribute : Attribute
     {
         public readonly Type[] Friends;
 
         public FriendAttribute(params Type[] friends)
         {
             Friends = friends;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface | AttributeTargets.Struct)]
+    public sealed class BestFriendAttribute : FriendAttribute
+    {
+        public BestFriendAttribute(params Type[] besties) : base(besties)
+        {
         }
     }
 }

d4a1fcc9c686511c3c9eabf8ef6cd1214fdedd96
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2022-05-10 15:44:16 +0200

    Fix database code for benchmarks.

--- Robust.Benchmarks/Migrations/20220510131430_fix-pk.Designer.cs
+++ Robust.Benchmarks/Migrations/20220510131430_fix-pk.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Robust.Benchmarks.Exporters;
@@ -11,9 +12,10 @@ using Robust.Benchmarks.Exporters;
 namespace Robust.Benchmarks.Migrations
 {
     [DbContext(typeof(BenchmarkContext))]
-    partial class BenchmarkContextModelSnapshot : ModelSnapshot
+    [Migration("20220510131430_fix-pk")]
+    partial class fixpk
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

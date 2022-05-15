c866c6b59b59acb8ff8eaf6860c80ef8c79c1537
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2022-05-16 07:37:01 +1000

    Optimise grid traversal for pop-in (#2825)
    This was like 1/3 of the frame spikes from PVS pop-in. The main benefit is just checking if the entity is anchored for grid traversal and the secondary was earlying-out handling component state if it's sent to nullspace.

--- Robust.Shared/GameObjects/Systems/SharedTransformSystem.Component.cs
+++ Robust.Shared/GameObjects/Systems/SharedTransformSystem.Component.cs
@@ -343,11 +343,15 @@ public abstract partial class SharedTransformSystem
         {
             var newParentId = newState.ParentID;
             var rebuildMatrices = false;
+
+            // Update to new parent.
+            // if the new one isn't valid (e.g. PVS) then we'll just return early after yeeting them to nullspace.
             if (component.ParentUid != newParentId)
             {
                 if (!newParentId.IsValid())
                 {
                     component.DetachParentToNull();
+                    return;
                 }
                 else
                 {

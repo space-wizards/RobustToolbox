3a8c5ce0f7d6f5217fdd580786a241f3a73b0867
metalgearsloth <comedian_vs_clown@hotmail.com>
2022-05-19 20:27:28 +1000

    Physics command niceties

--- Robust.Client/Console/Commands/PhysicsOverlayCommands.cs
+++ Robust.Client/Console/Commands/PhysicsOverlayCommands.cs
@@ -1,6 +1,7 @@
 using Robust.Client.Debugging;
 using Robust.Shared.Console;
 using Robust.Shared.GameObjects;
+using Robust.Shared.Localization;
 
 namespace Robust.Client.Console.Commands
 {
@@ -9,11 +10,12 @@ namespace Robust.Client.Console.Commands
         public string Command => "physics";
         public string Description => $"Shows a debug physics overlay. The arg supplied specifies the overlay.";
         public string Help => $"{Command} <aabbs / com / contactnormals / contactpoints / joints / shapeinfo / shapes>";
+
         public void Execute(IConsoleShell shell, string argStr, string[] args)
         {
             if (args.Length != 1)
             {
-                shell.WriteError($"Invalid number of args supplied");
+                shell.WriteError(Loc.GetString("shell-wrong-arguments-number-need-specific", ("properAmount", 1), ("currentAmount", args.Length)));
                 return;
             }
 
@@ -43,11 +45,27 @@ namespace Robust.Client.Console.Commands
                     system.Flags ^= PhysicsDebugFlags.Shapes;
                     break;
                 default:
-                    shell.WriteError($"{args[0]} is not a recognised overlay");
+                    shell.WriteError(Loc.GetString("cmd-physics-overlay", ("overlay", args[0])));
                     return;
             }
 
             return;
+        }
+
+        public CompletionResult GetCompletion(IConsoleShell shell, string[] args)
+        {
+            if (args.Length != 1) return CompletionResult.Empty;
+
+            return CompletionResult.FromOptions(new[]
+            {
+                "aabbs",
+                "com",
+                "contactnormals",
+                "contactpoints",
+                "joints",
+                "shapeinfo",
+                "shapes",
+            });
         }
     }
 }

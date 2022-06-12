1e7956eaa873b6039f06b1db9f36cdaa33fdfc0d
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2022-06-12 11:30:16 +0200

    Remove MarshalHelper
    Not necessary anymore since .NET Core

--- Robust.Client/UserInterface/FileDialogManager.cs
+++ Robust.Client/UserInterface/FileDialogManager.cs
@@ -196,10 +196,10 @@ namespace Robust.Client.UserInterface
             {
                 case sw_nfdresult.SW_NFD_ERROR:
                     var errPtr = sw_NFD_GetError();
-                    throw new Exception(MarshalHelper.PtrToStringUTF8(errPtr));
+                    throw new Exception(Marshal.PtrToStringUTF8((IntPtr) errPtr));
 
                 case sw_nfdresult.SW_NFD_OKAY:
-                    var str = MarshalHelper.PtrToStringUTF8(outPath)!;
+                    var str = Marshal.PtrToStringUTF8((IntPtr) outPath)!;
 
                     sw_NFD_Free(outPath);
                     return str;

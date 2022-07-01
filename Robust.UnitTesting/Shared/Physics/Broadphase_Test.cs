2dadbe29a30540c925555081effa217323833d80
metalgearsloth <comedian_vs_clown@hotmail.com>
2022-07-01 15:47:21 +1000

    Also nullspace coz sure.

--- Robust.UnitTesting/Shared/Physics/Broadphase_Test.cs
+++ Robust.UnitTesting/Shared/Physics/Broadphase_Test.cs
@@ -57,4 +57,51 @@ public sealed class Broadphase_Test
         Assert.That(child1Body.Broadphase, Is.EqualTo(mapBroapdhase));
         Assert.That(child2Body.Broadphase, Is.EqualTo(null));
     }
+
+    /// <summary>
+    /// If an entity's broadphase is changed to nullspace are its children updated.
+    /// </summary>
+    [Test]
+    public void BroadphaseRecursiveNullspaceUpdate()
+    {
+        var sim = RobustServerSimulation.NewSimulation().InitializeInstance();
+        var entManager = sim.Resolve<IEntityManager>();
+        var xformSystem = sim.Resolve<IEntitySystemManager>().GetEntitySystem<SharedTransformSystem>();
+        var mapManager = sim.Resolve<IMapManager>();
+
+        var mapId = mapManager.CreateMap();
+        var mapBroapdhase = entManager.GetComponent<BroadphaseComponent>(mapManager.GetMapEntityId(mapId));
+
+        Assert.That(entManager.EntityQuery<BroadphaseComponent>(true).Count(), Is.EqualTo(1));
+
+        var parent = entManager.SpawnEntity(null, new MapCoordinates(Vector2.Zero, mapId));
+        var parentXform = entManager.GetComponent<TransformComponent>(parent);
+        var parentBody = entManager.AddComponent<PhysicsComponent>(parent);
+
+        var child1 = entManager.SpawnEntity(null, new EntityCoordinates(parent, Vector2.Zero));
+        var child1Xform = entManager.GetComponent<TransformComponent>(child1);
+        var child1Body = entManager.AddComponent<PhysicsComponent>(child1);
+
+        // Have a non-collidable child and check it doesn't get added too.
+        var child2 = entManager.SpawnEntity(null, new EntityCoordinates(child1, Vector2.Zero));
+        var child2Xform = entManager.GetComponent<TransformComponent>(child2);
+        var child2Body = entManager.AddComponent<PhysicsComponent>(child2);
+        child2Body.CanCollide = false;
+        Assert.That(!child2Body.CanCollide);
+
+        Assert.That(child1Xform.ParentUid, Is.EqualTo(parent));
+        Assert.That(child2Xform.ParentUid, Is.EqualTo(child1));
+
+        Assert.That(parentBody.Broadphase, Is.EqualTo(mapBroapdhase));
+        Assert.That(child1Body.Broadphase, Is.EqualTo(mapBroapdhase));
+        Assert.That(child2Body.Broadphase, Is.EqualTo(null));
+
+        // They should get deparented to the map and updated to the map's broadphase instead.
+        xformSystem.DetachParentToNull(parentXform);
+        Assert.That(parentBody.Broadphase, Is.EqualTo(null));
+        Assert.That(child1Body.Broadphase, Is.EqualTo(null));
+        Assert.That(child2Body.Broadphase, Is.EqualTo(null));
+
+        // Can't assert CanCollide because they may still want to be valid when coming out of nullspace.
+    }
 }

474dfe94b3bc1651f9cffdce3d257f4756aa26cf
metalgearsloth <comedian_vs_clown@hotmail.com>
2022-07-01 15:35:43 +1000

    Add test for recursive broadphase updates
    Checked it failed before my PR a fortnight ago.

---
+++ Robust.UnitTesting/Shared/Physics/Broadphase_Test.cs
@@ -0,0 +1,61 @@
+using System.Linq;
+using NUnit.Framework;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Map;
+using Robust.Shared.Maths;
+using Robust.Shared.Physics;
+using Robust.UnitTesting.Server;
+
+namespace Robust.UnitTesting.Shared.Physics;
+
+[TestFixture]
+public sealed class Broadphase_Test
+{
+    /// <summary>
+    /// If an entity's broadphase is changed are its children's broadphases recursively changed.
+    /// </summary>
+    [Test]
+    public void BroadphaseRecursiveUpdate()
+    {
+        var sim = RobustServerSimulation.NewSimulation().InitializeInstance();
+        var entManager = sim.Resolve<IEntityManager>();
+        var mapManager = sim.Resolve<IMapManager>();
+
+        var mapId = mapManager.CreateMap();
+        var grid = mapManager.CreateGrid(mapId);
+
+        grid.SetTile(Vector2i.Zero, new Tile(1));
+        var gridBroadphase = entManager.GetComponent<BroadphaseComponent>(grid.GridEntityId);
+        var mapBroapdhase = entManager.GetComponent<BroadphaseComponent>(mapManager.GetMapEntityId(mapId));
+
+        Assert.That(entManager.EntityQuery<BroadphaseComponent>(true).Count(), Is.EqualTo(2));
+
+        var parent = entManager.SpawnEntity(null, new EntityCoordinates(grid.GridEntityId, new Vector2(0.5f, 0.5f)));
+        var parentBody = entManager.AddComponent<PhysicsComponent>(parent);
+
+        var child1 = entManager.SpawnEntity(null, new EntityCoordinates(parent, Vector2.Zero));
+        var child1Xform = entManager.GetComponent<TransformComponent>(child1);
+        var child1Body = entManager.AddComponent<PhysicsComponent>(child1);
+
+        // Have a non-collidable child and check it doesn't get added too.
+        var child2 = entManager.SpawnEntity(null, new EntityCoordinates(child1, Vector2.Zero));
+        var child2Xform = entManager.GetComponent<TransformComponent>(child2);
+        var child2Body = entManager.AddComponent<PhysicsComponent>(child2);
+        child2Body.CanCollide = false;
+        Assert.That(!child2Body.CanCollide);
+        Assert.That(child2Body.Broadphase, Is.EqualTo(null));
+
+        Assert.That(child1Xform.ParentUid, Is.EqualTo(parent));
+        Assert.That(child2Xform.ParentUid, Is.EqualTo(child1));
+
+        Assert.That(parentBody.Broadphase, Is.EqualTo(gridBroadphase));
+        Assert.That(child1Body.Broadphase, Is.EqualTo(gridBroadphase));
+
+        // They should get deparented to the map and updated to the map's broadphase instead.
+        grid.SetTile(Vector2i.Zero, Tile.Empty);
+        Assert.That(parentBody.Broadphase, Is.EqualTo(mapBroapdhase));
+        Assert.That(child1Body.Broadphase, Is.EqualTo(mapBroapdhase));
+
+        Assert.That(child2Body.Broadphase, Is.EqualTo(null));
+    }
+}

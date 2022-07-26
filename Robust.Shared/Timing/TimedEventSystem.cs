2a65a1a466023ec5c1b8b4739f0ad5b534850500
wrexbe <wrexbe@protonmail.com>
2022-07-25 20:31:34 -0700

    Timed Event System

---
+++ Robust.Shared/Timing/TimedEventSystem.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using Robust.Shared.GameObjects;
+using Robust.Shared.IoC;
+
+namespace Robust.Shared.Timing;
+public sealed class TimedEventSystem : EntitySystem
+{
+    [Dependency] private readonly IGameTiming _gameTiming = default!;
+    private readonly ComponentTimedEvent _componentTimedEventInstance = new();
+    private readonly PriorityQueue<(Component Component, string Key), TimeSpan> _timeQueue = new ();
+
+    public override void Update(float frameTime)
+    {
+        var time = _gameTiming.RealTime;
+        while (_timeQueue.TryDequeue(out var pair, out var triggerTime))
+        {
+            if (triggerTime <= time)
+            {
+                _componentTimedEventInstance.Key = pair.Key;
+                EntityManager.EventBus.RaiseComponentEvent(pair.Component, _componentTimedEventInstance);
+            }
+            else
+            {
+                _timeQueue.Enqueue(pair, triggerTime);
+                break;
+            }
+        }
+    }
+
+    [PublicAPI]
+    public void AddTimedEvent<TComponent>(TComponent component, TimeSpan waitTime, string key) where TComponent: Component
+    {
+        _timeQueue.Enqueue((component, key), _gameTiming.RealTime+waitTime);
+    }
+
+}
+
+/// <summary>
+/// Raised when a timed event happens on a component
+/// </summary>
+[ComponentEvent]
+public sealed class ComponentTimedEvent : EntityEventArgs
+{
+    public string? Key { get; set; }
+}

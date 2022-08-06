c7a3c43a4fe5c200c782f0065eb711b480211c76
Leon Friedrich <60421075+ElectroJr@users.noreply.github.com>
2022-08-06 16:51:23 +1200

    More BUI changes (#3104)

--- Robust.Client/GameObjects/EntitySystems/UserInterfaceSystem.cs
+++ Robust.Client/GameObjects/EntitySystems/UserInterfaceSystem.cs
@@ -1,4 +1,4 @@
-﻿using JetBrains.Annotations;
+using JetBrains.Annotations;
 using Robust.Client.Player;
 using Robust.Shared.GameObjects;
 using Robust.Shared.IoC;
@@ -16,8 +16,16 @@ namespace Robust.Client.GameObjects
 
             SubscribeNetworkEvent<BoundUIWrapMessage>(MessageReceived);
             SubscribeLocalEvent<ClientUserInterfaceComponent, ComponentShutdown>(OnUserInterfaceShutdown);
+            SubscribeLocalEvent<ClientUserInterfaceComponent, ComponentInit>(OnAdd);
         }
 
+        private void OnAdd(EntityUid uid, ClientUserInterfaceComponent component, ComponentInit args)
+        {
+            foreach (var data in component._interfaceData)
+            {
+                component._interfaces.Add(data.UiKey, data);
+            }
+        }
         private void OnUserInterfaceShutdown(EntityUid uid, ClientUserInterfaceComponent component, ComponentShutdown args)
         {
             foreach (var bui in component.Interfaces)

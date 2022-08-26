a94a37d5da6d64e6705c6eadca639fa87318ecf2
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2022-08-26 22:28:57 +0200

    Support connecting over named pipe.
    This is probably sanest to use on Windows.

---
+++ Robust.LanguageServer/CommandLineArgs.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using C = System.Console;
+
+namespace Robust.LanguageServer;
+
+internal sealed record CommandLineArgs(string? CommunicationPipe)
+{
+    public static bool TryParse(IReadOnlyList<string> args, [NotNullWhen(true)] out CommandLineArgs? parsedArgs)
+    {
+        string? commPipe = null;
+        parsedArgs = null;
+
+        // ReSharper disable once GenericEnumeratorNotDisposed
+        var enumerator = args.GetEnumerator();
+
+        while (enumerator.MoveNext())
+        {
+            var arg = enumerator.Current;
+            if (arg == "--pipe")
+            {
+                if (!enumerator.MoveNext())
+                {
+                    C.WriteLine("Expected pipe name");
+                    return false;
+                }
+
+                commPipe = enumerator.Current;
+            }
+            else
+            {
+                C.WriteLine("Unknown argument");
+                return false;
+            }
+        }
+
+        parsedArgs = new CommandLineArgs(commPipe);
+        return true;
+    }
+}
--- Robust.LanguageServer/Program.cs
+++ Robust.LanguageServer/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.IO.Pipes;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using OmniSharp.Extensions.LanguageServer.Protocol.Window;
 using OmniSharp.Extensions.LanguageServer.Server;
@@ -11,12 +12,51 @@ using Robust.Shared.Network;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Reflection;
 using Robust.Shared.Timing;
+using Serilog;
+
+if (!CommandLineArgs.TryParse(args, out var cliArgs))
+    return 1;
+
+Stream outStream;
+Stream inStream;
+
+if (cliArgs.CommunicationPipe is { } pipe)
+{
+    // Using pipe means we can log to stdout without ruining everything.
+    Log.Logger = new LoggerConfiguration()
+        .Enrich.FromLogContext()
+        .WriteTo.Console()
+        .MinimumLevel.Verbose()
+        .CreateLogger();
+
+    Log.Debug("Communicating using pipe: {PipeName}", pipe);
+
+    var stream = new NamedPipeServerStream(
+        pipe,
+        PipeDirection.InOut,
+        1,
+        PipeTransmissionMode.Byte,
+        PipeOptions.Asynchronous);
+
+    outStream = stream;
+    inStream = stream;
+
+    stream.WaitForConnection();
+
+    Log.Debug("Received pipe connection!");
+}
+else
+{
+    outStream = Console.OpenStandardOutput();
+    inStream = Console.OpenStandardInput();
+}
 
 var server = await LanguageServer.From(options => options
-    .WithInput(Console.OpenStandardInput())
-    .WithOutput(Console.OpenStandardOutput())
+    .WithInput(inStream)
+    .WithOutput(outStream)
     .ConfigureLogging(x => x
         .AddLanguageProtocolLogging()
+        .AddSerilog()
         .SetMinimumLevel(LogLevel.Debug))
     .WithServices(x => x.AddSingleton<TextDocumentsCache>())
     .AddHandler<DocumentSymbolHandler>()
@@ -38,12 +78,18 @@ var server = await LanguageServer.From(options => options
     })
     .OnStarted((languageServer, token) =>
     {
+        var prototypes = languageServer.Services.GetRequiredService<IPrototypeManager>();
+
+        const string prototypeDir = @"C:\Users\Pieter-Jan Briers\Projects\ss14\space-station-14\Resources\Prototypes";
+
         languageServer.LogInfo("started");
 
         return Task.CompletedTask;
     }));
 
 await server.WaitForExit;
+
+return 0;
 
 void InitIoC(IServiceCollection services)
 {
@@ -66,4 +112,49 @@ void InitIoC(IServiceCollection services)
 
     deps.BuildGraph();
 }
+sealed class FlushWrapStream : Stream
+{
+    private readonly Stream _baseStream;
 
+    public FlushWrapStream(Stream baseStream)
+    {
+        _baseStream = baseStream;
+    }
+
+    public override void Flush()
+    {
+        _baseStream.Flush();
+    }
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        return _baseStream.Read(buffer, offset, count);
+    }
+
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+        throw new NotSupportedException();
+    }
+
+    public override void SetLength(long value)
+    {
+        throw new NotSupportedException();
+    }
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        _baseStream.Write(buffer, offset, count);
+        _baseStream.Flush();
+    }
+
+    public override bool CanRead => true;
+    public override bool CanSeek => false;
+    public override bool CanWrite => true;
+    public override long Length => throw new NotSupportedException();
+
+    public override long Position
+    {
+        get => throw new NotSupportedException();
+        set => throw new NotSupportedException();
+    }
+}

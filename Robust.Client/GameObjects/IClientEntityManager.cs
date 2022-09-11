1ed77e4dc1a9306e5fbe0ef541995ee0d6d15025
ElectroJr <leonsfriedrich@gmail.com>
2022-09-11 17:37:41 -0400

    Make RaisePredictiveEvent check IsPredictionEnabled

--- Robust.Client/GameObjects/IClientEntityManager.cs
+++ Robust.Client/GameObjects/IClientEntityManager.cs
@@ -4,6 +4,9 @@ namespace Robust.Client.GameObjects
 {
     public interface IClientEntityManager : IEntityManager, IEntityNetworkManager
     {
-
+        /// <summary>
+        ///     Sends a networked message to the server, while also repeatedly raising it locally for every time this tick gets re-predicted.
+        /// </summary>
+        void RaisePredictiveEvent<T>(T msg) where T : EntityEventArgs;
     }
 }

fa5fdcb5a50f221626fe6cd1ad2d2aeede97f49c
Leon Friedrich <60421075+ElectroJr@users.noreply.github.com>
2022-09-14 20:20:44 +1200

    Move appearance state to shared (#3252)
    Co-authored-by: metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>

--- Robust.Shared/GameObjects/Systems/SharedAppearanceSystem.cs
+++ Robust.Shared/GameObjects/Systems/SharedAppearanceSystem.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using Robust.Shared.GameStates;
 using Robust.Shared.IoC;
 using Robust.Shared.Serialization;
 using Robust.Shared.Timing;
@@ -12,6 +13,17 @@ public abstract class SharedAppearanceSystem : EntitySystem
 {
     [Dependency] private readonly IGameTiming _timing = default!;
 
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<AppearanceComponent, ComponentGetState>(OnAppearanceGetState);
+    }
+
+    private static void OnAppearanceGetState(EntityUid uid, AppearanceComponent component, ref ComponentGetState args)
+    {
+        args.State = new AppearanceComponentState(component.AppearanceData);
+    }
+
     /// <summary>
     ///     Mark an appearance component as dirty, so that the appearance will get updated in the next frame update.
     /// </summary>
@@ -21,13 +33,14 @@ public abstract class SharedAppearanceSystem : EntitySystem
 
     public void SetData(EntityUid uid, Enum key, object value, AppearanceComponent? component = null)
     {
-        // If appearance data is changing due to server state application, the server's comp state is getting applied
-        // anyways, so we can skip this.
-        if (_timing.ApplyingState)
-            return; 
-
         if (!Resolve(uid, ref component, false))
             return;
+
+        // If appearance data is changing due to server state application, the server's comp state is getting applied
+        // anyways, so we can skip this.
+        if (_timing.ApplyingState
+            && component.NetSyncEnabled) // TODO consider removing this and avoiding the component resolve altogether.
+            return; 
 
         if (component.AppearanceData.TryGetValue(key, out var existing) && existing.Equals(value))
             return;

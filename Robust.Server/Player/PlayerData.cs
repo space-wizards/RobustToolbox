1587ee3c93e1a8bcfb9812bf2a546ecc1d7c8932
Leon Friedrich <60421075+ElectroJr@users.noreply.github.com>
2022-09-20 12:44:00 +1200

    Store username inn player data (#3265)

--- Robust.Server/Player/PlayerData.cs
+++ Robust.Server/Player/PlayerData.cs
@@ -5,13 +5,17 @@ namespace Robust.Server.Player
 {
     sealed class PlayerData : IPlayerData
     {
-        public PlayerData(NetUserId userId)
+        public PlayerData(NetUserId userId, string userName)
         {
             UserId = userId;
+            UserName = userName;
         }
 
         [ViewVariables]
         public NetUserId UserId { get; }
+
+        [ViewVariables]
+        public string UserName { get; }
 
         [ViewVariables]
         public object? ContentDataUncast { get; set; }

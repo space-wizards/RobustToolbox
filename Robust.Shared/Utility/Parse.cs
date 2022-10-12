80e390a74bd5c9d5323f8b6e2086cd6c78f68f3b
Игорь Спичкин <55196698+igorsaux@users.noreply.github.com>
2022-10-12 15:10:04 +0300

    Use invariant culture in serialization (#3323)

--- Robust.Shared/Utility/Parse.cs
+++ Robust.Shared/Utility/Parse.cs
@@ -13,6 +13,40 @@ namespace Robust.Shared.Utility;
 /// for the privilege of having code that isn't gonna break for French people.</remarks>
 public static class Parse
 {
+    // INT16
+
+    public static bool TryInt16(ReadOnlySpan<char> text, out short result)
+    {
+        return TryInt16(text, NumberStyles.Integer, out result);
+    }
+
+    public static bool TryInt16(ReadOnlySpan<char> text, NumberStyles style, out short result)
+    {
+        return short.TryParse(text, style, CultureInfo.InvariantCulture, out result);
+    }
+
+    public static short Int16(ReadOnlySpan<char> text, NumberStyles style = NumberStyles.Integer)
+    {
+        return short.Parse(text, style, CultureInfo.InvariantCulture);
+    }
+
+    // UINT16
+
+    public static bool TryUInt16(ReadOnlySpan<char> text, out ushort result)
+    {
+        return TryUInt16(text, NumberStyles.Integer, out result);
+    }
+
+    public static bool TryUInt16(ReadOnlySpan<char> text, NumberStyles style, out ushort result)
+    {
+        return ushort.TryParse(text, style, CultureInfo.InvariantCulture, out result);
+    }
+
+    public static ushort UInt16(ReadOnlySpan<char> text, NumberStyles style = NumberStyles.Integer)
+    {
+        return ushort.Parse(text, style, CultureInfo.InvariantCulture);
+    }
+
     // INT32
 
     public static bool TryInt32(ReadOnlySpan<char> text, out int result)
@@ -28,6 +62,23 @@ public static class Parse
     public static int Int32(ReadOnlySpan<char> text, NumberStyles style = NumberStyles.Integer)
     {
         return int.Parse(text, style, CultureInfo.InvariantCulture);
+    }
+
+    // UINT32
+
+    public static bool TryUInt32(ReadOnlySpan<char> text, out uint result)
+    {
+        return TryUInt32(text, NumberStyles.Integer, out result);
+    }
+
+    public static bool TryUInt32(ReadOnlySpan<char> text, NumberStyles style, out uint result)
+    {
+        return uint.TryParse(text, style, CultureInfo.InvariantCulture, out result);
+    }
+
+    public static uint UInt32(ReadOnlySpan<char> text, NumberStyles style = NumberStyles.Integer)
+    {
+        return uint.Parse(text, style, CultureInfo.InvariantCulture);
     }
 
     // INT64
@@ -46,6 +97,24 @@ public static class Parse
     {
         return long.Parse(text, style, CultureInfo.InvariantCulture);
     }
+
+    // UINT64
+
+    public static bool TryUInt64(ReadOnlySpan<char> text, out ulong result)
+    {
+        return TryUInt64(text, NumberStyles.Integer, out result);
+    }
+
+    public static bool TryUInt64(ReadOnlySpan<char> text, NumberStyles style, out ulong result)
+    {
+        return ulong.TryParse(text, style, CultureInfo.InvariantCulture, out result);
+    }
+
+    public static ulong UInt64(ReadOnlySpan<char> text, NumberStyles style = NumberStyles.Integer)
+    {
+        return ulong.Parse(text, style, CultureInfo.InvariantCulture);
+    }
+
 
     // FLOAT
 
@@ -79,5 +148,22 @@ public static class Parse
     public static double Double(ReadOnlySpan<char> text, NumberStyles style = NumberStyles.Float)
     {
         return double.Parse(text, style, CultureInfo.InvariantCulture);
+    }
+
+    // DECIMAL
+
+    public static bool TryDecimal(ReadOnlySpan<char> text, out decimal result)
+    {
+        return TryDecimal(text, NumberStyles.Float, out result);
+    }
+
+    public static bool TryDecimal(ReadOnlySpan<char> text, NumberStyles style, out decimal result)
+    {
+        return decimal.TryParse(text, style, CultureInfo.InvariantCulture, out result);
+    }
+
+    public static decimal Decimal(ReadOnlySpan<char> text, NumberStyles style = NumberStyles.Float)
+    {
+        return decimal.Parse(text, style, CultureInfo.InvariantCulture);
     }
 }

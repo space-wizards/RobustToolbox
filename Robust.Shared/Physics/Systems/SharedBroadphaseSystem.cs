8b9637070262510ce14287357a877e3b8b35040c
metalgearsloth <metalgearsloth@gmail.com>
2022-10-16 14:48:37 +1100

    Pool contact pairbuffer
    Unfortunately to do box2d's fancy thing we'd need to make further changes to the movebuffer, but this is easy.

--- Robust.Shared/Physics/Systems/SharedBroadphaseSystem.cs
+++ Robust.Shared/Physics/Systems/SharedBroadphaseSystem.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Microsoft.Extensions.ObjectPool;
 using Robust.Shared.Configuration;
 using Robust.Shared.GameObjects;
 using Robust.Shared.IoC;
@@ -38,6 +39,11 @@ namespace Robust.Shared.Physics.Systems
         /// This only has a noticeable performance impact where multiple broadphases are in close proximity.
         /// </summary>
         private float _broadphaseExpand;
+
+        private readonly ObjectPool<HashSet<FixtureProxy>> _proxyPool =
+            new DefaultObjectPool<HashSet<FixtureProxy>>(new SetPolicy<FixtureProxy>(), 4096);
+
+        private readonly Dictionary<FixtureProxy, HashSet<FixtureProxy>> _pairBuffer = new(64);
 
         public override void Initialize()
         {
@@ -142,7 +148,6 @@ namespace Robust.Shared.Physics.Systems
         {
             var moveBuffer = component.MoveBuffer;
             var movedGrids = _mapManager.GetMovedGrids(mapId);
-            var pairBuffer = new Dictionary<FixtureProxy, HashSet<FixtureProxy>>();
             var gridMoveBuffer = new Dictionary<FixtureProxy, Box2>();
             var gridsPool = new List<MapGrid>();
 
@@ -166,7 +171,7 @@ namespace Robust.Shared.Physics.Systems
             // Handle grids first as they're not stored on map broadphase at all.
             HandleGridCollisions(mapId, contactManager, movedGrids, physicsQuery, xformQuery);
 
-            DebugTools.Assert(moveBuffer.Count > 0 || pairBuffer.Count == 0);
+            DebugTools.Assert(moveBuffer.Count > 0 || _pairBuffer.Count == 0);
 
             foreach (var (proxy, worldAABB) in moveBuffer)
             {
@@ -176,14 +181,14 @@ namespace Robust.Shared.Physics.Systems
                 // Get every broadphase we may be intersecting.
                 foreach (var grid in _mapManager.FindGridsIntersecting(mapId, worldAABB.Enlarged(_broadphaseExpand), gridsPool, xformQuery, physicsQuery))
                 {
-                    FindPairs(proxy, worldAABB, grid.GridEntityId, pairBuffer, xformQuery, broadphaseQuery);
+                    FindPairs(proxy, worldAABB, grid.GridEntityId, _pairBuffer, xformQuery, broadphaseQuery);
                 }
 
                 gridsPool.Clear();
-                FindPairs(proxy, worldAABB, _mapManager.GetMapEntityId(mapId), pairBuffer, xformQuery, broadphaseQuery);
+                FindPairs(proxy, worldAABB, _mapManager.GetMapEntityId(mapId), _pairBuffer, xformQuery, broadphaseQuery);
             }
 
-            foreach (var (proxyA, proxies) in pairBuffer)
+            foreach (var (proxyA, proxies) in _pairBuffer)
             {
                 var proxyABody = proxyA.Fixture.Body;
 
@@ -205,6 +210,12 @@ namespace Robust.Shared.Physics.Systems
                 }
             }
 
+            foreach (var (_, proxies) in _pairBuffer)
+            {
+                _proxyPool.Return(proxies);
+            }
+
+            _pairBuffer.Clear();
             moveBuffer.Clear();
             _mapManager.ClearMovedGrids(mapId);
         }
@@ -324,7 +335,13 @@ namespace Robust.Shared.Physics.Systems
             }
 
             var broadphaseComp = broadphaseQuery.GetComponent(broadphase);
-            var proxyPairs = pairBuffer.GetOrNew(proxy);
+
+            if (!pairBuffer.TryGetValue(proxy, out var proxyPairs))
+            {
+                proxyPairs = _proxyPool.Get();
+                pairBuffer[proxy] = proxyPairs;
+            }
+
             var state = (proxyPairs, pairBuffer, proxy);
 
             broadphaseComp.Tree.QueryAabb(ref state, static (

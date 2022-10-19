c53228c258b6c41c3eba32704722eb9a43ce52ba
Paul <ritter.paul1@googlemail.com>
2022-10-19 22:20:49 +0200

    more progress

--- Robust.Shared/Serialization/TypeSerializers/Implementations/FormattedMessageSerializer.cs
+++ Robust.Shared/Serialization/TypeSerializers/Implementations/FormattedMessageSerializer.cs
@@ -5,6 +5,7 @@ using Robust.Shared.Serialization.Manager.Attributes;
 using Robust.Shared.Serialization.Markdown;
 using Robust.Shared.Serialization.Markdown.Validation;
 using Robust.Shared.Serialization.Markdown.Value;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
 using Robust.Shared.Serialization.TypeSerializers.Interfaces;
 using Robust.Shared.Utility;
 
@@ -13,17 +14,22 @@ namespace Robust.Shared.Serialization.TypeSerializers.Implementations
     [TypeSerializer]
     public sealed class FormattedMessageSerializer : ITypeSerializer<FormattedMessage, ValueDataNode>
     {
+        private LocStringSerializer _loc = new();
+
         public FormattedMessage Read(ISerializationManager serializationManager,
             ValueDataNode node, IDependencyCollection dependencies, bool skipHook,
             ISerializationContext? context = null, FormattedMessage? value = default)
         {
-            return FormattedMessage.FromMarkup(node.Value);
+            return FormattedMessage.FromMarkup(_loc.Read(serializationManager, node, dependencies, skipHook, context));
         }
 
         public ValidationNode Validate(ISerializationManager serializationManager, ValueDataNode node,
             IDependencyCollection dependencies,
             ISerializationContext? context = null)
         {
+            var valNode = _loc.Validate(serializationManager, node, dependencies, context);
+            if (valNode is ErrorNode) return valNode;
+
             return FormattedMessage.ValidMarkup(node.Value)
                 ? new ValidatedValueNode(node)
                 : new ErrorNode(node, "Invalid markup in FormattedMessage.");
@@ -34,7 +40,7 @@ namespace Robust.Shared.Serialization.TypeSerializers.Implementations
             bool alwaysWrite = false,
             ISerializationContext? context = null)
         {
-            return new ValueDataNode(value.ToMarkup());
+            return _loc.Write(serializationManager, value.ToMarkup(), dependencies, alwaysWrite, context);
         }
 
         [MustUseReturnValue]

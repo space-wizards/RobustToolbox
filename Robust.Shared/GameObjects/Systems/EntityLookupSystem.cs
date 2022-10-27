44290f690f68ce9287999aaf5d011915c72c965f
metalgearsloth <comedian_vs_clown@hotmail.com>
2022-10-28 01:25:30 +1100

    Fix lookup parenting

--- Robust.Shared/GameObjects/Systems/EntityLookupSystem.cs
+++ Robust.Shared/GameObjects/Systems/EntityLookupSystem.cs
@@ -423,24 +423,23 @@ namespace Robust.Shared.GameObjects
 
         private void OnMove(ref MoveEvent args)
         {
-            UpdatePosition(args.Sender, args.Component);
-        }
-
-        private void UpdatePosition(EntityUid uid, TransformComponent xform)
-        {
             // Even if the entity is contained it may have children that aren't so we still need to update.
-            if (!CanMoveUpdate(uid)) return;
+            if (!CanMoveUpdate(args.Sender)) return;
 
             var broadQuery = GetEntityQuery<BroadphaseComponent>();
             var xformQuery = GetEntityQuery<TransformComponent>();
-            var lookup = GetBroadphase(uid, xform, broadQuery, xformQuery);
 
-            if (lookup == null) return;
+            // TODO: For now we'll just let parent change handle this.
+            var oldLookup = GetBroadphase(args.OldPosition.EntityId, broadQuery, xformQuery);
+            var newLookup = GetBroadphase(args.NewPosition.EntityId, broadQuery, xformQuery);
 
+            if (newLookup == null || oldLookup != newLookup) return;
+
+            var xform = args.Component;
             var coordinates = _transform.GetMoverCoordinates(xform.Coordinates, xformQuery);
-            var lookupRotation = _transform.GetWorldRotation(lookup.Owner, xformQuery);
-            var aabb = GetAABB(uid, coordinates.Position, _transform.GetWorldRotation(xform) - lookupRotation, xform, xformQuery);
-            AddToEntityTree(lookup, xform, aabb, xformQuery, lookupRotation);
+            var lookupRotation = _transform.GetWorldRotation(newLookup.Owner, xformQuery);
+            var aabb = GetAABB(args.Sender, coordinates.Position, _transform.GetWorldRotation(xform) - lookupRotation, xform, xformQuery);
+            AddToEntityTree(newLookup, xform, aabb, xformQuery, lookupRotation);
         }
 
         private bool CanMoveUpdate(EntityUid uid)

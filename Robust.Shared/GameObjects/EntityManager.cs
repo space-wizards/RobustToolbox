8ed453709db6266677763cb4c1397157f3525593
ElectroJr <leonsfriedrich@gmail.com>
2022-10-28 13:45:25 -0400

    Add additional container assert

--- Robust.Shared/GameObjects/EntityManager.cs
+++ Robust.Shared/GameObjects/EntityManager.cs
@@ -334,9 +334,6 @@ namespace Robust.Shared.GameObjects
             var transform = xformQuery.GetComponent(metadata.Owner);
             metadata.EntityLifeStage = EntityLifeStage.Terminating;
             var ev = new EntityTerminatingEvent(metadata.Owner);
-
-            // TODO: consider making this a meta-data flag?
-            // veeeery few entities make use of this event.
             EventBus.RaiseLocalEvent(metadata.Owner, ref ev, false);
 
             foreach (var child in transform._children)

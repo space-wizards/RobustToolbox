53d45ad770609cf047974163eb87f17f2c3df741
metalgearsloth <comedian_vs_clown@hotmail.com>
2022-10-31 23:56:43 +1100

    Add lookup reparenting tests
    The 2nd one was failing on the old build as the 0-fixture entity was being added.

--- Robust.UnitTesting/Shared/Physics/Broadphase_Test.cs
+++ Robust.UnitTesting/Shared/Physics/Broadphase_Test.cs
@@ -4,7 +4,9 @@ using Robust.Shared.GameObjects;
 using Robust.Shared.Map;
 using Robust.Shared.Maths;
 using Robust.Shared.Physics;
+using Robust.Shared.Physics.Collision.Shapes;
 using Robust.Shared.Physics.Components;
+using Robust.Shared.Physics.Dynamics;
 using Robust.Shared.Physics.Systems;
 using Robust.UnitTesting.Server;
 
@@ -13,6 +15,89 @@ namespace Robust.UnitTesting.Shared.Physics;
 [TestFixture]
 public sealed class Broadphase_Test
 {
+    /// <summary>
+    /// If we reparent a sundries entity to another broadphase does it correctly update.
+    /// </summary>
+    [Test]
+    public void ReparentSundries()
+    {
+        var sim = RobustServerSimulation.NewSimulation().InitializeInstance();
+        var entManager = sim.Resolve<IEntityManager>();
+        var mapManager = sim.Resolve<IMapManager>();
+
+        var mapId = mapManager.CreateMap();
+        var mapEnt = mapManager.GetMapEntityId(mapId);
+        var grid = mapManager.CreateGrid(mapId);
+
+        grid.SetTile(Vector2i.Zero, new Tile(1));
+        Assert.That(entManager.HasComponent<BroadphaseComponent>(grid.GridEntityId));
+        var broadphase = entManager.GetComponent<BroadphaseComponent>(grid.GridEntityId);
+
+        var ent = entManager.SpawnEntity(null, new EntityCoordinates(grid.GridEntityId, new Vector2(0.5f, 0.5f)));
+        Assert.That(broadphase.SundriesTree, Does.Contain(ent));
+
+        var xform = entManager.GetComponent<TransformComponent>(ent);
+
+        xform.Coordinates = new EntityCoordinates(mapEnt, Vector2.One);
+        Assert.That(broadphase.SundriesTree, Does.Not.Contain(ent));
+
+        Assert.That(entManager.GetComponent<BroadphaseComponent>(mapEnt).SundriesTree, Does.Contain(ent));
+    }
+
+    /// <summary>
+    /// If we reparent a colliding physics entity to another broadphase does it correctly update.
+    /// </summary>
+    [Test]
+    public void ReparentBroadphase()
+    {
+        var sim = RobustServerSimulation.NewSimulation().InitializeInstance();
+        var entManager = sim.Resolve<IEntityManager>();
+        var mapManager = sim.Resolve<IMapManager>();
+        var fixturesSystem = entManager.EntitySysManager.GetEntitySystem<FixtureSystem>();
+        var physicsSystem = entManager.EntitySysManager.GetEntitySystem<SharedPhysicsSystem>();
+
+        var mapId = mapManager.CreateMap();
+        var mapEnt = mapManager.GetMapEntityId(mapId);
+        var grid = mapManager.CreateGrid(mapId);
+
+        grid.SetTile(Vector2i.Zero, new Tile(1));
+        Assert.That(entManager.HasComponent<BroadphaseComponent>(grid.GridEntityId));
+        var broadphase = entManager.GetComponent<BroadphaseComponent>(grid.GridEntityId);
+
+        var ent = entManager.SpawnEntity(null, new EntityCoordinates(grid.GridEntityId, new Vector2(0.5f, 0.5f)));
+        var physics = entManager.AddComponent<PhysicsComponent>(ent);
+
+        // If we're not collidable we're still on the sundries tree.
+        Assert.That(broadphase.StaticSundriesTree, Does.Contain(ent));
+
+        var shape = new PolygonShape();
+        shape.SetAsBox(0.5f, 0.5f);
+        var fixture = new Fixture(physics, shape);
+        fixturesSystem.CreateFixture(physics, fixture);
+        physicsSystem.SetCanCollide(physics, true);
+
+        // Now that we're collidable should be correctly on the grid's tree.
+        Assert.That(fixture.ProxyCount, Is.EqualTo(1));
+        Assert.That(broadphase.StaticSundriesTree, Does.Not.Contain(ent));
+
+        Assert.That(broadphase.StaticTree.GetProxy(fixture.Proxies[0].ProxyId)!.Equals(fixture.Proxies[0]));
+
+        var xform = entManager.GetComponent<TransformComponent>(ent);
+
+        // Now check we go to the map's tree correctly.
+        xform.Coordinates = new EntityCoordinates(mapEnt, Vector2.One);
+        Assert.That(entManager.GetComponent<BroadphaseComponent>(mapEnt).StaticTree.GetProxy(fixture.Proxies[0].ProxyId)!.Equals(fixture.Proxies[0]));
+    }
+
+    /// <summary>
+    /// If we update an entity are its trees correctly updated.
+    /// </summary>
+    [Test]
+    public void TestBroadphaseTree()
+    {
+
+    }
+
     /// <summary>
     /// If we change a grid's map does it still remain not on the general broadphase.
     /// </summary>

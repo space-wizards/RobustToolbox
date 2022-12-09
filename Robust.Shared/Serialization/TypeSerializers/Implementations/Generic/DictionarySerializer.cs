16cfc3307112b00978ad42fbd01cea8620a335ae
Paul <ritter.paul1@googlemail.com>
2022-12-09 20:03:51 +0100

    analyzer to warn about using a boxing variant

--- Robust.Shared/Serialization/TypeSerializers/Implementations/Generic/DictionarySerializer.cs
+++ Robust.Shared/Serialization/TypeSerializers/Implementations/Generic/DictionarySerializer.cs
@@ -81,8 +81,8 @@ public sealed class DictionarySerializer<TKey, TValue> :
         var mapping = new Dictionary<ValidationNode, ValidationNode>();
         foreach (var (key, val) in node.Children)
         {
-            mapping.Add(serializationManager.ValidateNode(typeof(TKey), key, context),
-                serializationManager.ValidateNode(typeof(TValue), val, context));
+            mapping.Add(serializationManager.ValidateNode<TKey>(key, context),
+                serializationManager.ValidateNode<TValue>(val, context));
         }
 
         return new ValidatedMappingNode(mapping);

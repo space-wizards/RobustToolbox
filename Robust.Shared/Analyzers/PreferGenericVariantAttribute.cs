660ae20f4033dbd7421497f28bf77df8a78e40a1
Paul <ritter.paul1@googlemail.com>
2022-12-11 00:30:16 +0100

    adjust nullability

--- Robust.Shared/Analyzers/PreferGenericVariantAttribute.cs
+++ Robust.Shared/Analyzers/PreferGenericVariantAttribute.cs
@@ -9,9 +9,9 @@ namespace Robust.Shared.Analyzers;
 [AttributeUsage(AttributeTargets.Method)]
 public sealed class PreferGenericVariantAttribute : Attribute
 {
-    public readonly string? GenericVariant;
+    public readonly string GenericVariant;
 
-    public PreferGenericVariantAttribute(string? genericVariant = null)
+    public PreferGenericVariantAttribute(string genericVariant = null!)
     {
         GenericVariant = genericVariant;
     }

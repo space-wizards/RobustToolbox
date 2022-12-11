fc968ba4701b6b83e7c660375e481a5a8f012224
Paul <ritter.paul1@googlemail.com>
2022-12-12 00:17:48 +0100

    hotfixes an oopsie

---
+++ Robust.Server/GameObjects/Components/UserInterface/ServerBoundUserInterfaceCopier.cs
@@ -0,0 +1,15 @@
+﻿using Robust.Shared.Serialization.Manager;
+using Robust.Shared.Serialization.Manager.Attributes;
+using Robust.Shared.Serialization.TypeSerializers.Interfaces;
+
+namespace Robust.Server.GameObjects;
+
+[TypeSerializer]
+public sealed class ServerBoundUserInterfaceCopier : ITypeCopyCreator<BoundUserInterface>
+{
+    public BoundUserInterface CreateCopy(ISerializationManager serializationManager, BoundUserInterface source, bool skipHook,
+        ISerializationContext? context = null)
+    {
+        return new BoundUserInterface(source.RequireInputValidation, source.UiKey, source.InteractionRangeSqrd);
+    }
+}

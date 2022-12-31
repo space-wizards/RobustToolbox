ed8cd7b32c5b003af5be992fdc34fe826c2221e0
ElectroJr <leonsfriedrich@gmail.com>
2022-12-31 15:59:16 -0500

    grid delta states

--- Robust.Shared/Map/Components/MapGridComponent.cs
+++ Robust.Shared/Map/Components/MapGridComponent.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using Robust.Shared.GameObjects;
 using Robust.Shared.GameStates;
 using Robust.Shared.IoC;
+using Robust.Shared.Localization;
 using Robust.Shared.Map.Enumerators;
 using Robust.Shared.Map.Events;
 using Robust.Shared.Maths;
@@ -901,7 +902,7 @@ namespace Robust.Shared.Map.Components
     ///     Serialized state of a <see cref="MapGridComponentState"/>.
     /// </summary>
     [Serializable, NetSerializable]
-    internal sealed class MapGridComponentState : ComponentState
+    internal sealed class MapGridComponentState : ComponentState, IComponentDeltaState
     {
         /// <summary>
         ///     The size of the chunks in the map grid.
@@ -914,12 +915,65 @@ namespace Robust.Shared.Map.Components
         public List<ChunkDatum>? ChunkData;
 
         /// <summary>
-        ///     Constructs a new instance of <see cref="MapGridComponentState"/>.
+        /// Networked chunk data containing the full grid state.
+        /// </summary>
+        public Dictionary<Vector2i, Tile[]>? FullGridData;
+
+        public bool FullState => FullGridData != null;
+
+        /// <summary>
+        ///     Constructs a new grid component delta state.
         /// </summary>
         public MapGridComponentState(ushort chunkSize, List<ChunkDatum>? chunkData)
         {
             ChunkSize = chunkSize;
             ChunkData = chunkData;
+        }
+
+        /// <summary>
+        ///     Constructs a new full component state.
+        /// </summary>
+        public MapGridComponentState(ushort chunkSize, Dictionary<Vector2i, Tile[]>? fullGridData)
+        {
+            ChunkSize = chunkSize;
+            FullGridData = fullGridData;
+        }
+
+        public void ApplyToFullState(ComponentState fullState)
+        {
+            var state = (MapGridComponentState)fullState;
+            DebugTools.Assert(!FullState && state.FullState);
+
+            state.ChunkSize = ChunkSize;
+
+            if (ChunkData == null)
+                return;
+
+            foreach (var data in ChunkData)
+            {
+                if (data.IsDeleted())
+                    state.FullGridData!.Remove(data.Index);
+                else
+                    state.FullGridData![data.Index] = data.TileData;
+            }
+        }
+
+        public ComponentState CreateNewFullState(ComponentState fullState)
+        {
+            var state = (MapGridComponentState)fullState;
+            DebugTools.Assert(!FullState && state.FullState);
+
+            var fullGridData = new Dictionary<Vector2i, Tile[]>(state.FullGridData!.Count);
+
+            foreach (var (key, value) in state.FullGridData)
+            {
+                var arr = fullGridData[key] = new Tile[value.Length];
+                Array.Copy(value, arr, value.Length);
+            }
+
+            var newState = new MapGridComponentState(ChunkSize, fullGridData);
+            ApplyToFullState(newState);
+            return newState;
         }
     }
 }

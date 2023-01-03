7920e7691383eb8e2b4e71cea61a42ace343d99b
Ygg01 <y.laughing.man.y@gmail.com>
2023-01-03 04:14:54 +0100

    WIP ResPath

---
+++ Robust.Shared/Utility/ResPath.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using JetBrains.Annotations;
+using Robust.Shared.Serialization;
+
+namespace Robust.Shared.Utility;
+
+[PublicAPI, Serializable, NetSerializable]
+public struct ResPath : IEquatable<ResPath>
+{
+    /// <summary>
+    ///     "." as a static. Separator used is <c>/</c>.
+    /// </summary>
+    public static readonly string Self = ".";
+
+    /// <summary>
+    ///     "/" (root) as a static. Separator used is <c>/</c>.
+    /// </summary>
+    public static readonly string Root = "/";
+
+    internal string CanonicalResource;
+
+
+    /// <summary>
+    ///     Create a new path from a string, splitting it by the separator provided.
+    /// </summary>
+    /// <param name="path">The string path to turn into a resource path.</param>
+    /// <param name="separator">The separator for the resource path.</param>
+    /// <exception cref="ArgumentException">Thrown if you try to use "." as separator.</exception>
+    /// <exception cref="ArgumentNullException">Thrown if either argument is null.</exception>
+    public ResPath(string path = ".", char separator = '/')
+    {
+        if (separator == '.')
+        {
+            throw new ArgumentException("Separator may not be .  Prefer \\ or /");
+        }
+
+        if (separator != '/')
+        {
+            // Stupid but should work in all cases (UTF-8/UTF-16 string)
+            // maybe replace with some unsafe in future for speed
+            var stringBuilder = new StringBuilder(path);
+            for (var pos = 0; pos < path.Length; pos++)
+            {
+                if (path[pos] == separator)
+                {
+                    stringBuilder[pos] = '/';
+                }
+            }
+
+            CanonicalResource = stringBuilder.ToString();
+        }
+        else
+        {
+            CanonicalResource = path;
+        }
+    }
+
+    public string Directory
+    {
+        get
+        {
+            if (IsSelf) return Self;
+
+            foreach (var VARIABLE in this.Segments())
+            {
+                return VARIABLE;
+            }
+
+            return "";
+        }
+    }
+
+    public bool IsSelf => CanonicalResource == Self;
+
+    public string Extension
+    {
+        get
+        {
+            if (IsSelf) return Self;
+
+            foreach (var VARIABLE in this.Segments())
+            {
+                return VARIABLE;
+            }
+
+            return "";
+        }
+    }
+
+    public string Filename
+    {
+        get
+        {
+            if (IsSelf) return Self;
+
+            foreach (var VARIABLE in this.Segments())
+            {
+                return VARIABLE;
+            }
+
+            return "";
+        }
+    }
+
+    public bool Equals(ResPath other)
+    {
+        return CanonicalResource == other.CanonicalResource;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is ResPath other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return CanonicalResource.GetHashCode();
+    }
+
+    public static bool operator ==(ResPath left, ResPath right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(ResPath left, ResPath right)
+    {
+        return !left.Equals(right);
+    }
+
+    public static ResPath operator /(ResPath left, ResPath right)
+    {
+        return new ResPath(left.CanonicalResource + "/" + right.CanonicalResource);
+    }
+
+    public static ResPath operator /(ResPath left, string right)
+    {
+        return new ResPath(left.CanonicalResource + "/" + right);
+    }
+
+    public ResPath ToRelativePath()
+    {
+        throw new NotImplementedException();
+    }
+
+    public object WithExtension(string newExt)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ResPath WithName(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ResPath RelativeTo(ResPath basePath)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsRooted()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ResPath ToRootedPath()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsRelative()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ResPath Clean()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsClean()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string ChangeSeparator(string s)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string FilenameWithoutExtension()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ResPath CommonBase(ResPath basePath)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public struct SegmentEnumerator : IEnumerator<string>
+{
+    private readonly ResPath _owner;
+    private int _positionStart;
+    private int _len;
+
+    public SegmentEnumerator(ResPath resPath)
+    {
+        _owner = resPath;
+        _positionStart = 0;
+        _len = 0;
+    }
+
+    public bool MoveNext()
+    {
+        if (_positionStart + _len >= _owner.CanonicalResource.Length)
+        {
+            return false;
+        }
+
+        _positionStart += _len;
+        if (_owner.CanonicalResource[_positionStart] == '/')
+        {
+            _positionStart++;
+        }
+
+        var ind = _owner.CanonicalResource[_positionStart ..].IndexOf('/');
+        _len = ind == -1 ? _owner.CanonicalResource.Length - _positionStart : ind;
+
+        // skip last `/`
+        return _len + _positionStart < _owner.CanonicalResource.Length || _len > 0;
+    }
+
+    public void Reset()
+    {
+        _positionStart = 0;
+        _len = 0;
+    }
+
+    object IEnumerator.Current => Current;
+
+    public string Current => _owner.CanonicalResource.AsSpan(_positionStart, _len).ToString();
+
+
+    public void Dispose()
+    {
+    }
+}
+
+public struct ReverseSegmentEnumerator : IEnumerator<string>
+{
+    private readonly ResPath _owner;
+    private int _positionEnd;
+    private int _len;
+
+    public ReverseSegmentEnumerator(ResPath resPath)
+    {
+        _owner = resPath;
+        _positionEnd = resPath.CanonicalResource.Length - 1;
+        _len = 0;
+    }
+
+    public bool MoveNext()
+    {
+        if (_positionEnd - _len <= 0)
+        {
+            return false;
+        }
+
+        _positionEnd -= _len;
+        if (_owner.CanonicalResource[_positionEnd] == '/')
+        {
+            _positionEnd--;
+        }
+
+        var ind = _owner.CanonicalResource[.._positionEnd].LastIndexOf('/');
+        if (ind == -1)
+        {
+            _len = _positionEnd ++;
+            _positionEnd = 0;
+        }
+        else
+        {
+            _len = _positionEnd - ind;
+            _positionEnd = ind + 1;
+        }
+        
+
+        // skip last `/`
+        return _positionEnd >= 0 || _len > 0;
+    }
+
+    public void Reset()
+    {
+        _positionEnd = 0;
+        _len = 0;
+    }
+
+    public string Current => _owner.CanonicalResource.AsSpan(_positionEnd, _len).ToString();
+
+    object IEnumerator.Current => Current;
+
+    public void Dispose()
+    {
+    }
+}
+
+public static class ResPathExtension
+{
+    public static IEnumerable<string> Segments(this ResPath resPath)
+    {
+        var iter = new SegmentEnumerator(resPath);
+        while (iter.MoveNext())
+        {
+            yield return iter.Current;
+        }
+    }
+
+    public static IEnumerable<string> ReverseSegments(this ResPath resPath)
+    {
+        var iter = new ReverseSegmentEnumerator(resPath);
+        while (iter.MoveNext())
+        {
+            yield return iter.Current;
+        }
+    }
+}
---
+++ Robust.UnitTesting/Shared/Utility/ResPath_Test.cs
@@ -0,0 +1,278 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Diagnostics.Tracing.Parsers.FrameworkEventSource;
+using NUnit.Framework;
+using Robust.Shared.Utility;
+// ReSharper disable AccessToStaticMemberViaDerivedType
+
+namespace Robust.UnitTesting.Shared.Utility;
+
+[TestFixture]
+[Parallelizable(ParallelScope.Fixtures | ParallelScope.All)]
+[TestOf(typeof(ResPath))]
+public sealed class ResPathTest
+{
+    [Test]
+    [TestCase("/Textures", new[]{"Textures"}, new[]{"Textures"})]
+    [TestCase("/Textures/", new[]{"Textures"}, new[]{"Textures"})]
+    public void IteratorTest(string input, string[] expectedSegments, string[] expectedRevSegments)
+    {
+        var res = new ResPath(input);
+        var segment = res.Segments().ToArray();
+        var reverseSegment = res.ReverseSegments().ToArray();
+        Assert.That(segment, Is.EqualTo(expectedSegments));
+        Assert.That(reverseSegment, Is.EqualTo(expectedRevSegments));
+    }
+    
+    
+    
+    public static List<(string, string)> InputCleanValues = new()
+    {
+        ("/Textures", "/Textures"),
+        ("Textures", "Textures"),
+        ("Textures/", "Textures"),
+        ("Textures/Laser.png", "Textures/Laser.png"),
+        ("Textures//Laser.png", "Textures/Laser.png"),
+        ("Textures/..//Radio.png/", "Textures/../Radio.png"),
+        ("", "."),
+        (".", "."),
+        ("./foo", "foo"),
+        ("foo/.", "foo"),
+        ("foo/./bar", "foo/bar"),
+        ("./", "."),
+        ("/.", "/"),
+        ("/", "/"),
+        (" ", " "), // Note the spaces here.
+        (" / ", " / "),
+        (". ", ". ")
+    };
+
+    // Tests whether input and output remains unchanged.
+    [Test]
+    public void InputClean_Test([ValueSource(nameof(InputCleanValues))] (string input, string expected) path)
+    {
+        var resPath = new ResPath(path.input);
+        Assert.That(resPath.ToString(), Is.EqualTo(path.expected));
+    }
+
+    public static List<(string, string)> ExtensionValues = new()
+    {
+        ("foo", ""),
+        ("foo.png", "png"),
+        ("test/foo.png", "png"),
+        (".bashrc", ""),
+        ("..png", "png"),
+        ("x.y.z", "z")
+    };
+
+    [Test]
+    public void Extension_Test([ValueSource(nameof(ExtensionValues))] (string path, string expected) data)
+    {
+        var respath = new ResPath(data.path);
+        Assert.That(respath.Extension, Is.EqualTo(data.expected));
+    }
+
+    public static List<(string, string)> FilenameValues = new()
+    {
+        ("foo", "foo"),
+        ("foo.png", "foo.png"),
+        ("x/y/z", "z"),
+        ("/bar", "bar"),
+        ("foo/", "foo") // Trailing / gets trimmed.
+    };
+
+    [Test]
+    public void Filename_Test([ValueSource(nameof(FilenameValues))] (string path, string expected) data)
+    {
+        var respath = new ResPath(data.path);
+        Assert.That(respath.Filename, Is.EqualTo(data.expected));
+    }
+
+    public static List<(string, string)> FilenameWithoutExtensionValues = new()
+    {
+        ("foo", "foo"),
+        ("foo.png", "foo"),
+        ("test/foo.png", "foo"),
+        ("derp/.bashrc", ".bashrc"),
+        ("..png", "."),
+        ("x.y.z", "x.y")
+    };
+
+    [Test]
+    public void FilenameWithoutExtension_Test([ValueSource(nameof(FilenameWithoutExtensionValues))] (string path, string expected) data)
+    {
+        var respath = new ResPath(data.path);
+        Assert.That(respath.FilenameWithoutExtension, Is.EqualTo(data.expected));
+    }
+
+    [TestCase(@"", ExpectedResult = @".")]
+    [TestCase(@".", ExpectedResult = @".")]
+    [TestCase(@"/foo/bar", ExpectedResult = @"/foo")]
+    [TestCase(@"/foo/bar.txt", ExpectedResult = @"/foo")]
+    public string DirectoryTest(string path)
+    {
+        return new ResPath(path).Directory;
+    }
+
+    [Test]
+    public void ChangeSeparator_Test()
+    {
+        var respath = new ResPath("a/b/c").ChangeSeparator("👏");
+        Assert.That(respath, Is.EqualTo("a👏b👏c"));
+    }
+
+    [Test]
+    public void ChangeSeparatorRooted_Test()
+    {
+        var respath = new ResPath("/a/b/c").ChangeSeparator("👏");
+        Assert.That(respath, Is.EqualTo("👏a👏b👏c"));
+    }
+
+    [Test]
+    public void Combine_Test()
+    {
+        var path1 = new ResPath("/a/b");
+        var path2 = new ResPath("c/d.png");
+        Assert.That((path1 / path2).ToString(), Is.EqualTo("/a/b/c/d.png"));
+        Assert.That((path1 / "z").ToString(), Is.EqualTo("/a/b/z"));
+    }
+
+    public static List<(string, string)> CleanValues = new()
+    {
+        ("//a/b/../c/./ss14.png", "/a/c/ss14.png"),
+        ("../a", "../a"),
+        ("../a/..", ".."),
+        ("../..", "../.."),
+        ("a/..", "."),
+        ("/../a", "/a"),
+        ("/..", "/"),
+    };
+
+    [Test]
+    public void Clean_Test([ValueSource(nameof(CleanValues))] (string path, string expected) data)
+    {
+        var path = new ResPath(data.path);
+        var cleaned = path.Clean();
+        Assert.Multiple(() =>
+        {
+            if (path == cleaned)
+            {
+                Assert.That(path.IsClean());
+            }
+            Assert.That(path.Clean(), Is.EqualTo(new ResPath(data.expected)));
+            Assert.That(cleaned.IsClean());
+        });
+    }
+
+    [Test]
+    public void RootedConversions_Test()
+    {
+        var path = new ResPath("/a/b");
+        Assert.That(path.IsRooted);
+        Assert.That(path.ToRootedPath(), Is.EqualTo(path));
+
+        var relative = path.ToRelativePath();
+        Assert.That(relative, Is.EqualTo(new ResPath("a/b")));
+        Assert.That(relative.IsRelative);
+
+        Assert.That(relative.ToRelativePath(), Is.EqualTo(relative));
+        Assert.That(relative.ToRootedPath(), Is.EqualTo(path));
+    }
+
+    public static List<(string, string, string)> RelativeToValues = new()
+    {
+        ("/a/b", "/a", "b"),
+        ("/a", "/", "a"),
+        ("/a/b/c", "/", "a/b/c"),
+        ("/a", "/a", "."),
+        ("a/b", "a", "b"),
+        ("/Textures/Weapons/laser.png", "/Textures/", "Weapons/laser.png")
+    };
+
+    [Test]
+    public void RelativeTo_Test([ValueSource(nameof(RelativeToValues))] (string source, string basePath, string expected) value)
+    {
+        var path = new ResPath(value.source);
+        var basePath = new ResPath(value.basePath);
+        Assert.That(path.RelativeTo(basePath), Is.EqualTo(new ResPath(value.expected)));
+    }
+
+    public static List<(string, string)> RelativeToFailValues = new()
+    {
+        ("/a/b", "/b"),
+        ("/a", "/c/d"),
+        ("/a/b", "/a/d"),
+        (".", "/"),
+        ("/", ".")
+    };
+
+    [Test]
+    public void RelativeToFail_Test([ValueSource(nameof(RelativeToFailValues))] (string source, string basePath) value)
+    {
+        var path = new ResPath(value.source);
+        var basePath = new ResPath(value.basePath);
+        Assert.That(() => path.RelativeTo(basePath), Throws.ArgumentException);
+    }
+
+    public static List<(string, string, string)> CommonBaseValues = new()
+    {
+        ("/a/b", "/a/c", "/a"),
+        ("a/b", "a/c", "a"),
+        ("/usr", "/bin", "/")
+    };
+
+    [Test]
+    public void CommonBase_Test([ValueSource(nameof(CommonBaseValues))] (string a, string b, string expected) value)
+    {
+        var path = new ResPath(value.a);
+        var basePath = new ResPath(value.b);
+        Assert.That(path.CommonBase(basePath), Is.EqualTo(new ResPath(value.expected)));
+    }
+
+    [Test]
+    public void CommonBaseFail_Test()
+    {
+        var path = new ResPath("a/b");
+        var basePath = new ResPath("b/a");
+        Assert.That(() => path.CommonBase(basePath), Throws.ArgumentException);
+    }
+
+    [Test]
+    public void WithNameTest()
+    {
+        var path = new ResPath("/a/b");
+        var modified = path.WithName("foo");
+        Assert.That(modified.Filename, Is.EqualTo("foo"));
+        modified = path.WithName("foo.exe");
+        Assert.That(modified.Filename, Is.EqualTo("foo.exe"));
+        Assert.That(modified.Extension, Is.EqualTo("exe"));
+    }
+
+    [Test]
+    public void WithNameExceptionTest()
+    {
+        var path = new ResPath("/a/b");
+        Assert.That(() => path.WithName("/foo"), Throws.ArgumentException);
+        Assert.That(() => path.WithName("."), Throws.ArgumentException);
+        Assert.That(() => path.WithName(""), Throws.ArgumentException);
+        Assert.That(() => path.WithName(null!), Throws.ArgumentException);
+    }
+
+    [Test]
+    [TestCase("/a/b.txt", "png", "/a/b.png")]
+    [TestCase("/a/b.txt.bak", "png", "/a/b.txt.png")]
+    public void WithExtensionTest(string start, string newExt, string expected)
+    {
+        var startPath = new ResPath(start);
+        Assert.That(startPath.WithExtension(newExt).ToString(), Is.EqualTo(expected));
+        Assert.That(startPath.WithExtension(newExt).ToString(), Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void RootToRelativeTest()
+    {
+        var path = new ResPath("/");
+
+        Assert.That(path.ToRelativePath(), Is.EqualTo(new ResPath(".")));
+    }
+}

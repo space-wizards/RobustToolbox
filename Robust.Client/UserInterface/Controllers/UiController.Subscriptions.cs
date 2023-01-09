fd9bf1f7cb8148a306c33244af1f338fc2255706
Jezithyr <Jezithyr@gmail.com>
2023-01-08 17:16:55 -0800

    added support for eventbus messages

--- Robust.Client/UserInterface/Controllers/UiController.Subscriptions.cs
+++ Robust.Client/UserInterface/Controllers/UiController.Subscriptions.cs
@@ -6,6 +6,14 @@ namespace Robust.Client.UserInterface.Controllers;
 
 public abstract partial class UIController : IEntityEventSubscriber
 {
+    protected void SubscribeLocalEvent<T>(
+        EntitySessionEventHandler<T> handler,
+        Type[]? before = null, Type[]? after = null)
+        where T : notnull
+    {
+        EntityManager.EventBus.SubscribeSessionEvent(EventSource.Local, this, handler,GetType(), before, after);
+    }
+
     protected void SubscribeLocalEvent<TComp, TEvent>(ComponentEventHandler<TComp, TEvent> handler,
         Type[]? before = null, Type[]? after = null)
         where TComp : IComponent

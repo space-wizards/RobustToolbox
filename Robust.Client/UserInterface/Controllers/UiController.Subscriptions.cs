b4291c0c70f3e705cfee225b57276ec2de8fe9e7
Jezithyr <Jezithyr@gmail.com>
2023-01-08 16:54:57 -0800

    Implemented EventBus support for UIControllers

---
+++ Robust.Client/UserInterface/Controllers/UiController.Subscriptions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Robust.Shared.GameObjects;
+
+namespace Robust.Client.UserInterface.Controllers;
+
+public abstract partial class UIController : IEntityEventSubscriber
+{
+    protected void SubscribeLocalEvent<TComp, TEvent>(ComponentEventHandler<TComp, TEvent> handler,
+        Type[]? before = null, Type[]? after = null)
+        where TComp : IComponent
+        where TEvent : notnull
+    {
+        EntityManager.EventBus.SubscribeLocalEvent(handler, GetType(), before, after);
+    }
+
+    protected void SubscribeLocalEvent<TComp, TEvent>(ComponentEventRefHandler<TComp, TEvent> handler,
+        Type[]? before = null, Type[]? after = null)
+        where TComp : IComponent
+        where TEvent : notnull
+    {
+        EntityManager.EventBus.SubscribeLocalEvent(handler, GetType(), before, after);
+    }
+
+    protected void SubscribeNetworkEvent<T>(
+        EntitySessionEventHandler<T> handler,
+        Type[]? before = null, Type[]? after = null)
+        where T : notnull
+    {
+        EntityManager.EventBus.SubscribeSessionEvent(EventSource.Network, this, handler, GetType(), before, after);
+    }
+
+    protected void SubscribeAllEvent<T>(
+        EntitySessionEventHandler<T> handler,
+        Type[]? before = null, Type[]? after = null)
+        where T : notnull
+    {
+        EntityManager.EventBus.SubscribeSessionEvent(EventSource.All, this, handler, GetType(), before, after);
+    }
+
+
+
+}

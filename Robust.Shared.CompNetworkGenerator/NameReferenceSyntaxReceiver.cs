25434499db4da7d694bed9853527724c80b6b9d7
Kara <lunarautomaton6@gmail.com>
2023-01-13 00:27:06 -0600

    dog what am i doing

---
+++ Robust.Shared.CompNetworkGenerator/NameReferenceSyntaxReceiver.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Robust.Shared.CompNetworkGenerator
+{
+    public sealed class NameReferenceSyntaxReceiver : ISyntaxReceiver
+    {
+        public List<ClassDeclarationSyntax> CandidateClasses { get; } = new List<ClassDeclarationSyntax>();
+
+        public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+        {
+            if (syntaxNode is ClassDeclarationSyntax classDeclarationSyntax &&
+                classDeclarationSyntax.AttributeLists.Count > 0)
+            {
+                var attrs = classDeclarationSyntax.AttributeLists.Select(a => a.Attributes);
+
+                CandidateClasses.Add(classDeclarationSyntax);
+            }
+        }
+    }
+}

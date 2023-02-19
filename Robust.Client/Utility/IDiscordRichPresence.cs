2b648ecd4e19808f154ebab622572cc768164297
faint <46868845+ficcialfaint@users.noreply.github.com>
2023-02-19 21:20:36 +0300

    Update IDiscordRichPresence.cs

--- Robust.Client/Utility/IDiscordRichPresence.cs
+++ Robust.Client/Utility/IDiscordRichPresence.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 
 namespace Robust.Client.Utility
 {
     public interface IDiscordRichPresence: IDisposable
     {
         void Initialize();
-        void Update(string serverName, string username, string maxUser);
+        void Update(string serverName, string username, string maxUser, string Users);
         void ClearPresence();
     }
 }

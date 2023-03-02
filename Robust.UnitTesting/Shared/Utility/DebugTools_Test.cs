ded5e7202523fc7f3b5ce714ff279a6f8d93a8c9
Pieter-Jan Briers <pieterjan.briers@gmail.com>
2023-03-02 23:09:47 +0100

    Fix TestAssertFormatNoAlloc

--- Robust.UnitTesting/Shared/Utility/DebugTools_Test.cs
+++ Robust.UnitTesting/Shared/Utility/DebugTools_Test.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using NUnit.Framework;
 using Robust.Shared.Utility;
 
@@ -12,15 +13,38 @@ public sealed class DebugTools_Test
 {
     [Test]
     [TestCase(true, 5)]
-    [Conditional("DEBUG")]
     public void TestAssertFormatNoAlloc(bool check, int val)
     {
+        // FUN FACT! This test is fragile as hell!
+        // The JIT is allowed to re-order allocations since it's not really a side effect it cares about.
+        // I had the boxing allocation at the bottom be re-ordered to be before the GetAllocatedBytesForCurrentThread().
+
+        NoInline();
+
         var allocA = GC.GetAllocatedBytesForCurrentThread();
 
-        DebugTools.Assert(check, $"Oops: {val}");
+        NoInline();
+
+        AssertWrap(check, val);
+
+        NoInline();
 
         var allocB = GC.GetAllocatedBytesForCurrentThread();
 
+        NoInline();
+
         Assert.That(allocB, Is.EqualTo(allocA));
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private static void AssertWrap(bool check, int val)
+    {
+        DebugTools.Assert(check, $"Oops: {val}");
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private static void NoInline()
+    {
+
     }
 }

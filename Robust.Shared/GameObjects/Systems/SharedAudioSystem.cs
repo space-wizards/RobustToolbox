14a9583a3e6e6cd93d19a2d77b223acd08fb70a8
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2023-03-22 20:39:13 +1100

    Add playpredicted for entitycoordinates audio (#3856)

--- Robust.Shared/GameObjects/Systems/SharedAudioSystem.cs
+++ Robust.Shared/GameObjects/Systems/SharedAudioSystem.cs
@@ -213,6 +213,17 @@ public abstract class SharedAudioSystem : EntitySystem
     /// <param name="audioParams">Audio parameters to apply when playing the sound. Defaults to using the sound specifier's parameters</param>
     public abstract IPlayingAudioStream? PlayPredicted(SoundSpecifier? sound, EntityUid source, EntityUid? user, AudioParams? audioParams = null);
 
+    /// <summary>
+    /// Plays a predicted sound following an EntityCoordinates. The server will send the sound to every player in PVS range,
+    /// unless that player is attached to the "user" entity that initiated the sound. The client-side system plays
+    /// this sound as normal
+    /// </summary>
+    /// <param name="sound">The sound specifier that points the audio file(s) that should be played.</param>
+    /// <param name="coordinates">The entitycoordinates "emitting" the audio</param>
+    /// <param name="user">The UID of the user that initiated this sound. This is usually some player's controlled entity.</param>
+    /// <param name="audioParams">Audio parameters to apply when playing the sound. Defaults to using the sound specifier's parameters</param>
+    public abstract IPlayingAudioStream? PlayPredicted(SoundSpecifier? sound, EntityCoordinates coordinates, EntityUid? user, AudioParams? audioParams = null);
+
     // TODO rename to play static
     /// <summary>
     /// Play an audio file at a static position.

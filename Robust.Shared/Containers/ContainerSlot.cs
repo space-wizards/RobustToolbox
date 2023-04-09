e7eb0bd9cb595a3b67f24e9a0206a381988e7b6f
ElectroJr <leonsfriedrich@gmail.com>
2023-04-09 13:12:59 -0400

    Replace assert with warning

--- Robust.Shared/Containers/ContainerSlot.cs
+++ Robust.Shared/Containers/ContainerSlot.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using JetBrains.Annotations;
 using Robust.Shared.GameObjects;
 using Robust.Shared.IoC;
+using Robust.Shared.Log;
 using Robust.Shared.Serialization;
 using Robust.Shared.Serialization.Manager.Attributes;
 using Robust.Shared.Utility;
@@ -85,7 +86,12 @@ namespace Robust.Shared.Containers
         protected override void InternalInsert(EntityUid toInsert, IEntityManager entMan)
         {
             DebugTools.Assert(ContainedEntity == null);
-            DebugTools.Assert(!toInsert.IsClientSide() || Owner.IsClientSide() || !Manager.NetSyncEnabled);
+
+            #if DEBUG
+            // TODO make this a proper debug assert when gun code no longer fudges client-side spawn prediction.
+            if (toInsert.IsClientSide() && !Owner.IsClientSide() && Manager.NetSyncEnabled)
+                Logger.Warning("Inserting a client-side entity into a networked container slot. This will block the container slot and may cause issues.");
+            #endif
             ContainedEntity = toInsert;
         }
 

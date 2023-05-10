b2045d8449a5acef58c63e8289a032168020cd54
moonheart08 <moonheart08@users.noreply.github.com>
2023-05-10 07:52:57 -0500

    Implement a default engine stylesheet.

--- Robust.Client/UserInterface/States/DebugBuiltinConnectionScreenState.cs
+++ Robust.Client/UserInterface/States/DebugBuiltinConnectionScreenState.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using Robust.Client.State;
 using Robust.Client.UserInterface.Screens;
+using Robust.Shared.ContentPack;
 
 namespace Robust.Client.UserInterface.States;
 
+[ContentAccessAllowed]
 public sealed class DebugBuiltinConnectionScreenState : State.State
 {
     protected override Type? LinkedScreenType { get; } = typeof(DebugBuiltinConnectionScreen);

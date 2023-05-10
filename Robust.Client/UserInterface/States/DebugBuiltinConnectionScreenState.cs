7ad071085198e548477942b43f680b6397aacd53
moonheart08 <moonheart08@users.noreply.github.com>
2023-05-10 06:42:52 -0500

    Debug connection screen

---
+++ Robust.Client/UserInterface/States/DebugBuiltinConnectionScreenState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Robust.Client.State;
+using Robust.Client.UserInterface.Screens;
+
+namespace Robust.Client.UserInterface.States;
+
+public sealed class DebugBuiltinConnectionScreenState : State.State
+{
+    protected override Type? LinkedScreenType { get; } = typeof(DebugBuiltinConnectionScreen);
+
+    protected override void Startup()
+    {
+    }
+
+    protected override void Shutdown()
+    {
+    }
+}

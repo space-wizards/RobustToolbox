75f0cf9dd77ea8b161abc6b47b721af9f73b2518
TemporalOroboros <TemporalOroboros@gmail.com>
2023-07-08 09:02:28 -0700

    Ecs's UserInterfaceComponent. (#4079)

--- Robust.Server/GameObjects/Components/UserInterface/ServerUserInterfaceComponent.cs
+++ Robust.Server/GameObjects/Components/UserInterface/ServerUserInterfaceComponent.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using JetBrains.Annotations;
 using Robust.Server.Player;
 using Robust.Shared.GameObjects;
-using Robust.Shared.IoC;
-using Robust.Shared.Serialization;
+using Robust.Shared.ViewVariables;
 using static Robust.Shared.GameObjects.SharedUserInterfaceComponent;
 
 namespace Robust.Server.GameObjects
@@ -16,22 +15,10 @@ namespace Robust.Server.GameObjects
     /// <seealso cref="BoundUserInterface"/>
     [PublicAPI]
     [RegisterComponent, ComponentReference(typeof(SharedUserInterfaceComponent))]
-    public sealed class ServerUserInterfaceComponent : SharedUserInterfaceComponent, ISerializationHooks
+    public sealed class ServerUserInterfaceComponent : SharedUserInterfaceComponent
     {
-        internal readonly Dictionary<Enum, BoundUserInterface> _interfaces =
-            new();
-
-        public IReadOnlyDictionary<Enum, BoundUserInterface> Interfaces => _interfaces;
-
-        void ISerializationHooks.AfterDeserialization()
-        {
-            _interfaces.Clear();
-
-            foreach (var prototypeData in _interfaceData)
-            {
-                _interfaces[prototypeData.UiKey] = new BoundUserInterface(prototypeData, this);
-            }
-        }
+        [ViewVariables]
+        public readonly Dictionary<Enum, BoundUserInterface> Interfaces = new();
     }
 
     [RegisterComponent]
@@ -49,8 +36,7 @@ namespace Robust.Server.GameObjects
         public float InteractionRangeSqrd;
 
         public Enum UiKey { get; }
-        public ServerUserInterfaceComponent Component { get; }
-        public EntityUid Owner => Component.Owner;
+        public EntityUid Owner { get; }
 
         internal readonly HashSet<IPlayerSession> _subscribedSessions = new();
         internal BoundUIWrapMessage? LastStateMsg;
@@ -66,71 +52,14 @@ namespace Robust.Server.GameObjects
         /// </summary>
         public IReadOnlySet<IPlayerSession> SubscribedSessions => _subscribedSessions;
 
-        [Obsolete("Use system events")]
-        public event Action<ServerBoundUserInterfaceMessage>? OnReceiveMessage;
-
-        public BoundUserInterface(PrototypeData data, ServerUserInterfaceComponent owner)
+        public BoundUserInterface(PrototypeData data, EntityUid owner)
         {
             RequireInputValidation = data.RequireInputValidation;
             UiKey = data.UiKey;
-            Component = owner;
+            Owner = owner;
 
             // One Abs(), because negative values imply no limit
             InteractionRangeSqrd = data.InteractionRange * MathF.Abs(data.InteractionRange);
-        }
-
-        [Obsolete("Use UserInterfaceSystem")]
-        public void SetState(BoundUserInterfaceState state, IPlayerSession? session = null, bool clearOverrides = true)
-        {
-            IoCManager.Resolve<IEntitySystemManager>().GetEntitySystem<UserInterfaceSystem>().SetUiState(this, state, session, clearOverrides);
-        }
-
-        [Obsolete("Use UserInterfaceSystem")]
-        public void Toggle(IPlayerSession session)
-        {
-            IoCManager.Resolve<IEntitySystemManager>().GetEntitySystem<UserInterfaceSystem>().ToggleUi(this, session);
-        }
-
-        [Obsolete("Use UserInterfaceSystem")]
-        public bool Open(IPlayerSession session)
-        {
-            return IoCManager.Resolve<IEntitySystemManager>().GetEntitySystem<UserInterfaceSystem>().OpenUi(this, session);
-        }
-
-        [Obsolete("Use UserInterfaceSystem")]
-        public bool Close(IPlayerSession session)
-        {
-            return IoCManager.Resolve<IEntitySystemManager>().GetEntitySystem<UserInterfaceSystem>().CloseUi(this, session);
-        }
-
-        [Obsolete("Use UserInterfaceSystem")]
-        public void CloseAll()
-        {
-            IoCManager.Resolve<IEntitySystemManager>().GetEntitySystem<UserInterfaceSystem>().CloseAll(this);
-        }
-
-        [Obsolete("Just check SubscribedSessions.Contains")]
-        public bool SessionHasOpen(IPlayerSession session)
-        {
-            if (session == null) throw new ArgumentNullException(nameof(session));
-            return _subscribedSessions.Contains(session);
-        }
-
-        [Obsolete("Use UserInterfaceSystem")]
-        public void SendMessage(BoundUserInterfaceMessage message)
-        {
-            IoCManager.Resolve<IEntitySystemManager>().GetEntitySystem<UserInterfaceSystem>().SendUiMessage(this, message);
-        }
-
-        [Obsolete("Use UserInterfaceSystem")]
-        public void SendMessage(BoundUserInterfaceMessage message, IPlayerSession session)
-        {
-            IoCManager.Resolve<IEntitySystemManager>().GetEntitySystem<UserInterfaceSystem>().TrySendUiMessage(this, message, session);
-        }
-
-        internal void InvokeOnReceiveMessage(ServerBoundUserInterfaceMessage message)
-        {
-            OnReceiveMessage?.Invoke(message);
         }
     }
 

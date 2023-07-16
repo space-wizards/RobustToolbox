03293d2c96b27695176db8482f2caeb4f1546420
Vera Aguilera Puerto <gradientvera@outlook.com>
2023-07-16 13:57:12 +0200

    oops

--- Robust.Shared/CVars.cs
+++ Robust.Shared/CVars.cs
@@ -1262,7 +1262,7 @@ namespace Robust.Shared
             CVarDef.Create("midi.min_renderers_parallel_update", 3, CVar.CLIENTONLY | CVar.ARCHIVE);
 
         public static readonly CVarDef<float> MidiPositionUpdateDelay =
-            CVarDef.Create("midi.occlusion_update_delay", 0.25f, CVar.CLIENTONLY | CVar.ARCHIVE);
+            CVarDef.Create("midi.position_update_delay", 0.25f, CVar.CLIENTONLY | CVar.ARCHIVE);
 
         public static readonly CVarDef<float> MidiOcclusionUpdateDelay =
             CVarDef.Create("midi.occlusion_update_delay", 0.75f, CVar.CLIENTONLY | CVar.ARCHIVE);

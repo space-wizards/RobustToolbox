9969899f38db5cca07fef6ff9a16f81ae5b8bb8b
Leon Friedrich <60421075+ElectroJr@users.noreply.github.com>
2023-07-30 05:31:17 +1200

    Add method to validate prototype ids in c# fields (#4189)

--- Robust.Shared/Serialization/TypeSerializers/Implementations/Custom/Prototype/PrototypeIdSerializer.cs
+++ Robust.Shared/Serialization/TypeSerializers/Implementations/Custom/Prototype/PrototypeIdSerializer.cs
@@ -19,6 +19,10 @@ namespace Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Pro
         }
     }
 
+    /// <summary>
+    /// Checks that a string corresponds to a valid prototype id. Note that any data fields using this serializer will
+    /// also be validated by <see cref="IPrototypeManager.ValidateFields"/>
+    /// </summary>
     [Virtual]
     public class PrototypeIdSerializer<TPrototype> : ITypeValidator<string, ValueDataNode> where TPrototype : class, IPrototype
     {

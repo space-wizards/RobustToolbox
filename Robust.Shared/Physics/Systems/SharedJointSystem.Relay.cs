5947590210d11fb78b9b212c405d9915ea5813c4
metalgearsloth <comedian_vs_clown@hotmail.com>
2023-08-02 13:38:40 +1000

    a

--- Robust.Shared/Physics/Systems/SharedJointSystem.Relay.cs
+++ Robust.Shared/Physics/Systems/SharedJointSystem.Relay.cs
@@ -64,6 +64,9 @@ public abstract partial class SharedJointSystem
     /// </summary>
     public void RefreshRelay(EntityUid uid, JointComponent? component = null)
     {
+        if (!Resolve(uid, ref component, false))
+            return;
+
         EntityUid? relay = null;
 
         if (_container.TryGetOuterContainer(uid, Transform(uid), out var container))

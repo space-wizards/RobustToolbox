2446e640335730b274a6f40048443b60a3eb362b
Pieter-Jan Briers <pieterjan.briers@gmail.com>
2023-08-06 00:24:20 +0200

    entitysystemupdateorder debug command

---
+++ Robust.Shared/GameObjects/EntitySystemUpdateOrderCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Robust.Shared.IoC;
+using Robust.Shared.Toolshed;
+
+namespace Robust.Shared.GameObjects;
+
+[ToolshedCommand]
+internal sealed class EntitySystemUpdateOrderCommand : ToolshedCommand
+{
+    [Dependency] private readonly IEntitySystemManager _entitySystemManager = default!;
+
+    [CommandImplementation("tick")]
+    public IEnumerable<Type> Tick()
+    {
+        var mgr = (EntitySystemManager)_entitySystemManager;
+
+        return mgr.TickUpdateOrder;
+    }
+
+    [CommandImplementation("frame")]
+    public IEnumerable<Type> Frame()
+    {
+        var mgr = (EntitySystemManager)_entitySystemManager;
+
+        return mgr.FrameUpdateOrder;
+    }
+}

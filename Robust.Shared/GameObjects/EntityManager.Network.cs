3957626e5c81772d9f15bc45111bc9db6fb84c6b
metalgearsloth <comedian_vs_clown@hotmail.com>
2023-08-10 13:03:15 +1000

    Helper methods

--- Robust.Shared/GameObjects/EntityManager.Network.cs
+++ Robust.Shared/GameObjects/EntityManager.Network.cs
@@ -1,10 +1,18 @@
 using System.Collections.Generic;
+using Microsoft.Extensions.ObjectPool;
+using Robust.Shared.Utility;
 
 namespace Robust.Shared.GameObjects;
 
 public partial class EntityManager
 {
+    private const int PoolSize = 256;
+
     private readonly Dictionary<NetEntity, EntityUid> _nentityLookup = new();
+
+    private readonly ObjectPool<HashSet<NetEntity>> _netEntitySetPool =
+        new DefaultObjectPool<HashSet<NetEntity>>(new SetPolicy<NetEntity>(), PoolSize);
+
 
     public EntityUid ToEntity(NetEntity nEntity)
     {
@@ -23,4 +31,29 @@ public partial class EntityManager
 
         return metadata.NetEntity;
     }
+
+    #region Helpers
+
+    public HashSet<EntityUid> ToEntityUids(HashSet<NetEntity> netEntities)
+    {
+
+    }
+
+    /// <summary>
+    /// Returns the <see cref="NetEntity"/> to a HashSet of <see cref="EntityUid"/>
+    /// </summary>
+    public HashSet<NetEntity> ToNetEntities(HashSet<EntityUid> entities)
+    {
+        var newSet = _netEntitySetPool.Get();
+        newSet.EnsureCapacity(entities.Count);
+
+        foreach (var ent in entities)
+        {
+            newSet.Add(ToNetEntity(ent));
+        }
+
+        return newSet;
+    }
+
+    #endregion
 }

474334aff26136a4906579e7abace5684c539d2d
Morb <14136326+Morb0@users.noreply.github.com>
2023-08-21 11:44:40 +0300

    Make DiscordRichPresence icon CVars server-side with replication (#4272)

--- Robust.Shared/CVars.cs
+++ Robust.Shared/CVars.cs
@@ -1195,10 +1195,10 @@ namespace Robust.Shared
             CVarDef.Create("discord.enabled", true, CVar.CLIENTONLY);
 
         public static readonly CVarDef<string> DiscordRichPresenceMainIconId =
-            CVarDef.Create("discord.rich_main_icon_id", "devstation", CVar.CLIENTONLY);
+            CVarDef.Create("discord.rich_main_icon_id", "devstation", CVar.SERVER | CVar.REPLICATED);
 
         public static readonly CVarDef<string> DiscordRichPresenceSecondIconId =
-            CVarDef.Create("discord.rich_second_icon_id", "logo", CVar.CLIENTONLY);
+            CVarDef.Create("discord.rich_second_icon_id", "logo", CVar.SERVER | CVar.REPLICATED);
 
         /*
          * RES

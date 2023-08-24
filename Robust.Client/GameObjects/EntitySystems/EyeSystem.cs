b62a7e66cd53a1c8c77a52657e40764f927f3a37
metalgearsloth <comedian_vs_clown@hotmail.com>
2023-08-24 18:04:38 +1000

    Updates

--- Robust.Client/GameObjects/EntitySystems/EyeSystem.cs
+++ Robust.Client/GameObjects/EntitySystems/EyeSystem.cs
@@ -1,4 +1,5 @@
 using Robust.Client.Graphics;
+using Robust.Client.Physics;
 using Robust.Shared.GameObjects;
 using Robust.Shared.GameStates;
 using Robust.Shared.Graphics;
@@ -14,6 +15,10 @@ public sealed class EyeSystem : SharedEyeSystem
     {
         base.Initialize();
         SubscribeLocalEvent<EyeComponent, ComponentInit>(OnInit);
+
+        // Make sure this runs *after* entities have been moved by interpolation and movement.
+        UpdatesAfter.Add(typeof(TransformSystem));
+        UpdatesAfter.Add(typeof(PhysicsSystem));
     }
 
     private void OnInit(EntityUid uid, EyeComponent component, ComponentInit args)
@@ -28,5 +33,25 @@ public sealed class EyeSystem : SharedEyeSystem
         // Who even knows if this is needed anymore.
         _eyeManager.ClearCurrentEye();
         _eyeManager.CurrentEye = component._eye;
+    }
+
+    /// <inheritdoc />
+    public override void FrameUpdate(float frameTime)
+    {
+        var query = AllEntityQuery<EyeComponent>();
+
+        while (query.MoveNext(out var uid, out var eyeComponent))
+        {
+            if (eyeComponent._eye == null)
+                continue;
+
+            if (!TryComp<TransformComponent>(eyeComponent.Target, out var xform))
+            {
+                xform = Transform(uid);
+                eyeComponent.Target = null;
+            }
+
+            eyeComponent._eye.Position = xform.MapPosition;
+        }
     }
 }
--- Robust.Shared/GameObjects/Systems/SharedEyeSystem.cs
+++ Robust.Shared/GameObjects/Systems/SharedEyeSystem.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using Robust.Shared.IoC;
 
 namespace Robust.Shared.GameObjects;
@@ -5,4 +6,16 @@ namespace Robust.Shared.GameObjects;
 public abstract class SharedEyeSystem : EntitySystem
 {
     [Dependency] protected readonly SharedTransformSystem TransformSystem = default!;
+
+    public void SetOffset(EntityUid uid, Vector2 value, EyeComponent? eyeComponent = null)
+    {
+        if (!Resolve(uid, ref eyeComponent))
+            return;
+
+        if (eyeComponent.Offset.Equals(value))
+            return;
+
+        eyeComponent.Offset = value;
+        Dirty(uid, eyeComponent);
+    }
 }

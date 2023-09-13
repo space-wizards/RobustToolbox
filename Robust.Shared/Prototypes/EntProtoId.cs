f4750caa6bcc060d3962356df2bb9f37a8f861ff
DrSmugleaf <drsmugleaf@gmail.com>
2023-09-12 21:31:16 -0700

    DOC 2

--- Robust.Shared/Prototypes/EntProtoId.cs
+++ Robust.Shared/Prototypes/EntProtoId.cs
@@ -1,10 +1,14 @@
-﻿namespace Robust.Shared.Prototypes;
+﻿using Robust.Shared.Serialization.TypeSerializers.Implementations;
+
+namespace Robust.Shared.Prototypes;
 
 /// <summary>
 ///     Wrapper type for an <see cref="EntityPrototype"/> with a given id.
 /// </summary>
 /// <param name="Id">The id of the prototype.</param>
-/// <remarks>This will be automatically validated if used in data fields.</remarks>
+/// <remarks>
+///     This will be automatically validated by <see cref="EntProtoIdSerializer"/> if used in data fields.
+/// </remarks>
 /// <remarks><seealso cref="ProtoId{T}"/> for a wrapper of other prototype kinds.</remarks>
 public readonly record struct EntProtoId(string Id)
 {
--- Robust.Shared/Prototypes/ProtoId.cs
+++ Robust.Shared/Prototypes/ProtoId.cs
@@ -1,11 +1,15 @@
-﻿namespace Robust.Shared.Prototypes;
+﻿using Robust.Shared.Serialization.TypeSerializers.Implementations.Generic;
+
+namespace Robust.Shared.Prototypes;
 
 /// <summary>
 ///     Wrapper type for a prototype id of kind <see cref="T"/>.
 /// </summary>
 /// <param name="Id">The id of the prototype.</param>
 /// <typeparam name="T">The kind of prototype to wrap, for example <see cref="TileAliasPrototype"/></typeparam>
-/// <remarks>This will be automatically validated if used in data fields.</remarks>
+/// <remarks>
+///     This will be automatically validated by <see cref="ProtoIdSerializer{T}"/> if used in data fields.
+/// </remarks>
 /// <remarks><seealso cref="EntProtoId"/> for an <see cref="EntityPrototype"/> alias.</remarks>
 public readonly record struct ProtoId<T>(string Id) where T : class, IPrototype
 {

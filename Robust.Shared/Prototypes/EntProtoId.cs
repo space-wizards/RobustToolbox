b66240fd6f2bfdd8d120485bd67682a475784a48
DrSmugleaf <drsmugleaf@gmail.com>
2023-09-12 21:03:02 -0700

    Add ProtoId, EntProtoId and serializers

---
+++ Robust.Shared/Prototypes/EntProtoId.cs
@@ -0,0 +1,14 @@
+﻿namespace Robust.Shared.Prototypes;
+
+public readonly record struct EntProtoId(string Id)
+{
+    public static implicit operator string(EntProtoId protoId)
+    {
+        return protoId.Id;
+    }
+
+    public static implicit operator EntProtoId(string id)
+    {
+        return new EntProtoId(id);
+    }
+}
---
+++ Robust.Shared/Prototypes/ProtoId.cs
@@ -0,0 +1,14 @@
+﻿namespace Robust.Shared.Prototypes;
+
+public readonly record struct ProtoId<T>(string Id) where T : class, IPrototype
+{
+    public static implicit operator string(ProtoId<T> protoId)
+    {
+        return protoId.Id;
+    }
+
+    public static implicit operator ProtoId<T>(string id)
+    {
+        return new ProtoId<T>(id);
+    }
+}

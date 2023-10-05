d2198b451081cd839c1ceb54db7626d8844080f6
Kara <lunarautomaton6@gmail.com>
2023-10-05 18:27:36 -0500

    Remove some timerext cruft

--- Robust.Shared/GameObjects/Components/Timers/TimerExtensions.cs
+++ Robust.Shared/GameObjects/Components/Timers/TimerExtensions.cs
@@ -15,43 +15,6 @@ namespace Robust.Shared.GameObjects
             return entMan.EnsureComponent<TimerComponent>(entity);
         }
 
-        public static void AddTimer(this EntityUid entity, Timer timer, CancellationToken cancellationToken = default)
-        {
-            entity
-                .EnsureTimerComponent()
-                .AddTimer(timer, cancellationToken);
-        }
-
-        /// <summary>
-        ///     Creates a task that will complete after a given delay.
-        ///     The task is resumed on the main game logic thread.
-        /// </summary>
-        /// <param name="entity">The entity to add the timer to.</param>
-        /// <param name="milliseconds">The length of time, in milliseconds, to delay for.</param>
-        /// <param name="cancellationToken"></param>
-        /// <returns>The task that can be awaited.</returns>
-        public static Task DelayTask(this EntityUid entity, int milliseconds, CancellationToken cancellationToken = default)
-        {
-            return entity
-                .EnsureTimerComponent()
-                .Delay(milliseconds, cancellationToken);
-        }
-
-        /// <summary>
-        ///     Creates a task that will complete after a given delay.
-        ///     The task is resumed on the main game logic thread.
-        /// </summary>
-        /// <param name="entity">The entity to add the timer to.</param>
-        /// <param name="duration">The length of time to delay for.</param>
-        /// <param name="cancellationToken"></param>
-        /// <returns>The task that can be awaited.</returns>
-        public static Task DelayTask(this EntityUid entity, TimeSpan duration, CancellationToken cancellationToken = default)
-        {
-            return entity
-                .EnsureTimerComponent()
-                .Delay((int) duration.TotalMilliseconds, cancellationToken);
-        }
-
         /// <summary>
         ///     Schedule an action to be fired after a certain delay.
         ///     The action will be resumed on the main game logic thread.
@@ -82,21 +45,6 @@ namespace Robust.Shared.GameObjects
             entity
                 .EnsureTimerComponent()
                 .Spawn((int) duration.TotalMilliseconds, onFired, cancellationToken);
-        }
-
-        /// <summary>
-        ///     Schedule an action that repeatedly fires after a delay specified in milliseconds.
-        /// </summary>
-        /// <param name="entity">The entity to add the timer to.</param>
-        /// <param name="milliseconds">The length of time, in milliseconds, to delay before firing the repeated action.</param>
-        /// <param name="onFired">The action to fire.</param>
-        /// <param name="cancellationToken">The CancellationToken for stopping the Timer.</param>
-        [Obsolete("Use a system update loop instead")]
-        public static void SpawnRepeatingTimer(this EntityUid entity, int milliseconds, Action onFired, CancellationToken cancellationToken)
-        {
-            entity
-                .EnsureTimerComponent()
-                .SpawnRepeating(milliseconds, onFired, cancellationToken);
         }
 
         /// <summary>

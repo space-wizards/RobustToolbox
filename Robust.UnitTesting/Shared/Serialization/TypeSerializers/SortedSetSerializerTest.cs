c49d4a8fe971b763630c500e95e9d18c30e0c208
deltanedas <@deltanedas:kde.org>
2023-10-05 20:38:01 +0100

    le sort

--- Robust.UnitTesting/Shared/Serialization/TypeSerializers/SortedSetSerializerTest.cs
+++ Robust.UnitTesting/Shared/Serialization/TypeSerializers/SortedSetSerializerTest.cs
@@ -16,19 +16,19 @@ public sealed class SortedSetSerializerTest : SerializationTest
     [Test]
     public void SerializationTest()
     {
-        var list = new SortedSet<string> {"A", "E", "B"};
+        var list = new SortedSet<string> {"A", "B", "C"};
         var node = Serialization.WriteValueAs<SequenceDataNode>(list);
 
         Assert.That(node.Cast<ValueDataNode>(0).Value, Is.EqualTo("A"));
-        Assert.That(node.Cast<ValueDataNode>(1).Value, Is.EqualTo("E"));
-        Assert.That(node.Cast<ValueDataNode>(2).Value, Is.EqualTo("B"));
+        Assert.That(node.Cast<ValueDataNode>(1).Value, Is.EqualTo("B"));
+        Assert.That(node.Cast<ValueDataNode>(2).Value, Is.EqualTo("C"));
     }
 
     [Test]
     public void DeserializationTest()
     {
-        var list = new SortedSet<string> {"A", "E", "B"};
-        var node = new SequenceDataNode("A", "E", "B");
+        var list = new SortedSet<string> {"A", "B", "C"};
+        var node = new SequenceDataNode("A", "B", "C");
         var deserializedList = Serialization.Read<SortedSet<string>>(node, notNullableOverride: true);
 
         Assert.That(deserializedList, Is.EqualTo(list));

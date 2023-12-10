a2cd33afe5891ef7f02fd7fcb3fe8118adbdb39f
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2023-12-10 21:25:43 +1100

    Add better OpenAL logging (#4687)

--- Robust.Client/Audio/Sources/BaseAudioSource.cs
+++ Robust.Client/Audio/Sources/BaseAudioSource.cs
@@ -194,7 +194,7 @@ internal abstract class BaseAudioSource : IAudioSource
 
             _gain = value;
             AL.Source(SourceHandle, ALSourcef.Gain, _gain * priorOcclusion);
-            Master._checkAlError();
+            Master.LogALError($"Gain is {_gain:0.00} and priorOcclusion is {priorOcclusion:0.00}. EFX supported: {IsEfxSupported}");
         }
     }
 
@@ -212,7 +212,7 @@ internal abstract class BaseAudioSource : IAudioSource
         {
             _checkDisposed();
             AL.Source(SourceHandle, ALSourcef.MaxDistance, value);
-            Master._checkAlError();
+            Master.LogALError($"MaxDistance is {value:0.00}");
         }
     }
 
@@ -230,7 +230,7 @@ internal abstract class BaseAudioSource : IAudioSource
         {
             _checkDisposed();
             AL.Source(SourceHandle, ALSourcef.RolloffFactor, value);
-            Master._checkAlError();
+            Master.LogALError($"RolloffFactor is {value:0.00}");
         }
     }
 
@@ -248,7 +248,7 @@ internal abstract class BaseAudioSource : IAudioSource
         {
             _checkDisposed();
             AL.Source(SourceHandle, ALSourcef.ReferenceDistance, value);
-            Master._checkAlError();
+            Master.LogALError($"ReferenceDistance is {value:0.00}");
         }
     }
 
@@ -294,7 +294,7 @@ internal abstract class BaseAudioSource : IAudioSource
         {
             _checkDisposed();
             AL.Source(SourceHandle, ALSourcef.SecOffset, value);
-            Master._checkAlError();
+            Master._checkAlError($"Tried to set invalid playback position of {value:0.00}");
         }
     }
 

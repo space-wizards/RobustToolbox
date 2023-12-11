c6a60e88b088a4a898300f796fbfb25dace4f0ac
metalgearsloth <comedian_vs_clown@hotmail.com>
2023-12-12 00:10:29 +1100

    Fix audio params variation

--- Robust.Client/Audio/AudioSystem.cs
+++ Robust.Client/Audio/AudioSystem.cs
@@ -424,7 +424,7 @@ public sealed partial class AudioSystem : SharedAudioSystem
     public override (EntityUid Entity, AudioComponent Component)? PlayPredicted(SoundSpecifier? sound, EntityUid source, EntityUid? user, AudioParams? audioParams = null)
     {
         if (Timing.IsFirstTimePredicted && sound != null)
-            return PlayEntity(sound, Filter.Local(), source, false, audioParams ?? sound.Params);
+            return PlayEntity(sound, Filter.Local(), source, false, audioParams);
 
         return null; // uhh Lets hope predicted audio never needs to somehow store the playing audio....
     }
@@ -432,7 +432,7 @@ public sealed partial class AudioSystem : SharedAudioSystem
     public override (EntityUid Entity, AudioComponent Component)? PlayPredicted(SoundSpecifier? sound, EntityCoordinates coordinates, EntityUid? user, AudioParams? audioParams = null)
     {
         if (Timing.IsFirstTimePredicted && sound != null)
-            return PlayStatic(sound, Filter.Local(), coordinates, false, audioParams ?? sound.Params);
+            return PlayStatic(sound, Filter.Local(), coordinates, false, audioParams);
 
         return null;
     }

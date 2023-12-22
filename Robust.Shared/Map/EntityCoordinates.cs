6573531d5d678a5cc315a8ba6a2fe7ce1aa59099
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2023-12-22 14:17:46 +1100

    GetEntitiesInRange flag + TryDelta (#4749)
    Needed for tesla too lazy to split

--- Robust.Shared/Map/EntityCoordinates.cs
+++ Robust.Shared/Map/EntityCoordinates.cs
@@ -366,14 +366,34 @@ namespace Robust.Shared.Map
             EntityCoordinates otherCoordinates,
             out float distance)
         {
+            if (TryDelta(entityManager, transformSystem, otherCoordinates, out var delta))
+            {
+                distance = delta.Length();
+                return true;
+            }
+
             distance = 0f;
+            return false;
+        }
+
+        /// <summary>
+        ///     Tries to calculate the distance vector between two sets of coordinates.
+        /// </summary>
+        /// <returns>True if it was possible to calculate the distance</returns>
+        public bool TryDelta(
+            IEntityManager entityManager,
+            SharedTransformSystem transformSystem,
+            EntityCoordinates otherCoordinates,
+            out Vector2 delta)
+        {
+            delta = Vector2.Zero;
 
             if (!IsValid(entityManager) || !otherCoordinates.IsValid(entityManager))
                 return false;
 
             if (EntityId == otherCoordinates.EntityId)
             {
-                distance = (Position - otherCoordinates.Position).Length();
+                delta = Position - otherCoordinates.Position;
                 return true;
             }
 
@@ -383,7 +403,7 @@ namespace Robust.Shared.Map
             if (mapCoordinates.MapId != otherMapCoordinates.MapId)
                 return false;
 
-            distance = (mapCoordinates.Position - otherMapCoordinates.Position).Length();
+            delta = mapCoordinates.Position - otherMapCoordinates.Position;
             return true;
         }
 

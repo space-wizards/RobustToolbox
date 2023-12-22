ce8982e371ed9e7d6008e0c8ecaa7c93abe74d3f
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2023-12-23 07:14:08 +1100

    Make audiostream methods public (#4752)

--- Robust.Client/Audio/IAudioManager.cs
+++ Robust.Client/Audio/IAudioManager.cs
@@ -1,3 +1,6 @@
+using System;
+using System.IO;
+
 namespace Robust.Client.Audio;
 
 /// <summary>
@@ -5,5 +8,11 @@ namespace Robust.Client.Audio;
 /// </summary>
 public interface IAudioManager
 {
+    AudioStream LoadAudioOggVorbis(Stream stream, string? name = null);
+
+    AudioStream LoadAudioWav(Stream stream, string? name = null);
+
+    AudioStream LoadAudioRaw(ReadOnlySpan<short> samples, int channels, int sampleRate, string? name = null);
+
     void SetMasterGain(float gain);
 }

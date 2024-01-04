371db7db2fe95c191da49ddbc74e56ad6b720b2f
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2024-01-04 15:05:18 +1100

    Add GetLocalEntitiesIntersecting for griduid + tilesize (#4807)

--- Robust.Shared/GameObjects/Systems/EntityLookupSystem.LocalQueries.cs
+++ Robust.Shared/GameObjects/Systems/EntityLookupSystem.LocalQueries.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Robust.Shared.Map.Components;
 using Robust.Shared.Maths;
 
 namespace Robust.Shared.GameObjects;
@@ -8,6 +9,25 @@ public sealed partial class EntityLookupSystem
     /*
      * Local AABB / Box2Rotated queries for broadphase entities.
      */
+
+    /// <summary>
+    /// Gets the entities intersecting the specified broadphase entity using a local AABB.
+    /// </summary>
+    public void GetLocalEntitiesIntersecting(EntityUid gridUid, Vector2i localTile, HashSet<EntityUid> intersecting,
+        float enlargement = TileEnlargementRadius, LookupFlags flags = DefaultFlags, MapGridComponent? gridComp = null)
+    {
+        ushort tileSize = 1;
+
+        if (_gridQuery.Resolve(gridUid, ref gridComp))
+        {
+            tileSize = gridComp.TileSize;
+        }
+
+        var localAABB = GetLocalBounds(localTile, tileSize);
+        localAABB = localAABB.Enlarged(TileEnlargementRadius);
+        AddLocalEntitiesIntersecting(gridUid, intersecting, localAABB, flags);
+        AddContained(intersecting, flags);
+    }
 
     /// <summary>
     /// Gets the entities intersecting the specified broadphase entity using a local AABB.

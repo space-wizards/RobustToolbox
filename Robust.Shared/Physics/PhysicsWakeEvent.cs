ac147dc2a1b6d5c062927a5d8056dee069d449b2
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2024-01-06 13:38:41 +1100

    Remove physics sleep cancelling (#4813)
    * Remove physics sleep cancelling
    
    Was a bad idea, in the future I'm going to add even more asserts around this but for now I can fix content.
    
    * Fix this

--- Robust.Shared/Physics/PhysicsWakeEvent.cs
+++ Robust.Shared/Physics/PhysicsWakeEvent.cs
@@ -7,11 +7,4 @@ namespace Robust.Shared.Physics;
 public readonly record struct PhysicsWakeEvent(EntityUid Entity, PhysicsComponent Body);
 
 [ByRefEvent]
-public record struct PhysicsSleepEvent(EntityUid Entity, PhysicsComponent Body)
-{
-    /// <summary>
-    /// Marks the entity as still being awake and cancels sleeping.
-    /// This only works if <see cref="PhysicsComponent.CanCollide"/> is still enabled and the object is not a static object..
-    /// </summary>
-    public bool Cancelled;
-};
+public record struct PhysicsSleepEvent(EntityUid Entity, PhysicsComponent Body);

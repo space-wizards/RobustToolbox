7879d6a1d1533c8a2ee5b6c8c2f374db9f32ac94
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2024-01-16 18:03:35 +0100

    Register Robust.UnitTesting as assembly for reflection.
    This is necessary so that serialized types in the assembly can be picked up by NetSerializer.
    
    Have to disable automatic reflection on all entity systems/components that tests register manually right now, because otherwise tests break.

--- Robust.UnitTesting/RobustIntegrationTest.cs
+++ Robust.UnitTesting/RobustIntegrationTest.cs
@@ -34,6 +34,7 @@ using Robust.Shared.Map;
 using Robust.Shared.Network;
 using Robust.Shared.Player;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Reflection;
 using Robust.Shared.Serialization;
 using Robust.Shared.Timing;
 using ServerProgram = Robust.Server.Program;
@@ -679,6 +680,7 @@ namespace Robust.UnitTesting
                 deps.BuildGraph();
                 //ServerProgram.SetupLogging();
                 ServerProgram.InitReflectionManager(deps);
+                deps.Resolve<IReflectionManager>().LoadAssemblies(typeof(RobustIntegrationTest).Assembly);
 
                 var server = DependencyCollection.Resolve<BaseServer>();
 

e9e0134ea150c4165776ebfa6c78e490800e41bb
metalgearsloth <comedian_vs_clown@hotmail.com>
2024-01-18 17:37:41 +1100

    Resolves

--- Robust.Shared/GameObjects/Systems/SharedMapSystem.Helpers.cs
+++ Robust.Shared/GameObjects/Systems/SharedMapSystem.Helpers.cs
@@ -8,42 +8,42 @@ public abstract partial class SharedMapSystem
     /// <summary>
     /// Converts a tileRef to EntityCoordinates.
     /// </summary>
-    public EntityCoordinates ToCoordinates(TileRef tileRef)
+    public EntityCoordinates ToCoordinates(TileRef tileRef, MapGridComponent? gridComponent = null)
     {
-        return ToCoordinates(tileRef.GridUid, tileRef.GridIndices);
+        return ToCoordinates(tileRef.GridUid, tileRef.GridIndices, gridComponent);
     }
 
     /// <summary>
     /// Converts a tileRef to EntityCoordinates.
     /// </summary>
-    public EntityCoordinates ToCoordinates(EntityUid gridUid, Vector2i tile)
+    public EntityCoordinates ToCoordinates(EntityUid gridUid, Vector2i tile, MapGridComponent? gridComponent = null)
     {
-        if (!_gridQuery.TryGetComponent(gridUid, out var mapGrid))
+        if (!_gridQuery.Resolve(gridUid, ref gridComponent))
         {
             return EntityCoordinates.Invalid;
         }
 
-        return new EntityCoordinates(gridUid, tile * mapGrid.TileSize);
+        return new EntityCoordinates(gridUid, tile * gridComponent.TileSize);
     }
 
     /// <summary>
     /// Converts a tileRef to EntityCoordinates for the center of the tile.
     /// </summary>
-    public EntityCoordinates ToCenterCoordinates(TileRef tileRef)
+    public EntityCoordinates ToCenterCoordinates(TileRef tileRef, MapGridComponent? gridComponent = null)
     {
-        return ToCenterCoordinates(tileRef.GridUid, tileRef.GridIndices);
+        return ToCenterCoordinates(tileRef.GridUid, tileRef.GridIndices, gridComponent);
     }
 
     /// <summary>
     /// Converts a tileRef to EntityCoordinates for the center of the tile.
     /// </summary>
-    public EntityCoordinates ToCenterCoordinates(EntityUid gridUid, Vector2i tile)
+    public EntityCoordinates ToCenterCoordinates(EntityUid gridUid, Vector2i tile, MapGridComponent? gridComponent = null)
     {
-        if (!_gridQuery.TryGetComponent(gridUid, out var mapGrid))
+        if (!_gridQuery.Resolve(gridUid, ref gridComponent))
         {
             return EntityCoordinates.Invalid;
         }
 
-        return new EntityCoordinates(gridUid, tile * mapGrid.TileSize + mapGrid.TileSizeHalfVector);
+        return new EntityCoordinates(gridUid, tile * gridComponent.TileSize + gridComponent.TileSizeHalfVector);
     }
 }

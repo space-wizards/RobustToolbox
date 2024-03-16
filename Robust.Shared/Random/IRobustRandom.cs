6a269d282f7d214c2ff9bd072241901ef75f4e66
pa.pecherskij <pa.pecherskij@interfax.ru>
2024-03-17 00:57:49 +0300

    feat: new methods for RobustRandom - getting random item from collections, getting random sub-set of items.

--- Robust.Shared/Random/IRobustRandom.cs
+++ Robust.Shared/Random/IRobustRandom.cs
@@ -1,106 +1,178 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Numerics;
 using System.Runtime.CompilerServices;
 using Robust.Shared.Collections;
 using Robust.Shared.Maths;
-using Robust.Shared.Toolshed.Commands.Generic;
 
 namespace Robust.Shared.Random;
 
+/// <summary>
+/// Wrapper around random number generator helping methods.
+/// </summary>
 public interface IRobustRandom
 {
-    /// <summary>
-    /// Get the underlying System.Random
-    /// </summary>
-    /// <returns></returns>
+    /// <summary> Get the underlying <see cref="Random"/>.</summary>
     System.Random GetRandom();
+
+    /// <summary> Set seed for underlying <see cref="Random"/>. </summary>
     void SetSeed(int seed);
 
+    /// <summary> Get random <see cref="float"/> value. </summary>
     float NextFloat();
+
+    /// <summary> Get random <see cref="float"/> value in range of <paramref name="minValue"/> and <paramref name="maxValue"/>. </summary>
+    /// <param name="minValue">Random value should be greater or equal to this value.</param>
+    /// <param name="maxValue">Random value should be less or equal to this value.</param>
     public float NextFloat(float minValue, float maxValue)
         => NextFloat() * (maxValue - minValue) + minValue;
+
+    /// <summary> Get random <see cref="float"/> value in range of 0 and <paramref name="maxValue"/>. </summary>
+    /// <param name="maxValue">Random value should be less or equal to this value.</param>
     public float NextFloat(float maxValue) => NextFloat() * maxValue;
+
+    /// <summary> Get random <see cref="int"/> value. </summary>
     int Next();
-    int Next(int minValue, int maxValue);
-    TimeSpan Next(TimeSpan minTime, TimeSpan maxTime);
-    TimeSpan Next(TimeSpan maxTime);
+
+    /// <summary> Get random <see cref="int"/> value in range of 0 and <paramref name="maxValue"/>. </summary>
+    /// <param name="maxValue">Random value should be less or equal to this value.</param>
     int Next(int maxValue);
+
+    /// <summary> Get random <see cref="int"/> value in range of <paramref name="minValue"/> and <paramref name="maxValue"/>. </summary>
+    /// <param name="minValue">Random value should be greater or equal to this value.</param>
+    /// <param name="maxValue">Random value should be less or equal to this value.</param>
+    int Next(int minValue, int maxValue);
+
+    /// <summary> Get random <see cref="byte"/> value. </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public byte NextByte() => NextByte(byte.MaxValue);
+
+    /// <summary> Get random <see cref="byte"/> value in range of 0 and <paramref name="maxValue"/>. </summary>
+    /// <param name="maxValue">Random value should be less or equal to this value.</param>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public byte NextByte(byte maxValue) => NextByte(0, maxValue);
+
+    /// <summary> Get random <see cref="byte"/> value in range of <paramref name="minValue"/> and <paramref name="maxValue"/>. </summary>
+    /// <param name="minValue">Random value should be greater or equal to this value.</param>
+    /// <param name="maxValue">Random value should be less or equal to this value.</param>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public byte NextByte(byte minValue, byte maxValue) => (byte)Next(minValue, maxValue);
+
+    /// <summary> Get random <see cref="double"/> value. </summary>
     double NextDouble();
+
+    /// <summary> Get random <see cref="double"/> value in range of 0 and <paramref name="maxValue"/>. </summary>
+    /// <param name="maxValue">Random value should be less or equal to this value.</param>
+    double Next(double maxValue) => NextDouble() * maxValue;
+
+    /// <summary> Get random <see cref="double"/> value in range of <paramref name="minValue"/> and <paramref name="maxValue"/>. </summary>
+    /// <param name="minValue">Random value should be greater or equal to this value.</param>
+    /// <param name="maxValue">Random value should be less or equal to this value.</param>
     double NextDouble(double minValue, double maxValue) => NextDouble() * (maxValue - minValue) + minValue;
+
+    /// <summary> Get random <see cref="TimeSpan"/> value in range of <see cref="TimeSpan.Zero"/> and <paramref name="maxTime"/>. </summary>
+    /// <param name="maxTime">Random value should be less or equal to this value.</param>
+    TimeSpan Next(TimeSpan maxTime);
+
+    /// <summary> Get random <see cref="TimeSpan"/> value in range of <paramref name="minTime"/> and <paramref name="maxTime"/>. </summary>
+    /// <param name="minTime">Random value should be greater or equal to this value.</param>
+    /// <param name="maxTime">Random value should be less or equal to this value.</param>
+    TimeSpan Next(TimeSpan minTime, TimeSpan maxTime);
+
+    /// <summary> Fill buffer with random bytes. </summary>
     void NextBytes(byte[] buffer);
 
+    /// <summary> Get random <see cref="Angle"/> value. </summary>
     public Angle NextAngle() => NextFloat() * MathF.Tau;
-    public Angle NextAngle(Angle minValue, Angle maxValue) => NextFloat() * (maxValue - minValue) + minValue;
+
+    /// <summary> Get random <see cref="Angle"/> value in range of 0 and <paramref name="maxValue"/>. </summary>
+    /// <param name="maxValue">Random value should be less or equal to this value.</param>
     public Angle NextAngle(Angle maxValue) => NextFloat() * maxValue;
+
+    /// <summary> Get random <see cref="Angle"/> value in range of <paramref name="minValue"/> and <paramref name="maxValue"/>. </summary>
+    /// <param name="minValue">Random value should be greater or equal to this value.</param>
+    /// <param name="maxValue">Random value should be less or equal to this value.</param>
+    public Angle NextAngle(Angle minValue, Angle maxValue) => NextFloat() * (maxValue - minValue) + minValue;
 
     /// <summary>
     ///     Random vector, created from a uniform distribution of magnitudes and angles.
     /// </summary>
+    /// <param name="maxMagnitude">Max value for randomized vector magnitude.</param>
+    public Vector2 NextVector2(float maxMagnitude = 1) => NextVector2(0, maxMagnitude);
+
+    /// <summary>
+    ///     Random vector, created from a uniform distribution of magnitudes and angles.
+    /// </summary>
+    /// <param name="minMagnitude">Min value for randomized vector magnitude.</param>
+    /// <param name="maxMagnitude">Max value for randomized vector magnitude.</param>
     /// <remarks>
     ///     In general, NextVector2(1) will tend to result in vectors with smaller magnitudes than
     ///     NextVector2Box(1,1), even if you ignored any vectors with a magnitude larger than one.
     /// </remarks>
     public Vector2 NextVector2(float minMagnitude, float maxMagnitude) => NextAngle().RotateVec(new Vector2(NextFloat(minMagnitude, maxMagnitude), 0));
-    public Vector2 NextVector2(float maxMagnitude = 1) => NextVector2(0, maxMagnitude);
 
     /// <summary>
     ///     Random vector, created from a uniform distribution of x and y coordinates lying inside some box.
     /// </summary>
     public Vector2 NextVector2Box(float minX, float minY, float maxX, float maxY) => new Vector2(NextFloat(minX, maxX), NextFloat(minY, maxY));
+
+    /// <summary>
+    ///     Random vector, created from a uniform distribution of x and y coordinates lying inside some box.
+    ///     Box will have coordinates starting at [-<paramref name="maxAbsX"/> , -<paramref name="maxAbsY"/>]
+    ///     and ending in [<paramref name="maxAbsX"/> , <paramref name="maxAbsY"/>]
+    /// </summary>
     public Vector2 NextVector2Box(float maxAbsX = 1, float maxAbsY = 1) => NextVector2Box(-maxAbsX, -maxAbsY, maxAbsX, maxAbsY);
 
-    void Shuffle<T>(IList<T> list)
-    {
-        var n = list.Count;
-        while (n > 1)
-        {
-            n -= 1;
-            var k = Next(n + 1);
-            (list[k], list[n]) = (list[n], list[k]);
-        }
-    }
+    /// <summary> Randomly switches positions in collection. </summary>
+    void Shuffle<T>(IList<T> list);
 
-    void Shuffle<T>(Span<T> list)
-    {
-        var n = list.Length;
-        while (n > 1)
-        {
-            n -= 1;
-            var k = Next(n + 1);
-            (list[k], list[n]) = (list[n], list[k]);
-        }
-    }
+    /// <summary> Randomly switches positions in collection. </summary>
+    void Shuffle<T>(Span<T> list);
 
-    void Shuffle<T>(ValueList<T> list)
-    {
-        var n = list.Count;
-        while (n > 1)
-        {
-            n -= 1;
-            var k = Next(n + 1);
-            (list[k], list[n]) = (list[n], list[k]);
-        }
-    }
+    /// <summary> Randomly switches positions in collection. </summary>
+    void Shuffle<T>(ValueList<T> list);
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public byte NextByte(byte maxValue)
-    {
-        return NextByte(0, maxValue);
-    }
+    /// <summary> Get random item from collection. </summary>
+    T GetItem<T>(IList<T> collection);
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public byte NextByte()
-    {
-        return NextByte(byte.MaxValue);
-    }
+    /// <summary> Get random item from collection. </summary>
+    T GetItem<T>(ValueList<T> list);
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public byte NextByte(byte minValue, byte maxValue)
-    {
-        return (byte) Next(minValue, maxValue);
-    }
+    /// <summary> Get random item from collection. </summary>
+    T GetItem<T>(Span<T> span);
+
+    /// <summary>
+    /// Get set amount of random items from collection.
+    /// If <paramref name="allowDuplicates"/> is false and <paramref name="collection"/>
+    /// is smaller then <paramref name="count"/> - returns shuffled <paramref name="collection"/> clone.
+    /// If <paramref name="collection"/> is empty, and/or <paramref name="count"/> is 0, reutns empty.
+    /// </summary>
+    /// <param name="collection">Collection from which items should be picked.</param>
+    /// <param name="count">Count of random items to be picked.</param>
+    /// <param name="allowDuplicates">Marker, are duplicate items allowed in results.</param>
+    IReadOnlyCollection<T> GetItems<T>(IList<T> collection, int count, bool allowDuplicates = true);
+
+    /// <summary>
+    /// Get set amount of random items from collection.
+    /// If <paramref name="allowDuplicates"/> is false and <paramref name="collection"/>
+    /// is smaller then <paramref name="count"/> - returns shuffled <paramref name="collection"/> clone.
+    /// If <paramref name="collection"/> is empty, and/or <paramref name="count"/> is 0, reutns empty.
+    /// </summary>
+    /// <param name="collection">Collection from which items should be picked.</param>
+    /// <param name="count">Count of random items to be picked.</param>
+    /// <param name="allowDuplicates">Marker, are duplicate items allowed in results.</param>
+    ValueList<T> GetItems<T>(ValueList<T> collection, int count, bool allowDuplicates = true);
+
+    /// <summary>
+    /// Get set amount of random items from collection.
+    /// If <paramref name="allowDuplicates"/> is false and <paramref name="collection"/>
+    /// is smaller then <paramref name="count"/> - returns shuffled <paramref name="collection"/> clone.
+    /// If <paramref name="collection"/> is empty, and/or <paramref name="count"/> is 0, reutns empty.
+    /// </summary>
+    /// <param name="collection">Collection from which items should be picked.</param>
+    /// <param name="count">Count of random items to be picked.</param>
+    /// <param name="allowDuplicates">Marker, are duplicate items allowed in results.</param>
+    Span<T> GetItems<T>(Span<T> collection, int count, bool allowDuplicates = true);
 }
 
 public static class RandomHelpers
@@ -136,6 +208,6 @@ public static class RandomHelpers
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static byte NextByte(this System.Random random, byte minValue, byte maxValue)
     {
-        return (byte) random.Next(minValue, maxValue);
+        return (byte)random.Next(minValue, maxValue);
     }
 }
--- Robust.Shared/Random/RobustRandom.cs
+++ Robust.Shared/Random/RobustRandom.cs
@@ -1,58 +1,243 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using Robust.Shared.Collections;
 using Robust.Shared.Utility;
 
-namespace Robust.Shared.Random
+namespace Robust.Shared.Random;
+
+public sealed class RobustRandom : IRobustRandom
 {
-    public sealed class RobustRandom : IRobustRandom
+    private System.Random _random = new();
+
+    public System.Random GetRandom() => _random;
+
+    public void SetSeed(int seed)
     {
-        private System.Random _random = new();
+        _random = new(seed);
+    }
 
-        public System.Random GetRandom() => _random;
+    public float NextFloat()
+    {
+        return _random.NextFloat();
+    }
 
-        public void SetSeed(int seed)
+    public int Next()
+    {
+        return _random.Next();
+    }
+
+    public int Next(int minValue, int maxValue)
+    {
+        return _random.Next(minValue, maxValue);
+    }
+
+    public TimeSpan Next(TimeSpan minTime, TimeSpan maxTime)
+    {
+        DebugTools.Assert(minTime < maxTime);
+        return minTime + (maxTime - minTime) * _random.NextDouble();
+    }
+
+    public TimeSpan Next(TimeSpan maxTime)
+    {
+        return Next(TimeSpan.Zero, maxTime);
+    }
+
+    public int Next(int maxValue)
+    {
+        return _random.Next(maxValue);
+    }
+
+    public double NextDouble()
+    {
+        return _random.NextDouble();
+    }
+
+    public void NextBytes(byte[] buffer)
+    {
+        _random.NextBytes(buffer);
+    }
+
+    /// <inheritdoc />
+    public void Shuffle<T>(IList<T> list)
+    {
+        var n = list.Count;
+        while (n > 1)
         {
-            _random = new(seed);
+            n -= 1;
+            var k = Next(n + 1);
+            (list[k], list[n]) = (list[n], list[k]);
+        }
+    }
+
+    /// <inheritdoc />
+    public void Shuffle<T>(Span<T> list)
+    {
+        var n = list.Length;
+        while (n > 1)
+        {
+            n -= 1;
+            var k = Next(n + 1);
+            (list[k], list[n]) = (list[n], list[k]);
+        }
+    }
+
+    /// <inheritdoc />
+    public void Shuffle<T>(ValueList<T> list)
+    {
+        var n = list.Count;
+        while (n > 1)
+        {
+            n -= 1;
+            var k = Next(n + 1);
+            (list[k], list[n]) = (list[n], list[k]);
+        }
+    }
+
+    /// <inheritdoc />
+    public T GetItem<T>(IList<T> collection)
+    {
+        var index = Next(collection.Count - 1);
+        return collection[index];
+    }
+
+    /// <inheritdoc />
+    public T GetItem<T>(ValueList<T> list)
+    {
+        var index = Next(list.Count - 1);
+        return list[index];
+    }
+
+    /// <inheritdoc />
+    public T GetItem<T>(Span<T> span)
+    {
+        var index = Next(span.Length - 1);
+        return span[index];
+    }
+
+    /// <inheritdoc />
+    public IReadOnlyCollection<T> GetItems<T>(IList<T> collection, int count, bool allowDuplicates = true)
+    {
+        if (collection.Count == 0 || count <= 0)
+        {
+            return Array.Empty<T>();
         }
 
-        public float NextFloat()
+        if (allowDuplicates == false)
         {
-            return _random.NextFloat();
+            if (collection.Count >= count)
+            {
+                var arr = collection.ToArray();
+                Shuffle(arr);
+                return arr;
+            }
         }
 
-        public int Next()
+        var maxCollectionIndex = collection.Count - 1;
+
+        var rolled = new T[count];
+        var selectedIndexes = new HashSet<int>();
+        for (int i = 0; i < count; i++)
         {
-            return _random.Next();
+            var index = Next(maxCollectionIndex);
+            var unique = selectedIndexes.Add(index);
+            if (!unique && allowDuplicates)
+            {
+                do
+                {
+                    index = index == collection.Count
+                        ? 0
+                        : index + 1;
+                } while (selectedIndexes.Add(index));
+            }
+
+            rolled[i] = collection[index];
         }
 
-        public int Next(int minValue, int maxValue)
+        return rolled;
+    }
+
+    /// <inheritdoc />
+    public ValueList<T> GetItems<T>(ValueList<T> collection, int count, bool allowDuplicates = true)
+    {
+        if (collection.Count == 0 || count <= 0)
         {
-            return _random.Next(minValue, maxValue);
+            return new(0);
         }
 
-        public TimeSpan Next(TimeSpan minTime, TimeSpan maxTime)
+        if (allowDuplicates == false)
         {
-            DebugTools.Assert(minTime < maxTime);
-            return minTime + (maxTime - minTime) * _random.NextDouble();
+            if (collection.Count >= count)
+            {
+                var arr = collection.ToArray();
+                Shuffle(arr);
+                return ValueList<T>.OwningArray(arr);
+            }
         }
 
-        public TimeSpan Next(TimeSpan maxTime)
+        var maxCollectionIndex = collection.Count - 1;
+
+        var rolled = new T[count];
+        var selectedIndexes = new HashSet<int>();
+        for (int i = 0; i < count; i++)
         {
-            return Next(TimeSpan.Zero, maxTime);
+            var index = Next(maxCollectionIndex);
+            var unique = selectedIndexes.Add(index);
+            if (!unique && allowDuplicates)
+            {
+                do
+                {
+                    index = index == collection.Count
+                        ? 0
+                        : index + 1;
+                } while (selectedIndexes.Add(index));
+            }
+
+            rolled[i] = collection[index];
         }
 
-        public int Next(int maxValue)
+        return ValueList<T>.OwningArray(rolled);
+    }
+
+    /// <inheritdoc />
+    public Span<T> GetItems<T>(Span<T> collection, int count, bool allowDuplicates = true)
+    {
+        if (collection.Length == 0 || count <= 0)
         {
-            return _random.Next(maxValue);
+            return default;
         }
 
-        public double NextDouble()
+        if (allowDuplicates == false)
         {
-            return _random.NextDouble();
+            if (collection.Length >= count)
+            {
+                var arr = collection.ToArray();
+                Shuffle(arr);
+                return arr;
+            }
         }
 
-        public void NextBytes(byte[] buffer)
+        var maxCollectionIndex = collection.Length - 1;
+
+        var rolled = new T[count];
+        var selectedIndexes = new HashSet<int>();
+        for (int i = 0; i < count; i++)
         {
-            _random.NextBytes(buffer);
+            var index = Next(maxCollectionIndex);
+            var unique = selectedIndexes.Add(index);
+            if (!unique && allowDuplicates)
+            {
+                do
+                {
+                    index = index == collection.Length
+                        ? 0
+                        : index + 1;
+                } while (selectedIndexes.Add(index));
+            }
+
+            rolled[i] = collection[index];
         }
+
+        return rolled;
     }
 }

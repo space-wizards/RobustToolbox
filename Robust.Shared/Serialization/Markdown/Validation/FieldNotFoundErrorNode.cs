dfba2250778d31d37657c44d4957c0524062e377
Tayrtahn <tayrtahn@gmail.com>
2024-04-07 21:22:52 -0400

    Add Type tracking to FieldNotFoundErrorNode

--- Robust.Shared/Serialization/Markdown/Validation/FieldNotFoundErrorNode.cs
+++ Robust.Shared/Serialization/Markdown/Validation/FieldNotFoundErrorNode.cs
@@ -5,7 +5,9 @@ namespace Robust.Shared.Serialization.Markdown.Validation;
 
 public sealed class FieldNotFoundErrorNode : ErrorNode
 {
+    public Type Type;
     public FieldNotFoundErrorNode(ValueDataNode key, Type type) : base(key, $"Field \"{key.Value}\" not found in \"{type}\".", false)
     {
+        Type = type;
     }
 }

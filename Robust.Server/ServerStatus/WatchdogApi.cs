51f0c60bd3d4573cb8bb9a9880ae7763e6231e96
Vasilis <vasilis@pikachu.systems>
2024-05-14 03:12:24 +0300

    Do not log wrong and correct watchdog token into info logs (#5133)
    * Lets not do this?
    
    * Webedit 2

--- Robust.Server/ServerStatus/WatchdogApi.cs
+++ Robust.Server/ServerStatus/WatchdogApi.cs
@@ -68,7 +68,7 @@ namespace Robust.Server.ServerStatus
             if (auth != _watchdogToken)
             {
                 // Holy shit nobody read these logs please.
-                _sawmill.Info(@"Failed auth: ""{0}"" vs ""{1}""", auth, _watchdogToken);
+                _sawmill.Verbose(@"Failed auth: ""{0}"" vs ""{1}""", auth, _watchdogToken);
                 await context.RespondErrorAsync(HttpStatusCode.Unauthorized);
                 return true;
             }

0a149fa91c6dabcccc189007c24f620961c9988b
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2024-05-31 09:51:11 +1000

    Expand PVS BUIs (#5179)
    Mainly happens to aghosts who go out of range. Shouldn't be a huge perf impact as the User component is added / removed as BUIs get opened.

--- Robust.Server/GameObjects/EntitySystems/UserInterfaceSystem.cs
+++ Robust.Server/GameObjects/EntitySystems/UserInterfaceSystem.cs
@@ -1,9 +1,29 @@
-using System;
-using System.Collections;
+using System.Collections.Generic;
+using Robust.Server.GameStates;
 using Robust.Shared.GameObjects;
 
 namespace Robust.Server.GameObjects;
 
 public sealed class UserInterfaceSystem : SharedUserInterfaceSystem
 {
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<UserInterfaceUserComponent, ExpandPvsEvent>(OnBuiUserExpand);
+    }
+
+    private void OnBuiUserExpand(Entity<UserInterfaceUserComponent> ent, ref ExpandPvsEvent args)
+    {
+        var buis = ent.Comp.OpenInterfaces.Keys;
+
+        if (buis.Count == 0)
+            return;
+
+        args.Entities ??= new List<EntityUid>(buis.Count);
+
+        foreach (var ui in buis)
+        {
+            args.Entities.Add(ui);
+        }
+    }
 }

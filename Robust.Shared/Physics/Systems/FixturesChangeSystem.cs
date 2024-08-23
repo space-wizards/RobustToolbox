e6a83057bb0d8eac02b64d69259ad18c24b2e87e
metalgearsloth <comedian_vs_clown@hotmail.com>
2024-08-23 18:28:11 +1000

    Fix allcomps test

--- Robust.Shared/Physics/Systems/FixturesChangeSystem.cs
+++ Robust.Shared/Physics/Systems/FixturesChangeSystem.cs
@@ -9,15 +9,23 @@ public sealed class FixturesChangeSystem : EntitySystem
     [Dependency] private readonly FixtureSystem _fixtures = default!;
     [Dependency] private readonly SharedPhysicsSystem _physics = default!;
 
+    private EntityQuery<FixturesComponent> _fixturesQuery;
+    private EntityQuery<PhysicsComponent> _physicsQuery;
+
     public override void Initialize()
     {
         base.Initialize();
+        _fixturesQuery = GetEntityQuery<FixturesComponent>();
+        _physicsQuery = GetEntityQuery<PhysicsComponent>();
         SubscribeLocalEvent<FixturesChangeComponent, ComponentStartup>(OnChangeStartup);
         SubscribeLocalEvent<FixturesChangeComponent, ComponentShutdown>(OnChangeShutdown);
     }
 
     private void OnChangeStartup(Entity<FixturesChangeComponent> ent, ref ComponentStartup args)
     {
+        if (!_physicsQuery.TryComp(ent, out var physics) || !_fixturesQuery.TryComp(ent, out var fixtures))
+            return;
+
         foreach (var (id, fixture) in ent.Comp.Fixtures)
         {
             _fixtures.TryCreateFixture(ent.Owner,
@@ -28,11 +36,13 @@ public sealed class FixturesChangeSystem : EntitySystem
                 fixture.CollisionLayer,
                 fixture.CollisionMask,
                 fixture.Friction,
-                fixture.Restitution);
+                fixture.Restitution,
+                manager: fixtures,
+                body: physics);
         }
 
         // TODO: Fixture creation should be handling this.
-        _physics.WakeBody(ent.Owner);
+        _physics.WakeBody(ent.Owner, manager: fixtures, body: physics);
     }
 
     private void OnChangeShutdown(Entity<FixturesChangeComponent> ent, ref ComponentShutdown args)

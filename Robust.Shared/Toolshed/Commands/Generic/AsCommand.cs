f1f3c60d1f31535c1b44c127042e29fd504e5973
Saphire Lattice <lattice@saphi.re>
2024-11-20 07:00:13 +0700

    Improve Toolshed type intersection mechanism, add WithCommand for ProtoId (#5515)

--- Robust.Shared/Toolshed/Commands/Generic/AsCommand.cs
+++ Robust.Shared/Toolshed/Commands/Generic/AsCommand.cs
@@ -5,8 +5,12 @@ namespace Robust.Shared.Toolshed.Commands.Generic;
 [ToolshedCommand]
 public sealed class AsCommand : ToolshedCommand
 {
-    public override Type[] TypeParameterParsers => new[] {typeof(Type)};
+    public override Type[] TypeParameterParsers => [ typeof(Type) ];
 
+    /// <summary>
+    ///     Uses a typecast to convert a type. It does not handle implicit casts, nor explicit ones.
+    ///     If you're thinking to extend this, you probably want to look into making a <see cref="TypeParser{T}"/>
+    /// </summary>
     [CommandImplementation, TakesPipedTypeAsGeneric]
     public TOut? As<TOut, TIn>([PipedArgument] TIn value)
         => (TOut?)(object?)value;

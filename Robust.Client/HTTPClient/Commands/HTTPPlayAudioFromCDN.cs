126842345e4a7f6d32ce4cb18cc1d63d58dfe9d8
luiz <luiz@mavromati.com>
2024-12-05 18:05:00 -0300

    added a way to play the files for testing purposes

--- Robust.Client/HTTPClient/Commands/HTTPPlayAudioFromCDN.cs
+++ Robust.Client/HTTPClient/Commands/HTTPPlayAudioFromCDN.cs
@@ -3,22 +3,22 @@ using Robust.Shared.IoC;
 
 namespace Robust.Client.HTTPClient.Commands
 {
-    public sealed class HTTPGetFileAsync : LocalizedCommands
+    public sealed class HTTPPlayAudioFromCDN : LocalizedCommands
     {
         [Dependency] private readonly ICDNConsumer _consumer = default!;
 
-        public override string Command => "getfileasync";
+        public override string Command => "playaudiocdn";
 
         public override void Execute(IConsoleShell shell, string argStr, string[] args)
         {
             if (args.Length < 1)
             {
-                shell.WriteLine("Usage: getfileasync <url>");
+                shell.WriteLine("Usage: playaudiocdn <url>");
                 return;
             }
 
             var url = args[0];
-            _consumer.GetFileAsync(url);
+            _consumer.PlayAudioFromCDN(url);
         }
     }
 }

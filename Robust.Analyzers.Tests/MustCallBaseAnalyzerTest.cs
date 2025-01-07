777ecb1cc3630cb2b293f975cbd9c387abe56391
PJB3005 <pieterjan.briers+git@gmail.com>
2025-01-07 18:57:52 +0100

    Dependencies update & cleanup
    Fixes security vuln warnings etc

--- Robust.Analyzers.Tests/MustCallBaseAnalyzerTest.cs
+++ Robust.Analyzers.Tests/MustCallBaseAnalyzerTest.cs
@@ -1,10 +1,9 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CSharp.Testing;
 using Microsoft.CodeAnalysis.Testing;
-using Microsoft.CodeAnalysis.Testing.Verifiers;
 using NUnit.Framework;
 using VerifyCS =
-    Microsoft.CodeAnalysis.CSharp.Testing.NUnit.AnalyzerVerifier<Robust.Analyzers.MustCallBaseAnalyzer>;
+    Microsoft.CodeAnalysis.CSharp.Testing.CSharpAnalyzerVerifier<Robust.Analyzers.MustCallBaseAnalyzer, Microsoft.CodeAnalysis.Testing.DefaultVerifier>;
 
 namespace Robust.Analyzers.Tests;
 
@@ -14,7 +13,7 @@ public sealed class MustCallBaseAnalyzerTest
 {
     private static Task Verifier(string code, params DiagnosticResult[] expected)
     {
-        var test = new CSharpAnalyzerTest<MustCallBaseAnalyzer, NUnitVerifier>()
+        var test = new CSharpAnalyzerTest<MustCallBaseAnalyzer, DefaultVerifier>()
         {
             TestState =
             {

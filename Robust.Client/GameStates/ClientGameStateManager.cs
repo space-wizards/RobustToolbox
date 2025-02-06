073dde51343f3c6ae8d7ddfa9a45f6534a493b20
TemporalOroboros <TemporalOroboros@gmail.com>
2025-02-05 19:33:54 -0800

    Fix unreachable code warnings in gamestate management Use #else sections to make sure they don't complain about being on the wrong side of a throw

--- Robust.Client/GameStates/ClientGameStateManager.cs
+++ Robust.Client/GameStates/ClientGameStateManager.cs
@@ -698,8 +698,9 @@ namespace Robust.Client.GameStates
 
 #if !EXCEPTION_TOLERANCE
                     throw new KeyNotFoundException();
-#endif
+#else
                     continue;
+#endif
                 }
 
                 var compData = _compDataPool.Get();
@@ -960,8 +961,9 @@ namespace Robust.Client.GameStates
                 RequestFullState();
 #if !EXCEPTION_TOLERANCE
                 throw;
-#endif
+#else
                 return;
+#endif
             }
 
             if (data.Created)
@@ -979,8 +981,9 @@ namespace Robust.Client.GameStates
                     RequestFullState();
 #if !EXCEPTION_TOLERANCE
                     throw;
-#endif
+#else
                     return;
+#endif
                 }
             }
 

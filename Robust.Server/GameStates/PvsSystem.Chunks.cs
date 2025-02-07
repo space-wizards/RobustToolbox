55eb9a69755955c2bf59c377b457c8c83a4f42f8
TemporalOroboros <TemporalOroboros@gmail.com>
2025-02-06 22:25:40 -0800

    Redirect .LocalMatrix references Converts uses of TransformComponent.LocalMatrix to use SharedTransformSystem.GetLocalMatrix

--- Robust.Server/GameStates/PvsSystem.Chunks.cs
+++ Robust.Server/GameStates/PvsSystem.Chunks.cs
@@ -72,7 +72,7 @@ internal sealed partial class PvsSystem
         var xform = Transform(chunk.Root);
         DebugTools.AssertEqual(chunk.Map.Owner, xform.MapUid);
         chunk.InvWorldMatrix = xform.InvLocalMatrix;
-        var worldPos = Vector2.Transform(chunk.Centre, xform.LocalMatrix);
+        var worldPos = Vector2.Transform(chunk.Centre, _transform.GetLocalMatrix(xform));
         chunk.Position = new(worldPos, xform.MapID);
         chunk.UpdateQueued = false;
     }

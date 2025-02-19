3171c8a39219dd4f02ba2d7431e4b6e5b9ba1f2d
Tayrtahn <tayrtahn@gmail.com>
2025-02-19 10:50:29 -0500

    Move back to Shared, change API

--- Robust.Shared/GameObjects/Systems/SingletonEntitySystem.cs
+++ Robust.Shared/GameObjects/Systems/SingletonEntitySystem.cs
@@ -1,5 +1,7 @@
+using System.Diagnostics.CodeAnalysis;
 using Robust.Shared.IoC;
 using Robust.Shared.Map;
+using Robust.Shared.Network;
 using Robust.Shared.Utility;
 
 namespace Robust.Shared.GameObjects;
@@ -7,19 +9,14 @@ namespace Robust.Shared.GameObjects;
 /// <summary>
 /// Extension of <see cref="EntitySystem"/> for use with singleton components.
 /// When accessed, an entity will be spawned in nullspace with an attached <typeparamref name="T"/>.
-/// The <typeparamref name="T"/> can be accessed through the <see cref="Instance"/> property.
 /// </summary>
 /// <typeparam name="T">Type of the attached component.</typeparam>
 public abstract partial class SingletonEntitySystem<T> : EntitySystem where T : Component, new()
 {
     [Dependency] protected readonly MetaDataSystem Meta = default!;
+    [Dependency] protected readonly INetManager NetMan = default!;
 
     private Entity<T>? _cachedEntity;
-
-    /// <summary>
-    /// Reference to the singleton instance of Entity{<typeparamref name="T"/>}.
-    /// </summary>
-    protected Entity<T> Instance => _cachedEntity ??= FindOrCreateHolder();
 
     public override void Initialize()
     {
@@ -41,13 +38,16 @@ public abstract partial class SingletonEntitySystem<T> : EntitySystem where T : 
         _cachedEntity = null;
     }
 
-    private Entity<T> FindOrCreateHolder()
+    private Entity<T>? FindOrCreateHolder()
     {
         var query = EntityQueryEnumerator<T>();
         while (query.MoveNext(out var uid, out var comp))
         {
             return (uid, comp);
         }
+
+        if (NetMan.IsClient)
+            return null;
 
         return CreateHolder();
     }
@@ -58,5 +58,19 @@ public abstract partial class SingletonEntitySystem<T> : EntitySystem where T : 
         var comp = AddComp<T>(uid);
         Meta.SetEntityName(uid, $"{typeof(T).Name} Holder");
         return (uid, comp);
+    }
+
+    /// <summary>
+    /// Attempts to get the singleton Entity{<typeparamref name="T"/>} instance.
+    /// This always succeeds on the server, where the instance will be created if
+    /// if does not exist yet. On the client, this can return false if attempting
+    /// to access the instance before it has been sent from the server.
+    /// </summary>
+    /// <param name="instance">The singleton Entity{<typeparamref name="T"/>} instance.</param>
+    /// <returns>True if the instance was found or created, otherwise false</returns>
+    public bool TryGetInstance([NotNullWhen(true)] out Entity<T>? instance)
+    {
+        instance = FindOrCreateHolder();
+        return instance != null;
     }
 }

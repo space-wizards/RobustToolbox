5f6932e74aed1c03e48eaee13837dd8210cff7a2
Tayrtahn <tayrtahn@gmail.com>
2025-03-22 10:18:00 -0400

    Add PrototypeManager Has.Index(EntProtoId)

--- Robust.UnitTesting/Has.cs
+++ Robust.UnitTesting/Has.cs
@@ -9,6 +9,8 @@ public sealed class Has : NUnit.Framework.Has
     public static PrototypeManagerIndexConstraint<T> Index<T>(ProtoId<T> protoId)
         where T : class, IPrototype => new(protoId);
 
+        public static PrototypeManagerIndexConstraint<EntityPrototype> Index(EntProtoId protoId) => new(protoId);
+
     public static EntityPrototypeComponentConstraint<T> Component<T>(IComponentFactory componentFactory)
         where T : IComponent, new() => new(componentFactory);
 }

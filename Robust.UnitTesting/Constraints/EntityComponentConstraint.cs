0f6d4de4d0c3b222957b18cbe7be9128d74279e4
Tayrtahn <tayrtahn@gmail.com>
2025-03-22 20:31:06 -0400

    Improve output for EntityComponentConstraint

--- Robust.UnitTesting/Constraints/EntityComponentConstraint.cs
+++ Robust.UnitTesting/Constraints/EntityComponentConstraint.cs
@@ -16,6 +16,6 @@ public sealed class EntityComponentConstraint<T>(IEntityManager entMan) : Constr
         var uid = ConstraintUtils.GetEntityUid(actual);
         var components = _entMan.GetComponents(uid).Select(c => _entMan.ComponentFactory.GetComponentName(c.GetType()))
             .HighlightMatches(_entMan.ComponentFactory.GetComponentName<T>());
-        return new ConstraintResult(this, components, _entMan.HasComponent<T>(uid));
+        return new ConstraintResult(this, string.Join(", ", components), _entMan.HasComponent<T>(uid));
     }
 }

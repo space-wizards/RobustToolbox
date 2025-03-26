7f5c70e16562b7084719e4a353d5010bc7d1fbcf
metalgearsloth <comedian_vs_clown@hotmail.com>
2025-03-26 19:25:09 +1100

    Add OtherBody API to contacts
    Thought I had a pr for this.

--- Robust.Shared/Physics/Dynamics/Contacts/Contact.cs
+++ Robust.Shared/Physics/Dynamics/Contacts/Contact.cs
@@ -417,6 +417,17 @@ namespace Robust.Shared.Physics.Dynamics.Contacts
 
             throw new InvalidOperationException();
         }
+
+        [Pure, PublicAPI]
+        public PhysicsComponent OtherBody(EntityUid uid)
+        {
+            if (uid == EntityA)
+                return BodyB!;
+            else if (uid == EntityB)
+                return BodyA!;
+
+            throw new InvalidOperationException();
+        }
     }
 
     [Flags]

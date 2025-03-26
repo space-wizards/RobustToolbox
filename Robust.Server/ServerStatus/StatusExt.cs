ceb59402a1a8ddc43b1ad32c44a12fed011d6ebb
PJB3005 <pieterjan.briers+git@gmail.com>
2025-03-26 01:15:33 +0100

    Make status and info APIs have CORS allow-origin: *
    Allows it to be queried from browser JS. No harm in not allowing this.
    
    Added helper function StatusExt.AddAllowOriginAny to make this easy to add.

---
+++ Robust.Server/ServerStatus/StatusExt.cs
@@ -0,0 +1,15 @@
+﻿namespace Robust.Server.ServerStatus;
+
+/// <summary>
+/// Helper functions for working with <see cref="IStatusHandlerContext"/>.
+/// </summary>
+public static class StatusExt
+{
+    /// <summary>
+    /// Add <c>Access-Control-Allow-Origin: *</c> to the response headers for this request.
+    /// </summary>
+    public static void AddAllowOriginAny(this IStatusHandlerContext context)
+    {
+        context.ResponseHeaders.Add("Access-Control-Allow-Origin", "*");
+    }
+}

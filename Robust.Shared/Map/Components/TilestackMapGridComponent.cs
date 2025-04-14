4b157bc02e29a98e7b988143b05f71854d3b9566
Killerqu00 <killerqueen1777@gmail.com>
2025-04-14 23:48:24 +0200

    networked

--- Robust.Shared/Map/Components/TilestackMapGridComponent.cs
+++ Robust.Shared/Map/Components/TilestackMapGridComponent.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using Robust.Shared.GameObjects;
+using Robust.Shared.GameStates;
 using Robust.Shared.Maths;
 using Robust.Shared.ViewVariables;
 
@@ -9,7 +10,7 @@ namespace Robust.Shared.Map.Components;
 ///     Stores tilestacks by grid indices for tiles.
 ///     This is only used in content for tiles that are placed not on their BaseTurfs.
 /// </summary>
-[RegisterComponent]
+[RegisterComponent, NetworkedComponent]
 public sealed partial class TilestackMapGridComponent : Component
 {
     [ViewVariables]

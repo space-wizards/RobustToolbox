90126613ac503b3ba7edd5567bfdf8acc63e8d8d
Errant <35878406+Errant-4@users.noreply.github.com>
2025-04-19 12:24:15 +0200

    string reading

--- Robust.Shared/Serialization/TypeSerializers/Implementations/TimespanSerializer.cs
+++ Robust.Shared/Serialization/TypeSerializers/Implementations/TimespanSerializer.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using JetBrains.Annotations;
 using Robust.Shared.IoC;
@@ -22,6 +24,9 @@ public sealed class TimespanSerializer : ITypeSerializer<TimeSpan, ValueDataNode
         ISerializationContext? context = null,
         ISerializationManager.InstantiationDelegate<TimeSpan>? instanceProvider = null)
     {
+        if (TryTimeSpan(node, out var time))
+            return time.Value;
+
         var seconds = double.Parse(node.Value, CultureInfo.InvariantCulture);
         return TimeSpan.FromSeconds(seconds);
     }
@@ -33,6 +38,7 @@ public sealed class TimespanSerializer : ITypeSerializer<TimeSpan, ValueDataNode
         ISerializationContext? context = null)
     {
         return double.TryParse(node.Value, NumberStyles.Any, CultureInfo.InvariantCulture, out _)
+               || TryTimeSpan(node, out _)
             ? new ValidatedValueNode(node)
             : new ErrorNode(node, "Failed parsing TimeSpan");
     }
@@ -56,5 +62,43 @@ public sealed class TimespanSerializer : ITypeSerializer<TimeSpan, ValueDataNode
         ISerializationContext? context = null)
     {
         return source;
+    }
+
+    /// <summary>
+    /// Convert strings from the compatible format into TimeSpan and output it. Returns true if successful.
+    /// The string must start with a number and end with a single letter referring to the time unit used.
+    /// It can NOT combine multiple types (like "1h30m"), but it CAN use decimals ("1.5h")
+    /// </summary>
+    private bool TryTimeSpan(ValueDataNode node, [NotNullWhen(true)] out TimeSpan? timeSpan)
+    {
+        timeSpan = null;
+        var units = new List<string>{"h", "m", "s"};
+
+        var input = node.Value.Replace(" ","");
+        var unit = input[^1].ToString();
+        var number = input.Substring(0, input.Length - 1);
+        var valid = false;
+
+        if (units.Contains(unit)
+            && double.TryParse(number, out var d))
+        {
+            switch (unit)
+            {
+                case "s":
+                    timeSpan = TimeSpan.FromSeconds(d);
+                    valid = true;
+                    break;
+                case "m":
+                    timeSpan = TimeSpan.FromMinutes(d);
+                    valid = true;
+                    break;
+                case "h":
+                    timeSpan = TimeSpan.FromHours(d);
+                    valid = true;
+                    break;
+            }
+        }
+
+        return valid;
     }
 }

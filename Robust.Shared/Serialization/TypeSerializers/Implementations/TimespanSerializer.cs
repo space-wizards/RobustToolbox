d75befb8e615ac3f616b38cd5a811a27a59b8912
Errant <35878406+Errant-4@users.noreply.github.com>
2025-04-19 09:44:03 +0200

    timespanserializer cleanup

--- Robust.Shared/Serialization/TypeSerializers/Implementations/TimespanSerializer.cs
+++ Robust.Shared/Serialization/TypeSerializers/Implementations/TimespanSerializer.cs
@@ -9,42 +9,52 @@ using Robust.Shared.Serialization.Markdown.Validation;
 using Robust.Shared.Serialization.Markdown.Value;
 using Robust.Shared.Serialization.TypeSerializers.Interfaces;
 
-namespace Robust.Shared.Serialization.TypeSerializers.Implementations
+namespace Robust.Shared.Serialization.TypeSerializers.Implementations;
+
+[TypeSerializer]
+public sealed class TimespanSerializer : ITypeSerializer<TimeSpan, ValueDataNode>, ITypeCopyCreator<TimeSpan>
 {
-    [TypeSerializer]
-    public sealed class TimespanSerializer : ITypeSerializer<TimeSpan, ValueDataNode>, ITypeCopyCreator<TimeSpan>
+    public TimeSpan Read(
+        ISerializationManager serializationManager,
+        ValueDataNode node,
+        IDependencyCollection dependencies,
+        SerializationHookContext hookCtx,
+        ISerializationContext? context = null,
+        ISerializationManager.InstantiationDelegate<TimeSpan>? instanceProvider = null)
     {
-        public TimeSpan Read(ISerializationManager serializationManager, ValueDataNode node,
-            IDependencyCollection dependencies,
-            SerializationHookContext hookCtx,
-            ISerializationContext? context = null,
-            ISerializationManager.InstantiationDelegate<TimeSpan>? instanceProvider = null)
-        {
-            var seconds = double.Parse(node.Value, CultureInfo.InvariantCulture);
-            return TimeSpan.FromSeconds(seconds);
-        }
+        var seconds = double.Parse(node.Value, CultureInfo.InvariantCulture);
+        return TimeSpan.FromSeconds(seconds);
+    }
 
-        public ValidationNode Validate(ISerializationManager serializationManager, ValueDataNode node,
-            IDependencyCollection dependencies,
-            ISerializationContext? context = null)
-        {
-            return double.TryParse(node.Value, NumberStyles.Any, CultureInfo.InvariantCulture, out _)
-                ? new ValidatedValueNode(node)
-                : new ErrorNode(node, "Failed parsing TimeSpan");
-        }
+    public ValidationNode Validate(
+        ISerializationManager serializationManager,
+        ValueDataNode node,
+        IDependencyCollection dependencies,
+        ISerializationContext? context = null)
+    {
+        return double.TryParse(node.Value, NumberStyles.Any, CultureInfo.InvariantCulture, out _)
+            ? new ValidatedValueNode(node)
+            : new ErrorNode(node, "Failed parsing TimeSpan");
+    }
 
-        public DataNode Write(ISerializationManager serializationManager, TimeSpan value,
-            IDependencyCollection dependencies, bool alwaysWrite = false,
-            ISerializationContext? context = null)
-        {
-            return new ValueDataNode(value.TotalSeconds.ToString(CultureInfo.InvariantCulture));
-        }
+    public DataNode Write(
+        ISerializationManager serializationManager,
+        TimeSpan value,
+        IDependencyCollection dependencies,
+        bool alwaysWrite = false,
+        ISerializationContext? context = null)
+    {
+        return new ValueDataNode(value.TotalSeconds.ToString(CultureInfo.InvariantCulture));
+    }
 
-        [MustUseReturnValue]
-        public TimeSpan CreateCopy(ISerializationManager serializationManager, TimeSpan source,
-            IDependencyCollection dependencies, SerializationHookContext hookCtx, ISerializationContext? context = null)
-        {
-            return source;
-        }
+    [MustUseReturnValue]
+    public TimeSpan CreateCopy(
+        ISerializationManager serializationManager,
+        TimeSpan source,
+        IDependencyCollection dependencies,
+        SerializationHookContext hookCtx,
+        ISerializationContext? context = null)
+    {
+        return source;
     }
 }

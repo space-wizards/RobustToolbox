87f91d1d055eedca5e87793db3313b30be63fbf4
Ciarán Walsh <git@ciaranwal.sh>
2025-04-28 12:15:22 +0100

    Show loading status

--- Robust.LanguageServer/LanguageServerContext.cs
+++ Robust.LanguageServer/LanguageServerContext.cs
@@ -1,12 +1,16 @@
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using EmmyLua.LanguageServer.Framework.Protocol.Message.Client.ShowMessage;
 using EmmyLua.LanguageServer.Framework.Protocol.Message.Configuration;
+using EmmyLua.LanguageServer.Framework.Protocol.Message.Progress;
+using EmmyLua.LanguageServer.Framework.Protocol.Model.WorkDoneProgress;
 using EmmyLua.LanguageServer.Framework.Server.Handler;
 using Robust.LanguageServer.Handler;
-using Robust.LanguageServer.Provider;
+using Robust.LanguageServer.Notifications;
 using Robust.Shared.IoC;
 
 namespace Robust.LanguageServer;
+
 using ELLanguageServer = EmmyLua.LanguageServer.Framework.Server.LanguageServer;
 
 public sealed class LanguageServerContext
@@ -26,53 +30,36 @@ public sealed class LanguageServerContext
             throw new NullReferenceException(nameof(deps));
 
         _languageServer = languageServer;
+        _languageServer.AddJsonSerializeContext(JsonGenerateContext.Default);
 
         _languageServer.OnInitialize((c, s) =>
         {
             IoCManager.InitThread(deps);
 
-            if (c.RootUri is {} rootUri)
+            if (c.RootUri is { } rootUri)
                 RootDirectory = rootUri.Uri;
-
-            // Here we should be trying to load data based on the client.rootUri
-            Console.Error.WriteLine("Starting loader…");
-            _languageServer.Client.ShowMessage(new() { Message = "Loading prototypes…", Type = MessageType.Info });
-            deps.Resolve<Loader>().Init(deps);
-
-            _languageServer.Client.ShowMessage(new() { Message = "Prototypes loaded.", Type = MessageType.Info });
-            Console.Error.WriteLine("Loaded");
 
             s.Name = "SS14 LSP";
             s.Version = "0.0.1";
             Console.Error.WriteLine("initialize");
             return Task.CompletedTask;
         });
+
         _languageServer.OnInitialized(async (c) =>
         {
             Console.Error.WriteLine("initialized");
-            var config = await _languageServer.Client.GetConfiguration(new ConfigurationParams()
-                {
-                    Items =
-                    [
-                        new ConfigurationItem()
-                        {
-                            Section = "files"
-                        }
-                    ]
-                },
-                CancellationToken.None);
 
-            Console.Error.WriteLine("Config:");
-            foreach (var item in config)
-            {
-                Console.Error.WriteLine($"Item: {item.Value}");
-                if (item.Value is JsonDocument doc)
-                {
-                    Console.Error.WriteLine($"Doc: {doc.RootElement.GetRawText()}");
-                }
-            }
+            // Here we should be trying to load data based on the client.rootUri
+            Console.Error.WriteLine("Starting loader…");
+
+            ShowProgress("Loading Prototypes…");
+
+            deps.Resolve<Loader>().Init(deps);
+
+            HideProgress();
+
+            Console.Error.WriteLine("Loaded");
         });
-
     }
 
     public void Initialize()
@@ -86,10 +73,7 @@ public sealed class LanguageServerContext
         AddHandler(new SemanticTokensHandler());
         AddHandler(new DocumentColorHandler());
 
-        _cache.DocumentChanged += (uri) =>
-        {
-            Console.Error.WriteLine($"Document changed! Uri: {uri}");
-        };
+        _cache.DocumentChanged += (uri) => { Console.Error.WriteLine($"Document changed! Uri: {uri}"); };
     }
 
     public Task Run()
@@ -102,5 +86,23 @@ public sealed class LanguageServerContext
     private void AddHandler(IJsonHandler handler)
     {
         _languageServer.AddHandler(IoCManager.InjectDependencies(handler));
+    }
+
+    private void ShowProgress(string text)
+    {
+        _languageServer.SendNotification(new("rt/showProgress",
+                JsonSerializer.SerializeToDocument(
+                    new ProgressInfo()
+                    {
+                        Text = text,
+                    },
+                    _languageServer.JsonSerializerOptions)
+            ))
+            .Wait();
+    }
+
+    private void HideProgress()
+    {
+        _languageServer.SendNotification(new("rt/hideProgress", null)).Wait();
     }
 }

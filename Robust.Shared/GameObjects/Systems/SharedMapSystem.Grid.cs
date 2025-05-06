7ebcf255e832d0cd9b92782ec8814108f00c78e0
Tayrtahn <tayrtahn@gmail.com>
2025-05-06 13:50:25 -0400

    Raise event after regenerating collision

--- Robust.Shared/GameObjects/Systems/SharedMapSystem.Grid.cs
+++ Robust.Shared/GameObjects/Systems/SharedMapSystem.Grid.cs
@@ -878,14 +878,14 @@ public abstract partial class SharedMapSystem
             chunk.SuppressCollisionRegeneration = false;
         }
 
+        RegenerateCollision(uid, grid, modified);
+
         // Notify of all tile changes in one event
         var ev = new TileChangedEvent((uid, grid), tileChanges.ToArray());
         RaiseLocalEvent(uid, ref ev, true);
 
         // Back to normal
         MapManager.SuppressOnTileChanged = false;
-
-        RegenerateCollision(uid, grid, modified);
     }
 
     public TilesEnumerator GetLocalTilesEnumerator(EntityUid uid, MapGridComponent grid, Box2 aabb,

4a6195b3d584c9c674303638117db4e3c3a7fa65
ElectroJr <leonsfriedrich@gmail.com>
2025-05-12 00:27:52 +1200

    Remove double delegate

--- Robust.Shared/GameObjects/EventBusAttributes.cs
+++ Robust.Shared/GameObjects/EventBusAttributes.cs
@@ -8,10 +8,14 @@ public sealed class ByRefEventAttribute : Attribute
 }
 
 /// <summary>
-/// Indicates that an eventbus event should only ever be raised through <see cref="IDirectedEventBus.RaiseComponentEvent{TEvent}(IComponent, TEvent)"/>.
-/// This allows extra optimizations.
+/// This attribute enables an event to be raised as a "component event" via <see cref="IDirectedEventBus.RaiseComponentEvent{TEvent}(IComponent, TEvent)"/>.
 /// </summary>
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
 internal sealed class ComponentEventAttribute : Attribute
 {
+    /// <summary>
+    /// If true, this event may **only** be raised via as a "component event". I.e., event handlers will not be
+    /// included in the normal event subscription tables.
+    /// </summary>
+    public bool Exclusive = true;
 }

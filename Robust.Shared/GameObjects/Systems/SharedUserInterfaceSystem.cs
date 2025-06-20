9bd7836d24183b46278994a3d684263d20f2e5d3
Tayrtahn <tayrtahn@gmail.com>
2025-06-20 11:17:26 -0400

    Revert "Restore comment"
    This reverts commit 4a47a365574f6e46dda401a3076514655314f836.

--- Robust.Shared/GameObjects/Systems/SharedUserInterfaceSystem.cs
+++ Robust.Shared/GameObjects/Systems/SharedUserInterfaceSystem.cs
@@ -682,8 +682,6 @@ public abstract class SharedUserInterfaceSystem : EntitySystem
         {
             if (_timing.IsFirstTimePredicted)
             {
-                // Not guaranteed to open so rely upon the event handling it.
-                // Also lets client request it to be opened remotely too.
                 RaisePredictiveEvent(new BoundUIWrapMessage(GetNetEntity(entity.Owner), new OpenBoundInterfaceMessage(), key));
             }
         }

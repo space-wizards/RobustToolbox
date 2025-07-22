75c17f723f7284cb634f0b6138e346014a93cbf7
Tayrtahn <tayrtahn@gmail.com>
2025-07-22 13:06:53 -0400

    Validate VV EntProtoId values

--- Robust.Client/ViewVariables/Editors/VVPropEditorEntProtoId.cs
+++ Robust.Client/ViewVariables/Editors/VVPropEditorEntProtoId.cs
@@ -1,16 +1,24 @@
 using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controls;
+using Robust.Shared.IoC;
 using Robust.Shared.Prototypes;
 
 namespace Robust.Client.ViewVariables.Editors;
 
 internal sealed class VVPropEditorEntProtoId : VVPropEditor
 {
+    [Dependency] private readonly IPrototypeManager _protoMan = default!;
+
+    public VVPropEditorEntProtoId()
+    {
+        IoCManager.InjectDependencies(this);
+    }
+
     protected override Control MakeUI(object? value)
     {
         var lineEdit = new LineEdit
         {
-            Text = (EntProtoId) (value ?? ""),
+            Text = (EntProtoId)(value ?? ""),
             Editable = !ReadOnly,
             HorizontalExpand = true,
         };
@@ -19,7 +27,9 @@ internal sealed class VVPropEditorEntProtoId : VVPropEditor
         {
             lineEdit.OnTextEntered += e =>
             {
-                ValueChanged((EntProtoId) e.Text);
+                var id = (EntProtoId)e.Text;
+                if (_protoMan.HasIndex(id))
+                    ValueChanged(id);
             };
         }
 

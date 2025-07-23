a648cf0cde8c692d9320cc86f3468969b824bc23
Jen Pollock <jen@jenpollock.ca>
2025-07-22 20:39:52 -0600

    Put back CheckButton.cs because old game versions exist

---
+++ Robust.Client/UserInterface/Controls/CheckButton.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Robust.Client.UserInterface.Controls
+{
+    /// This was only ever a synonym for a regular button. Use Button
+    /// for regular buttons and SwitchButton for switch-like buttons.
+    [Obsolete]
+    [Virtual]
+    public class CheckButton : Button
+    {
+    }
+}

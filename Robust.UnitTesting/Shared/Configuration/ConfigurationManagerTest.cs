e52c1c924ed0e4d5a57f367a574d2af558e2fd63
pa.pecherskij <pa.pecherskij@interfax.ru>
2025-07-30 22:39:04 +0300

    refactor: reusing stateful object in tests is not smart

--- Robust.UnitTesting/Shared/Configuration/ConfigurationManagerTest.cs
+++ Robust.UnitTesting/Shared/Configuration/ConfigurationManagerTest.cs
@@ -15,19 +15,12 @@ namespace Robust.UnitTesting.Shared.Configuration
     [TestOf(typeof(ConfigurationManager))]
     public sealed class ConfigurationManagerTest
     {
-        private IConfigurationManager _mgr;
-
-        [SetUp]
-        public void Setup()
-        {
-            _mgr = MakeCfg();
-        }
-
         [Test]
         public void TestSubscribeUnsubscribe()
         {
+            var mgr = MakeCfg();
 
-            _mgr.RegisterCVar("foo.bar", 5);
+            mgr.RegisterCVar("foo.bar", 5);
 
             var lastValue = 0;
             var timesRan = 0;
@@ -38,42 +31,44 @@ namespace Robust.UnitTesting.Shared.Configuration
                 lastValue = value;
             }
 
-            _mgr.OnValueChanged<int>("foo.bar", ValueChanged);
+            mgr.OnValueChanged<int>("foo.bar", ValueChanged);
 
-            _mgr.SetCVar("foo.bar", 2);
+            mgr.SetCVar("foo.bar", 2);
 
             Assert.That(timesRan, Is.EqualTo(1), "OnValueChanged did not run!");
             Assert.That(lastValue, Is.EqualTo(2), "OnValueChanged value was wrong!");
 
-            _mgr.UnsubValueChanged<int>("foo.bar", ValueChanged);
+            mgr.UnsubValueChanged<int>("foo.bar", ValueChanged);
 
             Assert.That(timesRan, Is.EqualTo(1), "UnsubValueChanged did not unsubscribe!");
         }
 
         [Test]
-        public void TestSubscribeUnsubscribe1()
+        public void TestSubscribe_Unsubscribe()
         {
-            _mgr.RegisterCVar("foo.bar", 5);
-            _mgr.RegisterCVar("foo.foo", 2);
+            var mgr = MakeCfg();
+
+            mgr.RegisterCVar("foo.bar", 5);
+            mgr.RegisterCVar("foo.foo", 2);
 
             var lastValueBar = 0;
             var lastValueFoo = 0;
 
-            var subscription = _mgr.SubscribeMultiple()
+            var subscription = mgr.SubscribeMultiple()
                                    .OnValueChanged<int>("foo.bar", value => lastValueBar = value)
                                    .OnValueChanged<int>("foo.foo", value => lastValueFoo = value)
                                    .Subscribe();
 
-            _mgr.SetCVar("foo.bar", 1);
-            _mgr.SetCVar("foo.foo", 3);
+            mgr.SetCVar("foo.bar", 1);
+            mgr.SetCVar("foo.foo", 3);
 
             Assert.That(lastValueBar, Is.EqualTo(1), "OnValueChanged value was wrong!");
             Assert.That(lastValueFoo, Is.EqualTo(3), "OnValueChanged value was wrong!");
 
             subscription.Dispose();
 
-            _mgr.SetCVar("foo.bar", 10);
-            _mgr.SetCVar("foo.foo", 30);
+            mgr.SetCVar("foo.bar", 10);
+            mgr.SetCVar("foo.foo", 30);
 
             Assert.That(lastValueBar, Is.EqualTo(1), "OnValueChanged value was wrong!");
             Assert.That(lastValueFoo, Is.EqualTo(3), "OnValueChanged value was wrong!");
@@ -82,28 +77,30 @@ namespace Robust.UnitTesting.Shared.Configuration
         [Test]
         public void TestOverrideDefaultValue()
         {
-            _mgr.RegisterCVar("foo.bar", 5);
+            var mgr = MakeCfg();
+
+            mgr.RegisterCVar("foo.bar", 5);
 
             var value = 0;
-            _mgr.OnValueChanged<int>("foo.bar", v => value = v);
+            mgr.OnValueChanged<int>("foo.bar", v => value = v);
 
             // Change default value, this fires the value changed callback.
-            _mgr.OverrideDefault("foo.bar", 10);
+            mgr.OverrideDefault("foo.bar", 10);
 
             Assert.That(value, Is.EqualTo(10));
-            Assert.That(_mgr.GetCVar<int>("foo.bar"), Is.EqualTo(10));
+            Assert.That(mgr.GetCVar<int>("foo.bar"), Is.EqualTo(10));
 
             // Modify the cvar programmatically, also fires the callback.
-            _mgr.SetCVar("foo.bar", 7);
+            mgr.SetCVar("foo.bar", 7);
 
             Assert.That(value, Is.EqualTo(7));
-            Assert.That(_mgr.GetCVar<int>("foo.bar"), Is.EqualTo(7));
+            Assert.That(mgr.GetCVar<int>("foo.bar"), Is.EqualTo(7));
 
             // We have a value set now, so changing the default won't do anything.
-            _mgr.OverrideDefault("foo.bar", 15);
+            mgr.OverrideDefault("foo.bar", 15);
 
             Assert.That(value, Is.EqualTo(7));
-            Assert.That(_mgr.GetCVar<int>("foo.bar"), Is.EqualTo(7));
+            Assert.That(mgr.GetCVar<int>("foo.bar"), Is.EqualTo(7));
         }
 
         private IConfigurationManager MakeCfg()

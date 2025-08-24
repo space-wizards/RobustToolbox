34e1702d6f8c5309f45cd38f18c7b4bb7fa2ec5f
Roudenn <romabond091@gmail.com>
2025-08-24 20:16:44 +0300

    New Type Serializers

---
+++ Robust.Shared/Serialization/TypeSerializers/Implementations/MapIdSerializer.cs
@@ -0,0 +1,67 @@
+﻿using Robust.Shared.EntitySerialization;
+using Robust.Shared.GameObjects;
+using Robust.Shared.IoC;
+using Robust.Shared.Map;
+using Robust.Shared.Map.Components;
+using Robust.Shared.Serialization.Manager;
+using Robust.Shared.Serialization.Manager.Attributes;
+using Robust.Shared.Serialization.Markdown;
+using Robust.Shared.Serialization.Markdown.Validation;
+using Robust.Shared.Serialization.Markdown.Value;
+using Robust.Shared.Serialization.TypeSerializers.Interfaces;
+
+namespace Robust.Shared.Serialization.TypeSerializers.Implementations;
+
+/// <summary>
+/// Stores a reference to a map using its EntityUid.
+/// </summary>
+[TypeSerializer]
+public sealed class MapIdSerializer : ITypeSerializer<MapId, ValueDataNode>
+{
+    public ValidationNode Validate(
+        ISerializationManager serializationManager,
+        ValueDataNode node,
+        IDependencyCollection dependencies,
+        ISerializationContext? context = null)
+    {
+        if (node.Value == "invalid")
+            return new ValidatedValueNode(node);
+
+        if (!int.TryParse(node.Value, out _))
+            return new ErrorNode(node, "Invalid NetEntity");
+
+        return new ValidatedValueNode(node);
+    }
+
+    public MapId Read(
+        ISerializationManager serializationManager,
+        ValueDataNode node,
+        IDependencyCollection dependencies,
+        SerializationHookContext hookCtx,
+        ISerializationContext? context = null,
+        ISerializationManager.InstantiationDelegate<MapId>? instanceProvider = null)
+    {
+        if (context is not EntitySerializer entSerializer)
+            return MapId.Nullspace;
+
+        var uid = serializationManager.Read<EntityUid>(node, context);
+        if (!entSerializer.EntMan.TryGetComponent(uid, out MapComponent? mapComp))
+            return MapId.Nullspace;
+
+        return mapComp.MapId;
+    }
+
+    public DataNode Write(
+        ISerializationManager serializationManager,
+        MapId value,
+        IDependencyCollection dependencies,
+        bool alwaysWrite = false,
+        ISerializationContext? context = null)
+    {
+        if (context is not EntitySerializer entSerializer)
+            return serializationManager.WriteValue(EntityUid.Invalid, alwaysWrite, context);
+
+        var uid = entSerializer.EntMan.System<SharedMapSystem>().GetMap(value);
+        return serializationManager.WriteValue(uid, alwaysWrite, context);
+    }
+}
---
+++ Robust.Shared/Serialization/TypeSerializers/Implementations/NetCoordinatesSerializer.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+using Robust.Shared.GameObjects;
+using Robust.Shared.IoC;
+using Robust.Shared.Map;
+using Robust.Shared.Serialization.Manager;
+using Robust.Shared.Serialization.Manager.Attributes;
+using Robust.Shared.Serialization.Markdown;
+using Robust.Shared.Serialization.Markdown.Mapping;
+using Robust.Shared.Serialization.Markdown.Validation;
+using Robust.Shared.Serialization.Markdown.Value;
+using Robust.Shared.Serialization.TypeSerializers.Interfaces;
+
+namespace Robust.Shared.Serialization.TypeSerializers.Implementations;
+
+[TypeSerializer]
+public sealed class NetCoordinatesSerializer : ITypeSerializer<NetCoordinates, MappingDataNode>
+{
+    public ValidationNode Validate(
+        ISerializationManager serializationManager,
+        MappingDataNode node,
+        IDependencyCollection dependencies,
+        ISerializationContext? context = null)
+    {
+        var mapping = new Dictionary<ValidationNode, ValidationNode>();
+
+        if (!node.TryGetValue("uid", out var uidNode))
+        {
+            mapping.Add(
+                new ErrorNode(new ValueDataNode(""), $"Failed to find uid key in {nameof(NetCoordinates)}!"),
+                new ErrorNode(new ValueDataNode(""), "Value must be a non-null scalar."));
+            return new ValidatedMappingNode(mapping);
+        }
+
+        if (!node.TryGetValue("pos", out var posNode))
+        {
+            mapping.Add(
+                new ErrorNode(new ValueDataNode(""), $"Failed to find position key in {nameof(NetCoordinates)}!"),
+                new ErrorNode(new ValueDataNode(""), "Value must be a non-null scalar."));
+            return new ValidatedMappingNode(mapping);
+        }
+
+        var validatedUid = serializationManager.ValidateNode<NetEntity>(uidNode, context);
+        var validatedPos = serializationManager.ValidateNode<Vector2>(posNode, context);
+
+        mapping.Add(new ValidatedValueNode(node.GetKeyNode("uid")), validatedUid);
+        mapping.Add(new ValidatedValueNode(node.GetKeyNode("pos")), validatedPos);
+
+        return new ValidatedMappingNode(mapping);
+    }
+
+    public NetCoordinates Read(
+        ISerializationManager serializationManager,
+        MappingDataNode node,
+        IDependencyCollection dependencies,
+        SerializationHookContext hookCtx,
+        ISerializationContext? context = null,
+        ISerializationManager.InstantiationDelegate<NetCoordinates>? instanceProvider = null)
+    {
+        var uid = NetEntity.Invalid;
+        var pos = Vector2.Zero;
+
+        if (node.TryGetValue("uid", out var uidNode))
+            uid = serializationManager.Read<NetEntity>(uidNode, context);
+
+        if (node.TryGetValue("pos", out var positionNode))
+            pos = serializationManager.Read<Vector2>(positionNode, hookCtx, context);
+
+        return new NetCoordinates(uid, pos);
+    }
+
+    public DataNode Write(
+        ISerializationManager serializationManager,
+        NetCoordinates value,
+        IDependencyCollection dependencies,
+        bool alwaysWrite = false,
+        ISerializationContext? context = null)
+    {
+        var seq = new MappingDataNode();
+
+        seq.Add("uid", serializationManager.WriteValue(value.NetEntity, alwaysWrite, context));
+        seq.Add("pos", serializationManager.WriteValue(value.Position, alwaysWrite, context));
+
+        return seq;
+    }
+}

250e5f10ec415c9a122f293d32ff191984bbf45c
ElectroJr <leonsfriedrich@gmail.com>
2025-09-13 00:06:33 +1200

    Add Box2Rotated.Transform Benchmark

---
+++ Robust.Benchmarks/NumericsHelpers/Box2RotatedBenchmark.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+using BenchmarkDotNet.Attributes;
+using Robust.Shared.Analyzers;
+using Robust.Shared.Maths;
+
+namespace Robust.Benchmarks.NumericsHelpers;
+
+[Virtual, DisassemblyDiagnoser]
+public class Box2RotatedBenchmark
+{
+    public Box2Rotated Box = new();
+
+    [Benchmark(Baseline = true)]
+    public Matrix3x2 GetTransformOld()
+    {
+        return Box.TransformOld;
+    }
+
+    [Benchmark]
+    public Matrix3x2 GetTransform()
+    {
+        return Box.Transform;
+    }
+}

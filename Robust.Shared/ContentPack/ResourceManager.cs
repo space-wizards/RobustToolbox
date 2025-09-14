e0ff1e83e227a0acb82c4775162179805c35f5a2
PJB3005 <pieterjan.briers+git@gmail.com>
2025-09-14 14:49:58 +0200

    Squashed commit of the following:
    commit d4f265c3143a053cffd181d22d5a5290b1044e35
    Author: PJB3005 <pieterjan.briers+git@gmail.com>
    Date:   Sun Sep 14 14:32:44 2025 +0200
    
        Fix incorrect path combine in DirLoader and WritableDirProvider
    
        This (and the other couple past commits) reported by Elelzedel.
    
    commit 7654d386120dadc7e217f003aafbedd7c92b46bb
    Author: PJB3005 <pieterjan.briers+git@gmail.com>
    Date:   Sat Sep 13 22:50:51 2025 +0200
    
        Move CEF cache out of data directory
    
        Don't want content messing with this...
    
    commit cdcc2551238f393f3a94668dc3228f09228d16e4
    Author: PJB3005 <pieterjan.briers+git@gmail.com>
    Date:   Sat Sep 13 19:11:16 2025 +0200
    
        Make Robust.Client.WebView.Cef.Program internal.
    
    commit 2f56a6a11024132db532a283a1c4f6db4beb688f
    Author: PJB3005 <pieterjan.briers+git@gmail.com>
    Date:   Sat Sep 13 19:10:46 2025 +0200
    
        Update SpaceWizards.NFluidSynth to 0.2.2
    
    commit 16fc48cef2e9cabf97cac4ddf4d6827adb09fe1a
    Author: PJB3005 <pieterjan.briers+git@gmail.com>
    Date:   Sat Sep 13 19:09:43 2025 +0200
    
        Hide IWritableDirProvider.RootDir on client
    
        This shouldn't be exposed.
    
    (cherry picked from commit 2f07159336bc640e41fbbccfdec4133a68c13bdb)
    (cherry picked from commit d6c3212c74373ed2420cc4be2cf10fcd899c2106)

--- Robust.Shared/ContentPack/ResourceManager.cs
+++ Robust.Shared/ContentPack/ResourceManager.cs
@@ -41,13 +41,13 @@ namespace Robust.Shared.ContentPack
         public IWritableDirProvider UserData { get; private set; } = default!;
 
         /// <inheritdoc />
-        public virtual void Initialize(string? userData)
+        public virtual void Initialize(string? userData, bool hideRootDir)
         {
             Sawmill = _logManager.GetSawmill("res");
 
             if (userData != null)
             {
-                UserData = new WritableDirProvider(Directory.CreateDirectory(userData));
+                UserData = new WritableDirProvider(Directory.CreateDirectory(userData), hideRootDir);
             }
             else
             {
@@ -378,6 +378,10 @@ namespace Robust.Shared.ContentPack
                 if (root is DirLoader loader)
                 {
                     var rootDir = loader.GetPath(new ResPath(@"/"));
+
+                    // TODO: GET RID OF THIS.
+                    // This code shouldn't be passing OS disk paths through ResPath.
+                    rootDir = rootDir.Replace(Path.DirectorySeparatorChar, '/');
 
                     yield return new ResPath(rootDir);
                 }

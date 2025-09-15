71390c99aa11a76cb88c6b3eb64eba976f5a734e
PJB3005 <pieterjan.briers+git@gmail.com>
2025-09-15 23:07:07 +0200

    Rewrite locking implementation.
    It is much smaller, less complicated and probably more robust too. Should be fully atomic (the previous one wasn't).

--- Robust.Client.WebView/Cef/WebViewManagerCef.Lock.cs
+++ Robust.Client.WebView/Cef/WebViewManagerCef.Lock.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.IO;
-using System.Linq;
 using Robust.Client.Utility;
 
 namespace Robust.Client.WebView.Cef;
@@ -16,171 +13,38 @@ internal sealed partial class WebViewManagerCef
 
     private string FindAndLockCacheDirectory()
     {
-        var finalAbsoluteCachePath = "";
-
         var rootDir = Path.Combine(UserDataDir.GetRootUserDataDir(_gameController), BaseCacheName);
 
-        try
+        for (var i = 0; i < MaxAttempts; i++)
         {
-            var existingCacheDirs = GetExistingCacheDirectories(rootDir);
-            existingCacheDirs.Sort();
+            var cacheDirPath = Path.Combine(rootDir, i.ToString());
 
-            // ReSharper disable once ForeachCanBePartlyConvertedToQueryUsingAnotherGetEnumerator
-            foreach (var relativeDirName in existingCacheDirs)
-            {
-                var absoluteDirPath = Path.Combine(rootDir, relativeDirName);
-
-                if (!Directory.Exists(absoluteDirPath)
-                    || !TryAcquireDirectoryLock(absoluteDirPath, out FileStream? lockStream)) continue;
-
-                _lockFileStream = lockStream;
-                finalAbsoluteCachePath = absoluteDirPath;
-                _sawmill.Debug($"Found and locked existing cache directory: {finalAbsoluteCachePath}");
-                break;
-            }
-
-            if (string.IsNullOrEmpty(finalAbsoluteCachePath))
-                finalAbsoluteCachePath = CreateLockNewCacheDir(rootDir);
-
-            return finalAbsoluteCachePath;
+            if (TryLockCacheDir(i, cacheDirPath))
+                return cacheDirPath;
         }
-        catch (Exception ex)
-        {
-            throw new InvalidOperationException("Failed to find or create cache directory", ex);
-        }
+
+        throw new Exception("Unable to locate available CEF cache directory!");
     }
 
-    private List<string> GetExistingCacheDirectories(string rootDir)
+    private bool TryLockCacheDir(int attempt, string path)
     {
-        List<string> existingCacheDirs = new();
+        _sawmill.Verbose($"Trying to lock cache directory {attempt}");
+
+        // Does not fail if directory already exists.
+        Directory.CreateDirectory(path);
+
+        var lockFilePath = Path.Combine(path, LockFileName);
 
         try
         {
-            // ReSharper disable once LoopCanBeConvertedToQuery
-            foreach (var entryName in Directory.EnumerateDirectories(rootDir))
-                if (Directory.Exists(Path.Combine(rootDir, entryName)))
-                    existingCacheDirs.Add(entryName);
+            var file = File.Open(lockFilePath, FileMode.Create, FileAccess.ReadWrite, FileShare.None);
+            _lockFileStream = file;
+            _sawmill.Debug($"Successfully locked CEF cache directory {attempt}");
+            return true;
         }
         catch (IOException ex)
         {
-            _sawmill.Warning($"Failed to enumerate cache directories: {ex.Message}");
-        }
-
-        return existingCacheDirs;
-    }
-
-    private bool TryAcquireDirectoryLock(string directoryPath, [NotNullWhen(true)] out FileStream? lockStream)
-    {
-        lockStream = null;
-        var lockFilePath = Path.Combine(directoryPath, LockFileName);
-
-        try
-        {
-            if (File.Exists(lockFilePath))
-            {
-                if (IsLockFileValid(lockFilePath))
-                {
-                    _sawmill.Debug($"Cache directory {directoryPath} is locked by active process");
-                    return false;
-                }
-
-                _sawmill.Debug($"Removing stale lock file: {lockFilePath}");
-                File.Delete(lockFilePath);
-            }
-
-            lockStream = new FileStream(lockFilePath,
-                FileMode.CreateNew,
-                FileAccess.ReadWrite,
-                FileShare.None,
-                4096,
-                FileOptions.DeleteOnClose);
-
-            return true;
-        }
-        catch (IOException)
-        {
-            lockStream?.Dispose();
-            lockStream = null;
-            return false;
-        }
-    }
-
-    // Check if this file is actually locked
-    private bool IsLockFileValid(string lockFilePath)
-    {
-        try
-        {
-            using FileStream testStream = new(lockFilePath, FileMode.Open, FileAccess.Read, FileShare.None);
-            return false;
-        }
-        catch (IOException)
-        {
-            return true;
-        }
-        catch (UnauthorizedAccessException)
-        {
-            return true;
-        }
-    }
-
-    private string CreateLockNewCacheDir(string rootDir)
-    {
-        for (var attempts = 0; attempts < MaxAttempts; attempts++)
-        {
-            var newRelativeCacheDir = attempts.ToString();
-            var absolutePath = Path.Combine(rootDir, newRelativeCacheDir);
-
-            try
-            {
-                if (!TryCreateLockDir(absolutePath, out var lockStream))
-                    continue;
-
-                _lockFileStream = lockStream;
-                _sawmill.Debug($"Created and locked new cache directory: {absolutePath}");
-                return absolutePath;
-            }
-            catch (Exception ex)
-            {
-                _sawmill.Warning($"Failed to create directory {absolutePath}: {ex.Message}");
-            }
-        }
-
-        throw new InvalidOperationException($"Failed to create any cache directory after {MaxAttempts} attempts");
-    }
-
-    private bool TryCreateLockDir(string directoryPath, [NotNullWhen(true)] out FileStream? lockStream)
-    {
-        lockStream = null;
-        var lockFilePath = Path.Combine(directoryPath, LockFileName);
-
-        try
-        {
-            if (!Directory.Exists(directoryPath)) Directory.CreateDirectory(directoryPath);
-
-            lockStream = new FileStream(lockFilePath,
-                FileMode.CreateNew,
-                FileAccess.ReadWrite,
-                FileShare.None,
-                4096,
-                FileOptions.DeleteOnClose);
-
-            return true;
-        }
-        catch (IOException)
-        {
-            lockStream?.Dispose();
-            lockStream = null;
-
-            try
-            {
-                if (Directory.Exists(directoryPath) && !Directory.EnumerateFileSystemEntries(directoryPath).Any())
-                    Directory.Delete(directoryPath);
-            }
-            catch
-            {
-                // ignored
-            }
-
+            _sawmill.Error($"Failed to lock cache directory {attempt}: {ex}");
             return false;
         }
     }

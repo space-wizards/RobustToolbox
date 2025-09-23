b9ff03510d82b86d018ecd484e87f7d997f94eba
Centronias <me@centronias.com>
2025-09-23 14:46:01 -0700

    rewritten to be more performant

---
+++ Robust.Shared/Analyzers/EntitySystemSubscriptionsGeneratorAttributes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using JetBrains.Annotations;
+using Robust.Shared.GameObjects;
+
+namespace Robust.Shared.Analyzers;
+
+[AttributeUsage(AttributeTargets.Method)]
+public sealed class CallAfterSubscriptionsAttribute : Attribute;
+
+[AttributeUsage(AttributeTargets.Method)]
+public sealed class LocalEventSubscriptionAttribute : Attribute;
+
+[AttributeUsage(AttributeTargets.Method)]
+public sealed class NetworkEventSubscriptionAttribute : Attribute;
+
+[AttributeUsage(AttributeTargets.Method)]
+public sealed class EventSubscriptionAttribute : Attribute;

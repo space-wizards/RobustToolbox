4ab61b840a7c4210ead39eb9780735c96df06d36
PJB3005 <pieterjan.briers+git@gmail.com>
2025-09-26 15:02:57 +0200

    Remove compat mode forcing
    Was broken and I'm moving it to the launcher

--- Robust.Client/Graphics/Clyde/Clyde.GLContext.cs
+++ Robust.Client/Graphics/Clyde/Clyde.GLContext.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Runtime.InteropServices;
 using Robust.Shared;
-#if WINDOWS
-using TerraFX.Interop.Windows;
-using TerraFX.Interop.DirectX;
-#endif
 
 namespace Robust.Client.Graphics.Clyde
 {
@@ -17,8 +12,6 @@ namespace Robust.Client.Graphics.Clyde
 
         private void InitGLContextManager()
         {
-            CheckForceCompatMode();
-
             // Advanced GL contexts currently disabled due to lack of testing etc.
             if (OperatingSystem.IsWindows() && _cfg.GetCVar(CVars.DisplayAngle))
             {
@@ -60,74 +53,6 @@ namespace Robust.Client.Graphics.Clyde
 
             _glContext = new GLContextWindow(this);
         }
-
-        private void CheckForceCompatMode()
-        {
-#if WINDOWS
-            // Qualcomm (Snapdragon/Adreno) devices have broken OpenGL drivers on Windows.
-
-            if (CheckIsQualcommDevice())
-            {
-                _sawmillOgl.Info("We appear to be on a Qualcomm device. Enabling compat mode due to broken OpenGL driver");
-                _cfg.OverrideDefault(CVars.DisplayCompat, true);
-            }
-#endif
-        }
-
-#if WINDOWS
-        private static unsafe bool CheckIsQualcommDevice()
-        {
-            // Ideally we would check the OpenGL driver instead... but OpenGL is terrible so that's impossible.
-            // Let's just check with DXGI instead.
-
-            IDXGIFactory1* dxgiFactory;
-            ThrowIfFailed(
-                nameof(DirectX.CreateDXGIFactory1),
-                DirectX.CreateDXGIFactory1(Windows.__uuidof<IDXGIFactory1>(), (void**) &dxgiFactory));
-
-            try
-            {
-                uint idx = 0;
-                IDXGIAdapter* adapter;
-                while (dxgiFactory->EnumAdapters(idx, &adapter) != DXGI.DXGI_ERROR_NOT_FOUND)
-                {
-                    try
-                    {
-                        DXGI_ADAPTER_DESC desc;
-                        ThrowIfFailed("GetDesc", adapter->GetDesc(&desc));
-
-                        var descString = ((ReadOnlySpan<char>)desc.Description).TrimEnd('\0');
-                        if (descString.Contains("qualcomm", StringComparison.OrdinalIgnoreCase) ||
-                            descString.Contains("snapdragon", StringComparison.OrdinalIgnoreCase) ||
-                            descString.Contains("adreno", StringComparison.OrdinalIgnoreCase))
-                        {
-                            return true;
-                        }
-                    }
-                    finally
-                    {
-                        adapter->Release();
-                    }
-
-                    idx += 1;
-                }
-            }
-            finally
-            {
-                dxgiFactory->Release();
-            }
-
-            return false;
-        }
-
-        private static void ThrowIfFailed(string methodName, HRESULT hr)
-        {
-            if (Windows.FAILED(hr))
-            {
-                Marshal.ThrowExceptionForHR(hr);
-            }
-        }
-#endif
 
         private struct GLContextSpec
         {

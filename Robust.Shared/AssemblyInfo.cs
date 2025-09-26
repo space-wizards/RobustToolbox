a3756c29bdd6f6e41dee49bb28b434aa1ef17666
PJB3005 <pieterjan.briers+git@gmail.com>
2025-09-26 14:15:14 +0200

    Merge duplicate AssemblyInfo.cs files in Robust.Shared

--- Robust.Shared/AssemblyInfo.cs
+++ Robust.Shared/AssemblyInfo.cs
@@ -1,3 +1,18 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Robust.UnitTesting", AllInternalsVisible = true)]
+// The following allows another friend assembly access to the types marked as internal.
+// SS14 engine assemblies are friends.
+// This way internal is "Content can't touch this".
+[assembly: InternalsVisibleTo("Robust.Server")]
+[assembly: InternalsVisibleTo("Robust.Client")]
+[assembly: InternalsVisibleTo("Robust.Lite")]
+[assembly: InternalsVisibleTo("Robust.UnitTesting")]
+[assembly: InternalsVisibleTo("OpenToolkit.GraphicsLibraryFramework")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")] // Gives access to Castle(Moq)
+[assembly: InternalsVisibleTo("Robust.Benchmarks")]
+[assembly: InternalsVisibleTo("Robust.Client.WebView")]
+[assembly: InternalsVisibleTo("Robust.Packaging")]
+
+#if NET5_0_OR_GREATER
+[module: SkipLocalsInit]
+#endif

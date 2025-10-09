408a3ad6fc6496a4f96aa6ce4ab8aaed61d7736a
ElectroJr <leonsfriedrich@gmail.com>
2025-10-09 21:35:26 +1300

    fix DebugLightTreeSystem

--- Robust.Client/GameObjects/EntitySystems/DebugLightTreeSystem.cs
+++ Robust.Client/GameObjects/EntitySystems/DebugLightTreeSystem.cs
@@ -1,7 +1,6 @@
 #if DEBUG
 using Robust.Client.ComponentTrees;
 using Robust.Client.Graphics;
-using Robust.Shared.ComponentTrees;
 using Robust.Shared.Enums;
 using Robust.Shared.GameObjects;
 using Robust.Shared.IoC;
@@ -30,7 +29,7 @@ namespace Robust.Client.GameObjects
                         EntityManager.System<EntityLookupSystem>(),
                         IoCManager.Resolve<IEyeManager>(),
                         IoCManager.Resolve<IMapManager>(),
-                        EntityManager.System<SharedLightTreeSystem>());
+                        EntityManager.System<LightTreeSystem>());
 
                     overlayManager.AddOverlay(_lightOverlay);
                 }
@@ -50,7 +49,7 @@ namespace Robust.Client.GameObjects
             private IEyeManager _eyeManager;
             private IMapManager _mapManager;
 
-            private SharedLightTreeSystem _trees;
+            private LightTreeSystem _trees;
 
             public override OverlaySpace Space => OverlaySpace.WorldSpace;
 

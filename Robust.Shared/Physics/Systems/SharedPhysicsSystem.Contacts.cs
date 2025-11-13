cd590270899e715b59da4ac159c9e8bfe52b0a73
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2025-11-13 21:28:13 +1100

    Double-buffer contact events (#6295)
    * Add
    
    * a
    
    * Fix test
    
    * note
    
    * Fix reflection

--- Robust.Shared/Physics/Systems/SharedPhysicsSystem.Contacts.cs
+++ Robust.Shared/Physics/Systems/SharedPhysicsSystem.Contacts.cs
@@ -353,8 +353,8 @@ public abstract partial class SharedPhysicsSystem
         {
             var ev1 = new EndCollideEvent(aUid, bUid, contact.FixtureAId, contact.FixtureBId, fixtureA, fixtureB, bodyA, bodyB);
             var ev2 = new EndCollideEvent(bUid, aUid, contact.FixtureBId, contact.FixtureAId, fixtureB, fixtureA, bodyB, bodyA);
-            RaiseLocalEvent(aUid, ref ev1);
-            RaiseLocalEvent(bUid, ref ev2);
+            _endCollideEvents[_endEventIndex].Add(ev1);
+            _endCollideEvents[_endEventIndex].Add(ev2);
         }
 
         if (contact.Manifold.PointCount > 0 && contact.FixtureA?.Hard == true && contact.FixtureB?.Hard == true)
@@ -603,8 +603,9 @@ public abstract partial class SharedPhysicsSystem
                 var ev1 = new StartCollideEvent(uidA, uidB, contact.FixtureAId, contact.FixtureBId, fixtureA, fixtureB, bodyA, bodyB, points, contact.Manifold.PointCount, worldNormal);
                 var ev2 = new StartCollideEvent(uidB, uidA, contact.FixtureBId, contact.FixtureAId, fixtureB, fixtureA, bodyB, bodyA, points, contact.Manifold.PointCount, worldNormal);
 
-                RaiseLocalEvent(uidA, ref ev1, true);
-                RaiseLocalEvent(uidB, ref ev2, true);
+                _startCollideEvents.Add(ev1);
+                _startCollideEvents.Add(ev2);
+
                 break;
             }
             case ContactStatus.Touching:
@@ -626,8 +627,8 @@ public abstract partial class SharedPhysicsSystem
                 var ev1 = new EndCollideEvent(uidA, uidB, contact.FixtureAId, contact.FixtureBId, fixtureA, fixtureB, bodyA, bodyB);
                 var ev2 = new EndCollideEvent(uidB, uidA, contact.FixtureBId, contact.FixtureAId, fixtureB, fixtureA, bodyB, bodyA);
 
-                RaiseLocalEvent(uidA, ref ev1);
-                RaiseLocalEvent(uidB, ref ev2);
+                _endCollideEvents[_endEventIndex].Add(ev1);
+                _endCollideEvents[_endEventIndex].Add(ev2);
                 break;
             }
             case ContactStatus.NoContact:

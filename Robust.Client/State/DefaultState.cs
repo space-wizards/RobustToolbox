b79c7d14c7e690ac9d7aa4f0ef4113a2d44b3930
PJB3005 <pieterjan.briers+git@gmail.com>
2025-11-25 13:22:56 +0100

    Mark DefaultState as ContentAccessAllowed

--- Robust.Client/State/DefaultState.cs
+++ Robust.Client/State/DefaultState.cs
@@ -1,6 +1,9 @@
+using Robust.Shared.ContentPack;
+
 namespace Robust.Client.State
 {
     // Dummy state that is only used to make sure there always is *a* state.
+    [ContentAccessAllowed]
     public sealed class DefaultState : State
     {
         protected override void Startup()

980296393379b81a77870e72a9c02b7991995fbb
PJB3005 <pieterjan.briers+git@gmail.com>
2025-12-20 14:11:36 +0100

    Add [NotContentImplementable] attribute

---
+++ Robust.Shared/Analyzers/NotContentImplementable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Robust.Shared.Analyzers;
+
+/// <summary>
+/// Marker attribute specifying that content <b>must not</b> implement this interface itself.
+/// </summary>
+/// <remarks>
+/// Interfaces with this attribute may have members added by the engine at any time,
+/// so implementing them yourself would not be API-stable.
+/// </remarks>
+[AttributeUsage(AttributeTargets.Interface)]
+public sealed class NotContentImplementableAttribute : Attribute;

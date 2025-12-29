f09910e35b583d17d38df3616a7c94e0b0f52ff1
DrSmugleaf <drsmugleaf@gmail.com>
2025-12-28 23:18:43 -0800

    Fix missing interface method

--- Robust.Shared/Network/IHttpManager.cs
+++ Robust.Shared/Network/IHttpManager.cs
@@ -5,5 +5,6 @@ public interface IHttpManager
     Task<Stream> GetStreamAsync(Uri uri, CancellationToken cancel);
 
     Task<string> GetStringAsync(Uri uri, CancellationToken cancel);
-}
 
+    Task<T?> GetFromJsonAsync<T>(Uri uri, CancellationToken cancel = default);
+}

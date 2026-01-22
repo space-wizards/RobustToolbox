a682f5cd418819a20531124f6371deed3a9ffb91
DrSmugleaf <drsmugleaf@gmail.com>
2026-01-22 00:39:10 -0800

    Private address check, add tests for both cases

--- Robust.Shared/Network/HttpManager.cs
+++ Robust.Shared/Network/HttpManager.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.IO;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Net.Sockets;
+using System.Runtime.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -18,16 +21,59 @@ public sealed class HttpManager : IHttpManagerInternal
 
     public async Task<Stream> GetStreamAsync(Uri uri, CancellationToken cancel = default)
     {
+        // Uri can be inherited which means it could be inherited by the user
+        // Am I going to check if that means they could modify it after the local address check?
+        // No, so we copy the original string instead just in case
+        // !!FUN!!
+        uri = new Uri(uri.OriginalString);
+        ThrowIfLocalAddress(uri);
         return await _client.GetStreamAsync(uri, cancel);
     }
 
     public async Task<string> GetStringAsync(Uri uri, CancellationToken cancel = default)
     {
+        uri = new Uri(uri.OriginalString);
+        ThrowIfLocalAddress(uri);
         return await _client.GetStringAsync(uri, cancel);
     }
 
     public async Task<T?> GetFromJsonAsync<T>(Uri uri, CancellationToken cancel = default)
     {
+        uri = new Uri(uri.OriginalString);
+        ThrowIfLocalAddress(uri);
         return await _client.GetFromJsonAsync<T>(uri, cancel);
+    }
+
+    private void ThrowIfLocalAddress(Uri uri)
+    {
+        if (!IPAddress.TryParse(uri.Host, out var ip))
+            return;
+
+        if (IPAddress.IsLoopback(ip) ||
+            ip.AddressFamily == AddressFamily.InterNetwork ||
+            ip.AddressFamily == AddressFamily.InterNetworkV6)
+        {
+            throw new InvalidAddressException($"{uri} is a local address");
+        }
+    }
+}
+
+public sealed class InvalidAddressException : ArgumentException
+{
+    internal InvalidAddressException()
+    {
+    }
+
+    [Obsolete("This API supports obsolete formatter-based serialization. It should not be called or extended by application code.")]
+    internal InvalidAddressException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+
+    internal InvalidAddressException(string? message) : base(message)
+    {
+    }
+
+    internal InvalidAddressException(string? message, Exception? innerException) : base(message, innerException)
+    {
     }
 }

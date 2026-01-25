4c6680432f7d217aaaa10516ff5971000aa9eb40
DrSmugleaf <drsmugleaf@gmail.com>
2026-01-25 04:44:23 -0800

    Fix EntMan resolve

--- Robust.Shared/GameObjects/Components/UserInterface/BoundUserInterface.cs
+++ Robust.Shared/GameObjects/Components/UserInterface/BoundUserInterface.cs
@@ -32,6 +32,7 @@ namespace Robust.Shared.GameObjects
 
         protected BoundUserInterface(EntityUid owner, Enum uiKey)
         {
+            IoCManager.Resolve(ref EntMan);
             EntMan.EntitySysManager.DependencyCollection.InjectDependencies(this);
             UiSystem = EntMan.System<SharedUserInterfaceSystem>();
 

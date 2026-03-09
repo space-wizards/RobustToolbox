b51a1f01830d5a6a6527c3a60e4728e2b4e95bfa
kaylie <moony@hellomouse.net>
2026-03-09 21:10:45 +0100

    Fix API + Tests.

---
+++ Robust.Shared.IntegrationTests/GameObjects/EntityManagerSingletonTests.cs
@@ -0,0 +1,119 @@
+using NUnit.Framework;
+using Robust.Shared.GameObjects;
+using Robust.Shared.IoC;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.Manager;
+
+namespace Robust.UnitTesting.Shared.GameObjects;
+
+[TestFixture, TestOf(typeof(EntityManager))]
+internal sealed class EntityManagerSingletonTests : OurRobustUnitTest
+{
+    private const string TestSingleton1 = "T_TestSingleton1";
+
+    private const string Prototypes = $"""
+        - type: entity
+          id: {TestSingleton1}
+          components:
+          - type: Marker1
+          - type: Marker2
+          - type: Marker3
+          - type: Marker4
+        """;
+
+    protected override Type[]? ExtraComponents =>
+    [
+        typeof(Marker1Component), typeof(Marker2Component), typeof(Marker3Component), typeof(Marker4Component)
+    ];
+
+    private PrototypeManager _protoMan = default!;
+    private IEntityManager _entMan = default!;
+
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        IoCManager.Resolve<ISerializationManager>().Initialize();
+
+        _protoMan = (PrototypeManager) IoCManager.Resolve<IPrototypeManager>();
+        _protoMan.RegisterKind(typeof(EntityPrototype), typeof(EntityCategoryPrototype));
+        _protoMan.LoadString(Prototypes);
+        _protoMan.ResolveResults();
+
+        _entMan = IoCManager.Resolve<IEntityManager>();
+    }
+
+    [Test]
+    public void SingleEntity()
+    {
+        var mySingle = _entMan.Spawn(TestSingleton1);
+
+        var single1 = _entMan.Single<Marker1Component>();
+        var single2 = _entMan.Single<Marker1Component, Marker2Component>();
+        var single3 = _entMan.Single<Marker1Component, Marker2Component, Marker3Component>();
+        var single4 = _entMan.Single<Marker1Component, Marker2Component, Marker3Component, Marker4Component>();
+
+        Assert.That(mySingle, NUnit.Framework.Is.EqualTo(single1.Owner));
+        Assert.That(mySingle, NUnit.Framework.Is.EqualTo(single2.Owner));
+        Assert.That(mySingle, NUnit.Framework.Is.EqualTo(single3.Owner));
+        Assert.That(mySingle, NUnit.Framework.Is.EqualTo(single4.Owner));
+
+        var bonusSingle = _entMan.Spawn(TestSingleton1);
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.Throws<NonUniqueSingletonException>(() => _entMan.Single<Marker1Component>());
+            Assert.Throws<NonUniqueSingletonException>(() => _entMan.Single<Marker1Component, Marker2Component>());
+            Assert.Throws<NonUniqueSingletonException>(() =>
+                _entMan.Single<Marker1Component, Marker2Component, Marker3Component>());
+            Assert.Throws<NonUniqueSingletonException>(() =>
+                _entMan.Single<Marker1Component, Marker2Component, Marker3Component, Marker4Component>());
+            Assert.Throws<NonUniqueSingletonException>(() => _entMan.TrySingle<Marker1Component>(out _));
+            Assert.Throws<NonUniqueSingletonException>(() =>
+                _entMan.TrySingle<Marker1Component, Marker2Component>(out _));
+            Assert.Throws<NonUniqueSingletonException>(() =>
+                _entMan.TrySingle<Marker1Component, Marker2Component, Marker3Component>(out _));
+            Assert.Throws<NonUniqueSingletonException>(() =>
+                _entMan.TrySingle<Marker1Component, Marker2Component, Marker3Component, Marker4Component>(out _));
+        }
+
+        _entMan.DeleteEntity(bonusSingle);
+        _entMan.DeleteEntity(mySingle);
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.Throws<MatchNotFoundException>(() => _entMan.Single<Marker1Component>());
+            Assert.Throws<MatchNotFoundException>(() => _entMan.Single<Marker1Component, Marker2Component>());
+            Assert.Throws<MatchNotFoundException>(() =>
+                _entMan.Single<Marker1Component, Marker2Component, Marker3Component>());
+            Assert.Throws<MatchNotFoundException>(() =>
+                _entMan.Single<Marker1Component, Marker2Component, Marker3Component, Marker4Component>());
+            Assert.That(_entMan.TrySingle<Marker1Component>(out _), NUnit.Framework.Is.False);
+            Assert.That(_entMan.TrySingle<Marker1Component, Marker2Component>(out _), NUnit.Framework.Is.False);
+            Assert.That(_entMan.TrySingle<Marker1Component, Marker2Component, Marker3Component>(out _),
+                NUnit.Framework.Is.False);
+            Assert.That(
+                _entMan.TrySingle<Marker1Component, Marker2Component, Marker3Component, Marker4Component>(out _),
+                NUnit.Framework.Is.False);
+        }
+    }
+}
+
+internal sealed partial class Marker1Component : Component
+{
+
+}
+
+internal sealed partial class Marker2Component : Component
+{
+
+}
+
+internal sealed partial class Marker3Component : Component
+{
+
+}
+
+internal sealed partial class Marker4Component : Component
+{
+
+}
---
+++ Robust.Shared/GameObjects/IEntityManager.Single.cs
@@ -0,0 +1,280 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Robust.Shared.Map;
+using Robust.Shared.Prototypes;
+
+namespace Robust.Shared.GameObjects;
+
+public partial interface IEntityManager
+{
+    /// <summary>
+    ///     Gets the sole entity with the given component.
+    /// </summary>
+    /// <typeparam name="TComp1">The component to look for as a tag.</typeparam>
+    /// <returns>The singleton entity.</returns>
+    /// <exception cref="NonUniqueSingletonException">Thrown when multiple entities match the request.</exception>
+    /// <exception cref="MatchNotFoundException">Thrown when no entities match the request.</exception>
+    public Entity<TComp1> Single<TComp1>()
+        where TComp1 : IComponent;
+
+    /// <summary>
+    ///     Gets the sole entity with the given components.
+    /// </summary>
+    /// <typeparam name="TComp1">The first component to look for as a tag.</typeparam>
+    /// <typeparam name="TComp2">The second component to look for as a tag.</typeparam>
+    /// <returns>The singleton entity.</returns>
+    /// <exception cref="NonUniqueSingletonException">Thrown when multiple entities match the request.</exception>
+    /// <exception cref="MatchNotFoundException">Thrown when no entities match the request.</exception>
+    /// <remarks>
+    ///     The least common component should be put in <typeparamref name="TComp1"/> for query performance.
+    /// </remarks>
+    public Entity<TComp1, TComp2> Single<TComp1, TComp2>()
+        where TComp1 : IComponent
+        where TComp2 : IComponent;
+
+    /// <summary>
+    ///     Gets the sole entity with the given components.
+    /// </summary>
+    /// <typeparam name="TComp1">The first component to look for as a tag.</typeparam>
+    /// <typeparam name="TComp2">The second component to look for as a tag.</typeparam>
+    /// <typeparam name="TComp3">The third component to look for as a tag.</typeparam>
+    /// <returns>The singleton entity.</returns>
+    /// <exception cref="NonUniqueSingletonException">Thrown when multiple entities match the request.</exception>
+    /// <exception cref="MatchNotFoundException">Thrown when no entities match the request.</exception>
+    /// <remarks>
+    ///     The least common component should be put in <typeparamref name="TComp1"/> for query performance.
+    /// </remarks>
+    public Entity<TComp1, TComp2, TComp3> Single<TComp1, TComp2, TComp3>()
+        where TComp1 : IComponent
+        where TComp2 : IComponent
+        where TComp3 : IComponent;
+
+    /// <summary>
+    ///     Gets the sole entity with the given components.
+    /// </summary>
+    /// <typeparam name="TComp1">The first component to look for as a tag.</typeparam>
+    /// <typeparam name="TComp2">The second component to look for as a tag.</typeparam>
+    /// <typeparam name="TComp3">The third component to look for as a tag.</typeparam>
+    /// <typeparam name="TComp4">The third component to look for as a tag.</typeparam>
+    /// <returns>The singleton entity.</returns>
+    /// <exception cref="NonUniqueSingletonException">Thrown when multiple entities match the request.</exception>
+    /// <exception cref="MatchNotFoundException">Thrown when no entities match the request.</exception>
+    /// <remarks>
+    ///     The least common component should be put in <typeparamref name="TComp1"/> for query performance.
+    /// </remarks>
+    public Entity<TComp1, TComp2, TComp3, TComp4> Single<TComp1, TComp2, TComp3, TComp4>()
+        where TComp1 : IComponent
+        where TComp2 : IComponent
+        where TComp3 : IComponent
+        where TComp4 : IComponent;
+
+    /// <summary>
+    ///     Gets the sole entity with the given component, if it exists. Still throws if there's more than one.
+    /// </summary>
+    /// <param name="entity">The singleton entity, if any.</param>
+    /// <typeparam name="TComp1">The component to look for as a tag.</typeparam>
+    /// <exception cref="NonUniqueSingletonException">Thrown when multiple entities match the request.</exception>
+    /// <returns>Success.</returns>
+    public bool TrySingle<TComp1>([NotNullWhen(true)] out Entity<TComp1>? entity)
+        where TComp1 : IComponent;
+
+    /// <summary>
+    ///     Gets the sole entity with the given components, if one exists, or returns if one does not.
+    /// </summary>
+    /// <param name="entity">The singleton entity, if any.</param>
+    /// <typeparam name="TComp1">The first component to look for as a tag.</typeparam>
+    /// <typeparam name="TComp2">The second component to look for as a tag.</typeparam>
+    /// <returns>Success.</returns>
+    /// <exception cref="NonUniqueSingletonException">Thrown when multiple entities match the request.</exception>
+    /// <remarks>
+    ///     The least common component should be put in <typeparamref name="TComp1"/> for query performance.
+    /// </remarks>
+    public bool TrySingle<TComp1, TComp2>([NotNullWhen(true)] out Entity<TComp1, TComp2>? entity)
+        where TComp1 : IComponent
+        where TComp2 : IComponent;
+
+    /// <summary>
+    ///     Gets the sole entity with the given components, if one exists, or returns if one does not.
+    /// </summary>
+    /// <param name="entity">The singleton entity, if any.</param>
+    /// <typeparam name="TComp1">The first component to look for as a tag.</typeparam>
+    /// <typeparam name="TComp2">The second component to look for as a tag.</typeparam>
+    /// <typeparam name="TComp3">The third component to look for as a tag.</typeparam>
+    /// <returns>Success.</returns>
+    /// <exception cref="NonUniqueSingletonException">Thrown when multiple entities match the request.</exception>
+    /// <remarks>
+    ///     The least common component should be put in <typeparamref name="TComp1"/> for query performance.
+    /// </remarks>
+    public bool TrySingle<TComp1, TComp2, TComp3>([NotNullWhen(true)] out Entity<TComp1, TComp2, TComp3>? entity)
+        where TComp1 : IComponent
+        where TComp2 : IComponent
+        where TComp3 : IComponent;
+
+    /// <summary>
+    ///     Gets the sole entity with the given components, if one exists, or returns if one does not.
+    /// </summary>
+    /// <param name="entity">The singleton entity, if any.</param>
+    /// <typeparam name="TComp1">The first component to look for as a tag.</typeparam>
+    /// <typeparam name="TComp2">The second component to look for as a tag.</typeparam>
+    /// <typeparam name="TComp3">The third component to look for as a tag.</typeparam>
+    /// <typeparam name="TComp4">The fourth component to look for as a tag.</typeparam>
+    /// <returns>Success.</returns>
+    /// <exception cref="NonUniqueSingletonException">Thrown when multiple entities match the request.</exception>
+    /// <remarks>
+    ///     The least common component should be put in <typeparamref name="TComp1"/> for query performance.
+    /// </remarks>
+    public bool TrySingle<TComp1, TComp2, TComp3, TComp4>(
+        [NotNullWhen(true)] out Entity<TComp1, TComp2, TComp3, TComp4>? entity)
+        where TComp1 : IComponent
+        where TComp2 : IComponent
+        where TComp3 : IComponent
+        where TComp4 : IComponent;
+
+    /// <summary>
+    ///     Gets the sole entity with the given component, or spawns it at the given location if it doesn't exist.
+    /// </summary>
+    /// <param name="fallback">The fallback prototype to spawn on failure.</param>
+    /// <param name="location">The location to spawn the singleton. Nullspace works.</param>
+    /// <typeparam name="TComp1">The component to look for as a tag.</typeparam>
+    /// <exception cref="NonUniqueSingletonException">Thrown when multiple entities match the request.</exception>
+    /// <exception cref="MatchNotFoundException">Thrown when no entities match the request after spawning the fallback.</exception>
+    /// <returns>The singleton entity.</returns>
+    /// <remarks>
+    ///     This does not return the entity it spawns, it tries to look it up, so if spawning that entity violates
+    ///     singleton status (or it lacks the necessary component) this will throw immediately after instead of later
+    ///     on next call.
+    ///
+    ///     This will also still throw <see cref="MatchNotFoundException"/> if the spawned entity doesn't match at all.
+    /// </remarks>
+    public Entity<TComp1> SingleOrSpawn<TComp1>(EntProtoId fallback, MapCoordinates location)
+        where TComp1 : IComponent;
+
+    /// <summary>
+    ///     Gets the sole entity with the given component, or spawns it at the given location if it doesn't exist.
+    /// </summary>
+    /// <param name="fallback">The fallback prototype to spawn on failure.</param>
+    /// <param name="location">The location to spawn the singleton. Nullspace works.</param>
+    /// <typeparam name="TComp1">The first component to look for as a tag.</typeparam>
+    /// <typeparam name="TComp2">The second component to look for as a tag.</typeparam>
+    /// <exception cref="NonUniqueSingletonException">Thrown when multiple entities match the request.</exception>
+    /// <exception cref="MatchNotFoundException">Thrown when no entities match the request after spawning the fallback.</exception>
+    /// <returns>The singleton entity.</returns>
+    /// <remarks>
+    ///     This does not return the entity it spawns, it tries to look it up, so if spawning that entity violates
+    ///     singleton status (or it lacks the necessary component) this will throw immediately after instead of later
+    ///     on next call.
+    ///
+    ///     This will also still throw <see cref="MatchNotFoundException"/> if the spawned entity doesn't match at all.
+    /// </remarks>
+    public Entity<TComp1, TComp2> SingleOrSpawn<TComp1, TComp2>(EntProtoId fallback, MapCoordinates location)
+        where TComp1 : IComponent
+        where TComp2 : IComponent;
+
+    /// <summary>
+    ///     Gets the sole entity with the given component, or spawns it at the given location if it doesn't exist.
+    /// </summary>
+    /// <param name="fallback">The fallback prototype to spawn on failure.</param>
+    /// <param name="location">The location to spawn the singleton. Nullspace works.</param>
+    /// <typeparam name="TComp1">The first component to look for as a tag.</typeparam>
+    /// <typeparam name="TComp2">The second component to look for as a tag.</typeparam>
+    /// <typeparam name="TComp3">The third component to look for as a tag.</typeparam>
+    /// <exception cref="NonUniqueSingletonException">Thrown when multiple entities match the request.</exception>
+    /// <exception cref="MatchNotFoundException">Thrown when no entities match the request after spawning the fallback.</exception>
+    /// <returns>The singleton entity.</returns>
+    /// <remarks>
+    ///     This does not return the entity it spawns, it tries to look it up, so if spawning that entity violates
+    ///     singleton status (or it lacks the necessary component) this will throw immediately after instead of later
+    ///     on next call.
+    ///
+    ///     This will also still throw <see cref="MatchNotFoundException"/> if the spawned entity doesn't match at all.
+    /// </remarks>
+    public Entity<TComp1, TComp2, TComp3> SingleOrSpawn<TComp1, TComp2, TComp3>(
+        EntProtoId fallback,
+        MapCoordinates location)
+        where TComp1 : IComponent
+        where TComp2 : IComponent
+        where TComp3 : IComponent;
+
+    /// <summary>
+    ///     Gets the sole entity with the given component, or spawns it at the given location if it doesn't exist.
+    /// </summary>
+    /// <param name="fallback">The fallback prototype to spawn on failure.</param>
+    /// <param name="location">The location to spawn the singleton. Nullspace works.</param>
+    /// <typeparam name="TComp1">The first component to look for as a tag.</typeparam>
+    /// <typeparam name="TComp2">The second component to look for as a tag.</typeparam>
+    /// <typeparam name="TComp3">The third component to look for as a tag.</typeparam>
+    /// <typeparam name="TComp4">The fourth component to look for as a tag.</typeparam>
+    /// <exception cref="NonUniqueSingletonException">Thrown when multiple entities match the request.</exception>
+    /// <exception cref="MatchNotFoundException">Thrown when no entities match the request after spawning the fallback.</exception>
+    /// <returns>The singleton entity.</returns>
+    /// <remarks>
+    ///     This does not return the entity it spawns, it tries to look it up, so if spawning that entity violates
+    ///     singleton status (or it lacks the necessary component) this will throw immediately after instead of later
+    ///     on next call.
+    ///
+    ///     This will also still throw <see cref="MatchNotFoundException"/> if the spawned entity doesn't match at all.
+    /// </remarks>
+    public Entity<TComp1, TComp2, TComp3, TComp4> SingleOrSpawn<TComp1, TComp2, TComp3, TComp4>(
+        EntProtoId fallback,
+        MapCoordinates location)
+        where TComp1 : IComponent
+        where TComp2 : IComponent
+        where TComp3 : IComponent
+        where TComp4 : IComponent;
+
+    /// <summary>
+    ///     Gets the sole entity with the given component, or spawns it at the given location if it doesn't exist.
+    /// </summary>
+    /// <param name="fallback">The action to call on fallback, should no match exist.</param>
+    /// <typeparam name="TComp1">The component to look for as a tag.</typeparam>
+    /// <exception cref="NonUniqueSingletonException">Thrown when multiple entities match the request.</exception>
+    /// <exception cref="MatchNotFoundException">Thrown when no entities match the request after calling fallback.</exception>
+    /// <returns>The singleton entity.</returns>
+    public Entity<TComp1> SingleOrInit<TComp1>(Action fallback)
+        where TComp1 : IComponent;
+
+    /// <summary>
+    ///     Gets the sole entity with the given component, or spawns it at the given location if it doesn't exist.
+    /// </summary>
+    /// <param name="fallback">The action to call on fallback, should no match exist.</param>
+    /// <typeparam name="TComp1">The first component to look for as a tag.</typeparam>
+    /// <typeparam name="TComp2">The second component to look for as a tag.</typeparam>
+    /// <exception cref="NonUniqueSingletonException">Thrown when multiple entities match the request.</exception>
+    /// <exception cref="MatchNotFoundException">Thrown when no entities match the request after calling fallback.</exception>
+    /// <returns>The singleton entity.</returns>
+    public Entity<TComp1, TComp2> SingleOrInit<TComp1, TComp2>(Action fallback)
+        where TComp1 : IComponent
+        where TComp2 : IComponent;
+
+    /// <summary>
+    ///     Gets the sole entity with the given component, or spawns it at the given location if it doesn't exist.
+    /// </summary>
+    /// <param name="fallback">The action to call on fallback, should no match exist.</param>
+    /// <typeparam name="TComp1">The first component to look for as a tag.</typeparam>
+    /// <typeparam name="TComp2">The second component to look for as a tag.</typeparam>
+    /// <typeparam name="TComp3">The third component to look for as a tag.</typeparam>
+    /// <exception cref="NonUniqueSingletonException">Thrown when multiple entities match the request.</exception>
+    /// <exception cref="MatchNotFoundException">Thrown when no entities match the request after calling fallback.</exception>
+    /// <returns>The singleton entity.</returns>
+    public Entity<TComp1, TComp2, TComp3> SingleOrInit<TComp1, TComp2, TComp3>(Action fallback)
+        where TComp1 : IComponent
+        where TComp2 : IComponent
+        where TComp3 : IComponent;
+
+    /// <summary>
+    ///     Gets the sole entity with the given component, or spawns it at the given location if it doesn't exist.
+    /// </summary>
+    /// <param name="fallback">The action to call on fallback, should no match exist.</param>
+    /// <typeparam name="TComp1">The first component to look for as a tag.</typeparam>
+    /// <typeparam name="TComp2">The second component to look for as a tag.</typeparam>
+    /// <typeparam name="TComp3">The third component to look for as a tag.</typeparam>
+    /// <typeparam name="TComp4">The fourth component to look for as a tag.</typeparam>
+    /// <exception cref="NonUniqueSingletonException">Thrown when multiple entities match the request.</exception>
+    /// <exception cref="MatchNotFoundException">Thrown when no entities match the request after calling fallback.</exception>
+    /// <returns>The singleton entity.</returns>
+    public Entity<TComp1, TComp2, TComp3, TComp4> SingleOrInit<TComp1, TComp2, TComp3, TComp4>(Action fallback)
+        where TComp1 : IComponent
+        where TComp2 : IComponent
+        where TComp3 : IComponent
+        where TComp4 : IComponent;
+}

cae52c98335f377238176598607061034718e66b
kaylie <moony@hellomouse.net>
2026-03-10 23:33:44 +0100

    Tests.

--- Robust.Shared.IntegrationTests/GameObjects/IEntityManagerTests.cs
+++ Robust.Shared.IntegrationTests/GameObjects/IEntityManagerTests.cs
@@ -1,8 +1,7 @@
-using System.Numerics;
 using NUnit.Framework;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Map;
-using Robust.Shared.Serialization.Manager.Attributes;
+using Robust.Shared.Map.Components;
 using Robust.UnitTesting.Server;
 
 namespace Robust.UnitTesting.Shared.GameObjects
@@ -31,6 +30,38 @@ namespace Robust.UnitTesting.Shared.GameObjects
             var entMan = sim.Resolve<IEntityManager>();
             var newEnt = entMan.SpawnEntity(null, new MapCoordinates(0, 0, map));
             Assert.That(newEnt, Is.Not.EqualTo(EntityUid.Invalid));
+        }
+
+        /// <summary>
+        /// The entity prototype can define field on the TransformComponent, just like any other component.
+        /// </summary>
+        [Test]
+        public void EntityQuery3_Works()
+        {
+            var sim = SimulationFactory();
+            var map = sim.CreateMap();
+            var map2 = sim.CreateMap();
+
+            var entMan = sim.Resolve<IEntityManager>();
+            // Query all maps.
+            var query = entMan.GetEntityQuery<TransformComponent, MetaDataComponent, MapComponent>();
+
+            Assert.That(query.Count(), NUnit.Framework.Is.EqualTo(2));
+
+            Assert.That(query.Matches(map.Uid));
+
+            foreach (var entity in query)
+            {
+                Assert.That(entity.Owner, NUnit.Framework.Is.EqualTo(map.Uid).Or.EqualTo(map2.Uid));
+                Assert.That(entity.Comp1, NUnit.Framework.Is.TypeOf<TransformComponent>());
+                Assert.That(entity.Comp2, NUnit.Framework.Is.TypeOf<MetaDataComponent>());
+                Assert.That(entity.Comp3, NUnit.Framework.Is.TypeOf<MapComponent>());
+#pragma warning disable CS0618 // Type or member is obsolete
+                Assert.That(entity.Comp1.Owner, NUnit.Framework.Is.EqualTo(entity.Owner));
+                Assert.That(entity.Comp2.Owner, NUnit.Framework.Is.EqualTo(entity.Owner));
+                Assert.That(entity.Comp3.Owner, NUnit.Framework.Is.EqualTo(entity.Owner));
+#pragma warning restore CS0618 // Type or member is obsolete
+            }
         }
 
         [Test]

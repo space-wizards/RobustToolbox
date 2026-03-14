c662f68b9b1809e9f5a1c321005be7ad051b46af
kaylie <moony@hellomouse.net>
2026-03-14 12:28:07 +0100

    tests.

--- Robust.Shared/Random/IRobustRandom.Constructors.cs
+++ Robust.Shared/Random/IRobustRandom.Constructors.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Runtime.InteropServices;
+using SpaceWizards.Sodium;
+
 namespace Robust.Shared.Random;
 
 public partial interface IRobustRandom
@@ -36,5 +40,26 @@ public partial interface IRobustRandom
         var seed = source.Next();
 
         return (CreateSeeded(seed), seed);
+    }
+
+    public static (IDedicatedRandom random, int seed) CreateSeededFromHashable<T>(Span<T> inputs)
+        where T : unmanaged
+    {
+        // Create a byte span so we can hash it
+        var bytes = MemoryMarshal.AsBytes(inputs);
+
+        // 16 is the minimum for this algorithm, we only use 4 bytes of it for the seed right now.
+        Span<byte> outputBuffer = stackalloc byte[16];
+
+        CryptoGenericHashBlake2B.Hash(outputBuffer, bytes, ReadOnlySpan<byte>.Empty);
+        var seed = BitConverter.ToInt32(outputBuffer[0..4]);
+
+        return (CreateSeeded(seed), seed);
+    }
+
+    public static (IDedicatedRandom, int seed) CreateSeededFromHashable<T>(T input)
+        where T : unmanaged
+    {
+        return CreateSeededFromHashable([input]);
     }
 }

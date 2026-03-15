39de97348b0d31858dba17b6ef870bca52f282bd
Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>
2026-03-14 22:23:38 -0700

    update event comment

--- Robust.Shared/GameObjects/MapInitEvent.cs
+++ Robust.Shared/GameObjects/MapInitEvent.cs
@@ -1,7 +1,7 @@
 namespace Robust.Shared.GameObjects;
 
 /// <summary>
-///     Raised directed on an entity when the map is initialized.
+///     Raised directed on an entity when the map has been initialized and the entity has been successfully reparented.
 /// </summary>
 [ComponentEvent(Exclusive = false)]
 public sealed class MapInitEvent : EntityEventArgs

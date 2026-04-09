9530cd7c1b2b2acaca8fb0bc980415e6cccce816
Ataman <Ataman@users.noreply.github.com>
2026-04-09 10:39:59 +0200

    Added [CustomChildTag] attribute, converted custom serializer to source generator with accompanying [YamlTagShortener] attribute.

---
+++ Robust.Roslyn.Shared/YamlTagShortenerHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace Robust.Roslyn.Shared;
+
+public static class YamlTagShortenerHelper
+{
+    public static string ReplaceLast(string currentString, string stringToReplace, string replacement)
+    {
+        var lastStart = currentString.LastIndexOf(stringToReplace, StringComparison.Ordinal);
+        return currentString.Remove(lastStart, stringToReplace.Length) + replacement;
+    }
+}

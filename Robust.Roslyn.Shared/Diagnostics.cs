92a8f0031ebfd8bd7d20da1ff4e047e0686cb720
geekyhobo <geekyhobo321@gmail.com>
2026-04-12 11:12:20 -0400

    temp becuase i am bad a git

--- Robust.Roslyn.Shared/Diagnostics.cs
+++ Robust.Roslyn.Shared/Diagnostics.cs
@@ -51,6 +51,9 @@ public static class Diagnostics
     public const string IdPreferProxy = "RA0045";
     public const string IdProxyForRedundantMethodName = "RA0046";
     public const string IdProxyForTargetMethodNotFound = "RA0047";
+    public const string IdAdminLogRedundantExplicitEntities = "RA0048";
+    public const string IdAdminLogUseSharedSemanticsHelper = "RA0049";
+    public const string IdAdminLogMissingSemanticLabel = "RA0050";
 
     public static SuppressionDescriptor MeansImplicitAssignment =>
         new SuppressionDescriptor("RADC1000", "CS0649", "Marked as implicitly assigned.");

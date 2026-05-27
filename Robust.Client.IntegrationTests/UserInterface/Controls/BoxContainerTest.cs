ff0f40aee2edefb9d4920d1bdef077117d17c937
Thomas (Aeshus) <git@aeshus.com>
2026-05-27 10:19:07 -0500

    Update BoxContainerTest namespace + format + filescoped namespace

--- Robust.Client.IntegrationTests/UserInterface/Controls/BoxContainerTest.cs
+++ Robust.Client.IntegrationTests/UserInterface/Controls/BoxContainerTest.cs
@@ -3,119 +3,128 @@ using NUnit.Framework;
 using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controls;
 using Robust.Shared.Maths;
+using Robust.UnitTesting;
 using static Robust.Client.UserInterface.Controls.BoxContainer;
 
-namespace Robust.UnitTesting.Client.UserInterface.Controls
+namespace Robust.Client.IntegrationTests.UserInterface.Controls;
+
+[TestFixture]
+[TestOf(typeof(BoxContainer))]
+public sealed class BoxContainerTest : RobustUnitTest
 {
-    [TestFixture]
-    [TestOf(typeof(BoxContainer))]
-    public sealed class BoxContainerTest : RobustUnitTest
+    public override UnitTestProject Project => UnitTestProject.Client;
+
+    [Test]
+    public void TestLayoutBasic()
     {
-        public override UnitTestProject Project => UnitTestProject.Client;
-
-        [Test]
-        public void TestLayoutBasic()
+        var root = new LayoutContainer();
+        var boxContainer = new BoxContainer
         {
-            var root = new LayoutContainer();
-            var boxContainer = new BoxContainer
-            {
-                Orientation = LayoutOrientation.Vertical,
-                MinSize = new Vector2(50, 60)
-            };
-            var control1 = new Control {MinSize = new Vector2(20, 20)};
-            var control2 = new Control {MinSize = new Vector2(30, 30)};
+            Orientation = LayoutOrientation.Vertical,
+            MinSize = new Vector2(50, 60)
+        };
+        var control1 = new Control { MinSize = new Vector2(20, 20) };
+        var control2 = new Control { MinSize = new Vector2(30, 30) };
 
-            root.AddChild(boxContainer);
+        root.AddChild(boxContainer);
 
-            boxContainer.AddChild(control1);
-            boxContainer.AddChild(control2);
+        boxContainer.AddChild(control1);
+        boxContainer.AddChild(control2);
 
-            root.Arrange(new UIBox2(0, 0, 50, 60));
+        root.Arrange(new UIBox2(0, 0, 50, 60));
 
+        using (Assert.EnterMultipleScope())
+        {
             Assert.That(control1.Position, Is.EqualTo(Vector2.Zero));
             Assert.That(control1.Size, Is.EqualTo(new Vector2(50, 20)));
             Assert.That(control2.Position, Is.EqualTo(new Vector2(0, 20)));
             Assert.That(control2.Size, Is.EqualTo(new Vector2(50, 30)));
             Assert.That(boxContainer.DesiredSize, Is.EqualTo(new Vector2(50, 60)));
         }
+    }
 
-        [Test]
-        public void TestLayoutExpand()
+    [Test]
+    public void TestLayoutExpand()
+    {
+        var root = new LayoutContainer();
+        var boxContainer = new BoxContainer
         {
-            var root = new LayoutContainer();
-            var boxContainer = new BoxContainer
-            {
-                Orientation = LayoutOrientation.Vertical,
-                MinSize = new Vector2(50, 60)
-            };
-            var control1 = new Control
-            {
-                VerticalExpand = true
-            };
-            var control2 = new Control {MinSize = new Vector2(30, 30)};
+            Orientation = LayoutOrientation.Vertical,
+            MinSize = new Vector2(50, 60)
+        };
+        var control1 = new Control
+        {
+            VerticalExpand = true
+        };
+        var control2 = new Control { MinSize = new Vector2(30, 30) };
 
-            boxContainer.AddChild(control1);
-            boxContainer.AddChild(control2);
+        boxContainer.AddChild(control1);
+        boxContainer.AddChild(control2);
 
-            root.AddChild(boxContainer);
+        root.AddChild(boxContainer);
 
-            root.Arrange(new UIBox2(0, 0, 100, 100));
+        root.Arrange(new UIBox2(0, 0, 100, 100));
 
+        using (Assert.EnterMultipleScope())
+        {
             Assert.That(control1.Position, Is.EqualTo(Vector2.Zero));
             Assert.That(control1.Size, Is.EqualTo(new Vector2(50, 30)));
             Assert.That(control2.Position, Is.EqualTo(new Vector2(0, 30)));
             Assert.That(control2.Size, Is.EqualTo(new Vector2(50, 30)));
             Assert.That(boxContainer.DesiredSize, Is.EqualTo(new Vector2(50, 60)));
         }
+    }
 
-        [Test]
-        public void TestCalcMinSize()
+    [Test]
+    public void TestCalcMinSize()
+    {
+        var boxContainer = new BoxContainer
         {
-            var boxContainer = new BoxContainer
-            {
-                Orientation = LayoutOrientation.Vertical
-            };
-            var control1 = new Control
-            {
-                MinSize = new Vector2(50, 30)
-            };
-            var control2 = new Control {MinSize = new Vector2(30, 50)};
-
-            boxContainer.AddChild(control1);
-            boxContainer.AddChild(control2);
-
-            boxContainer.Measure(new Vector2(100, 100));
-
-            Assert.That(boxContainer.DesiredSize, Is.EqualTo(new Vector2(50, 80)));
-        }
-
-        [Test]
-        public void TestTwoExpand()
+            Orientation = LayoutOrientation.Vertical
+        };
+        var control1 = new Control
         {
-            var root = new LayoutContainer();
-            var boxContainer = new BoxContainer
-            {
-                Orientation = LayoutOrientation.Vertical,
-                MinSize = new Vector2(30, 80)
-            };
-            var control1 = new Control
-            {
-                VerticalExpand = true,
-            };
-            var control2 = new Control
-            {
-                VerticalExpand = true,
-            };
-            var control3 = new Control {MinSize = new Vector2(0, 50)};
+            MinSize = new Vector2(50, 30)
+        };
+        var control2 = new Control { MinSize = new Vector2(30, 50) };
 
-            root.AddChild(boxContainer);
+        boxContainer.AddChild(control1);
+        boxContainer.AddChild(control2);
 
-            boxContainer.AddChild(control1);
-            boxContainer.AddChild(control3);
-            boxContainer.AddChild(control2);
+        boxContainer.Measure(new Vector2(100, 100));
 
-            root.Arrange(new UIBox2(0, 0, 250, 250));
+        Assert.That(boxContainer.DesiredSize, Is.EqualTo(new Vector2(50, 80)));
+    }
 
+    [Test]
+    public void TestTwoExpand()
+    {
+        var root = new LayoutContainer();
+        var boxContainer = new BoxContainer
+        {
+            Orientation = LayoutOrientation.Vertical,
+            MinSize = new Vector2(30, 80)
+        };
+        var control1 = new Control
+        {
+            VerticalExpand = true,
+        };
+        var control2 = new Control
+        {
+            VerticalExpand = true,
+        };
+        var control3 = new Control { MinSize = new Vector2(0, 50) };
+
+        root.AddChild(boxContainer);
+
+        boxContainer.AddChild(control1);
+        boxContainer.AddChild(control3);
+        boxContainer.AddChild(control2);
+
+        root.Arrange(new UIBox2(0, 0, 250, 250));
+
+        using (Assert.EnterMultipleScope())
+        {
             Assert.That(control1.Position, Is.EqualTo(Vector2.Zero));
             Assert.That(control1.Size, Is.EqualTo(new Vector2(30, 15)));
             Assert.That(control3.Position, Is.EqualTo(new Vector2(0, 15)));
@@ -123,71 +132,71 @@ namespace Robust.UnitTesting.Client.UserInterface.Controls
             Assert.That(control2.Position, Is.EqualTo(new Vector2(0, 65)));
             Assert.That(control2.Size, Is.EqualTo(new Vector2(30, 15)));
         }
+    }
 
-        [Test]
-        public void TestTwoExpandRatio()
+    [Test]
+    public void TestTwoExpandRatio()
+    {
+        var boxContainer = new BoxContainer
         {
-            var boxContainer = new BoxContainer
+            Orientation = LayoutOrientation.Horizontal,
+            SetSize = new Vector2(100, 10),
+            Children =
             {
-                Orientation = LayoutOrientation.Horizontal,
-                SetSize = new Vector2(100, 10),
-                Children =
+                new Control
                 {
-                    new Control
-                    {
-                        MinWidth = 10,
-                        HorizontalExpand = true,
-                        SizeFlagsStretchRatio = 20,
-                    },
-                    new Control
-                    {
-                        MinWidth = 10,
-                        HorizontalExpand = true,
-                        SizeFlagsStretchRatio = 80
-                    }
+                    MinWidth = 10,
+                    HorizontalExpand = true,
+                    SizeFlagsStretchRatio = 20,
+                },
+                new Control
+                {
+                    MinWidth = 10,
+                    HorizontalExpand = true,
+                    SizeFlagsStretchRatio = 80
                 }
-            };
+            }
+        };
 
-            boxContainer.Arrange(UIBox2.FromDimensions(Vector2.Zero, boxContainer.SetSize));
+        boxContainer.Arrange(UIBox2.FromDimensions(Vector2.Zero, boxContainer.SetSize));
 
-            Assert.Multiple(() =>
-            {
-                Assert.That(boxContainer.GetChild(0).Width, Is.EqualTo(20));
-                Assert.That(boxContainer.GetChild(1).Width, Is.EqualTo(80));
-            });
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(boxContainer.GetChild(0).Width, Is.EqualTo(20));
+            Assert.That(boxContainer.GetChild(1).Width, Is.EqualTo(80));
         }
+    }
 
-        [Test]
-        public void TestTwoExpandOneSmall()
+    [Test]
+    public void TestTwoExpandOneSmall()
+    {
+        var boxContainer = new BoxContainer
         {
-            var boxContainer = new BoxContainer
+            Orientation = LayoutOrientation.Horizontal,
+            SetSize = new Vector2(100, 10),
+            Children =
             {
-                Orientation = LayoutOrientation.Horizontal,
-                SetSize = new Vector2(100, 10),
-                Children =
+                new Control
                 {
-                    new Control
-                    {
-                        MinWidth = 30,
-                        HorizontalExpand = true,
-                        SizeFlagsStretchRatio = 20,
-                    },
-                    new Control
-                    {
-                        MinWidth = 30,
-                        HorizontalExpand = true,
-                        SizeFlagsStretchRatio = 80
-                    }
+                    MinWidth = 30,
+                    HorizontalExpand = true,
+                    SizeFlagsStretchRatio = 20,
+                },
+                new Control
+                {
+                    MinWidth = 30,
+                    HorizontalExpand = true,
+                    SizeFlagsStretchRatio = 80
                 }
-            };
+            }
+        };
 
-            boxContainer.Arrange(UIBox2.FromDimensions(Vector2.Zero, boxContainer.SetSize));
+        boxContainer.Arrange(UIBox2.FromDimensions(Vector2.Zero, boxContainer.SetSize));
 
-            Assert.Multiple(() =>
-            {
-                Assert.That(boxContainer.GetChild(0).Width, Is.EqualTo(30));
-                Assert.That(boxContainer.GetChild(1).Width, Is.EqualTo(70));
-            });
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(boxContainer.GetChild(0).Width, Is.EqualTo(30));
+            Assert.That(boxContainer.GetChild(1).Width, Is.EqualTo(70));
         }
     }
 }

e1ebbd0acb939281b5ad38032f60a04177e249e5
deltanedas <@deltanedas:goida.zip>
2026-06-18 15:17:41 +0100

    !

--- Robust.Shared/GameObjects/EntitySystem.Proxy.cs
+++ Robust.Shared/GameObjects/EntitySystem.Proxy.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Diagnostics.Contracts;
 using System.Runtime.CompilerServices;
 using JetBrains.Annotations;
 using Robust.Shared.Containers;

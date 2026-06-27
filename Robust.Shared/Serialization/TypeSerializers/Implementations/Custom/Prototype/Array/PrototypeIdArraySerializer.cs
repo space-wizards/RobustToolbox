895a66255cacb4f3ad335bb205e7b4bcc71d4565
deltanedas <@deltanedas:goida.zip>
2026-06-27 20:11:36 +0100

    remove regular PrototypeIdArraySerializer

--- Robust.Shared/Serialization/TypeSerializers/Implementations/Custom/Prototype/Array/PrototypeIdArraySerializer.cs
+++ Robust.Shared/Serialization/TypeSerializers/Implementations/Custom/Prototype/Array/PrototypeIdArraySerializer.cs
@@ -6,32 +6,25 @@ using Robust.Shared.Serialization.Markdown.Sequence;
 using Robust.Shared.Serialization.Markdown.Validation;
 using Robust.Shared.Serialization.Markdown.Value;
 using Robust.Shared.Serialization.TypeSerializers.Interfaces;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Generic;
 
 namespace Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.Array;
 
-public sealed class AbstractPrototypeIdArraySerializer<TPrototype> : PrototypeIdArraySerializer<TPrototype> where TPrototype : class, IPrototype, IInheritingPrototype
+// TODO: make AbstractProtoId and change every prototype to use it...
+public sealed class AbstractPrototypeIdArraySerializer<TPrototype> : ITypeValidator<string[], SequenceDataNode>,
+    ITypeValidator<string[], ValueDataNode> where TPrototype : class, IPrototype, IInheritingPrototype
 {
-    protected override PrototypeIdSerializer<TPrototype> PrototypeSerializer =>
-        new AbstractPrototypeIdSerializer<TPrototype>();
-}
-
-[Virtual]
-public class PrototypeIdArraySerializer<TPrototype> : ITypeValidator<string[], SequenceDataNode>,
-    ITypeValidator<string[], ValueDataNode> where TPrototype : class, IPrototype
-{
-    protected virtual PrototypeIdSerializer<TPrototype> PrototypeSerializer => new();
-
     public ValidationNode Validate(ISerializationManager serializationManager, SequenceDataNode node,
         IDependencyCollection dependencies, ISerializationContext? context = null)
     {
         return new ValidatedSequenceNode(node.Select(x =>
             x is ValueDataNode valueDataNode
-                ? PrototypeSerializer.Validate(serializationManager, valueDataNode, dependencies, context)
+                ? ProtoIdSerializer<TPrototype>.Validate(dependencies, valueDataNode)
                 : new ErrorNode(x, $"Cannot cast node {x} to ValueDataNode.")).ToList());
     }
 
     public ValidationNode Validate(ISerializationManager serializationManager, ValueDataNode node,
-        IDependencyCollection dependencies, ISerializationContext? context = null) =>
-        PrototypeSerializer.Validate(serializationManager, node, dependencies, context);
+        IDependencyCollection dependencies, ISerializationContext? context = null)
+        => ProtoIdSerializer<TPrototype>.Validate(dependencies, node);
 }
 

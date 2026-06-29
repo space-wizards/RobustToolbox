1bfa606b0d2b6c1d5d9ec7fffa766aa892991c10
chromiumboy <chromium.boy@gmail.com>
2026-06-29 15:21:22 -0500

    Minor corrections

--- Robust.Shared/Prototypes/CreateVariants.cs
+++ Robust.Shared/Prototypes/CreateVariants.cs
@@ -3,14 +3,14 @@ using Robust.Shared.Serialization.Manager.Attributes;
 namespace Robust.Shared.Prototypes;
 
 /// <summary>
-/// This class is part of a mechanism to automatically generate variants of a prototype at run time.
-/// It is substituted in the place of a value or collection in a YAML prototype definition.
+/// This class is part of a mechanism that is used to automatically generate variants of prototypes at run time.
+/// It is substituted in the place of a value or linear collection (e.g., lists and arrays) in a YAML prototype definition.
 /// When the prototype manager reads this class, it will use the arrays defined here to dynamically populate the associated value/collection.
 /// To generate prototype variants, the ID of the prototype must also be replaced with a reference to this class.
 /// </summary>
 /// <example>
-/// This following code will generate three different prototypes with differing values at runtime:
-///
+/// Here is example YAML code that will auto-generate three different prototypes, each with its own distinct set of values, at runtime:
+/// 
 /// - type: entity
 ///   id: !type:CreateVariants
 ///     variants: [ TestEntityA, TestEntityB, TestEntityC ]
@@ -32,7 +32,7 @@ namespace Robust.Shared.Prototypes;
 ///     - state: overlayState
 ///       map: ["secondLayer"]
 ///
-/// For example, the second prototype to be generated will have the following values:
+/// The second prototype to be generated will have the following values:
 ///
 /// - type: entity
 ///   id: TestEntityB

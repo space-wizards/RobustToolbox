12f7f9b954a403fc20821291b07a6bbb8b9a2a6a
Jezithyr <Jezithyr@gmail.com>
2026-07-07 22:40:26 -0700

    initial editor project

---
+++ Robust.Editor.App/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Photino.Blazor;
+
+namespace Robust.Editor.App
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var appBuilder = PhotinoBlazorAppBuilder.CreateDefault(args);
+
+            appBuilder.Services
+                .AddLogging();
+
+            // register root component and selector
+            appBuilder.RootComponents.Add<App>("app");
+
+            var app = appBuilder.Build();
+
+            // customize window
+            app.MainWindow
+                .SetIconFile("favicon.ico")
+                .SetTitle("Photino Blazor Sample");
+
+            AppDomain.CurrentDomain.UnhandledException += (sender, error) =>
+            {
+                app.MainWindow.ShowMessage("Fatal exception", error.ExceptionObject.ToString());
+            };
+
+            app.Run();
+
+        }
+    }
+}

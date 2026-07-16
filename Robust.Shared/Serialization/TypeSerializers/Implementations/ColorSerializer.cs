18b29344856ba55a553562804e8126b60494c8f8
ArtisticRoomba <145879011+ArtisticRoomba@users.noreply.github.com>
2026-07-16 08:10:21 -0700

    release notes and misc cleanup because I can

--- Robust.Shared/Serialization/TypeSerializers/Implementations/ColorSerializer.cs
+++ Robust.Shared/Serialization/TypeSerializers/Implementations/ColorSerializer.cs
@@ -8,45 +8,55 @@ using Robust.Shared.Serialization.Markdown.Validation;
 using Robust.Shared.Serialization.Markdown.Value;
 using Robust.Shared.Serialization.TypeSerializers.Interfaces;
 
-namespace Robust.Shared.Serialization.TypeSerializers.Implementations
+namespace Robust.Shared.Serialization.TypeSerializers.Implementations;
+
+[TypeSerializer]
+public sealed class ColorSerializer : ITypeSerializer<Color, ValueDataNode>, ITypeCopyCreator<Color>
 {
-    [TypeSerializer]
-    public sealed class ColorSerializer : ITypeSerializer<Color, ValueDataNode>, ITypeCopyCreator<Color>
+    public Color Read(
+        ISerializationManager serializationManager,
+        ValueDataNode node,
+        IDependencyCollection dependencies,
+        SerializationHookContext hookCtx,
+        ISerializationContext? context = null,
+        ISerializationManager.InstantiationDelegate<Color>? instanceProvider = null)
     {
-        public Color Read(ISerializationManager serializationManager, ValueDataNode node,
-            IDependencyCollection dependencies,
-            SerializationHookContext hookCtx,
-            ISerializationContext? context = null,
-            ISerializationManager.InstantiationDelegate<Color>? instanceProvider = null)
-        {
-            var deserializedColor = Color.TryFromName(node.Value, out var color)
-                ? color :
-                Color.FromHex(node.Value);
+        var deserializedColor = Color.TryFromName(node.Value, out var color)
+            ? color
+            : Color.FromHex(node.Value);
 
-            return deserializedColor;
-        }
+        return deserializedColor;
+    }
 
-        public ValidationNode Validate(ISerializationManager serializationManager, ValueDataNode node,
-            IDependencyCollection dependencies,
-            ISerializationContext? context = null)
-        {
-            return Color.TryFromName(node.Value, out _) || Color.TryFromHex(node.Value, out _)
-                ? new ValidatedValueNode(node)
-                : new ErrorNode(node, "Failed parsing Color.");
-        }
+    public ValidationNode Validate(
+        ISerializationManager serializationManager,
+        ValueDataNode node,
+        IDependencyCollection dependencies,
+        ISerializationContext? context = null)
+    {
+        return Color.TryFromName(node.Value, out _) || Color.TryFromHex(node.Value, out _)
+            ? new ValidatedValueNode(node)
+            : new ErrorNode(node, "Failed parsing Color.");
+    }
 
-        public DataNode Write(ISerializationManager serializationManager, Color value,
-            IDependencyCollection dependencies, bool alwaysWrite = false,
-            ISerializationContext? context = null)
-        {
-            return new ValueDataNode(value.ToHex());
-        }
+    public DataNode Write(
+        ISerializationManager serializationManager,
+        Color value,
+        IDependencyCollection dependencies,
+        bool alwaysWrite = false,
+        ISerializationContext? context = null)
+    {
+        return new ValueDataNode(value.ToHex());
+    }
 
-        [MustUseReturnValue]
-        public Color CreateCopy(ISerializationManager serializationManager, Color source,
-            IDependencyCollection dependencies, SerializationHookContext hookCtx, ISerializationContext? context = null)
-        {
-            return new(source.R, source.G, source.B, source.A);
-        }
+    [MustUseReturnValue]
+    public Color CreateCopy(
+        ISerializationManager serializationManager,
+        Color source,
+        IDependencyCollection dependencies,
+        SerializationHookContext hookCtx,
+        ISerializationContext? context = null)
+    {
+        return new Color(source.R, source.G, source.B, source.A);
     }
 }

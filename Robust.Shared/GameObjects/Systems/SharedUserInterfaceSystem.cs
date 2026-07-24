08b3257ecea1a76deed417945e3fae2cee125afd
Whatstone <166147148+whatston3@users.noreply.github.com>
2026-07-23 23:03:30 -0400

    Hotfix: pass last state in SharedUserInterfaceSystem.EnsureClientBui (#6840)
    * sins
    
    * New AddQueuedState function, comment both
    
    * I like this name better actually
    
    ---------
    
    Co-authored-by: Princess Cheeseballs <66055347+Princess-Cheeseballs@users.noreply.github.com>

--- Robust.Shared/GameObjects/Systems/SharedUserInterfaceSystem.cs
+++ Robust.Shared/GameObjects/Systems/SharedUserInterfaceSystem.cs
@@ -91,6 +91,19 @@ public abstract partial class SharedUserInterfaceSystem : EntitySystem
         SubscribeLocalEvent<UserInterfaceUserComponent, ComponentShutdown>(OnActorShutdown);
     }
 
+    /// <summary>
+    /// Enqueues a BUI command to be processed in the next Update call,
+    /// getting the last available state for the given UserInterfaceComponent at the last state.
+    /// </summary>
+    private void AddQueued(BoundUserInterface bui, QueuedUpdate type, Entity<UserInterfaceComponent> ent, Enum key)
+    {
+        var state = ent.Comp.States.GetValueOrDefault(key);
+        _queuedBuis.Add((bui, type, state));
+    }
+
+    /// <summary>
+    /// Enqueues a BUI command to be processed in the next Update call.
+    /// </summary>
     private void AddQueued(BoundUserInterface bui, QueuedUpdate type, BoundUserInterfaceState? state = null)
     {
         _queuedBuis.Add((bui, type, state));
@@ -292,8 +305,7 @@ public abstract partial class SharedUserInterfaceSystem : EntitySystem
         // PlayerAttachedEvent will catch some of these.
         foreach (var (key, bui) in ent.Comp.ClientOpenInterfaces)
         {
-            var state = ent.Comp.States.GetValueOrDefault(key);
-            AddQueued(bui, QueuedUpdate.Open, state);
+            AddQueued(bui, QueuedUpdate.Open, ent, key);
         }
     }
 
@@ -554,7 +566,7 @@ public abstract partial class SharedUserInterfaceSystem : EntitySystem
         if (!open)
             return;
 
-        AddQueued(boundUserInterface, QueuedUpdate.Open);
+        AddQueued(boundUserInterface, QueuedUpdate.Open, entity, key);
     }
 
     /// <summary>

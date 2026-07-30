099e9b0f85256d5dbc61bcba89bf34bd7d4bbe9c
neomoth <69372252+neomoth@users.noreply.github.com>
2026-07-29 23:05:27 -0400

    support for skipping optional parameters

--- Robust.Shared/Toolshed/TypeParsers/OptionalValueTypeParser.cs
+++ Robust.Shared/Toolshed/TypeParsers/OptionalValueTypeParser.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using Robust.Shared.Console;
 using Robust.Shared.Toolshed.Syntax;
 
@@ -8,6 +9,17 @@ public sealed class OptionalValueTypeParser<T> : TypeParser<OptionalValue<T>>
     public override bool TryParse(ParserContext ctx, out OptionalValue<T> result)
     {
         result = default;
+
+        var restore = ctx.Save();
+
+        if (ctx.PeekRune() == new Rune('_'))
+        {
+            var word = ctx.GetWord(ParserContext.IsToken);
+            if (word == "_") return true; // This is how I am choosing to represent skipping the parameter.
+        }
+
+        ctx.Restore(restore);
+
         if (!Toolshed.TryParse(ctx, typeof(T), out var parsed)) return false;
         if (parsed is not T value)
             return false;

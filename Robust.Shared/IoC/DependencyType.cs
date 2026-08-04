ac38294bf05b27315d90f10e7e0db6a42e38d5d7
DrSmugleaf <drsmugleaf@gmail.com>
2026-08-04 16:54:48 -0700

    Add missing type Rider pranked me

---
+++ Robust.Shared/IoC/DependencyType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using System.Threading;
+
+namespace Robust.Shared.IoC;
+
+internal static class DependencyType
+{
+    internal static int Index = -1;
+
+    internal static int GetIndex(Type type)
+    {
+        var depType = typeof(DependencyType<>).MakeGenericType(type);
+        var prop = depType.GetProperty("Index", BindingFlags.Static | BindingFlags.Public)!;
+        return (int) prop.GetValue(null, null)!;
+    }
+}
+
+[Obsolete("Used only in source generation internally.")]
+internal static class DependencyType<T>
+{
+    // ReSharper disable once StaticMemberInGenericType
+    [Obsolete("Used only in source generation internally.")]
+    internal static readonly int Index = Interlocked.Increment(ref DependencyType.Index);
+}

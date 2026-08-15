8338dec7fb479ca2747ea78a488c3272b7f16098
Whatstone <whatston3@gmail.com>
2026-08-14 22:51:15 -0400

    reorder dependencies

--- Robust.Client/GameObjects/EntitySystems/AppearanceSystem.cs
+++ Robust.Client/GameObjects/EntitySystems/AppearanceSystem.cs
@@ -1,12 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using JetBrains.Annotations;
 using Robust.Shared.GameObjects;
 using Robust.Shared.IoC;
 using Robust.Shared.GameStates;
-using Robust.Shared.Utility;
 using Robust.Shared.Serialization.Manager;
-using System.Diagnostics.CodeAnalysis;
+using Robust.Shared.Utility;
 
 namespace Robust.Client.GameObjects
 {

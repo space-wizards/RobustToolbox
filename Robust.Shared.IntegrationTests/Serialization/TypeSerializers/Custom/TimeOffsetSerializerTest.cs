60983b31d83e809de44ea7995053fe5749dbe176
metalgearsloth <comedian_vs_clown@hotmail.com>
2026-08-20 16:06:23 +1000

    Tighten time offset serializer tests

--- Robust.Shared.IntegrationTests/Serialization/TypeSerializers/Custom/TimeOffsetSerializerTest.cs
+++ Robust.Shared.IntegrationTests/Serialization/TypeSerializers/Custom/TimeOffsetSerializerTest.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using NUnit.Framework;
 using Robust.Shared.IoC;
 using Robust.Shared.Serialization.Markdown.Value;
@@ -12,28 +11,6 @@ namespace Robust.UnitTesting.Shared.Serialization.TypeSerializers.Custom;
 [TestOf(typeof(TimeOffsetSerializer))]
 internal sealed class TimeOffsetSerializerTest : OurSerializationTest
 {
-    private static readonly TimeSpan CopyCurTime = TimeSpan.FromSeconds(10);
-
-    private static IEnumerable<TestCaseData> CopyTestCases()
-    {
-        yield return new TestCaseData(TimeSpan.FromSeconds(1), CopyCurTime + TimeSpan.FromSeconds(1))
-            .SetName("Copy applies current time");
-        yield return new TestCaseData(TimeSpan.Zero, CopyCurTime)
-            .SetName("Copy zero applies current time");
-        yield return new TestCaseData(TimeSpan.FromSeconds(-1), CopyCurTime + TimeSpan.FromSeconds(-1))
-            .SetName("Copy negative applies current time");
-        yield return new TestCaseData(TimeSpan.MaxValue, TimeSpan.MaxValue)
-            .SetName("Copy max value clamps to max value");
-    }
-
-    private static IEnumerable<TestCaseData> ApplyOffsetTestCases()
-    {
-        yield return new TestCaseData(TimeSpan.MaxValue, TimeSpan.FromSeconds(10), TimeSpan.MaxValue)
-            .SetName("Apply offset max value clamps to max value");
-        yield return new TestCaseData(TimeSpan.MinValue, TimeSpan.FromSeconds(-10), TimeSpan.MinValue)
-            .SetName("Apply offset min value clamps to min value");
-    }
-
     [Test]
     public void ReadReturnsRawOffset()
     {
@@ -42,21 +19,44 @@ internal sealed class TimeOffsetSerializerTest : OurSerializationTest
         Assert.That(result, Is.EqualTo(TimeSpan.FromSeconds(1)));
     }
 
-    [TestCaseSource(nameof(CopyTestCases))]
-    public void CopyAppliesCurrentTime(TimeSpan source, TimeSpan expected)
+    [Test]
+    public void CreateCopyAppliesCurrentTime()
     {
-        var timing = IoCManager.Resolve<IGameTiming>();
-        timing.TimeBase = (CopyCurTime, timing.CurTick);
-        timing.TickRemainder = TimeSpan.Zero;
-
-        var result = Serialization.CreateCopy<TimeSpan, TimeOffsetSerializer>(source);
-
-        Assert.That(result, Is.EqualTo(expected));
+        WithCurTime(TimeSpan.FromSeconds(10), () =>
+        {
+            Assert.That(
+                Serialization.CreateCopy<TimeSpan, TimeOffsetSerializer>(TimeSpan.FromSeconds(1)),
+                Is.EqualTo(TimeSpan.FromSeconds(11)));
+        });
     }
 
-    [TestCaseSource(nameof(ApplyOffsetTestCases))]
-    public void ApplyOffsetClamps(TimeSpan source, TimeSpan offset, TimeSpan expected)
+    [Test]
+    public void CreateCopyClampsOverflow()
     {
-        Assert.That(TimeOffsetSerializer.ApplyOffset(source, offset), Is.EqualTo(expected));
+        WithCurTime(TimeSpan.FromSeconds(10), () =>
+        {
+            Assert.That(
+                Serialization.CreateCopy<TimeSpan, TimeOffsetSerializer>(TimeSpan.MaxValue),
+                Is.EqualTo(TimeSpan.MaxValue));
+        });
+    }
+
+    private static void WithCurTime(TimeSpan curTime, Action action)
+    {
+        var timing = IoCManager.Resolve<IGameTiming>();
+        var oldTimeBase = timing.TimeBase;
+        var oldTickRemainder = timing.TickRemainder;
+
+        try
+        {
+            timing.TimeBase = (curTime, timing.CurTick);
+            timing.TickRemainder = TimeSpan.Zero;
+            action();
+        }
+        finally
+        {
+            timing.TimeBase = oldTimeBase;
+            timing.TickRemainder = oldTickRemainder;
+        }
     }
 }

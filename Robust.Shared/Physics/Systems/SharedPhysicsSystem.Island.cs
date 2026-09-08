99e4bb8a631e620f9e1d38ebbcf5292361848132
deltanedas <@deltanedas:goida.zip>
2026-09-08 08:20:05 +0100

    make island solver more robust

--- Robust.Shared/Physics/Systems/SharedPhysicsSystem.Island.cs
+++ Robust.Shared/Physics/Systems/SharedPhysicsSystem.Island.cs
@@ -431,6 +431,7 @@ public abstract partial class SharedPhysicsSystem
                 }
 
                 // Handle joints
+                _islandJoints.Clear();
                 if (RelayTargetQuery.TryGetComponent(bodyUid, out var relayComp))
                 {
                     foreach (var relay in relayComp.Relayed)
@@ -500,10 +501,10 @@ public abstract partial class SharedPhysicsSystem
                 foreach (var (original, joint) in _islandJoints)
                 {
                     // TODO: Same here store physicscomp + transform on the joint, the savings are worth it.
-                    var bodyA = PhysicsQuery.GetComponent(joint.BodyAUid);
-                    var bodyB = PhysicsQuery.GetComponent(joint.BodyBUid);
-
-                    if (!bodyA.CanCollide || !bodyB.CanCollide)
+                    if (!PhysicsQuery.TryComp(joint.BodyAUid, out var bodyA) ||
+                        !PhysicsQuery.TryComp(joint.BodyBUid, out var bodyB) ||
+                        !bodyA.CanCollide ||
+                        !bodyB.CanCollide)
                         continue;
 
                     joints.Add((original, joint));
@@ -520,8 +521,6 @@ public abstract partial class SharedPhysicsSystem
                         bodyB.Island = true;
                     }
                 }
-
-                _islandJoints.Clear();
             }
 
             int idx;

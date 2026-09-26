72503ec71a2710a250fa50fede064dbbd3fccbbe
MissKay1994 <15877268+MissKay1994@users.noreply.github.com>
2026-09-26 05:29:36 -0400

    Sound hunting is suffering

--- Schemas/rga_validators.py
+++ Schemas/rga_validators.py
@@ -17,6 +17,7 @@ class License(Validator):
         "CC-BY-NC-ND-4.0",
         "CC0-1.0",
         "MIT",
+        "CC SAMPLING+ 1.0",
         "Custom" # implies that the license is described in the copyright field.
         ]
 
